d30ce4d790084ef8d12dc097afa55dbe0efdb3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:17:28 +0000

    Add package derm-ita/0.0.3/derm-ita-0.0.3.tar.gz

---
+++ packages/derm-ita/derm-ita-0.0.3.tar.gz/derm-ita-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["numpy>=1.21", "Pillow>=8.4", "setuptools>=57"]
+
+setup(
+    name="derm-ita",
+    version="0.0.3",
+    author="Adam Corbin",
+    author_email="acorbin3@gmail.com",
+    description="A package with different strategies to compute individual typology angle",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/acorbin3/derm_ita",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
