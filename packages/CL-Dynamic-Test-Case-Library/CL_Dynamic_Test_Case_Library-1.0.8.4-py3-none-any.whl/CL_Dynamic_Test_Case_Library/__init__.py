b32ea610289249ab6b93bb43dd964064898d8a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:02 +0000

    Add package CL-Dynamic-Test-Case-Library/1.0.8.4/CL_Dynamic_Test_Case_Library-1.0.8.4-py3-none-any.whl

---
+++ packages/CL-Dynamic-Test-Case-Library/CL_Dynamic_Test_Case_Library-1.0.8.4-py3-none-any.whl/CL_Dynamic_Test_Case_Library/__init__.py
@@ -0,0 +1,6 @@
+from CL_Dynamic_Test_Case_Library import CL_Dynamic_Test_Case_Library
+
+# Create an object of CL_Dynamic_Test_Case_Library class & call its method
+
+ModCL_Dynamic_Test_Case_Library = CL_Dynamic_Test_Case_Library()
+ModCL_Dynamic_Test_Case_Library.create_test_case()
