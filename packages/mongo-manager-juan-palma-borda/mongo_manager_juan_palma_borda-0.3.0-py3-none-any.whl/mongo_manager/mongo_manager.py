abd560cb2c6fa11a630a0ef01a36d049302fadd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:25 +0000

    Add package mongo-manager-juan-palma-borda/0.3.0/mongo_manager_juan_palma_borda-0.3.0-py3-none-any.whl

---
+++ packages/mongo-manager-juan-palma-borda/mongo_manager_juan_palma_borda-0.3.0-py3-none-any.whl/mongo_manager/mongo_manager.py
@@ -0,0 +1,37 @@
+from pymongo import MongoClient
+
+from .patrones.singleton import SingletonMeta
+
+mongo_manager_gl = None
+
+
+class MongoManager(metaclass=SingletonMeta):
+    __bd = None
+
+    def __init__(self, username: str = '', password: str = '', db: str = '', auth_source: str = '',
+                 bd_online: bool = False, port_local: int = 27017,
+                 url_online='') -> None:
+        """
+        Crea la instancia conectada a la collecion en cuestion.
+        """
+        if bd_online:
+            self.__bd = MongoClient(url_online)[db]
+        else:
+            self.__bd = \
+                MongoClient('mongodb://{}:{}@localhost:{}'.format(username, password, port_local),
+                            authSource=auth_source)[db]
+        global mongo_manager_gl
+        mongo_manager_gl = self
+
+    @property
+    def bd(self):
+        return self.__bd
+
+    def collection(self, collection):
+        return self.bd[collection]
+
+
+class MongoException(Exception):
+    pass
+
+# bd = MongoManager('tmo', 'tmo', 'tmo', 'tmo')
