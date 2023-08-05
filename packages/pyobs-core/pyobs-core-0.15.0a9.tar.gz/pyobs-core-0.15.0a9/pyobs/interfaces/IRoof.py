4a00903f753dbb66d7d52be6f8f6523975631b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package pyobs-core/0.15.0a9/pyobs-core-0.15.0a9.tar.gz

---
+++ packages/pyobs-core/pyobs-core-0.15.0a9.tar.gz/pyobs-core-0.15.0a9/pyobs/interfaces/IRoof.py
@@ -0,0 +1,10 @@
+from .IMotion import IMotion
+
+
+class IRoof(IMotion):
+    """The module controls a roof."""
+    __module__ = 'pyobs.interfaces'
+    pass
+
+
+__all__ = ['IRoof']
---
+++ packages/pyobs-core/pyobs-core-0.15.0a9.tar.gz/pyobs-core-0.15.0a9/pyobs/interfaces/interface.py
@@ -0,0 +1,7 @@
+class Interface(object):
+    """Base class for all interfaces in pyobs."""
+    __module__ = 'pyobs.interfaces'
+    pass
+
+
+__all__ = ['Interface']
