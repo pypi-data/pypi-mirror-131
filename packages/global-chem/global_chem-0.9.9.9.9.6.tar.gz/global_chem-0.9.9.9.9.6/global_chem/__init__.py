de61d9e1895fde7bcfb9ed86deb73ac648e1e95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:00 +0000

    Add package global-chem/0.9.9.9.9.6/global_chem-0.9.9.9.9.6.tar.gz

---
+++ packages/global-chem/global_chem-0.9.9.9.9.6.tar.gz/global_chem-0.9.9.9.9.6/global_chem/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# Intialization of the package
+#
+# ----------------------------
+
+from global_chem.environment.emerging_perfluoroalkyls import EmergingPerFluoroAlkyls
+
+from global_chem.global_chem import GlobalChem
+
+__all__ = ['GlobalChem']
+
+__name__ = 'GlobalChem'
