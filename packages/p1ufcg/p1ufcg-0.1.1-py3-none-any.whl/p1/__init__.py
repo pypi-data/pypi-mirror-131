987491c2059d120f558034ecac5aef22d10247bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:12 +0000

    Add package p1ufcg/0.1.1/p1ufcg-0.1.1-py3-none-any.whl

---
+++ packages/p1ufcg/p1ufcg-0.1.1-py3-none-any.whl/p1/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .utils import _assert, cprint
+import p1.popargs
+from .colors import *
+
+from .p1 import save_assignment
+from .p1 import coverit
+from .p1 import get_config
+from .p1 import dirtype
+from .p1 import read_specification
+from .p1 import get_site
