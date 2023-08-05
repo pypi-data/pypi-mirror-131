9e96e5a40804586368313f1a86bcd852d4191848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:11 +0000

    Add package dewloosh.math/0.0.dev5/dewloosh.math-0.0.dev5.tar.gz

---
+++ packages/dewloosh.math/dewloosh.math-0.0.dev5.tar.gz/dewloosh.math-0.0.dev5/src/dewloosh/math/linalg/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 18:31:06 2018
+
+@author: Bence
+"""
+
+from dewloosh.math.linalg.linalg import *
+from dewloosh.math.linalg.array import *
+from dewloosh.math.linalg.vector import *
+from dewloosh.math.linalg.tensor import *
+from dewloosh.math.linalg._solve import *
