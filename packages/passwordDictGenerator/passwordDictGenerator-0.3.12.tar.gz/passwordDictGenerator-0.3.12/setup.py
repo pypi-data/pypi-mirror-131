2850c9dde34051ca281bb3f5fa0cfc9404c54d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:53 +0000

    Add package passwordDictGenerator/0.3.12/passwordDictGenerator-0.3.12.tar.gz

---
+++ packages/passwordDictGenerator/passwordDictGenerator-0.3.12.tar.gz/passwordDictGenerator-0.3.12/setup.py
@@ -0,0 +1,13 @@
+#  AUTHOR: Roman Bergman <roman.bergman@protonmail.com>
+# LICENSE: AGPL3.0
+# VERSION: 0.0.2
+
+import setuptools
+
+setuptools.setup(
+    entry_points = {
+        'console_scripts': [
+            'snake = pwdgen.pwdgen:main'
+        ]
+  }
+)
