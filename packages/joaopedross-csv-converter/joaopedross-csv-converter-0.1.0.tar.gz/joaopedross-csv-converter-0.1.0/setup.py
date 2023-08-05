6891f584a57b9a9f2dcf1faf97744cb11de799c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:36 +0000

    Add package joaopedross-csv-converter/0.1.0/joaopedross-csv-converter-0.1.0.tar.gz

---
+++ packages/joaopedross-csv-converter/joaopedross-csv-converter-0.1.0.tar.gz/joaopedross-csv-converter-0.1.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['joaopedross_csv_converter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0', 'pandas>=1.3.4,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['csv_converter = '
+                     'joaopedross_csv_converter.converter:converter']}
+
+setup_kwargs = {
+    'name': 'joaopedross-csv-converter',
+    'version': '0.1.0',
+    'description': 'Converter CSV para JSON e JSON para CSV.',
+    'long_description': '',
+    'author': 'João Pedro',
+    'author_email': 'joaopedr0ss1697@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
