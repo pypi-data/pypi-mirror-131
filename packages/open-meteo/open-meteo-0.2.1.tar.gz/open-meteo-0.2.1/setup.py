f758d574fcb2c362e5545338d7a37cbd6848a439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:11 +0000

    Add package open-meteo/0.2.1/open-meteo-0.2.1.tar.gz

---
+++ packages/open-meteo/open-meteo-0.2.1.tar.gz/open-meteo-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['open_meteo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.8.1,<4.0.0', 'pydantic>=1.8.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'open-meteo',
+    'version': '0.2.1',
+    'description': 'Asynchronous client for the Open-Meteo API.',
+    'long_description': '# Python: Asynchronous client for the Open-Meteo API.\n\n[![GitHub Release][releases-shield]][releases]\n[![Python Versions][python-versions-shield]][pypi]\n![Project Stage][project-stage-shield]\n![Project Maintenance][maintenance-shield]\n[![License][license-shield]](LICENSE.md)\n\n[![Build Status][build-shield]][build]\n[![Code Coverage][codecov-shield]][codecov]\n[![Code Quality][code-quality-shield]][code-quality]\n\n[![Sponsor Frenck via GitHub Sponsors][github-sponsors-shield]][github-sponsors]\n\n[![Support Frenck on Patreon][patreon-shield]][patreon]\n\nAsynchronous client for the Open-Meteo API.\n\n## About\n\nOpen-Meteo offers free weather forecast APIs for open-source developers and\nnon-commercial use. No API key is required. You can start using it immediately!\n\n## Installation\n\n```bash\npip install open-meteo\n```\n\n## Usage\n\n```python\nimport asyncio\n\nfrom open_meteo import OpenMeteo\nfrom open_meteo.models import DailyParameters, HourlyParameters\n\n\nasync def main():\n    """Show example on using the Open-Meteo API client."""\n    async with OpenMeteo() as open_meteo:\n        forecast = await open_meteo.forecast(\n            latitude=52.27,\n            longitude=6.87417,\n            current_weather=True,\n            daily=[\n                DailyParameters.SUNRISE,\n                DailyParameters.SUNSET,\n            ],\n            hourly=[\n                HourlyParameters.TEMPERATURE_2M,\n                HourlyParameters.RELATIVE_HUMIDITY_2M,\n            ],\n        )\n        print(forecast)\n\n\nif __name__ == "__main__":\n    asyncio.run(main())\n```\n\n## Changelog & Releases\n\nThis repository keeps a change log using [GitHub\'s releases][releases]\nfunctionality. The format of the log is based on\n[Keep a Changelog][keepchangelog].\n\nReleases are based on [Semantic Versioning][semver], and use the format\nof `MAJOR.MINOR.PATCH`. In a nutshell, the version will be incremented\nbased on the following:\n\n- `MAJOR`: Incompatible or major changes.\n- `MINOR`: Backwards-compatible new features and enhancements.\n- `PATCH`: Backwards-compatible bugfixes and package updates.\n\n## Contributing\n\nThis is an active open-source project. We are always open to people who want to\nuse the code or contribute to it.\n\nWe\'ve set up a separate document for our\n[contribution guidelines](CONTRIBUTING.md).\n\nThank you for being involved! :heart_eyes:\n\n## Setting up development environment\n\nThis Python project is fully managed using the [Poetry][poetry] dependency\nmanager. But also relies on the use of NodeJS for certain checks during\ndevelopment.\n\nYou need at least:\n\n- Python 3.8+\n- [Poetry][poetry-install]\n- NodeJS 14+ (including NPM)\n\nTo install all packages, including all development requirements:\n\n```bash\nnpm install\npoetry install\n```\n\nAs this repository uses the [pre-commit][pre-commit] framework, all changes\nare linted and tested with each commit. You can run all checks and tests\nmanually, using the following command:\n\n```bash\npoetry run pre-commit run --all-files\n```\n\nTo run just the Python tests:\n\n```bash\npoetry run pytest\n```\n\n## Authors & contributors\n\nThe original setup of this repository is by [Franck Nijhof][frenck].\n\nFor a full list of all authors and contributors,\ncheck [the contributor\'s page][contributors].\n\n## License\n\nMIT License\n\nCopyright (c) 2021 Franck Nijhof\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the "Software"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.\n\n[build-shield]: https://github.com/frenck/python-open-meteo/actions/workflows/tests.yaml/badge.svg\n[build]: https://github.com/frenck/python-open-meteo/actions/workflows/tests.yaml\n[code-quality-shield]: https://img.shields.io/lgtm/grade/python/g/frenck/python-open-meteo.svg?logo=lgtm&logoWidth=18\n[code-quality]: https://lgtm.com/projects/g/frenck/python-open-meteo/context:python\n[codecov-shield]: https://codecov.io/gh/frenck/python-open-meteo/branch/master/graph/badge.svg\n[codecov]: https://codecov.io/gh/frenck/python-open-meteo\n[contributors]: https://github.com/frenck/python-open-meteo/graphs/contributors\n[frenck]: https://github.com/frenck\n[github-sponsors-shield]: https://frenck.dev/wp-content/uploads/2019/12/github_sponsor.png\n[github-sponsors]: https://github.com/sponsors/frenck\n[keepchangelog]: http://keepachangelog.com/en/1.0.0/\n[license-shield]: https://img.shields.io/github/license/frenck/python-open-meteo.svg\n[maintenance-shield]: https://img.shields.io/maintenance/yes/2021.svg\n[patreon-shield]: https://frenck.dev/wp-content/uploads/2019/12/patreon.png\n[patreon]: https://www.patreon.com/frenck\n[poetry-install]: https://python-poetry.org/docs/#installation\n[poetry]: https://python-poetry.org\n[pre-commit]: https://pre-commit.com/\n[project-stage-shield]: https://img.shields.io/badge/Project%20Stage-Concept-red.svg\n[pypi]: https://pypi.org/project/open-meteo/\n[python-versions-shield]: https://img.shields.io/pypi/pyversions/open-meteo\n[releases-shield]: https://img.shields.io/github/release/frenck/python-open-meteo.svg\n[releases]: https://github.com/frenck/python-open-meteo/releases\n[semver]: http://semver.org/spec/v2.0.0.html\n',
+    'author': 'Franck Nijhof',
+    'author_email': 'opensource@frenck.dev',
+    'maintainer': 'Franck Nijhof',
+    'maintainer_email': 'opensource@frenck.dev',
+    'url': 'https://github.com/frenck/python-open-meteo',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
