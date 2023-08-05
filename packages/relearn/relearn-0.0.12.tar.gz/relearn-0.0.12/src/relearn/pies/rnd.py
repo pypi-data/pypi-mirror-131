8b25d1ac25222e16b1acecba17caf45ab0630791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:47 +0000

    Add package relearn/0.0.12/relearn-0.0.12.tar.gz

---
+++ packages/relearn/relearn-0.0.12.tar.gz/relearn-0.0.12/src/relearn/pies/rnd.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+class PIE:
+    """ Implements Random Policy """
+
+    def __init__(self, nos_actions, seed=None):
+        self.A = nos_actions
+        self.Q = None
+        self.train_count=0
+        self.rng = np.random.default_rng(seed)
+        
+    def predict(self, state):
+        return self.rng.integers(0, self.A-1)
+    def qredict(self, state):
+        return self.rng.integers(0, self.A-1)
+        
+    def qvals(self, state):
+        qvals = [0 for _ in range(self.A)]
+        return qvals
+        
+    def learn(self, memory, batch):
+        self.train_count+=1
+        return
+    
+    def clear(self):
+        self.Q = None
+        self.train_count=0
+        return
+        
+    def render(self, mode=0):
+        return "=-=-=-=-==-=-=-=-=\n RANDOM POLICY \n=-=-=-=-==-=-=-=-="
+    
+    def save(self, path):
+        return
+        
+    def load(self, path):
+        return
+        
+#-------------------
