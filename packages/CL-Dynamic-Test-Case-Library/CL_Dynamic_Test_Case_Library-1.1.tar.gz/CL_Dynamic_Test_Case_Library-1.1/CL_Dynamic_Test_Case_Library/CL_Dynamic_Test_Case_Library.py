c0b6ea98519100b4259e5dabf80e0c5f91d15d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:10 +0000

    Add package CL-Dynamic-Test-Case-Library/1.1/CL_Dynamic_Test_Case_Library-1.1.tar.gz

---
+++ packages/CL-Dynamic-Test-Case-Library/CL_Dynamic_Test_Case_Library-1.1.tar.gz/CL_Dynamic_Test_Case_Library-1.1/CL_Dynamic_Test_Case_Library/CL_Dynamic_Test_Case_Library.py
@@ -0,0 +1,39 @@
+from robot.running.model import TestSuite
+
+
+class CL_Dynamic_Test_Case_Library(object):
+    ROBOT_LISTENER_API_VERSION = 3
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+    ROBOT_LIBRARY_VERSION = 0.1
+
+    def __init__(self):
+        self.ROBOT_LIBRARY_LISTENER = self
+        self.current_suite = None
+
+    def _start_suite(self, suite, result):
+        """
+        Starts running the suite of test cases that were dynamically created via
+        the python module create_test_case
+        and remove the test suite placeholder test so that it
+        is not counted in the test results metrics
+        """
+        self.current_suite = suite
+        self.current_suite.tests.clear()  # remove placeholder test
+
+    def create_test_case(self, tc_name, kwname, arguments):
+        """
+        Creates a dynamic test case for the tc_name (test case name) and uses the keyword and
+        arguments (keyword arguments) provided as a template to run the test case for each row
+        of data in the excel data file worksheet
+
+        'tc_name' is the test case name to be created
+        'kwname' is the keyword to call
+        'arguments' are the arguments to pass to the keyword
+
+        Example:
+            Create Test Case   Test Case Name
+            ...     Keyword Name    Arg1   Arg2  Arg3
+        """
+        tc = self.current_suite.tests.create(name=tc_name)
+        tc.body.create_keyword(name=kwname, args=arguments)
+
