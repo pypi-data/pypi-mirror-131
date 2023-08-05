6f35ef11e25103ab6660da24857c7dbd797c8008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:33 +0000

    Add package arrakis/0.1.0/arrakis-0.1.0.tar.gz

---
+++ packages/arrakis/arrakis-0.1.0.tar.gz/arrakis-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['arrakis']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'arrakis',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Alan Munoz',
+    'author_email': 'alan.munoz@ed.ac.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
