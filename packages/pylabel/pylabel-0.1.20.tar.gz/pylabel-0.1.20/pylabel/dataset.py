e8f3d86c9066c0737f9800db5928aedc0d137e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:25 +0000

    Add package pylabel/0.1.20/pylabel-0.1.20.tar.gz

---
+++ packages/pylabel/pylabel-0.1.20.tar.gz/pylabel-0.1.20/pylabel/dataset.py
@@ -0,0 +1,24 @@
+from pylabel.analyze import Analyze
+from pylabel.exporter import Export
+from pylabel.visualize import Visualize
+from pylabel.labeler import Labeler
+from pylabel.splitter import Split
+
+
+#from pylabel.splitter import GroupShuffleSplit, test
+#from pylabel.splitter import StratifiedGroupShuffleSplit
+
+def FilterList(the_list):
+    """To do: Check why this code is here"""
+    the_list = [i for i in the_list if len(i.strip()) > 0]
+
+class Dataset:
+    def __init__(self, df):
+        self.df = df
+        self.name = "dataset"
+        self.path_to_annotations = ""    
+        self.export = Export(dataset=self)
+        self.visualize = Visualize(dataset=self)
+        self.analyze = Analyze(dataset=self)
+        self.labeler = Labeler(self)
+        self.splitter = Split(dataset=self)
