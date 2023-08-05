82fda9e37f9e8f4608337f9678218dd7394b0bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:39 +0000

    Add package strid/0.2.0/strid-0.2.0.tar.gz

---
+++ packages/strid/strid-0.2.0.tar.gz/strid-0.2.0/strid/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from . import spatial
+from .stabdiag import *
+from . import stabdiag
+from ._subspaceid import *
+from . import utils
+from .utils import *
+
+
+__version__ = "0.2.0"
