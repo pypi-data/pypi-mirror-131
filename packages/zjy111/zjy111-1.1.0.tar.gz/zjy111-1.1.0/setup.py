d9380b0552d7d8bac062f7e9c36c25716e010810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:17:25 +0000

    Add package zjy111/1.1.0/zjy111-1.1.0.tar.gz

---
+++ packages/zjy111/zjy111-1.1.0.tar.gz/zjy111-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from distutils.core import setup
+setup(
+    name = 'zjy111',
+    version = '1.1.0',
+    py_modules = ['zjy111'],
+    author = 'zjy_study',
+    author_email = 'zjy_home@qq.com',
+    url = 'https://test.pypi.org/legacy/',
+    description = 'A simple printer of nested lists'    
+    )
