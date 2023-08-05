e90d9dd5f2f493e55811e3d25d1f64b4c7269af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:52 +0000

    Add package frabit/2.1.0/frabit-2.1.0-py3-none-any.whl

---
+++ packages/frabit/frabit-2.1.0-py3-none-any.whl/frabit/version.py
@@ -0,0 +1,10 @@
+# (c) 2020 Frabit Project maintained and limited by Blylei < blylei.info@gmail.com >
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+# This file is part of Frabit
+#
+"""
+This module contains the current Frabit version.
+"""
+
+__version__ = '2.1.0'
