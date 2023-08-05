51c437afa450040b44673866b6af557d36574c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:23 +0000

    Add package crocodile/4.5/crocodile-4.5-py3-none-any.whl

---
+++ packages/crocodile/crocodile-4.5-py3-none-any.whl/crocodile/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "4.5"
+docs = r"https://crocodile.readthedocs.io/en/latest/"
+git = r"https://github.com/thisismygitrepo/crocodile"
+release_notes = """
+
+Added Log and Frdige classes.
+"""
