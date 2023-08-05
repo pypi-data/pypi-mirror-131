f470405f534b9657547bb666e70ce8abe562f174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:58 +0000

    Add package frabit-server/2.0.3/frabit_server-2.0.3-py3-none-any.whl

---
+++ packages/frabit-server/frabit_server-2.0.3-py3-none-any.whl/frabitd/scanner.py
@@ -0,0 +1,29 @@
+# (c) 2020 frabit-server Project maintained and limited by FrabiTech < blylei.info@gmail.com >
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+# This file is part of frabit-server
+"""
+后台备份任务扫描器
+"""
+
+import os
+import re
+import logging
+
+import frabitd
+from frabitd.process import ProcessManager
+from frabitd.exceptions import ProcessException
+
+_logger = logging.getLogger(__name__)
+
+
+class Scanner:
+    """
+    add doc here
+    """
+    def __init__(self, config):
+        self.config = config
+        self.process_manager = ProcessManager(self.config)
+
+    def process(self):
+        pass
---
+++ packages/frabit-server/frabit_server-2.0.3-py3-none-any.whl/frabitd/version.py
@@ -0,0 +1,10 @@
+# (c) 2020 frabit-server Project maintained and limited by FrabiTech < blylei.info@gmail.com >
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+# This file is part of frabit-server
+#
+"""
+This module contains the current frabit-server version.
+"""
+
+__version__ = '2.0.3'
