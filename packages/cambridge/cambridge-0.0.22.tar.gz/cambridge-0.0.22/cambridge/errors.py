3c6dc7dc12b670c807e1128f78c32b96cf1d3d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:27 +0000

    Add package cambridge/0.0.22/cambridge-0.0.22.tar.gz

---
+++ packages/cambridge/cambridge-0.0.22.tar.gz/cambridge-0.0.22/cambridge/errors.py
@@ -0,0 +1,10 @@
+"""
+This script sets up self-defined errors.
+"""
+
+
+class ParsedNoneError(Exception):
+    """Used when bs4 returned None whereas there's target content existing within the document"""
+
+    def __init__(self):
+        self.message = "Got None wth content in place"
