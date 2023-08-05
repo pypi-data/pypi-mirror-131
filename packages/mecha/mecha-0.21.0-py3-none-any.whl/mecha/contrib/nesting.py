1e3e59eb38ae4e69900a026b4e8f5828227ccc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:19 +0000

    Add package mecha/0.21.0/mecha-0.21.0-py3-none-any.whl

---
+++ packages/mecha/mecha-0.21.0-py3-none-any.whl/mecha/contrib/nesting.py
@@ -0,0 +1,251 @@
+"""Plugin for handling nested commands."""
+
+
+__all__ = [
+    "AstNestedExecute",
+    "NestedExecuteParser",
+    "NestedCommandsTransformer",
+    "parse_nested_root",
+]
+
+
+from dataclasses import dataclass, replace
+from typing import Any, List, cast
+
+from beet import Context, Function
+from beet.core.utils import required_field
+from tokenstream import TokenStream, set_location
+
+from mecha import (
+    AstChildren,
+    AstCommand,
+    AstNode,
+    AstResourceLocation,
+    AstRoot,
+    CompilationDatabase,
+    Diagnostic,
+    Mecha,
+    MutatingReducer,
+    Parser,
+    consume_line_continuation,
+    delegate,
+    get_stream_properties,
+    get_stream_scope,
+    rule,
+)
+
+COMMAND_TREE = {
+    "type": "root",
+    "children": {
+        "execute": {
+            "type": "literal",
+            "children": {
+                "expand": {
+                    "type": "literal",
+                    "children": {
+                        "commands": {
+                            "type": "argument",
+                            "parser": "mecha:nested_root",
+                            "executable": True,
+                        }
+                    },
+                },
+            },
+        },
+        "function": {
+            "type": "literal",
+            "children": {
+                "name": {
+                    "type": "argument",
+                    "parser": "minecraft:function",
+                    "executable": True,
+                    "children": {
+                        "commands": {
+                            "type": "argument",
+                            "parser": "mecha:nested_root",
+                            "executable": True,
+                        }
+                    },
+                }
+            },
+        },
+    },
+}
+
+
+@dataclass(frozen=True)
+class AstNestedExecute(AstNode):
+    """Ast nested execute node."""
+
+    root: AstRoot = required_field()
+
+
+def beet_default(ctx: Context):
+    mc = ctx.inject(Mecha)
+
+    mc.spec.multiline = True
+    mc.spec.add_commands(COMMAND_TREE)
+
+    mc.spec.parsers["nested_root"] = parse_nested_root
+    mc.spec.parsers["command:argument:mecha:nested_root"] = delegate("nested_root")
+    mc.spec.parsers["command"] = NestedExecuteParser(parser=mc.spec.parsers["command"])
+
+    mc.transform.extend(NestedCommandsTransformer(ctx=ctx, database=mc.database))
+
+
+def parse_nested_root(stream: TokenStream) -> AstRoot:
+    """Parse nested root."""
+    properties = get_stream_properties(stream)
+
+    if properties.get("check_nesting", True):
+        with stream.intercept("indent"):
+            stream.expect("indent")
+
+    level, command_level = stream.indentation[-2:]
+
+    commands: List[AstCommand] = []
+
+    with stream.intercept("newline"), stream.provide(
+        scope=(),
+        line_indentation=command_level,
+    ):
+        while True:
+            commands.append(delegate("command", stream))
+
+            # The command parser consumes the trailing newline so we need to rewind
+            # to be able to use "consume_line_continuation()".
+            while (token := stream.peek()) and not token.match("newline", "eof"):
+                stream.index -= 1
+
+            with stream.provide(multiline=True, line_indentation=level):
+                if not consume_line_continuation(stream):
+                    break
+
+    node = AstRoot(commands=AstChildren(commands))
+    return set_location(node, commands[0], commands[-1])
+
+
+@dataclass
+class NestedExecuteParser:
+    """Parser for nested execute."""
+
+    parser: Parser
+
+    def __call__(self, stream: TokenStream) -> Any:
+        scope = get_stream_scope(stream)
+
+        if scope == ("execute",):
+            with stream.checkpoint(), stream.intercept("indent"):
+                indent = stream.get("indent")
+
+            if indent:
+                node = delegate("nested_root", stream)
+                return set_location(AstNestedExecute(root=node), node)
+
+        return self.parser(stream)
+
+
+@dataclass
+class NestedCommandsTransformer(MutatingReducer):
+    """Transformer that handles nested commands."""
+
+    ctx: Context = required_field()
+    database: CompilationDatabase = required_field()
+
+    def emit_function(self, path: str, root: AstRoot):
+        """Helper method for emitting nested commands into a separate function."""
+        function = Function()
+        self.ctx.data[path] = function
+        self.database[function] = replace(
+            self.database[self.database.current],
+            ast=root,
+            resource_location=path,
+        )
+        self.database.enqueue(function, self.database.step + 1)
+
+    @rule(AstCommand, identifier="function:name:commands")
+    def nested_function(self, node: AstCommand):
+        name, root = node.arguments
+
+        if isinstance(name, AstResourceLocation) and isinstance(root, AstRoot):
+            path = name.get_canonical_value()
+
+            if path in self.ctx.data.functions:
+                d = Diagnostic("error", f"Function {path!r} already exists.")
+                raise set_location(d, name)
+
+            self.emit_function(path, root)
+
+        return replace(node, identifier="function:name", arguments=AstChildren([name]))
+
+    @rule(AstNestedExecute)
+    def nested_execute(self, node: AstNestedExecute):
+        generate = self.ctx.generate["nested_execute"]
+
+        if len(node.root.commands) == 1:
+            subcommand = node.root.commands[0]
+
+            if subcommand.identifier == "execute:subcommand":
+                return subcommand.arguments[0]
+
+        else:
+            if path := self.database[self.database.current].resource_location:
+                path = generate.format(path + "/nested_execute_{incr}")
+            else:
+                path = generate.path()
+
+            self.emit_function(path, node.root)
+
+            resource_location = AstResourceLocation.from_value(path)
+
+            subcommand = AstCommand(
+                identifier="function:name",
+                arguments=AstChildren([cast(AstNode, resource_location)]),
+            )
+
+        return AstCommand(
+            identifier="execute:run:subcommand",
+            arguments=AstChildren([cast(AstNode, subcommand)]),
+        )
+
+    @rule(AstRoot)
+    def execute_expand(self, node: AstRoot):
+        commands: List[AstCommand] = []
+
+        for command in node.commands:
+            args = command.arguments
+            stack: List[AstCommand] = [command]
+
+            expand = None
+
+            while args and isinstance(subcommand := args[-1], AstCommand):
+                if subcommand.identifier == "execute:expand:commands":
+                    expand = subcommand
+                    break
+                stack.append(subcommand)
+                args = subcommand.arguments
+
+            if expand:
+                for nested_command in cast(AstRoot, expand.arguments[0]).commands:
+                    if nested_command.identifier == "execute:subcommand":
+                        expansion = cast(AstCommand, nested_command.arguments[0])
+                    else:
+                        expansion = AstCommand(
+                            identifier="execute:run:subcommand",
+                            arguments=AstChildren([cast(AstNode, nested_command)]),
+                        )
+                        expansion = set_location(expansion, nested_command)
+
+                    for prefix in reversed(stack):
+                        args = AstChildren([*prefix.arguments[:-1], expansion])
+                        expansion = replace(prefix, arguments=args)
+
+                    commands.append(expansion)
+
+            else:
+                commands.append(command)
+
+        if len(commands) > len(node.commands):
+            return replace(node, commands=AstChildren(commands))
+
+        return node
---
+++ packages/mecha/mecha-0.21.0-py3-none-any.whl/mecha/resources/scripting_examples.mcfunction
@@ -0,0 +1,497 @@
+0
+###
+123
+###
+123.456
+###
+22220099.6667e-3
+###
+0.67e6
+###
+1 + 2
+###
+1 - 2
+###
+1 - 2 + 3
+###
+1 - (2 + 3)
+###
+2 + -54
+###
+2 + -54 * 10
+###
+(2 + -54) * 10
+###
+---64+++64
+###
+1 * 2 / 3 * 4
+###
+1 * 2 / (3 * 4)
+###
+16 // 5 % 7
+###
+-2 ** 3 + ~4 * 5 // 6 / 7 % 9 + 10 - 11 << 12 >> 13 & 14 ^ 15 | 16
+###
+False or True and "hello" or not "foo" == "bar" or 1 != 2 and 1 < 2 and 2 <= 3 and 5 > 4 and 6 >= 5 and 1 in 1 and 2 not in 2 and 3 is 3 and 3 is not 3
+###
+64 §§§
+###
+17 % %3
+###
+True and (False or True)
+###
+"this"
+###
+"this\nthat"
+###
+"\"\u2588this\nthat"
+###
+''
+###
+'"\'"'
+###
+'something \t\f\n\\\'here'
+###
+'abc\"'
+###
+"hey" "hey"
+###
+None
+###
+foo
+###
+"hello" + foo
+###
+foo = 1
+foo
+###
+foo = 1
+"hello" + foo
+###
+a = 1
+if a == 1
+    a = 0
+###
+if True
+    say hello
+elif True or False
+    say hello
+else
+    say hello
+###
+if True
+    say hello
+elif True or False
+    say hello
+else "blah"
+    say hello
+###
+if True
+    say hello
+a = 1
+else
+    say hello
+###
+if
+###
+if True and
+###
+if True
+say hello
+###
+while True
+    say hello
+###
+if score @s tmp matches 0
+    if "thing" == "bar"
+        say hello
+    if "thing" == "foo"
+        say hello
+###
+if score @s tmp matches 0
+    if "thing" == "foo"
+        say hello
+###
+if score @s tmp matches 0
+    while True
+        say hello
+    while True
+        say hello
+###
+if score @s tmp matches 0
+    while True
+        say hello
+###
+if score @s tmp matches 42
+    count = 42
+    while count > 0
+        say hello
+        count = count - 1
+###
+foo = (
+    "abc"
+    + "def"
+)
+###
+1 * 2
+* 3
+###
+true | false | null
+###
+foo = 1
+foo == 1
+###
+for c in "abc"
+    say something
+###
+foo = foo
+###
+foo = 1
+foo = foo
+###
+for foo in foo
+    foo = foo
+###
+for foo in "foo"
+    foo = foo
+###
+foo += "hey"
+###
+foo = ""
+foo += "hey"
+###
+wat = the = "f"
+###
+break
+###
+continue
+###
+if true
+    break
+###
+for i in ""
+    say hello
+    continue
+###
+while false
+    break
+###
+for i in "abc"
+    if i == "b"
+        break
+###
+"foo".bar
+###
+"foo".bar.baz
+###
+"foo".bar()[0]
+###
+"foo".bar()[0]."hello".99."with space"("thing" * 7)
+###
+"foo"(
+    1,
+    2,
+    3,
+)
+###
+def foo()
+    say hello
+foo()
+###
+def foo()
+    def bar()
+        foo()
+    bar()
+###
+def foo(
+    a=1,
+    b=a,
+    c=a + b,
+)
+    say hello
+###
+def f
+###
+def f()
+###
+def f()
+say yolo
+###
+def f(a=a)
+    say wat
+###
+def f(a, b=a)
+    b += a
+    say wat
+###
+def f(a, b)
+    def g(c)
+        a + b
+    c
+g()
+###
+def f(a, b)
+    def g(c)
+        a + b
+    f(c)
+###
+def foo(something)
+    def wat()
+        say
+            yo
+            wat
+            is
+            dat
+# this is a comment
+        x = "hello"
+        for i in somehng
+            x += i * 3
+
+        say wow
+    wat()
+###
+def foo(something)
+    def wat()
+        say
+            yo
+            wat
+            is
+            dat
+# this is a comment
+        x = "hello"
+        for i in something
+            x += i * 3
+
+        say wow
+    wat()
+###
+def foo()
+    thing += bar()
+def bar()
+    thing += foo()
+thing = bar()
+###
+def foo()
+    bar() + thing
+def bar()
+    foo() + thin
+thing = bar()
+###
+def foo()
+    return
+###
+return "hello"
+###
+return foo
+###
+def f()
+    def g()
+        return foo
+    foo = 0
+###
+def f()
+    foo = 0
+f(foo)
+###
+def f(x)
+    tellraw @a x
+f("thing")
+###
+def f(x)
+    tellraw @a xx
+f("thing")
+###
+{
+    foo: "bar"
+}
+###
+foo = "hello"
+{
+    foo: "bar"
+}
+###
+foo = "hello"
+{
+    "foo": foo
+}
+###
+foo = "hello"
+{
+    foo * 3: 42,
+    16 + 3: [],
+}
+###
+predicate = {
+  "condition": "minecraft:entity_scores",
+  "entity": "this",
+  "scores": {
+    "score1": {
+      "min": {
+        "type": "minecraft:score",
+        "target": "this",
+        "score": "score2",
+        "scale": 1
+      }
+    }
+  }
+}
+###
+predicate = {
+  condition: "minecraft:entity_scores",
+  entity: "this",
+  scores: {
+    score1: {
+      "min": {
+        "type": "minecraft:score",
+        target: "this",
+        score: "score2",
+        scale: 1
+      }
+    }
+  }
+}
+###
+def f()
+    return {f(): f(), other: [{}, {}, "wat"]}
+###
+at @s if "foo" == "bar"
+    say yolo
+###
+at @s for i in "foo"
+    say yolo
+###
+say hello
+###
+def wat()
+    say hello
+###
+a = ''
+tellraw @p a
+###
+foo = '[{"text": "Hello", "bold": true}]'
+as @a at @s if block ~ ~-1 ~ #wool give @s stone{
+    display: {
+        Name: foo,
+        Lore: [
+            '[{ "text": "Something else here" }]',
+        ]
+    }
+}
+###
+thing = (1, 'hey') + () + ("wow",)
+###
+f"hello"
+###
+foo = "world"
+f"hello {foo}"
+###
+foo = "world"
+f"hello {foo!r}"
+###
+f"hello {1 + 1}"
+###
+f"hello {1 + 1:03}"
+###
+f'{{}}nope{1}'
+###
+yo = f"{{{f"{{{f"{{{7:08}}}"}}}"}}}"
+###
+yo = f""
+###
+yo = f"\""
+###
+yo = f"\\"
+###
+yo = f"}"
+###
+yo = f'thing \e'
+###
+x = 8
+if score @s tmp matches (x, None) say wat
+###
+def f()
+    yield 1
+    yield 2
+    yield 3
+for i in f()
+    say i
+###
+if True
+    yield
+###
+def f()
+    yield
+###
+import demo:foo
+###
+import ./thing
+###
+import wat/is/that
+###
+from wat/is/that import foo
+###
+import math
+mah.sin(1)
+###
+import math
+math.sin(1)
+###
+import math as m
+m.sin(1)
+###
+from demo:foo import thing
+say thing
+###
+def f()
+    from demo:foo import a b c
+say a
+###
+if score @s tmp matches 1.. if entity @p[tag=foo]
+    if True
+        say 42
+    else
+        say no
+###
+setblock = 0
+###
+if 1
+    say 1
+elif 2
+    say 2
+elif 3
+    say 3
+elif 4
+    say 4
+###
+if 1
+    say 1
+elif 2
+    say 2
+elif 3
+    say 3
+elif 4
+    say 4
+say done
+###
+if 1
+    say 1
+elif 2
+    say 2
+elif 3
+    say 3
+elif 4
+    say 4
+else
+    say other
+###
+if 1
+    say 1
+elif 2
+    say 2
+elif 3
+    say 3
+elif 4
+    say 4
+else
+    say other
+say done
