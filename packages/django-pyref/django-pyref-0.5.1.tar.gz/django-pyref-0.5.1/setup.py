1049710330fb15d1c185b7d703e30636913938f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:58 +0000

    Add package django-pyref/0.5.1/django-pyref-0.5.1.tar.gz

---
+++ packages/django-pyref/django-pyref-0.5.1.tar.gz/django-pyref-0.5.1/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+long_description = """django-pyref is a Python package for managing, importing and searching bibliographic references to scientific articles.
+
+Import of article metadata from DOI and Bibcode is supported.
+
+See https://github.com/xnx/django-pyref for more information.
+"""
+# Read in dependencies list from requirements.txt
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name = 'django-pyref',
+    version = '0.5.1',
+    author = 'Frances Skinner, Iouli Gordon, Christian Hill, Robert Hargreaves, Kelly Lockhart',
+    author_email = 'xn.hill@gmail.com',
+    description = 'A package for managing bibliographic references',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url = 'https://github.com/xnx/django-pyref',
+    packages = find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires='>=3.4',
+)
