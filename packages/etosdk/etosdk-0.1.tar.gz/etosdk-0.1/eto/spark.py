857716eefa3b5225ebcf34411ebbbad16c4680f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:03 +0000

    Add package etosdk/0.1/etosdk-0.1.tar.gz

---
+++ packages/etosdk/etosdk-0.1.tar.gz/etosdk-0.1/eto/spark.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+
+def get_session():
+    spark = (
+        SparkSession.builder.appName("eto-sdk-spark")
+        .master("local[1]")
+        .getOrCreate()
+    )
+    return spark
