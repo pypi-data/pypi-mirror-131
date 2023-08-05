9c50ae6c1387e151d32add5847adce59ba65ec09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:48 +0000

    Add package mcush/1.5.6/mcush-1.5.6.tar.gz

---
+++ packages/mcush/mcush-1.5.6.tar.gz/mcush-1.5.6/mcush/__init__.py
@@ -0,0 +1,24 @@
+# coding: utf8
+__doc__ = 'MCUSH module'
+__version__ = '1.5.6'
+__author__ = 'Peng Shulin <trees_peng@163.com>'
+__website__ = 'https://github.com/pengshulin/mcush/'
+__license__ = 'MCUSH designed by Peng Shulin, all rights reserved.'
+
+from . import Env
+from . import Utils
+from . import Instrument
+from . import Register
+from . import Mcush
+from . import AppUtils
+from . import Modbus
+
+try:
+    from .android import *
+    from .linkong import *
+    from .fluke import *
+    from .uni_trend import *
+    from .rigol import *
+    from .misc import *  # require pymodbus
+except ImportError as e:
+    print( 'import error:', e )
