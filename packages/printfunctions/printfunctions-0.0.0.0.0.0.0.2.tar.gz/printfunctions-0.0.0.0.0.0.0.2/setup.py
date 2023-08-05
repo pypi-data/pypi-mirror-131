522fd3ab8937a91c679ede51bd1557d67aa35600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:49 +0000

    Add package printfunctions/0.0.0.0.0.0.0.2/printfunctions-0.0.0.0.0.0.0.2.tar.gz

---
+++ packages/printfunctions/printfunctions-0.0.0.0.0.0.0.2.tar.gz/printfunctions-0.0.0.0.0.0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='printfunctions',
+    version='0.0.0.0.0.0.0.2',
+    packages=['printfunctions'],
+    url='',
+    license='The Unlicense',
+    author='EnderC00kiez',
+    author_email='EnderC00kiez@outlook.com',
+    description='ez text handling'
+)
