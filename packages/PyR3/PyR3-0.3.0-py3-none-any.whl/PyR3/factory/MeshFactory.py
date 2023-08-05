87f6b70f033dad61f783fe135dce5a072607658e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:47 +0000

    Add package PyR3/0.3.0/PyR3-0.3.0-py3-none-any.whl

---
+++ packages/PyR3/PyR3-0.3.0-py3-none-any.whl/PyR3/factory/MeshFactory.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from abc import ABCMeta, abstractmethod
+from inspect import isclass
+from typing import Tuple, Type
+
+from PyR3.factory.fields.Field import Field
+from PyR3.shortcut.context import temporary_scene
+
+
+class _MeshFactoryMeta(ABCMeta):
+
+    required_members = [
+        ["__doc__", lambda v: isinstance(v, str)],
+        ["__author__", lambda v: isinstance(v, str)],
+        ["__version__", lambda v: isinstance(v, str)],
+    ]
+
+    def __new__(cls, name, bases, attributes) -> None:
+        cls.check_has_members(name, attributes)
+        attributes["__factory_fields__"] = cls.get_field_names(
+            cls, name, attributes
+        )
+        attributes["__factory_fields__"] |= cls.get_inherited_fields(bases)
+        instance = ABCMeta.__new__(cls, name, bases, attributes)
+        cls.wrap_render(instance)
+        return instance
+
+    def get_field_names(cls, classname, attributes: dict) -> dict:
+        fields = {}
+        for name, field in attributes.items():
+            if isinstance(field, Field):
+                field._set_trace_info(name, classname)
+                fields[name] = field
+            elif isclass(field) and issubclass(field, Field):
+                field = field()
+                field._set_trace_info(name, classname)
+                fields[name] = field
+        return fields
+
+    def get_inherited_fields(bases: Tuple[Type[MeshFactory]]) -> dict:
+        inherited_fields = {}
+        for base in bases:
+            if issubclass(base, MeshFactory):
+                inherited_fields.update(getfields(base))
+        return inherited_fields
+
+    @classmethod
+    def check_has_members(cls, classname, attributes):
+        for name, validator in cls.required_members:
+            if (value := attributes.get(name, None)) is None:
+                raise TypeError(
+                    f"Trying to create class {classname} without required member {name}."
+                )
+            if not validator(value):
+                raise TypeError(
+                    f"Trying to create class {classname} with {name} of invalid type."
+                )
+
+    @classmethod
+    def wrap_render(cls, instance: MeshFactory):
+        old_render = instance.render
+
+        def render(*args, **kwargs):
+            with temporary_scene():
+                old_render(*args, **kwargs)
+
+        instance.render = render
+
+
+class MeshFactory(metaclass=_MeshFactoryMeta):
+    """Base class for a mesh factory object.
+
+    Mesh factory requires __doc__, __author__ and __version__ to be
+    defined in mesh factory subclass, otherwise class instantiation will
+    fail. Mesh factory can (and should) make use of Fields (subclasses
+    of Field class) to specify mesh factory customization params. See
+    PyR3.factory.fields modules for first-party fields. To specify field
+    just set class attribute to instance of Field subclass. See
+    :doc:`MeshFactory usage <../usage/factory>`.
+    """
+
+    __author__ = "Krzysztof Wiśniewski"
+    __version__ = "2.0.0"
+
+    def __init__(self, params: dict) -> None:
+        for name, field in getfields(self).items():
+            param_value = params.get(name, None)
+            cleaned_value = field.digest(param_value)
+            setattr(self, name, cleaned_value)
+
+    @abstractmethod
+    def render(self):
+        """Implements rendering process.
+
+        You can access cleaned contents of fields via self.field_name.
+        Rendering process shouldn't alter scenes, as it's always
+        happening in isolated scene. Only things that are selected when
+        this function returns will be copied into callers scene.
+        """
+
+
+def getfields(mesh_factory: MeshFactory) -> dict:
+    """Returns fields specified for given MeshFactory.
+
+    :param mesh_factory: Object to fetch fields from.
+    :type mesh_factory: MeshFactory
+    :return: dictionary of factory fields.
+    :rtype: dict
+    """
+    if isclass(mesh_factory):
+        return mesh_factory.__dict__["__factory_fields__"]
+    else:
+        return mesh_factory.__class__.__dict__["__factory_fields__"]
---
+++ packages/PyR3/PyR3-0.3.0-py3-none-any.whl/PyR3/factory/fields/String.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import re
+
+from .Field import Field
+
+
+class String(Field):
+    """String factory field. You can specify length and default for it. Its
+    value is always a string.
+
+    :param default: Default value, used if no value is provided.
+        If None, exception will be raised if no value will be given, defaults to None
+    :type default: str, optional
+    :param min_length: Minimal length of string. Exception will be raised
+        if requirement will not be fullfiled. Defaults to None
+    :type min_length: int, optional
+    :param max_length: Minimal length of string. Exception will be raised
+        if requirement will not be fullfiled. Defaults to None
+    :type max_length: int, optional
+    """
+
+    def __init__(
+        self,
+        *,
+        default: str = None,
+        min_length: int = None,
+        max_length: int = None,
+    ) -> None:
+        self.default = default
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def digest(self, value: str = None) -> str:
+        """Consumes value and returns cleaned string. Raises exception if
+        requirements for string format are not met.
+
+        :param value: value to consume, defaults to None
+        :type value: str, optional
+        :return: cleaned string.
+        :rtype: str
+        """
+        if value is None:
+            return self._get_default()
+        else:
+            string = str(value)
+            self._check_conditions(string)
+            return string
+
+    def _check_conditions(self, string: str):
+        if self.min_length is not None:
+            if not (self.min_length <= len(string)):
+                raise ValueError(
+                    f"String for {self._trace_location()} is too short ({len(string)}, min length: {self.min_length})"
+                )
+        if self.max_length is not None:
+            if not (len(string) <= self.max_length):
+                raise ValueError(
+                    f"String for {self._trace_location()} is too long ({len(string)}, min length: {self.max_length})"
+                )
+
+
+class Regex(String):
+    """String field with possibility to use regular expression to check if
+    string format is valid.
+
+    :param pattern: Regular expression patter. String will be automatically compiled.
+    :type pattern: re.Pattern or str
+    :param default: [description], defaults to None
+    :type default: str, optional
+    :param flags: regular expression flags, from re module, defaults to 0
+    :type flags: int, optional
+    :raises TypeError: if pattern is neither str or re.Pattern.
+    """
+
+    _pattern: re.Pattern
+
+    def __init__(
+        self,
+        pattern: re.Pattern | str,
+        *,
+        default: str = None,
+        flags: int = 0,
+    ) -> None:
+        if isinstance(pattern, str):
+            self._pattern = re.compile(pattern, flags=flags)
+        elif isinstance(pattern, re.Pattern):
+            self._pattern = pattern
+        else:
+            raise TypeError(
+                f"Invalid type of pattern parameter: {type(pattern)}"
+            )
+        self.default = default
+
+    def _check_conditions(self, string: str):
+        if not self._pattern.fullmatch(string):
+            raise ValueError(
+                f"Value '{string}' doesn't match pattern '{self._pattern.pattern}' of field {self._trace_location()}"
+            )
