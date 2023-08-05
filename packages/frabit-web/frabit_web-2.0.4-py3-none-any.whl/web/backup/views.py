7100988c59f92f64452ab0fd16a7f644d0962a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:18 +0000

    Add package frabit-web/2.0.4/frabit_web-2.0.4-py3-none-any.whl

---
+++ packages/frabit-web/frabit_web-2.0.4-py3-none-any.whl/web/backup/views.py
@@ -0,0 +1,25 @@
+# (c) 2021 frabit-web Project maintained and limited by FrabiTech < blylei.info@gmail.com >
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+# This file is part of frabit-web
+
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required, \
+    current_user
+from . import backup
+from .. import db
+from ..models import User
+from ..email import send_email
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
+    PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint \
+                and request.blueprint != 'auth' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
