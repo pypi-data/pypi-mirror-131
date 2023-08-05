a5e1afa32ddf796c298aa1592f8b41d289c4bdce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:16 +0000

    Add package patientMatcher/2.13/patientMatcher-2.13.tar.gz

---
+++ packages/patientMatcher/patientMatcher-2.13.tar.gz/patientMatcher-2.13/tests/utils/test_notify.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from patientMatcher.utils.notify import html_format, notify_match_external, notify_match_internal
+
+
+def test_notify_match_external(match_objs, mock_sender, mock_mail):
+
+    match_obj = match_objs[0]  # an external match object with results
+    assert match_obj["match_type"] == "external"
+
+    # When calling the function that sends external match notifications
+    notify_complete = True  # test notification of complete patient data by email
+    notify_match_external(match_obj, mock_sender, mock_mail, notify_complete)
+
+    # make sure send method was called
+    assert mock_mail._send_was_called
+
+    # and that mail object message was set correctly
+    assert mock_mail._message
+
+
+def test_notify_match_internal(database, match_objs, mock_sender, mock_mail):
+
+    match_obj = match_objs[2]  # an internal match object with results
+    assert match_obj["match_type"] == "internal"
+
+    # insert patient used as query in database:
+    assert database["patients"].find_one() is None
+    assert database["patients"].insert_one({"_id": "external_patient_1"}).inserted_id
+
+    # When calling the function that sends internal match notifications
+    notify_complete = False  # test notification of partial patient data by email
+    notify_match_internal(database, match_obj, mock_sender, mock_mail, notify_complete)
+
+    # Test the function that formats the matching results to HTML:
+    formatted_results = html_format(match_obj["results"])
+    assert '<div style="margin-left: 0em">' in formatted_results
+
+    # make sure send method was called
+    assert mock_mail._send_was_called
+
+    # and that mail object message was set correctly
+    assert mock_mail._message
