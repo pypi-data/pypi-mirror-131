aad39c9df3aafe6b44297a8dca6f72df1dc95fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package magic-class/0.5.13/magic-class-0.5.13.tar.gz

---
+++ packages/magic-class/magic-class-0.5.13.tar.gz/magic-class-0.5.13/magicclass/widgets/qtgraph/__init__.py
@@ -0,0 +1,33 @@
+"""
+PyQtGraph wrapper classes.
+
+``QtPlotCanvas`` can treat line plots and scatter plots as "layers" and has similar API as 
+``napari.Viewer`` and ``matplotlib``:
+
+.. code-block:: python
+
+    from magicclass.widgets import QtPlotCanvas
+    
+    canvas = QtPlotCanvas()
+    canvas.add_curve(np.random.random(100), color="r")
+    canvas.add_scatter(np.random.random(100), ls=":")
+    canvas.layers[1].visible = False
+    canvas.interactive = False
+    canvas.show()
+
+``QtImageCanvas`` is also designed in a similar way as ``QtPlotCanvas`` but aims at 2D image
+visualization.
+
+.. code-block:: python
+
+    from magicclass.widgets import QtImageCanvas
+    
+    image = np.random.random((128, 128))
+    canvas = QtImageCanvas(image)
+    canvas.show()
+
+"""
+
+from .qt_graph import QtPlotCanvas, QtImageCanvas
+
+__all__ = ["QtPlotCanvas", "QtImageCanvas"]
