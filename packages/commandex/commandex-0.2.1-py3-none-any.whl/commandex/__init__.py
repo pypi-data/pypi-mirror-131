099b85394eb784f0b4ae6bfbe57957235ec51654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:04 +0000

    Add package commandex/0.2.1/commandex-0.2.1-py3-none-any.whl

---
+++ packages/commandex/commandex-0.2.1-py3-none-any.whl/commandex/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2021, A.A Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab
+# smartlegiondev@gmail.com
+# --------------------------------------------------------
+"""
+Command executors library.
+
+- A cross-platform library for creation, storage, management
+of commands and command packages. Execution of commands,
+parsing of files with command packages.
+
+"""
+from commandex.commander import Commander
+__version__ = '0.2.1'
---
+++ packages/commandex/commandex-0.2.1-py3-none-any.whl/commandex/commander.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2021, A.A Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab
+# smartlegiondev@gmail.com
+# --------------------------------------------------------
+from commandex.filters import Filters
+from commandex.makers import Makers
+from commandex.parsers import Parsers
+from commandex.executors import Executors
+from commandex.factories import Factories
+
+
+class Commander:
+    factories = Factories()
+    executors = Executors()
+    filters = Filters()
+    makers = Makers()
+    parsers = Parsers()
