387e5b8ee8b9bbbede7061a4bad7a59af69f9a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:41 +0000

    Add package galaxychop/0.2.dev3/galaxychop-0.2.dev3-cp37-cp37m-win_amd64.whl

---
+++ packages/galaxychop/galaxychop-0.2.dev3-cp37-cp37m-win_amd64.whl/galaxychop/__init__.py
@@ -0,0 +1,31 @@
+# This file is part of
+# the galaxy-chop project (https://github.com/vcristiani/galaxy-chop)
+# Copyright (c) 2021, Valeria Cristiani
+# License: MIT
+# Full Text: https://github.com/vcristiani/galaxy-chop/blob/master/LICENSE.txt
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""
+GalaxyChop.
+
+Implementation of a few galaxy dynamic decomposition methods.
+"""
+
+# =============================================================================
+# META
+# =============================================================================
+
+__version__ = "0.2.dev3"
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+from .data import *  # noqa
+from .io import *  # noqa
+from .utils import *  # noqa
+from . import models  # noqa
