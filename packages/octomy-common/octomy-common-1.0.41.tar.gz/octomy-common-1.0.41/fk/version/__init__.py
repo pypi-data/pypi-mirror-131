43340da000a44e687f153ff82f07358ea80343eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:15 +0000

    Add package octomy-common/1.0.41/octomy-common-1.0.41.tar.gz

---
+++ packages/octomy-common/octomy-common-1.0.41.tar.gz/octomy-common-1.0.41/fk/version/__init__.py
@@ -0,0 +1,12 @@
+import os
+import pkg_resources
+from fk.utils import get_package_file_content
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+version_filename = "VERSION"
+pkg="fk-api"
+
+__version__ = get_package_file_content(pkg, version_filename) or "0.0.0"
