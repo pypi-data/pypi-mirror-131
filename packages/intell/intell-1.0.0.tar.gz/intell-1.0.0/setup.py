f35cfb0f0dfe25be28476ed8fdfc45c59ee4efcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:06 +0000

    Add package intell/1.0.0/intell-1.0.0.tar.gz

---
+++ packages/intell/intell-1.0.0.tar.gz/intell-1.0.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['intell']
+setup_kwargs = {
+    'name': 'intell',
+    'version': '1.0.0',
+    'description': 'Make a functional code start!',
+    'long_description': None,
+    'author': 'OwoNicoo',
+    'author_email': '86409467+DiscordBotML@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
