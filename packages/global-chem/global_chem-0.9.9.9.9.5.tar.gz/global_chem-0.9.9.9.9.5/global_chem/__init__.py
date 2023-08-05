4b8a51b0bb2c962a86f12cb1a60331cf64dcb393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:21 +0000

    Add package global-chem/0.9.9.9.9.5/global_chem-0.9.9.9.9.5.tar.gz

---
+++ packages/global-chem/global_chem-0.9.9.9.9.5.tar.gz/global_chem-0.9.9.9.9.5/global_chem/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#
+# Intialization of the package
+#
+# ----------------------------
+
+from global_chem.global_chem import GlobalChem
+
+__all__ = ['GlobalChem']
+
+__name__ = 'GlobalChem'
+__version__ = '0.9.9.9.1'
