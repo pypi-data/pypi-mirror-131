ad17b333cfa02e222ec75465d88ef8804b51a4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:17 +0000

    Add package mclumix/0.0.4/mclumix-0.0.4.tar.gz

---
+++ packages/mclumix/mclumix-0.0.4.tar.gz/mclumix-0.0.4/mclumi/trim/__init__.py
@@ -0,0 +1,18 @@
+# ## /*** block. remote ***/
+from .Fixed import *
+from .Filter import *
+from .Reader import *
+from .SeqRuleOut import *
+from .Template import *
+from .UMIRuleOut import *
+
+# ## /*** block. local ***/
+# try:
+#     from mclumi.trim.Fixed import *
+#     from mclumi.trim.Filter import *
+#     from mclumi.trim.Reader import *
+#     from mclumi.trim.SeqRuleOut import *
+#     from mclumi.trim.Template import *
+#     from mclumi.trim.UMIRuleOut import *
+# except ImportError:
+#     pass
