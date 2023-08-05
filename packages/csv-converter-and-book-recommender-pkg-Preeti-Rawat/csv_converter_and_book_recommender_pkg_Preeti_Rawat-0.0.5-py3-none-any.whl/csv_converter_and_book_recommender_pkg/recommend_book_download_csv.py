908fe33ccfc353ef0de5d722f8dbcdb32d4dcdec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package csv-converter-and-book-recommender-pkg-Preeti-Rawat/0.0.5/csv_converter_and_book_recommender_pkg_Preeti_Rawat-0.0.5-py3-none-any.whl

---
+++ packages/csv-converter-and-book-recommender-pkg-Preeti-Rawat/csv_converter_and_book_recommender_pkg_Preeti_Rawat-0.0.5-py3-none-any.whl/csv_converter_and_book_recommender_pkg/recommend_book_download_csv.py
@@ -0,0 +1,27 @@
+from .download_csv import CSV_Downloader
+from .fetch_best_books import BooksRecommender
+
+#DownloadCSVAndRecommendBooks handles two core functionality of library:
+#recommend books based on a book name.
+#convert model data to csv and download it.
+class DownloadCSVAndRecommendBooks(object):
+    #constructor to initialize fields for recommendation of book
+    def __init__(self, join_on, book_title_column, user_column, value_column):
+        self.joinon=join_on
+        self.book_col_name=book_title_column
+        self.user_col_name=user_column
+        self.value_col=value_column
+        
+    def recommended_books(self,bookName, issued_book_df, book_df):
+        
+        book_recc=BooksRecommender(self.joinon, self.book_col_name, self.user_col_name,self.value_col)
+        response=book_recc.get_books(bookName, issued_book_df, book_df)
+        return response
+        
+    @classmethod
+    def download_csv(cls, model_data):
+            csv_downloader=CSV_Downloader(model_data)
+            downloaded_csv=csv_downloader.download_csv_file()
+            return downloaded_csv
+            
+            
