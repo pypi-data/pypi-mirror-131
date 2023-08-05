ff4226316e2b258d4ea5a9b96447d72485277b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:17:29 +0000

    Add package arsenal-cli/1.1.0/arsenal-cli-1.1.0.tar.gz

---
+++ packages/arsenal-cli/arsenal-cli-1.1.0.tar.gz/arsenal-cli-1.1.0/arsenal/__init__.py
@@ -0,0 +1,10 @@
+"""
+pentest command launcher
+"""
+name = "arsenal"
+
+__author__ = 'Guillaume Muh, mayfly'
+__author_email__ = 'csr-audit.so@orange.com'
+__license__ = 'GPL-3.0'
+__url__ = 'https://github.com/Orange-Cyberdefense/arsenal'
+__version__ = '1.1.0'
