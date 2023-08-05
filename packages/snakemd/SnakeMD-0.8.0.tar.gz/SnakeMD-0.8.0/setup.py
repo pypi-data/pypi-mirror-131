adffe78d362e86f3bb577c696ff408143fe56d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:58 +0000

    Add package snakemd/0.8.0/SnakeMD-0.8.0.tar.gz

---
+++ packages/snakemd/SnakeMD-0.8.0.tar.gz/SnakeMD-0.8.0/setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+cmdclass = {}
+
+try:
+    from sphinx.setup_command import BuildDoc
+    cmdclass['build_sphinx'] = BuildDoc
+except ImportError:
+    print("WARNING: sphinx not available")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+MAJOR = 0
+MINOR = 8
+PATCH = 0
+
+name = "SnakeMD"
+version = f"{MAJOR}.{MINOR}"
+release = f"{MAJOR}.{MINOR}.{PATCH}"
+setuptools.setup(
+    name=name,
+    version=release,
+    author="The Renegade Coder",
+    author_email="jeremy.grifski@therenegadecoder.com",
+    description="A markdown generation library for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheRenegadeCoder/SnakeMD",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "sphinx",
+        "sphinx_rtd_theme",
+        "sphinx-issues"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass=cmdclass,
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version),
+            'release': ('setup.py', release),
+            'source_dir': ('setup.py', 'docsrc'),
+            'build_dir': ('setup.py', 'docsrc/_build'),
+            'builder': ("setup.py", "dirhtml")
+        }
+    },
+)
