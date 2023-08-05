3694de7a46589c2a108856ce742f055fa0d82f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:47 +0000

    Add package decimal-python-sdk/0.1.3/decimal-python-sdk-0.1.3.tar.gz

---
+++ packages/decimal-python-sdk/decimal-python-sdk-0.1.3.tar.gz/decimal-python-sdk-0.1.3/decimal_sdk/__init__.py
@@ -0,0 +1,13 @@
+from .api import DecimalAPI
+from .wallet import Wallet
+from .tx_types import *
+from .transactions import (Transaction,
+                           SendCoinTransaction, BuyCoinTransaction, CreateCoinTransaction, UpdateCoinTransaction, SellAllCoinsMsgTransaction,
+                           DelegateTransaction, UnbondTransaction,
+                           DeclareCandidateTransaction, EditCandidateTransaction,
+                           DisableEnableValidatorTransaction,
+                           MultysigCreateTransaction, MultysigCreateTXTransaction, MultysigSignTXTransaction, MultisendCoinTransaction,
+                           SubmitProposalTransaction, VoteProposalTransaction,
+                           SwapRedeemTransaction, SwapInitTransaction,
+                           NftMintTransaction, NftBurnTransaction, NftEditMetadataTransaction, NftTransferTransaction, NftDelegateTransaction, NftUnboundTransaction, NftUpdateReserveTransaction,
+                           )
---
+++ packages/decimal-python-sdk/decimal-python-sdk-0.1.3.tar.gz/decimal-python-sdk-0.1.3/decimal_sdk/utils/helpers.py
@@ -0,0 +1,47 @@
+import locale
+
+
+def number_format(num, places=0):
+    return locale.format_string("%.*f", (places, num), True)
+
+
+def get_amount_uni(amount, reverse=False):
+    exp = 18
+    if reverse:
+        exp = -18
+    numb = amount * pow(10, exp)
+    prepared_num = number_format(numb, 0)
+    return prepared_num
+
+
+def from_words(words):
+    res = convert(words, 5, 8, False)
+    return res
+
+
+def convert(data, in_bits, out_bits, pad):
+    value = 0
+    bits = 0
+    max_v = (1 << out_bits) - 1
+
+    result = []
+    i = 0
+    while i < len(data):
+        value = (value << in_bits) | data[i]
+        bits += in_bits
+
+        while bits >= out_bits:
+            bits -= out_bits
+            result.append((value >> bits) & max_v)
+        i += 1
+
+    if pad:
+        if bits > 0:
+            result.append((value << (out_bits - bits)) & max_v)
+    else:
+        if bits >= in_bits:
+            return 'Excess padding'
+        if (value << (out_bits - bits)) & max_v:
+            return 'Non-zero padding'
+
+    return result
