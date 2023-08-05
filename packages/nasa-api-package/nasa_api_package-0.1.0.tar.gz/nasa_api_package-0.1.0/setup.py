9e16b6a5f8a62e01ba88f886f4e25174a7f7f834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:58 +0000

    Add package nasa-api-package/0.1.0/nasa_api_package-0.1.0.tar.gz

---
+++ packages/nasa-api-package/nasa_api_package-0.1.0.tar.gz/nasa_api_package-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nasa_api_package']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['DateTime>=4.3,<5.0', 'requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'nasa-api-package',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
