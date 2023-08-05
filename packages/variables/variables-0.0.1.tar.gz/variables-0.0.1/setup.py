ac44bb0b015623603ff84cb12069a1ed4394c4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:27 +0000

    Add package variables/0.0.1/variables-0.0.1.tar.gz

---
+++ packages/variables/variables-0.0.1.tar.gz/variables-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = 'variables',
+    version = '0.0.1',
+    description = 'variables',
+    author = 'Wolphin',
+    author_email = 'q@wolph.in',
+    url = 'https://gitlab.com/qwolphin/variables',
+    packages = find_packages(),
+    classifiers = [
+        'Typing :: Typed',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
---
+++ packages/variables/variables-0.0.1.tar.gz/variables-0.0.1/variables/__init__.py
@@ -0,0 +1,1 @@
+variables: str = 'True'
