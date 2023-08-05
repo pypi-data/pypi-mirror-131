e5aeb2da4b4d9e824d2eee6bea53d30a9c8cc849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:27 +0000

    Add package lib/4.0.0/lib-4.0.0.tar.gz

---
+++ packages/lib/lib-4.0.0.tar.gz/lib-4.0.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name = 'lib',
+  packages = ['lib'],
+  version = '4.0.0',
+  description = 'Autocode standard library Python bindings',
+  author = 'Keith Horwood',
+  author_email = 'keithwhor@gmail.com',
+  url = 'https://github.com/acode/lib-python',
+  keywords = ['autocode', 'stdlib', 'microservice', 'serverless', 'faas', 'lib'],
+  license='MIT',
+  classifiers = [
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3'
+  ],
+)
