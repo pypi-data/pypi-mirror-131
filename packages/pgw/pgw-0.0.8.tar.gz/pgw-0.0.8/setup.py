a9ee0c457550621212fb1282cd5a2dda8ead9086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:10 +0000

    Add package pgw/0.0.8/pgw-0.0.8.tar.gz

---
+++ packages/pgw/pgw-0.0.8.tar.gz/pgw-0.0.8/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='pgw',packages=['pygwin'],version='0.0.8',author='themixray',
+    description='A library for creating Python applications.',
+    license='MIT',install_requires=['cython','pywin32','pygame','inputs',
+     'pydub','wxPython','pyautogui','moviepy','pipwin','wave','opencv-python'])
