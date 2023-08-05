12076450f1ff747dfb634c11e7db6e04b70c7135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package rexmex/0.0.13/rexmex-0.0.13.tar.gz

---
+++ packages/rexmex/rexmex-0.0.13.tar.gz/rexmex-0.0.13/rexmex/utils.py
@@ -0,0 +1,49 @@
+from functools import wraps
+
+import numpy as np
+
+
+def binarize(metric):
+    """
+    Binarize the predictions for a ground-truth - prediction vector pair.
+
+    Args:
+        metric (function): The metric function which needs a binarization pre-processing step.
+    Returns:
+        metric_wrapper (function): The function which wraps the metric and binarizes the probability scores.
+    """
+
+    @wraps(metric)
+    def metric_wrapper(*args, **kwargs):
+        # TODO: Move to optimal binning. Youden’s J statistic.
+        y_score = args[1]
+        y_score[y_score < 0.5] = 0
+        y_score[y_score >= 0.5] = 1
+        score = metric(*args, **kwargs)
+        return score
+
+    return metric_wrapper
+
+
+def normalize(metric):
+    """
+    Normalize the predictions for a ground-truth - prediction vector pair.
+
+    Args:
+        metric (function): The metric function which needs a normalization pre-processing step.
+    Returns:
+        metric_wrapper (function): The function which wraps the metric and normalizes predictions.
+    """
+
+    @wraps(metric)
+    def metric_wrapper(*args, **kwargs):
+        y_true = args[0]
+        y_score = args[1]
+        y_mean = np.mean(y_true)
+        y_std = np.std(y_true)
+        y_true[:] = (y_true - y_mean) / y_std
+        y_score[:] = (y_score - y_mean) / y_std
+        score = metric(*args, **kwargs)
+        return score
+
+    return metric_wrapper
