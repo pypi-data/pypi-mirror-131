3706ef134c3ca70606e9a2c2fb7cdf5bcf4c4173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:40 +0000

    Add package sdgym/0.5.0/sdgym-0.5.0-py2.py3-none-any.whl

---
+++ packages/sdgym/sdgym-0.5.0-py2.py3-none-any.whl/sdgym/__init__.py
@@ -0,0 +1,26 @@
+"""SDGym - Synthetic data Gym.
+
+SDGym is a framework to benchmark the performance of synthetic data generators for
+tabular data.
+"""
+
+__author__ = 'MIT Data To AI Lab'
+__copyright__ = 'Copyright (c) 2018, MIT Data To AI Lab'
+__email__ = 'dailabmit@gmail.com'
+__license__ = 'MIT'
+__version__ = '0.5.0'
+
+from sdgym import benchmark, synthesizers
+from sdgym.benchmark import run
+from sdgym.collect import collect_results
+from sdgym.datasets import load_dataset
+from sdgym.summary import make_summary_spreadsheet
+
+__all__ = [
+    'benchmark',
+    'synthesizers',
+    'run',
+    'load_dataset',
+    'collect_results',
+    'make_summary_spreadsheet'
+]
