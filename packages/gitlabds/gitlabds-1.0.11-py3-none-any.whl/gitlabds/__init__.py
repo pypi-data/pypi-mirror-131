ab07596fe51dd576bac15d05141497ee1d41f8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:51 +0000

    Add package gitlabds/1.0.11/gitlabds-1.0.11-py3-none-any.whl

---
+++ packages/gitlabds/gitlabds-1.0.11-py3-none-any.whl/gitlabds/__init__.py
@@ -0,0 +1,7 @@
+from .outliers import mad_outliers
+from .dummies import dummy_code, dummy_top
+from .missing_fill import missing_fill
+from .feature_reduction import drop_categorical, remove_low_variation, dv_proxies, correlation_reduction
+from .split_data import split_data
+from .mars_modeling import mars_modeling
+from .model_metrics import model_metrics
