b7ee3333ba80d8fca1f8addcf51fc4bafc8d4889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:18 +0000

    Add package discord-rpc/2.0/discord_rpc-2.0-py3-none-any.whl

---
+++ packages/discord-rpc/discord_rpc-2.0-py3-none-any.whl/DiscordRPC/__init__.py
@@ -0,0 +1,9 @@
+from .presence import *
+from .button import button
+from .exceptions import *
+
+__title__ = "Discord-RPC"
+__version__ = "2.0"
+__authors__ = ["LyQuid", "EterNomm"]
+__license__ = "Apache License 2.0"
+__copyright__ = "Copyright 2021 EterNomm"
