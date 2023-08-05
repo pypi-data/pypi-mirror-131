295723c8ae87c87885ec8f0cb88706e1df6faf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:08 +0000

    Add package irdatacleaning/2021.0.13/irdatacleaning-2021.0.13.tar.gz

---
+++ packages/irdatacleaning/irdatacleaning-2021.0.13.tar.gz/irdatacleaning-2021.0.13/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '2021.0.13'
+DESCRIPTION = 'module designed to make your data preprocessing experience easier'
+with open("README.md") as description:
+    LONG_DESCRIPTION = description.read()
+
+# Setting up
+setup(
+    name="irdatacleaning",
+    version=VERSION,
+    author="Islander Robotics (William McKeon)",
+    author_email="<IslanderRobotics@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+
+    install_requires=["pandas","matplotlib","PyQt5","scikit-learn","IslanderDataPreprocessing","numpy"],
+    keywords=['python',"Machine Learning", "Artificial Intelligence","Data Science","Data Cleaning"],
+)
