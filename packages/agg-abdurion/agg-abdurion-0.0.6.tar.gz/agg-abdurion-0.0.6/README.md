e3f9bd4c9c65182df9aa9df01e953d7b003b0992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:34 +0000

    Add package agg-abdurion/0.0.6/agg-abdurion-0.0.6.tar.gz

---
+++ packages/agg-abdurion/agg-abdurion-0.0.6.tar.gz/agg-abdurion-0.0.6/README.md
@@ -0,0 +1,2 @@
+### Aggregation Package Details
+    - This package computes the mean, median, min, max, sum of positives, and count of negatives of a list.
---
+++ packages/agg-abdurion/agg-abdurion-0.0.6.tar.gz/agg-abdurion-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+# Loads your README
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="agg-abdurion", # Replace with your own username
+    version="0.0.6", # Your version number, needs to be incremented with each iteration
+    author="Abdulrahman AlQumaidy", # Your name
+    author_email="author@example.com", # Your email
+    description="A small example package", # A one sentence description of your package
+    long_description=long_description, # A longer description from your `README.md`
+    long_description_content_type="text/markdown", # The format of you long_description file
+    url="https://github.com/abdurion", # GitHub where to find your code
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], # Additional required pieces of metadata (Don't Change)
+    packages=setuptools.find_packages(), # Find all packages used and get them ready for distribution (Dont Change)
+    python_requires=">=3.7", # The required version of python
+)
