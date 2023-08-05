71a8417fbbd0a6264e6b9445b9f316e7834ac769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:49 +0000

    Add package trabalho-jeansilva-converter-csv-json/0.1.1/Trabalho_JeanSilva_Converter_CSV_JSON-0.1.1.tar.gz

---
+++ packages/trabalho-jeansilva-converter-csv-json/Trabalho_JeanSilva_Converter_CSV_JSON-0.1.1.tar.gz/Trabalho_JeanSilva_Converter_CSV_JSON-0.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['trabalho_jeansilva_converter_csv_json']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bpython>=0.22.1,<0.23.0', 'click>=8.0.3,<9.0.0', 'path>=16.2.0,<17.0.0']
+
+entry_points = \
+{'console_scripts': ['csv_converter = converter_js.converter:converter',
+                     'json_converter = converter_js.converter:converter2']}
+
+setup_kwargs = {
+    'name': 'trabalho-jeansilva-converter-csv-json',
+    'version': '0.1.1',
+    'description': 'Função de converter arquivo csv para json, e json para csv.',
+    'long_description': None,
+    'author': 'Jean Silva',
+    'author_email': 'JeanSilvadSantos@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
