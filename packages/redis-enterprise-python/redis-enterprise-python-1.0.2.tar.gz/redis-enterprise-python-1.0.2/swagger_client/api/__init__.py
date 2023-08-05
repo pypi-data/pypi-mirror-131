067aa780ff0fb4dd9753bcdfaa9fc88e8431d2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:16 +0000

    Add package redis-enterprise-python/1.0.2/redis-enterprise-python-1.0.2.tar.gz

---
+++ packages/redis-enterprise-python/redis-enterprise-python-1.0.2.tar.gz/redis-enterprise-python-1.0.2/swagger_client/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.actions_api import ActionsApi
+from swagger_client.api.backup_api import BackupApi
+from swagger_client.api.backups_api import BackupsApi
+from swagger_client.api.bdbs_api import BdbsApi
+from swagger_client.api.cluster_api import ClusterApi
+from swagger_client.api.crdbs_api import CrdbsApi
+from swagger_client.api.default_api import DefaultApi
+from swagger_client.api.shards_api import ShardsApi
+from swagger_client.api.users_api import UsersApi
