5b1939350fa75b5b28e24e9401bea02dc2ee2519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:06 +0000

    Add package CL-Write-Results-Library/1.0/CL_Write_Results_Library-1.0.tar.gz

---
+++ packages/CL-Write-Results-Library/CL_Write_Results_Library-1.0.tar.gz/CL_Write_Results_Library-1.0/CL_Write_Results_Library/__init__.py
@@ -0,0 +1,6 @@
+from CL_Write_Results_Library import CL_Write_Results_Library
+
+# Create an object of CL_Dynamic_Test_Case_Library class & call its method
+
+ModCL_Write_Results_Library = CL_Write_Results_Library()
+ModCL_Write_Results_Library.end_test()
