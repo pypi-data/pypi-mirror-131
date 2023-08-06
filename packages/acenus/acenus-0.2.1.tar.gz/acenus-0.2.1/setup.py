01e79b3c34268c8a34cf6ab3ab82c05a239b8ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:46 +0000

    Add package acenus/0.2.1/acenus-0.2.1.tar.gz

---
+++ packages/acenus/acenus-0.2.1.tar.gz/acenus-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['acenus']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['loguru>=0.5.3,<0.6.0']
+
+setup_kwargs = {
+    'name': 'acenus',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'tsarbas',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
