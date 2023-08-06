5dbeea2bbaf952b2b180c6766655e28366edb52d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:16 +0000

    Add package DimensionalityReduction/1.1.3/DimensionalityReduction-1.1.3.tar.gz

---
+++ packages/DimensionalityReduction/DimensionalityReduction-1.1.3.tar.gz/DimensionalityReduction-1.1.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import setuptools
+
+with open('C:/Users/Hindy/Desktop/python packages/DimensionalityReduction/README.md', 'r') as fh:
+    long_descripion = fh.read()
+    
+setuptools.setup(
+    name='DimensionalityReduction', 
+    version='1.1.3',
+    author='Hindy Yuen', 
+    author_email='hindy888@hotmail.com',
+    license='MIT',
+    description='Dimensionality reduction automatically', 
+    long_description=long_descripion, 
+    long_description_content_type='text/markdown', 
+    url='https://github.com/HindyDS/DimensionalityReduction', 
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+],
+    keywords='Dimensionality Reduction',
+    package_dir={"":"DimensionalityReduction"},
+    packages=['DimensionalityReduction'],
+    python_requires='>=3.6',
+)
+
