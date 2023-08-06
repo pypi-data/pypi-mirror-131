59d073244e4f2496a07c558315a346651c117e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:41 +0000

    Add package gammagang/1.0.0/gammagang-1.0.0.tar.gz

---
+++ packages/gammagang/gammagang-1.0.0.tar.gz/gammagang-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gammagang",                     
+    version="1.0.0",                        
+    author="Ryan Watts",                     
+    description="GammaGang Package for distribution",
+    long_description=long_description,      
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      
+    python_requires='>=2',                
+    py_modules=["gammagang"],             
+    package_dir={'':'gammagang'},     
+    install_requires=["pandas==1.1.3","boto3"]                     
+)
