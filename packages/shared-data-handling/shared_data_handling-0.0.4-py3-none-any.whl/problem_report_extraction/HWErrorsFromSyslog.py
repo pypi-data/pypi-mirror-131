548cde069f8046399c0e02fb79e640d117edb020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:14 +0000

    Add package shared-data-handling/0.0.4/shared_data_handling-0.0.4-py3-none-any.whl

---
+++ packages/shared-data-handling/shared_data_handling-0.0.4-py3-none-any.whl/problem_report_extraction/HWErrorsFromSyslog.py
@@ -0,0 +1,14 @@
+import re, json
+import shared_data_handling
+from shared_data_handling import process_data  
+
+dataPatterns = [r"""Hardware\semergency\sreport""",
+                r"""CameraCommunicationError""",
+                r"""DrawerLockFailed"""]
+                #Add Retries
+
+def ParseFile(fullFilePath):
+    '''
+    gets the matching regex patterns and keeps them in a list
+    '''
+    return process_data.ParseFileRegexPatternList(fullFilePath, dataPatterns, completeOnlyLine=True)
