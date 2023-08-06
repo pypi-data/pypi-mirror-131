73f1b3e444b8ff4b60267172869416a2b8f1015b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:41 +0000

    Add package deepprojection/0.0.10/DeepProjection-0.0.10.tar.gz

---
+++ packages/deepprojection/DeepProjection-0.0.10.tar.gz/DeepProjection-0.0.10/deepprojection/predict.py
@@ -0,0 +1,251 @@
+import gc
+
+import numpy as np
+
+from .utils import *
+from .ProjNet import ProjNet
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+class PredictMovie:
+    """
+    Class for prediction of movies
+    """
+
+    def __init__(self, input_path, weights, model=ProjNet, filename_output=None, resize_dim=(512, 1024),
+                 clip_thrs=(0, 99.9), n_filter=8, mask_thrs=None, input_color=None, normalization_mode='movie',
+                 export_masks=False, invert_slices=False, temp_folder='../temp/', bigtiff=False):
+        """
+
+        Parameters
+        ----------
+        input_path : str
+            For individual 3D stacks: input_path containing stacks (filesnames need to have time at end).
+            For 4D stacks: filename.
+        weights : str
+            Trained model weights
+        model
+            Network model
+        filename_output : str
+            If not None, output is save to filename_output. If None, it is saved in the parent directory of the input_path
+        resize_dim : tuple(int, int)
+            Resize dimensions (has to be divisible by 8)
+        clip_thrs : tuple(float, float)
+            Lower and higher percentile for intensity clipping
+        n_filter : int
+            Number of convolutional filters
+        mask_thrs : float = None
+            If not None, additional binary thresholding of predicted masks
+        input_color
+            If not None, masks are applied to stacks of second fluorescent channel in folder_color. Not yet implemented.
+        normalization_mode : str
+            If 'movie', the intensity is normalized based on a cummulative histogram of all stacks, if 'stack',
+            the intensities are normalized individually for each stack, if 'first', only the histogram of the first
+            frame is used.
+        export_masks : bool
+            If True, the predicted masks are stored
+        invert_slices :
+            If True, z order of stacks is inverted prior to prediction
+        bigtiff : bool
+            If True, bigtiff format is used (file size >4GB)
+        """
+        print(f'Predicting {input_path} ...')
+
+        self.input = input_path
+        self.input_color = input_color
+        self.filename_output = filename_output
+        self.temp_folder = temp_folder
+
+        # params
+        self.resize_dim = resize_dim
+        # check resize_dim
+        if np.count_nonzero(np.mod(resize_dim, 8)) > 0:
+            raise ValueError(f'resize_dim {resize_dim} has to be divisible by 8.')
+        self.n_filter = n_filter
+        self.mask_thrs = mask_thrs
+        self.export_masks = export_masks
+        self.info = {'model': weights, 'mask_thrs': mask_thrs, 'clip_thrs': clip_thrs,
+                     'normalization_mode': normalization_mode}
+        # temp input_path
+        if os.path.exists(self.temp_folder):
+            shutil.rmtree(self.temp_folder)
+        os.makedirs(self.temp_folder, exist_ok=True)
+
+        # read and preprocess data
+        print('Copying stacks to temp input_path...')
+        self.n_frames, self.n_slices, self.n_pixel = convert_to_stack(self.input, self.temp_folder,
+                                                                      invert_order=invert_slices)
+
+        # normalization modes
+        print('Normalizing movie...')
+        if normalization_mode == 'stack':
+            clip_values = None
+        elif normalization_mode == 'first':
+            first_frame = tifffile.imread(self.temp_folder + 'stack_0.tif')
+            clip_values = (np.percentile(first_frame, clip_thrs[0]), np.percentile(first_frame, clip_thrs[1]))
+        elif normalization_mode == 'movie':
+            # get percentiles of all stacks
+            percentiles = np.zeros((self.n_frames, 2))
+            for t in tqdm(range(self.n_frames)):
+                stack_t = tifffile.imread(self.temp_folder + f'/stack_{t}.tif')
+                percentiles[t] = (np.percentile(stack_t, clip_thrs[0]),
+                                  np.percentile(stack_t, clip_thrs[1]))
+            clip_values = np.mean(percentiles, axis=0)
+        else:
+            raise ValueError('Specify correct normalization mode (stack, first or movie)')
+
+        if filename_output is None:
+            if '.tif' in self.input:
+                filename_output = self.input[:-4] + '_result.tif'
+            else:
+                filename_output = self.input[:-1] + '.tif'
+        # predict stacks and write in tiff file
+        print('Predicting stacks...')
+        with tifffile.TiffWriter(filename_output, bigtiff=bigtiff) as tif:
+            for t in tqdm(range(self.n_frames)):
+                stack_t = PredictStack(self.temp_folder + f'/stack_{t}.tif', filename_output=None, weights=weights,
+                                       model=model, resize_dim=resize_dim, clip_thrs=clip_thrs,
+                                       clip_values=clip_values, n_filter=n_filter, mask_thrs=mask_thrs,
+                                       export_masks=False, invert_slices=invert_slices)
+                tif.write(stack_t.result, metadata=self.info, contiguous=True)
+
+        print(f'Result saved to {filename_output}.')
+        # delete temp input_path
+        shutil.rmtree(self.temp_folder)
+
+
+class PredictStack:
+    """
+    Class for prediction of single stacks
+    """
+
+    def __init__(self, filename, filename_output, weights, model=ProjNet, resize_dim=(512, 1024), clip_thrs=(0, 99.98),
+                 clip_values=None, n_filter=8, mask_thrs=None, export_masks=True, add_tile=0, invert_slices=False):
+        """
+
+        Parameters
+        ----------
+        filename : str
+            Filename of stack
+        filename_output :
+            Filename of output. If None, result is not saved as tif file.
+        weights : str
+            Trained model weights
+        model
+            Network model
+        resize_dim : tuple(int, int)
+            Resize dimensions (2**n, 2**n) with n>4
+        clip_thrs : tuple(float, float)
+            Lower and higher percentile for intensity clipping
+        clip_values : tuple
+            If not None, clip_values are applied for normalization and clip_thrs is ignored.
+        n_filter : int
+            Number of convolutional filters
+        mask_thrs : float = None
+            If not None, additional binary thresholding of predicted masks
+        export_masks : bool
+            If True, the predicted masks are stored
+        invert_slices : book
+            If True, z order of stacks is inverted prior to prediction
+        """
+        self.filename = filename
+        self.filename_output = filename_output
+        self.resize_dim = resize_dim
+        self.n_filter = n_filter
+        self.mask_thrs = mask_thrs
+        self.export_masks = export_masks
+        self.add_tile = add_tile
+        # read and preprocess data
+        self.stack = tifffile.imread(self.filename).astype('float32')
+        if invert_slices:
+            self.stack = self.stack[::-1]
+        if len(self.stack.shape) == 4:  # if two color stack
+            self.stack = self.stack[:, 0]
+        self.n_slices, self.n_pixel = self.stack.shape[0], self.stack.shape[1:]
+        if clip_values is None:
+            clip_values = (np.percentile(self.stack, clip_thrs[0]), np.percentile(self.stack, clip_thrs[1]))
+        self.stack = np.clip(self.stack, a_min=clip_values[0], a_max=clip_values[1])
+        self.stack = self.stack - np.min(self.stack)
+        self.stack = self.stack / np.max(self.stack)
+        # split stacks in patches
+        self.split()
+        # load model and predict data
+        self.model = model(n_filter=n_filter).to(device)
+        self.model.load_state_dict(torch.load(weights)['state_dict'])
+        self.model.eval()
+        self.predict()
+        # stitch patches back together
+        self.stitch()
+        # save result
+        if self.filename_output is not None:
+            tifffile.imsave(self.filename_output + '_result.tif', self.result.astype('uint8'))
+            if self.export_masks:
+                self.masks = self.masks[:, :self.n_pixel[0], :self.n_pixel[1]]
+                tifffile.imsave(self.filename_output + '_masks.tif', self.masks.astype('uint8'))
+
+    def split(self):
+        # number of patches in x and y
+        self.n_x = int(np.ceil(self.n_pixel[1] / self.resize_dim[1])) + self.add_tile
+        self.n_y = int(np.ceil(self.n_pixel[0] / self.resize_dim[0])) + self.add_tile
+        self.n = self.n_x * self.n_y  # number of patches
+        # starting indices of patches
+        self.x_start = np.linspace(0, self.n_pixel[1] - self.resize_dim[1], self.n_x).astype('uint16')
+        self.y_start = np.linspace(0, self.n_pixel[0] - self.resize_dim[0], self.n_y).astype('uint16')
+        self.patches = np.zeros((self.n, self.n_slices, self.resize_dim[0], self.resize_dim[1]), dtype='float32')
+        # padding if image dim smaller than resize_dim
+        self.stack = np.pad(self.stack, ((0, 0), (0, np.max((0, self.resize_dim[0] - self.n_pixel[0]))),
+                                         (0, np.max((0, self.resize_dim[1] - self.n_pixel[1])))), constant_values=0)
+        # split in resize_dim
+        n = 0
+        for j in range(self.n_y):
+            for k in range(self.n_x):
+                self.patches[n, :, :, :] = self.stack[:, self.y_start[j]:self.y_start[j] + self.resize_dim[0],
+                                           self.x_start[k]:self.x_start[k] + self.resize_dim[1]]
+                n += 1
+
+    def predict(self):
+        with torch.no_grad():
+            self.patches_result = np.zeros((self.n, self.resize_dim[0], self.resize_dim[1]), dtype='float32')
+            self.patches_masks = np.zeros_like(self.patches, dtype='float32')
+            for j, patch_j in enumerate(self.patches):
+                patch_j_torch = torch.from_numpy(patch_j).to(device)
+                patch_j_torch = patch_j_torch.view((1, 1, self.n_slices, self.resize_dim[0], self.resize_dim[1]))
+                res_j, mask_j, edge_j = self.model(patch_j_torch)
+                mask_j = mask_j.view((self.n_slices, self.resize_dim[0], self.resize_dim[1])).detach().cpu().numpy()
+                res_j = res_j.view((self.resize_dim[0], self.resize_dim[1])).detach().cpu().numpy()
+                # threshold mask
+                if self.mask_thrs is not None:
+                    mask_j_thrs = np.copy(mask_j)
+                    mask_j_thrs[mask_j < self.mask_thrs] = 0
+                    mask_j_thrs[mask_j >= self.mask_thrs] = 1
+                    res_j = np.max(patch_j * mask_j_thrs, axis=0)
+                    mask_j = mask_j_thrs
+                # write in array
+                self.patches_result[j] = res_j * 255
+                self.patches_masks[j] = mask_j * 255
+            gc.collect()
+            torch.cuda.empty_cache()
+
+    def stitch(self):
+        # create array
+        result_temp = np.zeros((self.n, np.max((self.resize_dim[0], self.n_pixel[0])),
+                                np.max((self.resize_dim[1], self.n_pixel[1]))), dtype='uint8')
+        if self.export_masks:
+            masks_temp = np.zeros((self.n, self.n_slices, np.max((self.resize_dim[0], self.n_pixel[0])),
+                                   np.max((self.resize_dim[1], self.n_pixel[1]))), dtype='uint8')
+        n = 0
+        for j in range(self.n_y):
+            for k in range(self.n_x):
+                result_temp[n, self.y_start[j]:self.y_start[j] + self.resize_dim[0],
+                self.x_start[k]:self.x_start[k] + self.resize_dim[1]] = self.patches_result[n, :, :]
+                if self.export_masks:
+                    masks_temp[n, :, self.y_start[j]:self.y_start[j] + self.resize_dim[0],
+                    self.x_start[k]:self.x_start[k] + self.resize_dim[1]] = self.patches_masks[n, :, :, :]
+                n += 1
+        # maximum of overlapping regions
+        self.result = np.max(result_temp, axis=0)
+        if self.export_masks:
+            self.masks = np.max(masks_temp, axis=0)
+        # change to input_path size (if zero padding) and save results
+        self.result = self.result[:self.n_pixel[0], :self.n_pixel[1]]
---
+++ packages/deepprojection/DeepProjection-0.0.10.tar.gz/DeepProjection-0.0.10/deepprojection/utils.py
@@ -0,0 +1,279 @@
+import glob
+import os
+import re
+import shutil
+
+import numpy as np
+import tifffile
+import torch
+from torch import nn as nn
+from tqdm import tqdm
+
+
+def get_stack_directories(base_folder, signatures=('.tif', '.TIF', '.tiff', '.TIFF'), n_min=40):
+    """ Get paths of directories containing {signature} """
+    paths = []
+    for root, dirs, files in os.walk(base_folder, topdown=False):
+        for name in files:
+            if np.count_nonzero([s in name for s in signatures]) > 0:
+                if len(files) >= n_min:
+                    paths.append(root + '/')
+    return paths
+
+
+def convert_to_stack(input_path, folder_out, invert_order=False):
+    """ Parse single image data from 3D stacks and convert to 2D stacks for each time point.
+        If data is already in stacks, data is only copied to folder_out
+
+    Parameters
+    ----------
+    input_path : str
+        For individual 3D stacks: input_path containing stacks (filesnames need to have time at end).
+        For 4D stacks: filename.
+    folder_out : str
+        Folder to copy data as individual 3D stacks
+    invert_order : bool
+        If True, invert z-order of stacks
+    """
+    os.makedirs(folder_out, exist_ok=True)
+    files = np.asarray([file for file in glob.glob(input_path + '*') if 'txt' not in file])
+    shape = tifffile.imread(files[0]).shape
+    # todo two color movies
+    if len(shape) == 4:  # TZXY stack
+        with tifffile.TiffFile(input_path) as tif:
+            volume = tif.asarray()
+            for t, stack_t in enumerate(volume):
+                tifffile.imwrite(folder_out + f'stack_{t}.tif', stack_t)
+        n_frames, n_slices, n_pixel = volume.shape[0], volume.shape[1], volume.shape[2:]
+    elif len(shape) == 3:  # if files are stacks
+        ts = []
+        for file_i in files:
+            basename_i = os.path.basename(file_i)
+            if 'time' in basename_i:
+                t = int(re.findall(r'time_?(\d+)', basename_i)[0])
+            else:
+                t = int(re.findall(r't_?(\d+)', basename_i)[0])
+            ts.append(t)
+        if 0 not in ts:
+            ts -= np.min(ts)
+        for i, file_i in enumerate(files):
+            shutil.copy(file_i, folder_out + f'stack_{ts[i]}.tif')
+        n_frames, n_slices, n_pixel = len(files), shape[0], shape[1:]
+    elif len(shape) == 2:  # if files are single images
+        # get # frames and stacks and create array
+        ts, zs = [], []
+        for file_i in files:
+            basename_i = os.path.basename(file_i)
+            if 'time' in basename_i:
+                t = int(re.findall(r'time_?(\d+)', basename_i)[0])
+            else:
+                t = int(re.findall(r't_?(\d+)', basename_i)[0])
+            z = int(re.findall(r'z_?(\d+)', basename_i)[0])
+            ts.append(t)
+            zs.append(z)
+        ts, zs = np.asarray(ts), np.asarray(zs)
+        # if counting starts not from zero, subtract offset
+        if 0 not in ts:
+            ts -= np.min(ts)
+        if 0 not in zs:
+            zs -= np.min(zs)
+        n_frames, n_slices, n_pixel = len(np.unique(ts)), len(np.unique(zs)), shape
+        # load data and save as stacks
+        for t in range(n_frames):
+            stack_t = np.zeros((len(np.unique(zs)), *shape), dtype='uint16')
+            files_t = files[ts == t]
+            z_t = zs[ts == t]
+            for z in range(n_slices):
+                file_z = files_t[z_t == z]
+                stack_t[z] = tifffile.imread(file_z)
+            if invert_order:
+                stack_t = stack_t[::-1]
+            tifffile.imsave(folder_out + f'stack_{t}.tif', stack_t)
+    else:
+        raise FileNotFoundError('Data structure unknown!')
+
+    return n_frames, n_slices, n_pixel
+
+
+class MaxProjection:
+    """maximum intensity projection w/o normalization"""
+
+    def __init__(self, folder, filename_output=None, bigtiff=False):
+        self.files = np.asarray([file for file in glob.glob(folder + '*') if 'txt' not in file])
+        self.filename_output = filename_output
+        if filename_output is None:
+            self.filename_output = folder[:-1] + '_max_int.tif'
+        self.bigtiff = bigtiff
+        self.shape = tifffile.imread(self.files[0]).shape
+        self.get_data_structure()
+        self.max_projection()
+
+    def get_data_structure(self):
+        if len(self.shape) == 3:  # if files are stacks
+            ts = []
+            self.data_mode = 'stacks'
+            self.n_frames, self.n_slices, self.n_pixel = len(self.files), self.shape[0], self.shape[1:]
+            for file_i in self.files:
+                basename_i = os.path.basename(file_i)
+                if 'time' in basename_i:
+                    t = int(re.findall(r'time_?(\d+)', basename_i)[0])
+                else:
+                    t = int(re.findall(r't_?(\d+)', basename_i)[0])
+                ts.append(t)
+                self.ts = ts
+                if 0 not in self.ts:
+                    self.ts -= np.min(self.ts)
+        elif len(self.shape) == 2:  # if files are single images
+            self.data_mode = 'images'
+            # get # frames and stacks and create array
+            ts, zs, cs = [], [], []
+            for file_i in self.files:
+                basename_i = os.path.basename(file_i)
+                if 'time' in basename_i:
+                    t = int(re.findall(r'time_?(\d+)', basename_i)[0])
+                else:
+                    t = int(re.findall(r't_?(\d+)', basename_i)[0])
+                z = int(re.findall(r'z_?(\d+)', basename_i)[0])
+                if 'channel' in basename_i:
+                    c = int(re.findall(r'channel_?(\d+)', basename_i)[0])
+                else:
+                    c = int(re.findall(r'c_?(\d+)', basename_i)[0])
+                ts.append(t)
+                zs.append(z)
+                cs.append(c)
+            self.ts, self.zs, self.cs = np.asarray(ts), np.asarray(zs), np.asarray(cs)
+            # if counting starts not from zero, subtract offset
+            if 0 not in self.ts:
+                self.ts -= np.min(self.ts)
+            if 0 not in self.zs:
+                self.zs -= np.min(self.zs)
+            self.n_frames = len(np.unique(self.ts))
+            self.n_slices = len(np.unique(self.zs))
+            self.n_channels = len(np.unique(self.cs))
+            self.n_pixel = self.shape
+            # check if two-color
+            if self.n_channels == 2:
+                self.data_mode = 'two_color'
+        else:
+            raise FileNotFoundError('Data structure unknown!')
+
+    def max_projection(self):
+        # iterate through files and max projection
+        if self.data_mode == 'stacks':
+            with tifffile.TiffWriter(self.filename_output) as tif:
+                for t in tqdm(range(self.n_frames)):
+                    stack_t = self.files[self.ts == t]
+                    tif.write(np.mean(stack_t, axis=0), contiguous=True)
+        if self.data_mode == 'images':
+            with tifffile.TiffWriter(self.filename_output, bigtiff=self.bigtiff) as tif:
+                for t in tqdm(range(self.n_frames)):
+                    stack_t = np.zeros((self.n_slices, *self.n_pixel), dtype='uint16')
+                    files_t = self.files[self.ts == t]
+                    z_t = self.zs[self.ts == t]
+                    for z in range(self.n_slices):
+                        file_z = files_t[z_t == z]
+                        stack_t[z] = tifffile.imread(file_z)
+                    tif.write(np.max(stack_t, axis=0), contiguous=True)
+
+        if self.data_mode == 'two_color':
+            with tifffile.TiffWriter(self.filename_output[:-4] + '_c0.tif', bigtiff=self.bigtiff) as tif_c0, \
+                    tifffile.TiffWriter(self.filename_output[:-4] + '_c1.tif', bigtiff=self.bigtiff) as tif_c1:
+                for t in tqdm(range(self.n_frames)):
+                    stack_c0_t = np.zeros((self.n_slices, *self.n_pixel), dtype='uint16')
+                    stack_c1_t = np.zeros((self.n_slices, *self.n_pixel), dtype='uint16')
+                    files_t = self.files[self.ts == t]
+                    z_t = self.zs[self.ts == t]
+                    c_t = self.cs[self.ts == t]
+                    for z in range(self.n_slices):
+                        file_c0_z = files_t[(z_t == z) & (c_t == 0)]
+                        file_c1_z = files_t[(z_t == z) & (c_t == 1)]
+                        stack_c0_t[z] = tifffile.imread(file_c0_z)
+                        stack_c1_t[z] = tifffile.imread(file_c1_z)
+                    tif_c0.write(np.max(stack_c0_t, axis=0), contiguous=True)
+                    tif_c1.write(np.max(stack_c1_t, axis=0), contiguous=True)
+
+
+class BCEDiceLoss(nn.Module):
+    def __init__(self, alpha, beta):
+        super(BCEDiceLoss, self).__init__()
+        self.bce = BCELoss2d()
+        self.dice = SoftDiceLoss()
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, inputs, targets):
+        return self.alpha * self.bce(inputs, targets) + self.beta * self.dice(inputs, targets)
+
+
+class BCELoss2d(nn.Module):
+    def __init__(self, weight=None, reduction='mean', **kwargs):
+        super(BCELoss2d, self).__init__()
+        self.bce_loss = nn.BCELoss(weight, reduction=reduction)
+
+    def forward(self, inputs, targets):
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        return self.bce_loss(inputs, targets)
+
+
+class SoftDiceLoss(nn.Module):
+    def __init__(self, smooth=0.5):
+        super(SoftDiceLoss, self).__init__()
+        self.smooth = smooth
+
+    def forward(self, inputs, targets):
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        intersection = (inputs * targets).sum()
+
+        dice = 2. * (intersection + self.smooth) / (inputs.sum() + targets.sum() + self.smooth)
+        return 1 - dice
+
+
+class TverskyLoss(nn.Module):
+    """
+    Salehi, S. S. M., Erdogmus, D. & Gholipour, A. Tversky loss function for image segmentation using 3D fully
+    convolutional deep networks. Arxiv (2017).
+    """
+
+    def __init__(self, alpha=0.5, beta=0.5, smooth=1):
+        super(TverskyLoss, self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+        self.smooth = smooth
+
+    def forward(self, inputs, targets):
+        #  inputs = torch.sigmoid(inputs)
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        # True Positives, False Positives & False Negatives
+        TP = (inputs * targets).sum()
+        FP = ((1 - targets) * inputs).sum()
+        FN = (targets * (1 - inputs)).sum()
+
+        Tversky = (TP + self.smooth) / (TP + self.alpha * FP + self.beta * FN + self.smooth)
+
+        return 1 - Tversky
+
+
+class logcoshTverskyLoss(nn.Module):
+    def __init__(self, alpha=0.5, beta=0.5, smooth=1):
+        super(logcoshTverskyLoss, self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+        self.smooth = smooth
+
+    def forward(self, inputs, targets):
+        #  inputs = torch.sigmoid(inputs)
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        # True Positives, False Positives & False Negatives
+        TP = (inputs * targets).sum()
+        FP = ((1 - targets) * inputs).sum()
+        FN = (targets * (1 - inputs)).sum()
+
+        Tversky = (TP + self.smooth) / (TP + self.alpha * FP + self.beta * FN + self.smooth)
+
+        return torch.log(torch.cosh(1 - Tversky))
