a5f76c70d0b3d301ff9825da3107b015a14db939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:05 +0000

    Add package hiccough/0.0.8/hiccough-0.0.8.tar.gz

---
+++ packages/hiccough/hiccough-0.0.8.tar.gz/hiccough-0.0.8/test/test_main.py
@@ -0,0 +1,61 @@
+from unittest.mock import MagicMock, patch
+
+from hiccough import html
+
+import hiccough
+import sys
+
+
+def test_simple_div():
+    assert html(["div"]) == "<div></div>"
+
+
+def test_class_shorthand():
+    assert html(["div.class"]) == '<div class="class"></div>'
+
+
+def test_nesting_and_class():
+    assert (
+        html(
+            [
+                "div.container>div.section>div.box",
+                ["form", ["input", None, {"type": "checkbox"}]],
+            ]
+        )
+        == '<div class="container"><div class="section"><div class="box"><form><input type="checkbox"></input></form></div></div></div>'
+    )
+
+
+def test_multi_class():
+    assert html(["div.a.b"]) == '<div class="a b"></div>'
+
+
+def test_class_merging():
+    assert (
+        html(["div.class-a", None, {"class": "class-b"}])
+        == '<div class="class-b class-a"></div>'
+    )
+
+
+def test_id_shorthand():
+    assert html(["div#name"]) == '<div id="name"></div>'
+
+
+def test_multi_id():
+    assert html(["div#name#blah"]) == '<div id="name"></div>'
+
+
+def test_main():
+    hiccough.html = MagicMock(return_value="")
+    testargs = ["hiccough", '["div"]']
+    with patch.object(sys, "argv", testargs):
+        assert hiccough.main() == 0
+        hiccough.html.assert_called_with(["div"])
+
+
+def test_main_missing_arg():
+    hiccough.html = MagicMock(return_value="")
+    testargs = ["hiccough"]
+    with patch.object(sys, "argv", testargs):
+        assert hiccough.main() == 1
+        hiccough.html.assert_not_called()
