928feaa2ede6c6e84e57de5e93d2a8c5c4ad455c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:16 +0000

    Add package pydantic-argparse/0.3.1/pydantic_argparse-0.3.1-py3-none-any.whl

---
+++ packages/pydantic-argparse/pydantic_argparse-0.3.1-py3-none-any.whl/pydantic_argparse/__version__.py
@@ -0,0 +1,13 @@
+"""__version__.py
+
+Exports the name, version, description and authors of the package
+
+@author Hayden Richards <SupImDos@gmail.com>
+"""
+
+
+# Duplicated from `pyproject.toml`
+__app__ = "pydantic-argparse"
+__description__ = "Typed Argument Parsing with Pydantic"
+__version__ = "0.3.1"
+__authors__ = ["Hayden Richards <SupImDos@gmail.com>"]
