e184c14385c7859b3b9992b49206f8093093acca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:14 +0000

    Add package fastnsfw/0.5/fastnsfw-0.5.tar.gz

---
+++ packages/fastnsfw/fastnsfw-0.5.tar.gz/fastnsfw-0.5/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+    name="fastnsfw",
+    packages=["fastnsfw"],
+    version="0.5",
+    license="MIT",
+    description="NSFW Detection Library",
+    author="Philippe Mathew",
+    author_email="philmattdev@gmail.com",
+    url="https://github.com/bossauh/fastnsfw",
+    download_url="https://github.com/bossauh/fastnsfw/archive/refs/tags/v_05.tar.gz",
+    keywords=["nsfw", "detection"],
+    install_requires=[
+        "fluxhelper",
+        "pillow",
+        "imagehash",
+        "opencv-python"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ]
+)
