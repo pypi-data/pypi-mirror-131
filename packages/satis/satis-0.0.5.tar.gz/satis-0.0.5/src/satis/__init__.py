dc06a1af1f7725456c2cf5e319210ac26de9ab0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:18 +0000

    Add package satis/0.0.5/satis-0.0.5.tar.gz

---
+++ packages/satis/satis-0.0.5.tar.gz/satis-0.0.5/src/satis/__init__.py
@@ -0,0 +1,9 @@
+# from .satis_library import *
+# from .cerfacs_plot import *
+# from .io import *
+# from .avbp2global_compute_ftf import *
+# from .avbp2global_diagnostics import *
+# from .avbp2global_read_inputs import *
+# from .avbp2global import *
+# from .cli import *
+# from .temporal_analysis_tool import *
