6e9395cea20dc2748d03e305e131b29e7252e921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:40 +0000

    Add package ramodels/3.24.0/ramodels-3.24.0.tar.gz

---
+++ packages/ramodels/ramodels-3.24.0.tar.gz/ramodels-3.24.0/ramodels/lora/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# --------------------------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------------
+from ._shared import LoraBase
+from .facet import Facet
+from .facet import FacetRead
+from .itsystem import ITSystem
+from .klasse import Klasse
+from .klasse import KlasseRead
+from .organisation import Organisation
+
+# --------------------------------------------------------------------------------------
+# All
+# --------------------------------------------------------------------------------------
+
+__all__ = [
+    "LoraBase",
+    "Facet",
+    "FacetRead",
+    "Klasse",
+    "KlasseRead",
+    "Organisation",
+    "ITSystem",
+]
