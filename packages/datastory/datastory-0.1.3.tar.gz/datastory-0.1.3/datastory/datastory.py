65a1c5342679df7d1756ecf1e6fef681d976805b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package datastory/0.1.3/datastory-0.1.3.tar.gz

---
+++ packages/datastory/datastory-0.1.3.tar.gz/datastory-0.1.3/datastory/datastory.py
@@ -0,0 +1,47 @@
+import json
+import os
+import requests
+import plotly
+
+
+class DataStory:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.views = []
+
+    def header(self, content: str, level: int = 1):
+        self.views.append({
+            "type": "header",
+            "spec": {
+                "content": content,
+                "level": level
+            }
+        })
+
+    def markdown(self, md: str):
+        self.views.append({
+            "type": "markdown",
+            "spec": {
+                "content": md
+            }
+        })
+
+    def plotly(self, fig: plotly.graph_objs.Figure):
+        self.views.append({
+            "type": "plotly",
+            "spec": json.loads(fig.to_json())
+        })
+
+    def to_json(self) -> str:
+        data = {
+            "name": self.name,
+            "views": [view for view in self.views]
+        }
+        return json.dumps(data)
+
+
+def publish(datastory: DataStory, url: str = os.getenv("DATASTORY_URL", "http://localhost:8080/api/story")) -> str:
+    res = requests.post(url, json=datastory.to_json())
+    res.raise_for_status()
+    return res.json()["url"]
