982bf3bf1513ca953646fb515c3611107a80b92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:39 +0000

    Add package aconnect/1.4/aconnect-1.4.tar.gz

---
+++ packages/aconnect/aconnect-1.4.tar.gz/aconnect-1.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'aconnect',
+  packages = ['aconnect'], # this must be the same as the name above
+  version = '1.4',
+  description = 'Library for easy deployment of A-Connect training methodology',
+  author = 'Ricardo Vergel, Edward Silva, Luis Rueda and Elkim Roa',
+  author_email = 'ricamatheo98@gmail.com,edwsilva09@gmail.com,lerugue@gmail.com, elkimr@gmail.com',
+  url = 'https://github.com/onchipuis/A-Connect', # use the URL to the github repo
+  download_url = 'https://github.com/onchipuis/A-Connect/tarball/1.3.9',
+  keywords = ['library', 'DNN', 'Training'],
+  classifiers = [],
+)
