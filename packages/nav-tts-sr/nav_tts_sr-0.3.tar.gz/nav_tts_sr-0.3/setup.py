64207ee329707f80cca693cfa83b7bff602d312a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:30 +0000

    Add package nav-tts-sr/0.3/nav_tts_sr-0.3.tar.gz

---
+++ packages/nav-tts-sr/nav_tts_sr-0.3.tar.gz/nav_tts_sr-0.3/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+    name='nav_tts_sr',
+    packages=['nav_tts_v1'],
+    version='0.3',
+    license='MIT',
+    description='A module which simplifies stuff related to voice',
+    author='Navaneeth K',
+    author_email='snmlogic111@gmail.com',
+    url='https://github.com/SnmLogic/nav',
+    download_url='https://github.com/SnmLogic/nav/archive/refs/tags/version_0.1.tar.gz',
+    keywords=['Voice', 'Nav', 'TTS'],
+    install_requires=[
+        'pyttsx3',
+        'pyaudio'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
