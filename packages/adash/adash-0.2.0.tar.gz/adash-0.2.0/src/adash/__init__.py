df015d5d75520a9a74e353ad2f9bca1ddfac7cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:55 +0000

    Add package adash/0.2.0/adash-0.2.0.tar.gz

---
+++ packages/adash/adash-0.2.0.tar.gz/adash-0.2.0/src/adash/__init__.py
@@ -0,0 +1,7 @@
+"""
+.. include:: ../../README.md
+"""
+from .download import download
+from .string_util import replace_all, to_half_string, to_number, to_date, split_uppercase
+from .proportion import proportion, progress_rate
+from .date_util import date_list
