6e0e1d928229f5fa7d49a8c1739b398ff3216844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:01 +0000

    Add package django-wysiwyg-redactor-ng/1.0.0/django-wysiwyg-redactor-ng-1.0.0.tar.gz

---
+++ packages/django-wysiwyg-redactor-ng/django-wysiwyg-redactor-ng-1.0.0.tar.gz/django-wysiwyg-redactor-ng-1.0.0/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='django-wysiwyg-redactor-ng',
+    version='1.0.0',
+    description='django-wysiwyg-redactor-ng is a lightweight responsive wysiwyg editor for Django',
+    long_description=readme,
+    author="Robert Chiang",
+    author_email='robert@streetvoice.com',
+    url='https://github.com/StreetVoice/django-wysiwyg-redactor',
+    license='MIT',
+    packages=['redactor'],
+    include_package_data=True,
+    install_requires=['setuptools'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',        
+    ],
+    keywords='django,admin,wysiwyg,editor,redactor,redactorjs',
+)
