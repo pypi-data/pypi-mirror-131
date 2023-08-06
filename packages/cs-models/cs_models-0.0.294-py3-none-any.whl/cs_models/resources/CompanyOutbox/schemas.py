f6802e92225c39217419d6e987682182f32ce96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:36 +0000

    Add package cs-models/0.0.294/cs_models-0.0.294-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.294-py3-none-any.whl/cs_models/resources/CompanyOutbox/schemas.py
@@ -0,0 +1,16 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class CompanyOutboxResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    cik_str = fields.String(required=True, validate=not_blank)
+    ticker = fields.String(required=True)
+    name = fields.String(required=True)
+    exchange = fields.String(allow_none=True)
+    updated_at = fields.DateTime(dump_only=True)
