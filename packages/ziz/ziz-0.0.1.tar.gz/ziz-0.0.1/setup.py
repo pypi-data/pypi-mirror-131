2f65564b7a8776bca49cda6dba1496af55da61a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:48 +0000

    Add package ziz/0.0.1/ziz-0.0.1.tar.gz

---
+++ packages/ziz/ziz-0.0.1.tar.gz/ziz-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cicd_example']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest==6.1.2', 'requests[security]==2.25.1']
+
+setup_kwargs = {
+    'name': 'ziz',
+    'version': '0.0.1',
+    'description': ' ',
+    'long_description': None,
+    'author': 'Erik Li',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
