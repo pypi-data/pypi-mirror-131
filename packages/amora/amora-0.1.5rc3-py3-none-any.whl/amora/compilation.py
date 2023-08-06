f43707c577562a658dcfa09e3163276f9f0cabc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:44 +0000

    Add package amora/0.1.5rc3/amora-0.1.5rc3-py3-none-any.whl

---
+++ packages/amora/amora-0.1.5rc3-py3-none-any.whl/amora/compilation.py
@@ -0,0 +1,37 @@
+import sqlparse
+from amora.types import Compilable
+import sqlalchemy
+from sqlalchemy_bigquery import BigQueryDialect
+from sqlalchemy_bigquery.base import BigQueryCompiler, unnest
+
+
+class fixed_unnest(sqlalchemy.sql.roles.InElementRole, unnest):
+    def __init__(self, *args, **kwargs):
+        self.name = "unnest"
+        super().__init__(*args, **kwargs)
+
+
+class AmoraBigQueryCompiler(BigQueryCompiler):
+    def visit_getitem_binary(self, binary, operator_, **kw):
+        left = self.process(binary.left, **kw)
+        right = self.process(binary.right, **kw)
+
+        try:
+            # Only integer values should be wrapped in OFFSET
+            return f"{left}[OFFSET({int(right)})]"
+        except ValueError:
+            return f"{left}[{right}]"
+
+
+dialect = BigQueryDialect()
+dialect.statement_compiler = AmoraBigQueryCompiler
+
+
+def compile_statement(statement: Compilable) -> str:
+    raw_sql = str(
+        statement.compile(
+            dialect=dialect, compile_kwargs={"literal_binds": True}
+        )
+    )
+    formatted_sql = sqlparse.format(raw_sql, reindent=True, indent_columns=True)
+    return formatted_sql
