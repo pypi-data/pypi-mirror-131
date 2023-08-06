9ae337e88bf775675329660e788bb46d97270310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:46 +0000

    Add package tiny-storage-unstable/0.0.0/tiny_storage-unstable-0.0.0.tar.gz

---
+++ packages/tiny-storage-unstable/tiny_storage-unstable-0.0.0.tar.gz/tiny_storage-unstable-0.0.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="tiny_storage-unstable",
+    author="Nikita Girvel Dobrynin",
+    author_email="widauka@ya.ru",
+    description="Tiny library for key-value single-file application data storage",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/girvel/tiny_storage",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=["tiny_storage"],
+    python_requires=">=3.6",
+)
