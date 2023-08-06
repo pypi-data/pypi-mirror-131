9b271b4a1152c2ae44dce6c7329bcef48986b01e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:38 +0000

    Add package nnmt/1.0.0/nnmt-1.0.0.tar.gz

---
+++ packages/nnmt/nnmt-1.0.0.tar.gz/nnmt-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='nnmt',
+      version='1.0.0',
+      description='Neuronal Network Meanfield Toolbox',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/INM-6/nnmt',
+      author='see authors.md',
+      author_email='m.layer@fz-juelich.de',
+      license='GNU GPLv3',
+      packages=find_packages(include=['nnmt', 'nnmt.*']),
+      install_requires=[
+        'setuptools>=23.1.0',
+        'numpy>=1.8',
+        'scipy>=0.14',
+        'Cython>=0.20',
+        'h5py>=2.5',
+        'matplotlib>=2.0',
+        'pint',
+        'pyyaml',
+        'requests',
+        'mpmath',
+        'decorator'],
+      python_requires='>=3')
