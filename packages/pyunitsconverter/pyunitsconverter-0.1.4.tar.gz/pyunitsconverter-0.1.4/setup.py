687de87952e0ac0aeb10a6cf894ed664a881f474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:13 +0000

    Add package pyunitsconverter/0.1.4/pyunitsconverter-0.1.4.tar.gz

---
+++ packages/pyunitsconverter/pyunitsconverter-0.1.4.tar.gz/pyunitsconverter-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+ name='pyunitsconverter',
+ version='0.1.4',
+ author='Sven Eschlbeck',
+ author_email='sven.eschlbeck@t-online.de',
+ packages=['pyunitsconverter'],
+ url='http://pypi.python.org/pypi/pyunitsconverter/',
+ license='LICENSE.txt',
+ description='A package for easy unit conversions',
+ long_description_content_type='text/markdown',
+ long_description=open('README.md').read(),
+ install_requires=[
+   ],
+)
