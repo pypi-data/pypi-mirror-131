d86153f97a898ae1e994c3b7fd85269239d2ac2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:49 +0000

    Add package gosu/1.1.10/gosu-1.1.10.tar.gz

---
+++ packages/gosu/gosu-1.1.10.tar.gz/gosu-1.1.10/tests/test_basic.py
@@ -0,0 +1,37 @@
+import os
+
+from gosu.cmds.ci import get_version, git_num_changes
+from gosu.cmds.python import (
+    _get_editable_installs,
+    _get_pkg_pyfiles,
+    _manage_env_vars,
+    env_from_sourcing,
+)
+
+
+def test_get_env_from_sourcing():
+    e = env_from_sourcing(".venv/bin/activate")
+    assert "PWD" in e
+
+
+def test_get_env_vars():
+    os.chdir("tests")
+    e = _manage_env_vars()
+    assert e["test"] == "1"
+
+
+def test_get_pkg_pyfiles():
+    e = _get_pkg_pyfiles()
+    assert len(e) > 0
+
+
+def test_get_version():
+    assert get_version() != ""
+
+
+def test_git_num_changes():
+    assert git_num_changes() != ""
+
+
+def test_get_editable_installs():
+    assert _get_editable_installs("pyproject.toml")["something"] == "../something"
