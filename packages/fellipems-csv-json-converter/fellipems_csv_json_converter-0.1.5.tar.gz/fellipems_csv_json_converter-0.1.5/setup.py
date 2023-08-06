15a32608e42e62724f52cbb903199e69aa5a4f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:09 +0000

    Add package fellipems-csv-json-converter/0.1.5/fellipems_csv_json_converter-0.1.5.tar.gz

---
+++ packages/fellipems-csv-json-converter/fellipems_csv_json_converter-0.1.5.tar.gz/fellipems_csv_json_converter-0.1.5/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fellipems_csv_json_converter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.1,<9.0.0', 'pandas>=1.3.3,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['csv_converter = '
+                     'fellipems_csv_json_converter.converter:Converte_JSONCSV']}
+
+setup_kwargs = {
+    'name': 'fellipems-csv-json-converter',
+    'version': '0.1.5',
+    'description': 'Curso de introducao ao python: Conversor CSV-JSON',
+    'long_description': "# File Converter\n\nCSV / JSON converter\n\n### project\n\nfuncao: Converte_JSONCSV(modo,sep)\n\nConverte um arquivo ou pasta de arquivos do formato **CSV** para **JSON**. a funcao possui as variaveis 'modo' (opcional), 'sep' (opcional) e 'Arq' (obrigatorio)\n        \n       Arq: um arquivo ou pasta (string). caso seja passado para a funcao um unico arquivo, este sera convertido. Caso seja passada uma pasta, serao convertidos todos os arquivos do tipo compativel\n       \n           Obs.:os arquivos convertidos serao salvos na mesma pasta do arquivo original e com mesmo nome, sobrescrevendo arquivos que eventualmente existam na pasta\n       \n       modo: o modo padrao de conversao e CSV para JSON (modo='csv2json'), porem caso seja passado o parametro 'json2csv' na variavel 'modo', a conversao sera feita no sentido contrario\n       \n       sep : o separador padrao para o arquivo CSV sera ',' (virgula), porem um separador diferente pode ser especificado atraves da variavel 'sep'\n\npip install fellipems_csv_json_converter\n\n#csv_converter --help\n",
+    'author': 'FellipeMS',
+    'author_email': 'fellipems.sc@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
