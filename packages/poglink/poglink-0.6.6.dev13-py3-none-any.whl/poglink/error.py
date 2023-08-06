8c128211e6a727334311a5e3dde309086cf93448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:39 +0000

    Add package poglink/0.6.6.dev13/poglink-0.6.6.dev13-py3-none-any.whl

---
+++ packages/poglink/poglink-0.6.6.dev13-py3-none-any.whl/poglink/error.py
@@ -0,0 +1,14 @@
+class RatesWriteError(Exception):
+    pass
+
+
+class RatesReadError(Exception):
+    pass
+
+
+class RatesFetchError(Exception):
+    pass
+
+
+class RatesProcessError(Exception):
+    pass
