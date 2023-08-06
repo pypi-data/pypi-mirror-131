4ff4b519b9163741e1e798c2f3f0c0b30da8d177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:50 +0000

    Add package sla/1.3.1/sla-1.3.1.tar.gz

---
+++ packages/sla/sla-1.3.1.tar.gz/sla-1.3.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='sla',  
+     version='1.3.1',
+     author="Gasymov Damir",
+     author_email="gasymov.df18@physics.msu.ru",
+     description="Non-parametric LOSVD analysis for galaxy spectra",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/gasymovdf/sla",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
