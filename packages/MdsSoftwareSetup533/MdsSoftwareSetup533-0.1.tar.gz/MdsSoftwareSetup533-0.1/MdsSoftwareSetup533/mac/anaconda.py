b1ae567a6436348535dfe3b75e6ff569a351777e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:38 +0000

    Add package MdsSoftwareSetup533/0.1/MdsSoftwareSetup533-0.1.tar.gz

---
+++ packages/MdsSoftwareSetup533/MdsSoftwareSetup533-0.1.tar.gz/MdsSoftwareSetup533-0.1/MdsSoftwareSetup533/mac/anaconda.py
@@ -0,0 +1,50 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.12.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+  
+import pandas as pd
+import MdsSoftwareSetup533.data_load as dl
+
+from MdsSoftwareSetup533 import base_class
+class conda_class(base_class.base):
+       
+    data=dl.read_data()
+    def __init__(self,software_name,os):
+        self.software_name=software_name
+        self.os=os
+    def Installation_Guide(self):
+        print('\033[1m'+base_class.base.message(self))
+        data1=conda_class.data[conda_class.data["OS"]=="Mac"]
+        data1=data1[data1["Software"]=="Anaconda"]
+        print('\033[1m'+"Below is the link containing step-by-step installation instructions:\n")
+        ig=data1.to_string(columns=['Installation_Guide'], index=False,header=False)
+        return ig
+    
+    def Description(self):
+        data1=conda_class.data[conda_class.data["OS"]=="Mac"]
+        data1=data1[data1["Software"]=="Anaconda"]
+        print('\033[1m'+"Below is a brief description of Anaconda: \n")
+        desc=data1.to_string(columns=['Description'], index=False,header=False)
+        return desc
+    
+    def Tutorial(self):
+        data1=conda_class.data[conda_class.data["OS"]=="Mac"]
+        data1=data1[data1["Software"]=="Anaconda"]
+        print('\033[1m'+"Below is the link for a video tutorial of Anaconda: \n ")
+        Tutorial=data1.to_string(columns=['Tutorial'], index=False,header=False)
+        return Tutorial
+
+    
+
---
+++ packages/MdsSoftwareSetup533/MdsSoftwareSetup533-0.1.tar.gz/MdsSoftwareSetup533-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MdsSoftwareSetup533',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/MayukhaB/Data533_Lab4.git',
+    author='Mayukha Bheemavarapu',
+    author_email='mayukhabheemavarapu@gmail.com'
+)
+include_package_data=True
