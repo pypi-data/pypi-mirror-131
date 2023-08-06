d54777775711f45318be9bc1384c57425ee2a11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:00 +0000

    Add package jk-exceptionhelper/0.2021.12.14.1/jk_exceptionhelper-0.2021.12.14.1.tar.gz

---
+++ packages/jk-exceptionhelper/jk_exceptionhelper-0.2021.12.14.1.tar.gz/jk_exceptionhelper-0.2021.12.14.1/jk_exceptionhelper/__init__.py
@@ -0,0 +1,14 @@
+﻿
+
+
+__version__ = "0.2021.12.14.1"
+
+
+
+from .StackTraceItem import StackTraceItem
+from .ExceptionObject import ExceptionObject
+
+from .exception_helper import analyseException
+
+
+
