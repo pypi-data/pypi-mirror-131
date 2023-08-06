da16688a055398b91644f0713b6d99d027b87c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package udata-front/1.2.1.dev1632/udata_front-1.2.1.dev1632-py2.py3-none-any.whl

---
+++ packages/udata-front/udata_front-1.2.1.dev1632-py2.py3-none-any.whl/udata_front/settings.py
@@ -0,0 +1,25 @@
+"""
+Default settings for udata-front
+"""
+
+# TODO: move this back to an extension
+RESOURCES_SCHEMAGOUVFR_ENABLED = True
+SCHEMA_GOUVFR_VALIDATA_URL = 'https://validata.etalab.studio'
+SCHEMA_CATALOG_URL = 'https://schema.data.gouv.fr/schemas/schemas.json'
+
+# Dataset settings
+# Default page size for resources on dataset page
+RESOURCES_DEFAULT_PAGE_SIZE = 6
+
+# Frontend banner parameters
+BANNER_ACTIVATED = False
+BANNER_HTML_CONTENT_EN = ''
+BANNER_HTML_CONTENT_FR = ''
+
+# Static pages from github repo
+PAGES_GH_REPO_NAME = 'etalab/datagouvfr-pages'
+PAGES_REPO_BRANCH = 'master'
+
+# api.gouv.fr
+APIGOUVFR_URL = 'https://api.gouv.fr/api/v1/apis'
+APIGOUVFR_ALLOW_OPENNESS = ['open', 'semi_open']
