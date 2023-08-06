67ee9d89ced4031ccda22ea0985aefb54964bfe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:48 +0000

    Add package pipen-report/0.1.1/pipen-report-0.1.1.tar.gz

---
+++ packages/pipen-report/pipen-report-0.1.1.tar.gz/pipen-report-0.1.1/pipen_report/frontend/src/toc.tpl.svelte
@@ -0,0 +1,31 @@
+<script>
+    import {
+        SideNavItems,
+        SideNavLink,
+        SideNavMenu,
+        SideNavMenuItem,
+    } from "carbon-components-svelte";
+    /**
+     * This is passed from python
+     * [{
+     *     slug: "h1-slug1", text: ..., children: {"h2-slug1": {text: ...}}}
+     * }]
+     */
+    const toc = {{toc}};
+</script>
+
+<SideNavItems>
+
+    {#each toc as h1_toc}
+        {#if h1_toc.children.length == 0}
+        <SideNavLink href={`#${h1_toc.slug}`} title={h1_toc.text} text={h1_toc.text} />
+        {:else}
+        <SideNavMenu href={`#${h1_toc.slug}`} title={h1_toc.text} text={h1_toc.text} expanded>
+        {#each h1_toc.children as h2_toc}
+            <SideNavMenuItem href={`#${h2_toc.slug}`} title={h2_toc.text} text={h2_toc.text} />
+        {/each}
+        </SideNavMenu>
+        {/if}
+    {/each}
+
+</SideNavItems>
