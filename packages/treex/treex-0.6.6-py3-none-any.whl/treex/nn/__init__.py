b064a88fd08c39502ddeb02374741aff3834a957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:55 +0000

    Add package treex/0.6.6/treex-0.6.6-py3-none-any.whl

---
+++ packages/treex/treex-0.6.6-py3-none-any.whl/treex/nn/__init__.py
@@ -0,0 +1,29 @@
+from treex import types
+
+from .batch_norm import BatchNorm
+from .conv import Conv
+from .dropout import Dropout
+from .flatten import Flatten
+from .flax_module import FlaxModule
+from .linear import Linear
+from .mlp import MLP
+from .sequential import Lambda, Sequential, sequence
+
+try:
+    from .haiku_module import HaikuModule
+except types.OptionalDependencyNotFound:
+    pass
+
+__all__ = [
+    "BatchNorm",
+    "Conv",
+    "Dropout",
+    "Flatten",
+    "FlaxModule",
+    "Linear",
+    "MLP",
+    "Lambda",
+    "Sequential",
+    "sequence",
+    "HaikuModule",
+]
