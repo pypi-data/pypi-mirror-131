f6c3acfea4f5322610c3e0e8918e6b77e583e533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:04 +0000

    Add package gns3-client/0.0.1/gns3-client-0.0.1.tar.gz

---
+++ packages/gns3-client/gns3-client-0.0.1.tar.gz/gns3-client-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gns3-client',
+    version='0.0.1',
+    py_modules=['pygns3'],
+    url='https://github.com/desnoe/gns3-client',
+    license='MIT License',
+    author='Olivier Desnoë',
+    author_email='olivier@delarche.fr',
+    description='Python API client for GNS3 simulation network simulation tool'
+)
