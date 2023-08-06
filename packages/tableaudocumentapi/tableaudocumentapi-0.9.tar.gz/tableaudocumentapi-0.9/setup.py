eec726233191d9f3adbe8254a3885bc206021e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:48 +0000

    Add package tableaudocumentapi/0.9/tableaudocumentapi-0.9.tar.gz

---
+++ packages/tableaudocumentapi/tableaudocumentapi-0.9.tar.gz/tableaudocumentapi-0.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+    
+setup(
+    name='tableaudocumentapi',
+    version='0.9',
+    author='Tableau',
+    author_email='github@tableau.com',
+    url='https://github.com/tableau/document-api-python',
+    packages=['tableaudocumentapi'],
+    license='MIT',
+    description='A Python module for working with Tableau files.',
+    test_suite='test'
+)
