e3337be61720cd9f20e05977242e0cf04c3f312d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:51 +0000

    Add package slurry-websocket/0.4.2/slurry-websocket-0.4.2.tar.gz

---
+++ packages/slurry-websocket/slurry-websocket-0.4.2.tar.gz/slurry-websocket-0.4.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['slurry_websocket']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['orjson>=3.6.4,<4.0.0',
+ 'slurry>=1.2.0,<2.0.0',
+ 'trio-websocket>=0.9.2,<0.10.0']
+
+setup_kwargs = {
+    'name': 'slurry-websocket',
+    'version': '0.4.2',
+    'description': 'A Websocket client section for the Slurry stream processing microframework',
+    'long_description': '================\nSlurry-Websocket\n================\n\n\nCredits\n-------\n\nSmall bits of code and documentation has been copied from the `Trio-Websocket`_ library.\n\n\n\n\n\n.. _`Trio-Websocket`: https://github.com/HyperionGray/trio-websocket',
+    'author': 'Anders Ellenshøj Andersen',
+    'author_email': 'andersa@ellenshoej.dk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/andersea/slurry-websocket',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
