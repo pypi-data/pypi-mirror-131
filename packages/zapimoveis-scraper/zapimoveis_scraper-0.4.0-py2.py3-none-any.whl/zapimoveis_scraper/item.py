e87ae50389e4f785fac41474d6d6a8185e2d57b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:26 +0000

    Add package zapimoveis-scraper/0.4.0/zapimoveis_scraper-0.4.0-py2.py3-none-any.whl

---
+++ packages/zapimoveis-scraper/zapimoveis_scraper-0.4.0-py2.py3-none-any.whl/zapimoveis_scraper/item.py
@@ -0,0 +1,11 @@
+class ZapItem:
+
+    description = None
+    price = None
+    condo_fee = None
+    bedrooms = None
+    bathrooms = None
+    total_area_m2 = None
+    vacancies = None
+    address = None
+    link = None
