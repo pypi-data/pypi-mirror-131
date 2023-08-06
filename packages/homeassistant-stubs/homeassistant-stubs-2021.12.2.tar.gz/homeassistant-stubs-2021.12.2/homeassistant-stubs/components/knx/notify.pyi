09b24b98b1b5b0d73aad105a31e47480fb8a76c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:03 +0000

    Add package homeassistant-stubs/2021.12.2/homeassistant-stubs-2021.12.2.tar.gz

---
+++ packages/homeassistant-stubs/homeassistant-stubs-2021.12.2.tar.gz/homeassistant-stubs-2021.12.2/homeassistant-stubs/components/knx/notify.pyi
@@ -0,0 +1,19 @@
+from .const import DOMAIN as DOMAIN, KNX_ADDRESS as KNX_ADDRESS
+from homeassistant.components.notify import BaseNotificationService as BaseNotificationService
+from homeassistant.const import CONF_NAME as CONF_NAME
+from homeassistant.core import HomeAssistant as HomeAssistant
+from homeassistant.helpers.typing import ConfigType as ConfigType, DiscoveryInfoType as DiscoveryInfoType
+from typing import Any
+from xknx import XKNX as XKNX
+from xknx.devices import Notification as XknxNotification
+
+async def async_get_service(hass: HomeAssistant, config: ConfigType, discovery_info: Union[DiscoveryInfoType, None] = ...) -> Union[KNXNotificationService, None]: ...
+
+class KNXNotificationService(BaseNotificationService):
+    devices: Any
+    def __init__(self, devices: list[XknxNotification]) -> None: ...
+    @property
+    def targets(self) -> dict[str, str]: ...
+    async def async_send_message(self, message: str = ..., **kwargs: Any) -> None: ...
+    async def _async_send_to_all_devices(self, message: str) -> None: ...
+    async def _async_send_to_device(self, message: str, names: str) -> None: ...
