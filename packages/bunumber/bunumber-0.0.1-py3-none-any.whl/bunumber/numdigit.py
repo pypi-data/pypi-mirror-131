43b3a5e1dee27f57fa024f8c47123cfc329858b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:26 +0000

    Add package bunumber/0.0.1/bunumber-0.0.1-py3-none-any.whl

---
+++ packages/bunumber/bunumber-0.0.1-py3-none-any.whl/bunumber/numdigit.py
@@ -0,0 +1,6 @@
+﻿class Numdigit:
+    def __init__(self):
+        self.members=['1','2','3']
+    def print_member(self):
+        for i in self.members:
+            print(i)
---
+++ packages/bunumber/bunumber-0.0.1-py3-none-any.whl/bunumber/numletter.py
@@ -0,0 +1,6 @@
+﻿class Numletter:
+    def __init__(self):
+        self.members=['one','two','three']
+    def print_member(self):
+        for i in self.members:
+            print(i)
