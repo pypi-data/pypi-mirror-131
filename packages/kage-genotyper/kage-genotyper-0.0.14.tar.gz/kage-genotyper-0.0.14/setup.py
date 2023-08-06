ed4a6ef9bab555c66290dbc5fab16d88a0c48e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:28 +0000

    Add package kage-genotyper/0.0.14/kage-genotyper-0.0.14.tar.gz

---
+++ packages/kage-genotyper/kage-genotyper-0.0.14.tar.gz/kage-genotyper-0.0.14/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, Extension
+
+setup(name='kage-genotyper',
+      version='0.0.14',
+      description='KAGE',
+      long_description_content_type="text/markdown",
+      url='http://github.com/ivargr/kage',
+      author='Ivar Grytten',
+      author_email='',
+      license='MIT',
+      packages=["kage"],
+      zip_safe=False,
+      install_requires=['numpy', 'tqdm', 'pyfaidx', 'pathos', 'cython', 'scipy',
+                        'obgraph',
+                        'graph_kmer_index>=0.0.11',
+                        'kmer_mapper',
+                        'graph_read_simulator', 'shared_memory_wrapper'
+                        ],
+      include_dirs=["."],
+      classifiers=[
+            'Programming Language :: Python :: 3'
+      ],
+      entry_points={
+            'console_scripts': ['kage=kage.command_line_interface:main']
+      }
+
+)
+
+""""
+rm -rf dist
+python3 setup.py sdist
+twine upload --skip-existing dist/*
+
+"""
