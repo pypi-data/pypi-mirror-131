76daa852d96f49573a67aeae7151639982ef854f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:40 +0000

    Add package certainty-estimator/1.6/certainty-estimator-1.6.tar.gz

---
+++ packages/certainty-estimator/certainty-estimator-1.6.tar.gz/certainty-estimator-1.6/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="certainty-estimator",
+    version="1.6",
+    author="Jiaxin Pei",
+    author_email="pedropei@umich.edu",
+    description="This package is used to estimate certainty for scientific findings",
+    python_requires=">=3.6",
+    url="https://github.com/Jiaxin-Pei/certainty-estimator",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+    #package_data = {'':['data']}
+)
