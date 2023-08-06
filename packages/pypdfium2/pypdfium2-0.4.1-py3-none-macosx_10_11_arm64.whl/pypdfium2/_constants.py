d4e1cea8c60b347c41985388db843b47cb622f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:22 +0000

    Add package pypdfium2/0.4.1/pypdfium2-0.4.1-py3-none-macosx_10_11_arm64.whl

---
+++ packages/pypdfium2/pypdfium2-0.4.1-py3-none-macosx_10_11_arm64.whl/pypdfium2/_constants.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2021 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+from enum import Enum
+
+
+class OptimiseMode (Enum):
+    """ How to optimise page rendering """
+    none = 0          #: Do not use any optimisations
+    lcd_display = 1   #: Optimise for LCD displays
+    printing = 2      #: Optimise for printing
+
+
+class ViewMode (Enum):
+    Unknown = 0
+    XYZ     = 1
+    Fit     = 2
+    FitH    = 3
+    FitV    = 4
+    FitR    = 5
+    FitB    = 6
+    FitBH   = 7
+    FitBV   = 8
