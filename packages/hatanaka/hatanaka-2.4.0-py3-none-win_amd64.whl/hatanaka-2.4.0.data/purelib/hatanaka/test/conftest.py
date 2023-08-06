be5b39d63f7875ea7e707da1b9cfba881a39fcff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:23 +0000

    Add package hatanaka/2.4.0/hatanaka-2.4.0-py3-none-win_amd64.whl

---
+++ packages/hatanaka/hatanaka-2.4.0-py3-none-win_amd64.whl/hatanaka-2.4.0.data/purelib/hatanaka/test/conftest.py
@@ -0,0 +1,81 @@
+import re
+
+import pytest
+from importlib_resources import files
+
+import hatanaka.test.data
+
+
+def get_data_path(fname):
+    for x in ['.21o', '.rnx']:
+        fname = fname.replace(x.lower(), '.rnx')
+        fname = fname.replace(x.upper(), '.rnx')
+    for x in ['.21d', '.crx']:
+        fname = fname.replace(x.lower(), '.crx')
+        fname = fname.replace(x.upper(), '.crx')
+    m = re.fullmatch(r'sample\.(zip|gz|bz2|Z)', fname)
+    if m:
+        fname = 'sample.crx.' + m.group(1)
+    return files(hatanaka.test.data) / fname
+
+
+def clean(txt):
+    # Remove rnx2crx version number and timestamp for testing
+    pattern = '^RNX2CRX.+CRINEX PROG / DATE$'
+    if isinstance(txt, bytes):
+        return re.sub(pattern.encode(), b'', txt.replace(b'\r', b''), flags=re.M)
+    return re.sub(pattern, '', txt.replace('\r', ''), flags=re.M)
+
+
+@pytest.fixture
+def crx_sample():
+    return get_data_path('sample.crx')
+
+
+@pytest.fixture
+def rnx_sample():
+    return get_data_path('sample.rnx')
+
+
+@pytest.fixture
+def crx_str(crx_sample):
+    return crx_sample.read_text()
+
+
+@pytest.fixture
+def rnx_str(rnx_sample):
+    return rnx_sample.read_text()
+
+
+@pytest.fixture
+def crx_str_stream(crx_sample):
+    with crx_sample.open('r', encoding='ascii') as f:
+        yield f
+
+
+@pytest.fixture
+def rnx_str_stream(rnx_sample):
+    with rnx_sample.open('r', encoding='ascii') as f:
+        yield f
+
+
+@pytest.fixture
+def crx_bytes(crx_sample):
+    return crx_sample.read_bytes()
+
+
+@pytest.fixture
+def rnx_bytes(rnx_sample):
+    return rnx_sample.read_bytes()
+
+
+@pytest.fixture
+def crx_bytes_stream(crx_sample):
+    with crx_sample.open('rb') as f:
+        yield f
+
+
+@pytest.fixture
+def rnx_bytes_stream(rnx_sample):
+    with rnx_sample.open('rb') as f:
+        yield f
---
+++ packages/hatanaka/hatanaka-2.4.0-py3-none-win_amd64.whl/hatanaka-2.4.0.data/purelib/hatanaka/test/test_general_compression.py
@@ -0,0 +1,215 @@
+import gzip
+import io
+import shutil
+
+import pytest
+
+from hatanaka import compress, compress_on_disk, decompress, decompress_on_disk
+from .conftest import clean, get_data_path
+
+decompress_pairs = [
+    ('.crx', '.rnx'),
+    ('.CRX', '.RNX'),
+    ('.21d', '.21o'),
+    ('.21D', '.21O'),
+    ('.21d.gz', '.21o'),
+    ('.21d.Z', '.21o'),
+    ('.21d.bz2', '.21o'),
+    ('.21d.zip', '.21o'),
+    ('.crx.gz', '.rnx'),
+    ('.crx.Z', '.rnx'),
+    ('.crx.bz2', '.rnx'),
+    ('.crx.zip', '.rnx'),
+    ('.rnx', '.rnx'),
+    ('.rnx.gz', '.rnx'),
+    ('.rnx.Z', '.rnx'),
+    ('.rnx.zip', '.rnx'),
+    ('.rnx.bz2', '.rnx'),
+    ('.21o', '.21o'),
+    ('.21o.gz', '.21o'),
+    ('.zip', '.rnx'),
+    ('.gz', '.rnx'),
+    ('.bz2', '.rnx'),
+]
+
+
+@pytest.mark.parametrize(
+    'input_suffix, expected_suffix',
+    decompress_pairs
+)
+def test_decompress(tmp_path, crx_sample, rnx_bytes, input_suffix, expected_suffix):
+    # prepare
+    sample_path = tmp_path / ('sample' + input_suffix)
+    in_file = 'sample' + input_suffix
+    shutil.copy(get_data_path(in_file), sample_path)
+    # decompress
+    converted = decompress(sample_path)
+    # check
+    assert clean(converted) == clean(rnx_bytes)
+    converted = decompress(sample_path.read_bytes())
+    assert clean(converted) == clean(rnx_bytes)
+
+
+@pytest.mark.parametrize(
+    'input_suffix, expected_suffix',
+    decompress_pairs
+)
+def test_decompress_on_disk(tmp_path, crx_sample, rnx_bytes, input_suffix, expected_suffix):
+    # prepare
+    sample_path = tmp_path / ('sample' + input_suffix)
+    in_file = 'sample' + input_suffix
+    shutil.copy(get_data_path(in_file), sample_path)
+    # decompress
+    out_path = decompress_on_disk(sample_path)
+    # check
+    assert out_path.exists()
+    print(list(tmp_path.glob('*')))
+    assert out_path == tmp_path / ('sample' + expected_suffix)
+    assert clean(out_path.read_bytes()) == clean(rnx_bytes)
+
+
+def make_nav(txt):
+    return txt.replace(b'OBSERVATION', b'NAVIGATION ')
+
+
+@pytest.mark.parametrize(
+    'input_suffix',
+    ['.rnx', '.RNX', '.21n']
+)
+def test_decompress_non_obs(tmp_path, rnx_bytes, input_suffix):
+    # prepare
+    txt = make_nav(rnx_bytes)
+    sample_path = tmp_path / ('sample' + input_suffix + '.gz')
+    sample_path.write_bytes(gzip.compress(txt))
+    # decompress
+    out_path = decompress_on_disk(sample_path)
+    # check
+    assert out_path.exists()
+    assert out_path == tmp_path / ('sample' + input_suffix)
+    assert clean(out_path.read_bytes()) == clean(txt)
+
+
+compress_pairs = [
+    ('.rnx', 'none', '.crx'),
+    ('.rnx', 'gz', '.crx.gz'),
+    ('.rnx', 'bz2', '.crx.bz2'),
+    ('.rnx', 'Z', '.crx.Z'),
+    ('.RNX', 'gz', '.CRX.gz'),
+    ('.21o', 'bz2', '.21d.bz2'),
+    ('.21o', 'Z', '.21d.Z'),
+    ('.21O', 'gz', '.21D.gz'),
+    ('.crx', 'gz', '.crx.gz'),
+    ('.21d', 'gz', '.21d.gz'),
+    ('.crx', 'none', '.crx'),
+    ('.21d', 'none', '.21d'),
+]
+
+
+@pytest.mark.parametrize(
+    'input_suffix, compression, expected_suffix',
+    compress_pairs
+)
+def test_compress(tmp_path, crx_sample, rnx_bytes, input_suffix, compression, expected_suffix):
+    # prepare
+    in_file = 'sample' + input_suffix
+    sample_path = tmp_path / in_file
+    shutil.copy(get_data_path(in_file), sample_path)
+    # compress
+    converted = compress(sample_path, compression=compression)
+    # check
+    assert clean(decompress(converted)) == clean(rnx_bytes)
+    converted = compress(sample_path.read_bytes(), compression=compression)
+    assert clean(decompress(converted)) == clean(rnx_bytes)
+
+
+@pytest.mark.parametrize(
+    'input_suffix, compression, expected_suffix',
+    compress_pairs
+)
+def test_compress_on_disk(tmp_path, crx_sample, rnx_bytes, input_suffix, compression,
+                          expected_suffix):
+    # prepare
+    sample_path = tmp_path / ('sample' + input_suffix)
+    in_file = 'sample' + input_suffix
+    shutil.copy(get_data_path(in_file), sample_path)
+    # compress
+    out_path = compress_on_disk(sample_path, compression=compression)
+    # check
+    assert out_path.exists()
+    assert out_path == tmp_path / ('sample' + expected_suffix)
+    assert clean(decompress(out_path)) == clean(rnx_bytes)
+
+
+@pytest.mark.parametrize(
+    'input_suffix',
+    ['.rnx', '.RNX', '.21n']
+)
+def test_compress_non_obs(tmp_path, rnx_bytes, input_suffix):
+    # prepare
+    txt = make_nav(rnx_bytes)
+    sample_path = tmp_path / ('sample' + input_suffix)
+    sample_path.write_bytes(txt)
+    # compress
+    out_path = compress_on_disk(sample_path)
+    # check
+    assert out_path.exists()
+    assert out_path == tmp_path / ('sample' + input_suffix + '.gz')
+    assert clean(decompress(out_path)) == clean(txt)
+
+
+def test_invalid_input(crx_str, rnx_bytes):
+    with pytest.raises(ValueError):
+        decompress(io.BytesIO(rnx_bytes))
+    with pytest.raises(ValueError):
+        compress(io.BytesIO(rnx_bytes))
+
+
+def test_invalid_name(tmp_path, rnx_sample):
+    sample_path = tmp_path / 'sample'
+    shutil.copy(rnx_sample, sample_path)
+    with pytest.raises(ValueError) as excinfo:
+        decompress_on_disk(sample_path)
+    msg = excinfo.value.args[0]
+    assert msg.endswith('is not a valid RINEX file name')
+    with pytest.raises(ValueError) as excinfo:
+        compress_on_disk(sample_path)
+    msg = excinfo.value.args[0]
+    assert msg.endswith('is not a valid RINEX file name')
+
+
+def test_decompress_on_disk_delete(tmp_path, rnx_bytes):
+    # prepare
+    in_file = 'sample.crx.gz'
+    sample_path = tmp_path / in_file
+    shutil.copy(get_data_path(in_file), sample_path)
+    # decompress and delete
+    out_path = decompress_on_disk(sample_path, delete=True)
+    # check
+    expected_path = tmp_path / 'sample.rnx'
+    assert not sample_path.exists()
+    assert out_path == expected_path
+    assert expected_path.exists()
+    assert clean(decompress(expected_path)) == clean(rnx_bytes)
+    # check that already decompressed is not deleted
+    out_path = decompress_on_disk(expected_path, delete=True)
+    assert out_path == expected_path
+    assert out_path.exists()
+
+
+def test_compress_on_disk_delete(tmp_path, rnx_bytes):
+    # prepare
+    in_file = 'sample.rnx'
+    sample_path = tmp_path / in_file
+    shutil.copy(get_data_path(in_file), sample_path)
+    # decompress and delete
+    out_path = compress_on_disk(sample_path, delete=True)
+    # check
+    expected_path = tmp_path / 'sample.crx.gz'
+    assert not sample_path.exists()
+    assert out_path == expected_path
+    assert expected_path.exists()
+    assert clean(decompress(expected_path)) == clean(rnx_bytes)
+    # check that already decompressed is not deleted
+    out_path = compress_on_disk(expected_path, delete=True)
+    assert out_path == expected_path
+    assert out_path.exists()
