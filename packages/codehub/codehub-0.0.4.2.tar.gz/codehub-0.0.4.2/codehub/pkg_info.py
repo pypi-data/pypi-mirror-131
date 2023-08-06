c9521761112139d32bedd005bcb207675b655fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:43 +0000

    Add package codehub/0.0.4.2/codehub-0.0.4.2.tar.gz

---
+++ packages/codehub/codehub-0.0.4.2.tar.gz/codehub-0.0.4.2/codehub/pkg_info.py
@@ -0,0 +1,7 @@
+import os
+pkg_root_dir=os.path.dirname(__file__)
+pkg_data_dir=os.path.join(pkg_root_dir,'data')
+code_dir=os.path.join(pkg_root_dir,'code')
+
+if not os.path.exists(code_dir):
+    os.makedirs(code_dir)
