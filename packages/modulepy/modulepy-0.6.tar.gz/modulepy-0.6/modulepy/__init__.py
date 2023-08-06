d0f957fdda5700d2added12a8f1c794a4a69e168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:17 +0000

    Add package modulepy/0.6/modulepy-0.6.tar.gz

---
+++ packages/modulepy/modulepy-0.6.tar.gz/modulepy-0.6/modulepy/__init__.py
@@ -0,0 +1,5 @@
+from modulepy.ModuleBase import ModuleBase, ModuleInformation, ModuleVersion, SharedData
+from modulepy.ModuleLoader import ModuleLoader
+from modulepy.ModuleManager import ModuleManager
+
+__all__ = ['ModuleBase', 'ModuleLoader', 'ModuleManager', 'ModuleInformation', 'ModuleVersion', 'SharedData']
