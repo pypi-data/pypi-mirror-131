97895b34bc990c0bef133cfca5ee2d321fd7cf59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:16 +0000

    Add package bamboolib/1.29.1/bamboolib-1.29.1-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/bamboolib/bamboolib-1.29.1-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl/bamboolib/loaders/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 8080 Labs GmbH.
+# Distributed under the terms of our End-User License Agreement (see bamboolib.com/eula
+# for more information).
+
+"""
+Setup loader plugins. The import order defines the order in the search results.
+"""
+
+from bamboolib.loaders.dummy_data import DummyData
