46898448ba4baed0839e3050b8d8d20680421b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:34 +0000

    Add package localstack/0.13.1.1/localstack-0.13.1.1-py3-none-any.whl

---
+++ packages/localstack/localstack-0.13.1.1-py3-none-any.whl/localstack/plugins.py
@@ -0,0 +1,15 @@
+import logging
+
+from localstack import config
+from localstack.runtime import hooks
+
+LOG = logging.getLogger(__name__)
+
+
+@hooks.configure_localstack_container()
+def configure_edge_port(container):
+    ports = [config.EDGE_PORT, config.EDGE_PORT_HTTP]
+    LOG.debug("configuring container with edge ports: %s", ports)
+    for port in ports:
+        if port:
+            container.ports.add(port)
