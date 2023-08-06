91d72f95ee91c2bcf29d3a6a8b9bbc3d526d1354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:11 +0000

    Add package Texture-Atlas/0.2.9/Texture_Atlas-0.2.9.tar.gz

---
+++ packages/Texture-Atlas/Texture_Atlas-0.2.9.tar.gz/Texture_Atlas-0.2.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.rst") as file:
+    readme = file.read()
+
+setup(
+  name = 'Texture_Atlas',
+  packages = ['TextureAtlas'],
+  version = '0.2.9',
+  license='MIT',
+  description = 'Create a texture dynamic texture atlas you can constantly change.',
+  author = 'Shaun Kulesa',
+  author_email = 'shaunkulesa@gmail.com',
+  url = 'https://github.com/ShaunKulesa/Texture-Atlas',
+  download_url = 'https://github.com/ShaunKulesa/Texture-Atlas',
+  keywords = ['Texture Atlas'],
+  install_requires=[
+          'pillow',
+          'rectpack',
+      ],
+  long_description = readme,
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.5',
+  ],
+)
