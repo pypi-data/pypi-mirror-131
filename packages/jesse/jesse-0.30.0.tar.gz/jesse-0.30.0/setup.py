6c53da507e9d6f90388b6e07b7397c905b98e7f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:57 +0000

    Add package jesse/0.30.0/jesse-0.30.0.tar.gz

---
+++ packages/jesse/jesse-0.30.0.tar.gz/jesse-0.30.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+# also change in version.py
+VERSION = '0.30.0'
+DESCRIPTION = "A trading framework for cryptocurrencies"
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='jesse',
+    version=VERSION,
+    author="Saleh Mir",
+    author_email="saleh@jesse.trade",
+    packages=find_packages(),
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://jesse.trade",
+    project_urls={
+        'Documentation': 'https://docs.jesse.trade',
+        'Say Thanks!': 'https://jesse.trade/discord',
+        'Source': 'https://github.com/jesse-ai/jesse',
+        'Tracker': 'https://github.com/jesse-ai/jesse/issues',
+    },
+    install_requires=requirements,
+    entry_points='''
+        [console_scripts]
+        jesse=jesse.__init__:cli
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    include_package_data=True,
+)
