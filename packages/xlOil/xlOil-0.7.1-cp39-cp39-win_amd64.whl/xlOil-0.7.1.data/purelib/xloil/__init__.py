1ba73091b00b57ffa1162995ea21700a4309bff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:16 +0000

    Add package xlOil/0.7.1/xlOil-0.7.1-cp39-cp39-win_amd64.whl

---
+++ packages/xlOil/xlOil-0.7.1-cp39-cp39-win_amd64.whl/xlOil-0.7.1.data/purelib/xloil/__init__.py
@@ -0,0 +1,37 @@
+
+from ._common import *
+
+from .shadow_core import *
+
+from .register import (
+    Arg, 
+    func,
+    scan_module,
+    register_functions,
+    Caller,
+    linked_workbook
+    )
+
+from .com import (
+    use_com_lib,
+    app,
+    EventsPaused
+    )
+
+from .rtd import RtdSimplePublisher
+
+from .type_converters import (
+    ExcelValue,
+    Cache, 
+    SingleValue,
+    AllowRange,
+    Array,
+    converter,
+    returner
+    )
+
+from .excelgui import (
+    CustomTaskPane,
+    find_task_pane,
+    create_task_pane
+    )
