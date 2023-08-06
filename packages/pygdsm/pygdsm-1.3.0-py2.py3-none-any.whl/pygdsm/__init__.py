b7b18b98ea6c31fb8c39adddf289ad3a303674d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:36 +0000

    Add package pygdsm/1.3.0/pygdsm-1.3.0-py2.py3-none-any.whl

---
+++ packages/pygdsm/pygdsm-1.3.0-py2.py3-none-any.whl/pygdsm/__init__.py
@@ -0,0 +1,12 @@
+
+from .pygsm import GlobalSkyModel
+from .pygsm import GSMObserver
+
+from .pygsm2016 import GlobalSkyModel2016
+from .pygsm2016 import GSMObserver2016
+
+from .lfsm import LowFrequencySkyModel
+from .lfsm import LFSMObserver
+
+from .haslam import HaslamSkyModel
+from .haslam import HaslamObserver
