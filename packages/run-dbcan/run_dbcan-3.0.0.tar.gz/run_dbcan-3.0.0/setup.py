e8f70e43fa7ceba7b175fc1f538ce576bcab70b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:10 +0000

    Add package run-dbcan/3.0.0/run_dbcan-3.0.0.tar.gz

---
+++ packages/run-dbcan/run_dbcan-3.0.0.tar.gz/run_dbcan-3.0.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# distutils setup script for dbcan package
+# also installs the bundled Hotpep scripts and data files
+
+from glob import glob
+from os import listdir
+from os.path import isfile
+from setuptools import setup, find_packages
+
+long_description = """This is the standalone version of dbCAN annotation tool for automated CAZyme annotation (known as run_dbCAN.py), written by Tanner Yohe and Le Huang.
+"""
+
+setup(name='run_dbcan',
+      version="3.0.0",
+    #   use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
+      description='Standalone version of dbCAN annotation tool for automated CAZyme annotation',
+      long_description=long_description,
+      author='Tanner Yohe, Le Huang and Qiwei Ge',
+      author_email='lehuang@unc.edu',
+      url='https://github.com/linnabrown/run_dbcan',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+      ],
+      packages=find_packages(),
+    #   include_package_data = True,
+      include_package_data=False,
+
+      scripts=[
+          'CGCFinder.py',
+          'hmmscan-parser.py',
+          'run_dbcan.py',
+          'simplify_cgc.py',
+          #'eCAMI/clustering.py',
+          #'eCAMI/prediction.py',
+          #'eCAMI/eCAMI_data.py'
+      ],
+      license='GPLv3',
+      install_requires=[
+          'natsort',
+          'setuptools',
+          'scipy',
+          'psutil',
+          'numpy'
+      ],
+      python_requires='>=3',
+     )
