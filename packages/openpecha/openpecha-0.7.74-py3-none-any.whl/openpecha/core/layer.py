e356762f3bebc544f419274dff7d6a82a60c403a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:33 +0000

    Add package openpecha/0.7.74/openpecha-0.7.74-py3-none-any.whl

---
+++ packages/openpecha/openpecha-0.7.74-py3-none-any.whl/openpecha/core/layer.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from enum import Enum
+from typing import Dict, Optional
+
+from pydantic import BaseModel, validator
+
+from .ids import get_pecha_id, get_uuid
+
+
+class LayerEnum(Enum):
+    index = "index"
+
+    book_title = "BookTitle"
+    sub_title = "SubTitle"
+    book_number = "BookNumber"
+    poti_title = "PotiTitle"
+    author = "Author"
+    chapter = "Chapter"
+
+    topic = "Text"
+    sub_topic = "SubText"
+
+    pagination = "Pagination"
+    citation = "Citation"
+    correction = "Correction"
+    error_candidate = "ErrorCandidate"
+    peydurma = "Peydurma"
+    sabche = "Sabche"
+    tsawa = "Tsawa"
+    yigchung = "Yigchung"
+    archaic = "Archaic"
+    durchen = "Durchen"
+    footnote = "Footnote"
+    segment = "Segment"
+
+
+class InitialCreationEnum(Enum):
+    ocr = "ocr"
+    ebook = "ebook"
+    input = "input"
+
+
+class Layer(BaseModel):
+    id: str = None
+    annotation_type: LayerEnum
+    revision: str = "00001"
+    annotations: Dict = {}
+
+    @validator("id", pre=True, always=True)
+    def set_id(cls, v):
+        return v or get_uuid()
+
+    @validator("revision")
+    def revision_must_int_parsible(cls, v):
+        assert v.isdigit(), "must integer parsible like `00002`"
+        return v
+
+    def bump_revision(self):
+        self.revision = f"{int(self.revision)+1:05}"
+
+    def reset(self):
+        self.revision = "00001"
+        self.annotations = {}
+
+
+class PechaMetaData(BaseModel):
+    id: str = None
+    initial_creation_type: InitialCreationEnum
+    source_metadata: Optional[Dict] = {}
+    created_at: datetime = None
+    last_modified_at: datetime = None
+
+    @validator("id", pre=True, always=True)
+    def set_id(cls, v):
+        return v or get_pecha_id()
