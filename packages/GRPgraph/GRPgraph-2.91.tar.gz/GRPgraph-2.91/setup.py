e04e748f027ffb17a0fd3342cea368add4570bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:33 +0000

    Add package GRPgraph/2.91/GRPgraph-2.91.tar.gz

---
+++ packages/GRPgraph/GRPgraph-2.91.tar.gz/GRPgraph-2.91/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup  
+setup(name='GRPgraph',
+       version='2.91',
+      description='small and compact graphick distributions',
+      packages=['GRPgraph'],       
+      author_email='pvana621@gmail.com',       
+      zip_safe=False)
