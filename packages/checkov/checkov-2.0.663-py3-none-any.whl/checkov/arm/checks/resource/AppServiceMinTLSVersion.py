d682e3dd8177aa32da303c71b4c414fb25d7bfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:38 +0000

    Add package checkov/2.0.663/checkov-2.0.663-py3-none-any.whl

---
+++ packages/checkov/checkov-2.0.663-py3-none-any.whl/checkov/arm/checks/resource/AppServiceMinTLSVersion.py
@@ -0,0 +1,26 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.arm.base_resource_check import BaseResourceCheck
+
+# https://docs.microsoft.com/en-us/azure/templates/microsoft.web/2019-08-01/sites
+
+
+class AppServiceMinTLSVersion(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure web app is using the latest version of TLS encryption"
+        id = "CKV_AZURE_15"
+        supported_resources = ["Microsoft.Web/sites"]
+        categories = [CheckCategories.NETWORKING]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        properties = conf.get("properties")
+        if properties:
+            site_config = properties.get("siteConfig")
+            if site_config:
+                min_tls_version = site_config.get("minTlsVersion")
+                if min_tls_version == '1.2':
+                    return CheckResult.PASSED
+        return CheckResult.FAILED
+
+
+check = AppServiceMinTLSVersion()
---
+++ packages/checkov/checkov-2.0.663-py3-none-any.whl/checkov/kubernetes/parser/k8_json.py
@@ -0,0 +1,46 @@
+import yaml
+from yaml.loader import SafeLoader
+
+def loads(filename):
+    """
+    Load the given YAML string
+    """
+    with open(filename, 'r') as fp:
+        content = fp.read()
+        content = "[" + content + "]"
+        content = content.replace('}{', '},{')
+        content = content.replace('}\n{', '},\n{')
+
+        template_temp = list(yaml.load_all(content, Loader=SafeLineLoader))
+
+    # Convert an empty file to an empty dict
+    if template_temp is None:
+        template = {}
+    else:
+        template = template_temp[0]
+
+    return template
+
+
+def load(filename):
+    """
+    Load the given JSON file
+    """
+    content = ''
+
+    with open(filename) as fp:
+        file_lines = [(ind + 1, line) for (ind, line) in enumerate(fp.readlines())]
+
+    template = loads(filename)
+
+    return (template, file_lines)
+
+
+class SafeLineLoader(SafeLoader):
+    def construct_mapping(self, node, deep=False):
+        mapping = super(SafeLineLoader, self).construct_mapping(node, deep=deep)
+        # Add 1 so line numbering starts at 1
+        # mapping['__line__'] = node.start_mark.line + 1
+        mapping['__startline__'] = node.start_mark.line + 1
+        mapping['__endline__'] = node.end_mark.line + 1
+        return mapping
