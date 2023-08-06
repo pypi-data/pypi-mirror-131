8339f56b732f8930fc51eddd062fe04c9df20c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:33 +0000

    Add package azurecr-browser/1.0.1/azurecr_browser-1.0.1-py3-none-any.whl

---
+++ packages/azurecr-browser/azurecr_browser-1.0.1-py3-none-any.whl/azurecr_browser/widgets/__init__.py
@@ -0,0 +1,18 @@
+from .flash import FlashWidget, ShowFlashNotification
+from .header import HeaderWidget
+from .help import HelpWidget
+from .repos import RepositoriesWidget
+from .search import SearchWidget
+from .tag_properties import TagPropertiesWidget
+from .tags import TagsWidget
+
+__all__ = (
+    "RepositoriesWidget",
+    "ShowFlashNotification",
+    "SearchWidget",
+    "FlashWidget",
+    "HeaderWidget",
+    "TagsWidget",
+    "TagPropertiesWidget",
+    "HelpWidget",
+)
