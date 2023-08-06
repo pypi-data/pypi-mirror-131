156de5a495f0a9e521baaef168838f3cc83d2a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:16 +0000

    Add package flagsmith-flag-engine/1.5.0/flagsmith-flag-engine-1.5.0.tar.gz

---
+++ packages/flagsmith-flag-engine/flagsmith-flag-engine-1.5.0.tar.gz/flagsmith-flag-engine-1.5.0/flag_engine/features/schemas.py
@@ -0,0 +1,62 @@
+import uuid
+
+from marshmallow import EXCLUDE, Schema, fields, post_load, validate
+
+from flag_engine.features.models import (
+    FeatureModel,
+    FeatureStateModel,
+    MultivariateFeatureOptionModel,
+    MultivariateFeatureStateValueModel,
+)
+from flag_engine.utils.marshmallow.schemas import LoadToModelSchema
+
+
+class FeatureSchema(LoadToModelSchema):
+    id = fields.Int()
+    name = fields.Str()
+    type = fields.Str()
+
+    class Meta:
+        model_class = FeatureModel
+
+
+class MultivariateFeatureOptionSchema(LoadToModelSchema):
+    value = fields.Field(allow_none=True)
+
+    class Meta:
+        model_class = MultivariateFeatureOptionModel
+
+
+class MultivariateFeatureStateValueSchema(LoadToModelSchema):
+    id = fields.Int()
+    multivariate_feature_option = fields.Nested(MultivariateFeatureOptionSchema)
+    percentage_allocation = fields.Decimal(validate=[validate.Range(0, 100)])
+
+    class Meta:
+        model_class = MultivariateFeatureStateValueModel
+
+
+class BaseFeatureStateSchema(Schema):
+    featurestate_uuid = fields.Str(dump_default=uuid.uuid4)
+    feature = fields.Nested(FeatureSchema)
+    enabled = fields.Bool()
+
+    class Meta:
+        unknown = EXCLUDE
+
+
+class FeatureStateSchema(BaseFeatureStateSchema):
+    feature_state_value = fields.Field(allow_none=True, required=False)
+    multivariate_feature_state_values = fields.List(
+        fields.Nested(MultivariateFeatureStateValueSchema)
+    )
+
+    class Meta:
+        unknown = EXCLUDE
+
+    @post_load()
+    def make_feature_state(self, data, **kwargs) -> FeatureStateModel:
+        value = data.pop("feature_state_value", None)
+        feature_state = FeatureStateModel(**data)
+        feature_state.set_value(value)
+        return feature_state
