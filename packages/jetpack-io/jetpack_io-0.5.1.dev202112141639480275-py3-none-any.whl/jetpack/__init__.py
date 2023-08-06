28714024482dae1224c06582ee1856adac6f47bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:49 +0000

    Add package jetpack-io/0.5.1.dev202112141639480275/jetpack_io-0.5.1.dev202112141639480275-py3-none-any.whl

---
+++ packages/jetpack-io/jetpack_io-0.5.1.dev202112141639480275-py3-none-any.whl/jetpack/__init__.py
@@ -0,0 +1,12 @@
+# __version__ is placeholder
+# It gets set in the build/publish process (publish_with_credentials.sh)
+__version__ = "0.5.1-dev202112141639480275"
+
+from jetpack._remote.interface import remote
+from jetpack._task.interface import function, schedule
+from jetpack.cmd import root
+from jetpack.redis import redis
+
+
+def run() -> None:
+    root.cli()
