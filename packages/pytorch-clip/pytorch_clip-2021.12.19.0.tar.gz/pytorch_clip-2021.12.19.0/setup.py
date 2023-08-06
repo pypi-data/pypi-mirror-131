7c09d3adbe4d5d058346990ec522c0f6486cb7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:03 +0000

    Add package pytorch-clip/2021.12.19.0/pytorch_clip-2021.12.19.0.tar.gz

---
+++ packages/pytorch-clip/pytorch_clip-2021.12.19.0.tar.gz/pytorch_clip-2021.12.19.0/setup.py
@@ -0,0 +1,63 @@
+"""
+Copyright 2021 by Sergei Belousov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from setuptools import setup, find_packages
+
+readme = open('README.md').read()
+
+VERSION = '2021.12.19.0'
+
+requirements = [
+    'wheel',
+    'Cython',
+    'cython',
+    'omegaconf',
+    'torch',
+    'transformers',
+    'huggingface_hub',
+    'youtokentome',
+    'googletrans',
+    'numpy'
+]
+
+setup(
+    # Metadata
+    name='pytorch_clip',
+    version=VERSION,
+    author='Sergei Belousov aka BeS',
+    author_email='sergei.o.belousov@gmail.com',
+    description='Pytorch API to work with CLIP models.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+
+    # Package info
+    packages=find_packages(exclude=('*test*',)),
+
+    #
+    zip_safe=True,
+    install_requires=requirements,
+
+    # Classifiers
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+
+
+    # install .json configs
+    package_data={
+        "pytorch_clip": ["configs/*.yml"]
+    },
+    include_package_data=True
+)
