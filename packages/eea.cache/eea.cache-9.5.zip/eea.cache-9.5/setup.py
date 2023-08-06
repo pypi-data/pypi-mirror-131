76156e57e476ec38f99c577fc7560d49aedfa904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:22 +0000

    Add package eea.cache/9.5/eea.cache-9.5.zip

---
+++ packages/eea.cache/eea.cache-9.5.zip/eea.cache-9.5/setup.py
@@ -0,0 +1,54 @@
+""" EEA Cache Installer
+"""
+import os
+from setuptools import setup, find_packages
+
+NAME = 'eea.cache'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
+
+setup(name=NAME,
+      version=VERSION,
+      description="Tools and config for memcache related caching",
+      long_description_content_type="text/x-rst",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+      ],
+      keywords='EEA cache memcache Add-ons Plone Zope',
+      author='European Environment Agency: IDM2 A-Team',
+      author_email="eea-edw-a-team-alerts@googlegroups.com",
+      download_url="https://pypi.python.org/pypi/eea.cache",
+      url='https://github.com/collective/eea.cache',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eea'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'python-memcached',
+        'plone.memoize',
+        'plone.uuid',
+      ],
+      extras_require={
+        'test': ['plone.app.testing']
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """
+      )
