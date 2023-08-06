1e49f966c3152663152ee3fd4258097bc187c089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:34 +0000

    Add package daipe-core/1.3.2.dev1/daipe-core-1.3.2.dev1.tar.gz

---
+++ packages/daipe-core/daipe-core-1.3.2.dev1.tar.gz/daipe-core-1.3.2.dev1/src/daipecore/function/input_decorator_function.py
@@ -0,0 +1,16 @@
+from daipecore.decorator.ContainerManager import ContainerManager
+from daipecore.function import arguments_transformer
+
+
+def input_decorator_function(func):
+    def wrapper(*args, **kwargs):
+        def executor():
+            container = ContainerManager.get_container()
+
+            transformed_args = tuple(arguments_transformer.transform(arg, container) for arg in args)
+
+            return func(*transformed_args, **kwargs)
+
+        return executor
+
+    return wrapper
