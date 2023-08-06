a3110cc87c00752ca82d0900c82682308ffb5edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:45 +0000

    Add package dl2050utils/1.0.107/dl2050utils-1.0.107.tar.gz

---
+++ packages/dl2050utils/dl2050utils-1.0.107.tar.gz/dl2050utils-1.0.107/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# setup.py
+# Create source distribution with: python setup.py sdist
+#
+from distutils.core import setup
+from dl2050utils.__config__ import name, package, description, author, author_email, keywords
+from dl2050utils.__config__ import pump_version, get_camel, save_version
+from dl2050utils.__version__ import version
+from dl2050utils.fs import sh_run
+
+sh_run('rm -rf dist/*.gz')
+version = pump_version(version)
+version_camel = get_camel(version)
+print(f'setup for package {package} version {version} {version_camel}')
+
+url = f'https://github.com/jn2050/{name}'
+download_url = f'https://github.com/jn2050/{name}/archive/v_{version_camel}.tar.gz'
+classifiers=[
+      'Development Status :: 4 - Beta',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.7',
+]
+
+setup(name=package,
+      packages=[package],
+      version=version,
+      license='MIT',
+      description=description,
+      author=author,
+      author_email=author_email,
+      keywords=keywords,
+      url=url,
+      download_url=download_url,
+      classifiers=classifiers
+)
+
+cmd = f'gh release create -t "{version_camel}" --notes "Update" dist/{name}-{version}.tar.gz'
+print(cmd)
+res, sout = sh_run(cmd)
+print(sout)
+
+cmd = f'twine upload dist/* --repository {package}'
+print(cmd)
+res, sout = sh_run(cmd)
+print(sout)
+
+save_version(version)
