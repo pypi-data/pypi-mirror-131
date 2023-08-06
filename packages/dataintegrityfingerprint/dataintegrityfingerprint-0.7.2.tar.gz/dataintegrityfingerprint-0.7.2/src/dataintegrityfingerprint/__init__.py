74aa340b59a72673d03360e0fe147cc8f3f51725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:51 +0000

    Add package dataintegrityfingerprint/0.7.2/dataintegrityfingerprint-0.7.2.tar.gz

---
+++ packages/dataintegrityfingerprint/dataintegrityfingerprint-0.7.2.tar.gz/dataintegrityfingerprint-0.7.2/src/dataintegrityfingerprint/__init__.py
@@ -0,0 +1,15 @@
+"""Data Integrity Fingerprint (DIF).
+
+Reference Python implementation.
+
+For specification see https://github.com/expyriment/DIF.
+
+"""
+
+__author__ = 'Oliver Lindemann <oliver@expyriment.org>, ' \
+             'Florian Krause <florian@expyriment.org>'
+
+__version__ = '0.7.2'
+
+
+from .dif import DataIntegrityFingerprint
