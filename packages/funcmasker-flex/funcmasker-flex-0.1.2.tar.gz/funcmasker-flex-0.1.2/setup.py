6052e3ae2c9e09ee1031ad8dc08671df71130a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:36 +0000

    Add package funcmasker-flex/0.1.2/funcmasker-flex-0.1.2.tar.gz

---
+++ packages/funcmasker-flex/funcmasker-flex-0.1.2.tar.gz/funcmasker-flex-0.1.2/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['funcmasker_flex', 'funcmasker_flex.workflow.scripts']
+
+package_data = \
+{'': ['*'],
+ 'funcmasker_flex': ['config/*',
+                     'resources/*',
+                     'workflow/*',
+                     'workflow/rules/*']}
+
+install_requires = \
+['batchgenerators==0.21',
+ 'matplotlib>=3.5.1,<4.0.0',
+ 'nnunet-inference-on-cpu-and-gpu==1.6.6',
+ 'snakebids==0.4.0',
+ 'snakemake>=6.12.3,<7.0.0']
+
+entry_points = \
+{'console_scripts': ['funcmasker-flex = funcmasker_flex.run:main']}
+
+setup_kwargs = {
+    'name': 'funcmasker-flex',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Ali Khan',
+    'author_email': 'alik@robarts.ca',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<3.10',
+}
+
+
+setup(**setup_kwargs)
