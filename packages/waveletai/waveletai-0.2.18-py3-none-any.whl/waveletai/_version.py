a738021828b8aff7bfbefe3ae83fcfd979e45978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:27 +0000

    Add package waveletai/0.2.18/waveletai-0.2.18-py3-none-any.whl

---
+++ packages/waveletai/waveletai-0.2.18-py3-none-any.whl/waveletai/_version.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/4/6 6:10
+@Author  : WaveletAI-Product-Team Janus
+@license : (C) Copyright 2019-2022, Visionet(Tianjin)Information Technology Co.,Ltd.
+@Site    : plus.xiaobodata.com
+@File    : _version.py
+@Desc    : 
+"""
+
+VERSION = "0.2.18"
+
+
+def get_versions():
+    return VERSION
+
+
+if __name__ == '__main__':
+    print(get_versions())
