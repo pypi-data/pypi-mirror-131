e6498670e5de0337965db7a67ea74906a46b8c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:53 +0000

    Add package argostranslate/1.5.4/argostranslate-1.5.4.tar.gz

---
+++ packages/argostranslate/argostranslate-1.5.4.tar.gz/argostranslate-1.5.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+with open("README.md") as f:
+    long_description = f.read()
+
+# py2app
+APP = ["bin/argos-translate-gui"]
+DATA_FILES = []
+OPTIONS = {"packages": ["sentencepiece"], "iconfile": "argostranslate/img/icon.icns"}
+
+setup(
+    name="argostranslate",
+    version="1.5.4",
+    description="Offline neural machine translation library and GUI.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Argos Open Technologies, LLC",
+    author_email="admin@argosopentech.com",
+    url="https://www.argosopentech.com",
+    packages=find_packages(),
+    install_requires=required_packages,
+    include_package_data=True,
+    scripts=["bin/argos-translate", "bin/argos-translate-gui", "bin/argospm"],
+    app=APP,
+    data_files=DATA_FILES,
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
+)
