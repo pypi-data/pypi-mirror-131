ae8672fb530676f4c7b55bad47420d981cec0ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:45 +0000

    Add package RV8803/0.0.4/RV8803-0.0.4.tar.gz

---
+++ packages/RV8803/RV8803-0.0.4.tar.gz/RV8803-0.0.4/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="RV8803",
+    version="0.0.4",
+    author="Tran Anh Nguyen",
+    author_email="nguyenta@umich.com",
+    description="A small package for using the RV8803 real time clock via I2C",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
