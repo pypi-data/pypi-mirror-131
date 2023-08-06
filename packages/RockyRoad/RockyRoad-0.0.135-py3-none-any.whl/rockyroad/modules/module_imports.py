884cb1dc56d83ba00d96f57c65237a30f3d7676b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:39 +0000

    Add package RockyRoad/0.0.135/RockyRoad-0.0.135-py3-none-any.whl

---
+++ packages/RockyRoad/RockyRoad-0.0.135-py3-none-any.whl/rockyroad/modules/module_imports.py
@@ -0,0 +1,37 @@
+from uplink import (
+    Consumer,
+    get,
+    post,
+    patch,
+    delete,
+    returns,
+    headers,
+    Body,
+    json,
+    Query,
+    Path,
+    multipart,
+    Part,
+    PartMap,
+)
+import os
+from uuid import UUID
+
+
+def get_key():
+
+    try:
+        key = os.environ["OCP_APIM_SUBSCRIPTION_KEY"]
+    except KeyError as e:
+        print(
+            f"""ERROR: Define the environment variable {e} with your subscription key.  For example:
+
+        export OCP_APIM_SUBSCRIPTION_KEY="INSERT_YOUR_SUBSCRIPTION_KEY"
+
+        """
+        )
+        key = None
+    return key
+
+
+key = get_key()
