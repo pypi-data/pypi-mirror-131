eb674bb7e69b517d56778d71285049feaf85cdf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:28 +0000

    Add package django-lb-workflow/1.0.2/django-lb-workflow-1.0.2.tar.gz

---
+++ packages/django-lb-workflow/django-lb-workflow-1.0.2.tar.gz/django-lb-workflow-1.0.2/lbworkflow/__init__.py
@@ -0,0 +1,5 @@
+VERSION = (1, 0, 2, "alpha", 0)
+
+__version__ = "1.0.2"
+
+default_app_config = "lbworkflow.apps.LBWorkflowConfig"
---
+++ packages/django-lb-workflow/django-lb-workflow-1.0.2.tar.gz/django-lb-workflow-1.0.2/lbworkflow/tests/test_flowgen.py
@@ -0,0 +1,15 @@
+from lbworkflow.flowgen import FlowAppGenerator, clean_generated_files
+from lbworkflow.tests.issue.models import Issue
+from lbworkflow.tests.purchase.models import Item, Purchase
+
+from .test_base import BaseTests
+
+
+class ViewTests(BaseTests):
+    def test_gen_no_item_list(self):
+        FlowAppGenerator().gen(Issue)
+        clean_generated_files(Issue)
+
+    def test_gen_with_item_list(self):
+        FlowAppGenerator().gen(Purchase, [Item])
+        clean_generated_files(Purchase)
