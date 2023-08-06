bb0ea924abe2f0d0f5c26bc6d47ed554aa336fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:20 +0000

    Add package morecontext/0.6.0/morecontext-0.6.0.tar.gz

---
+++ packages/morecontext/morecontext-0.6.0.tar.gz/morecontext-0.6.0/test/test_openclosable.py
@@ -0,0 +1,28 @@
+from typing import List
+from morecontext import OpenClosable
+
+
+class OpenCloser(OpenClosable):
+    def __init__(self) -> None:
+        self.calls: List[str] = []
+
+    def open(self) -> None:
+        self.calls.append("open")
+
+    def close(self) -> None:
+        self.calls.append("close")
+
+
+def test_openclosable() -> None:
+    oc = OpenCloser()
+    assert oc.calls == []
+    with oc as oc2:
+        assert oc is oc2
+        assert oc.calls == ["open"]
+        with oc:
+            assert oc.calls == ["open"]
+            with oc:
+                assert oc.calls == ["open"]
+            assert oc.calls == ["open"]
+        assert oc.calls == ["open"]
+    assert oc.calls == ["open", "close"]
