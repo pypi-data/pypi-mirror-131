bc7a04fc83963cad1cece257331deb43d7f376dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:53 +0000

    Add package pyagxrobots/0.8.4/pyagxrobots-0.8.4-py3-none-any.whl

---
+++ packages/pyagxrobots/pyagxrobots-0.8.4-py3-none-any.whl/pyagxrobots/__init__.py
@@ -0,0 +1,18 @@
+from pyagxrobots.agxrobots import UGV
+from pyagxrobots.agxrobots import CanMsgsGet
+from pyagxrobots.agxrobots import DeviceCan
+from pyagxrobots import UGVConfigMsg
+from pyagxrobots import pysdkugv
+from pyagxrobots import agxbase
+__all__ = [
+    'UGV',
+    'CanMsgsGet',
+    'DeviceCan',
+    'UGVConfigMsg',
+    'pysdkugv',
+    'agxbase',
+]
+
+
+
+
