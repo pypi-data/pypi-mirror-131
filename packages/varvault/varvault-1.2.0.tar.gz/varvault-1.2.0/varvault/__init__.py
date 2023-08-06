d5b77373b55994012e41dbe42b79cb86367b6782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:32 +0000

    Add package varvault/1.2.0/varvault-1.2.0.tar.gz

---
+++ packages/varvault/varvault-1.2.0.tar.gz/varvault-1.2.0/varvault/__init__.py
@@ -0,0 +1,24 @@
+from .keyring import Key, Keyring, validator
+
+from .minivault import MiniVault
+
+from .utils import *
+
+from .vault import VarVault
+
+from .vaultflags import VaultFlags
+
+from .vaultfactory import create_vault
+from .vaultfactory import from_vault
+
+from .vaultstructs import *
+
+import distutils.sysconfig
+
+def clear_logs():
+    import os
+    import glob
+
+    files = glob.glob("/tmp/varvault-logs/*.log")
+    for f in files:
+        os.remove(f)
