e9eb6f666d6dc50e70c499902b2c364880e4cdc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:14 +0000

    Add package manim-kunyuan/0.32/manim_kunyuan-0.32-py3-none-any.whl

---
+++ packages/manim-kunyuan/manim_kunyuan-0.32-py3-none-any.whl/manimlib/utils/sounds.py
@@ -0,0 +1,10 @@
+from ..utils.file_ops import find_file
+from ..utils.directories import get_sound_dir
+
+
+def get_full_sound_file_path(sound_file_name):
+    return find_file(
+        sound_file_name,
+        directories=[get_sound_dir()],
+        extensions=[".wav", ".mp3", ""]
+    )
