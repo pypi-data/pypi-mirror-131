158c01ece2f74683f58e0faf512e45f525f4be9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:54 +0000

    Add package sherpa-streamlit/0.2.16/sherpa-streamlit-0.2.16.tar.gz

---
+++ packages/sherpa-streamlit/sherpa-streamlit-0.2.16.tar.gz/sherpa-streamlit-0.2.16/examples/streamlit_app.py
@@ -0,0 +1,7 @@
+from sherpa_streamlit import visualize
+
+default_text = """This is a simple sample text
+"""
+visualize(default_text,
+          sidebar_title="KAIRNTECH Sherpa",
+          sidebar_description="Customizable Sherpa demonstration")
