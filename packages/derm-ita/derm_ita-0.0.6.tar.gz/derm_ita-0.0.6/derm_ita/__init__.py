011ab2efeeb4ca093f4a29e8876beb523813d71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package derm-ita/0.0.6/derm_ita-0.0.6.tar.gz

---
+++ packages/derm-ita/derm_ita-0.0.6.tar.gz/derm_ita-0.0.6/derm_ita/__init__.py
@@ -0,0 +1,4 @@
+from derm_ita.derm_ita import get_ita, get_cropped_center_ita, get_random_patches_ita, get_structured_patches_ita
+from derm_ita.skin_tone_classification import get_del_bino_type, get_groh_ita_category, get_kinyanjui_type,\
+    get_kinyanjui_groh_type,get_fitzpatrick_type
+
