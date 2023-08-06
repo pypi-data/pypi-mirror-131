b477674034b19873c0ce801b595614625666d13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:11 +0000

    Add package libdev/0.10/libdev-0.10-py3-none-any.whl

---
+++ packages/libdev/libdev-0.10-py3-none-any.whl/libdev/aws.py
@@ -0,0 +1,54 @@
+"""
+Functionality of Amazon Web Services
+"""
+
+import boto3
+from botocore.exceptions import ClientError
+from .cfg import cfg
+from .gen import generate
+
+
+LINK = (
+    f"https://{cfg('amazon.bucket')}.s3."
+    f"{cfg('amazon.region')}.amazonaws.com/"
+)
+
+
+s3 = boto3.resource(
+    's3',
+    region_name=cfg('amazon.region'),
+    aws_access_key_id=cfg('amazon.key'),
+    aws_secret_access_key=cfg('amazon.secret'),
+)
+s3client = s3.meta.client
+
+
+def upload_file(
+    file,
+    bucket=cfg('amazon.bucket'),
+    directory=cfg('amazon.directory'),
+):
+    """ Upload file """
+
+    file_type = file.split('.')[-1]
+    name = f"{directory}/{generate()}.{file_type}"
+
+    s3client.upload_file(
+        file, bucket, name,
+        ExtraArgs={'ACL': 'public-read'},
+    )
+
+    return LINK + name
+
+def get_policy(bucket=cfg('amazon.bucket')):
+    """ Get bucket policy """
+    return s3client.get_bucket_policy(Bucket=bucket)
+
+
+__all__ = (
+    's3',
+    's3client',
+    'ClientError',
+    'upload_file',
+    'get_policy',
+)
