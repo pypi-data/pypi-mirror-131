9b1fb7cfd5d8456d099f8c9933ceab416b02f89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:59 +0000

    Add package circuit/0.4.0/circuit-0.4.0.tar.gz

---
+++ packages/circuit/circuit-0.4.0.tar.gz/circuit-0.4.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+# The lines below can be parsed by `docs/conf.py`.
+name = "circuit"
+version = "0.4.0"
+
+setup(
+    name=name,
+    version=version,
+    packages=[name,],
+    install_requires=[
+        "parts~=1.2",
+        "logical~=0.3"
+    ],
+    license="MIT",
+    url="https://github.com/reity/circuit",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Minimal native Python library for building " +\
+                "and working with logical circuits.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
