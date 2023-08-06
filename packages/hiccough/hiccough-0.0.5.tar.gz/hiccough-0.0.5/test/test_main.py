b94db9a2700b445b7625bddbcfec95b754dd0726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:53 +0000

    Add package hiccough/0.0.5/hiccough-0.0.5.tar.gz

---
+++ packages/hiccough/hiccough-0.0.5.tar.gz/hiccough-0.0.5/test/test_main.py
@@ -0,0 +1,40 @@
+from hiccough import html
+
+
+def test_simple_div():
+    assert html(["div"]) == "<div></div>"
+
+
+def test_class_shorthand():
+    assert html(["div.class"]) == '<div class="class"></div>'
+
+
+def test_nesting_and_class():
+    assert (
+        html(
+            [
+                "div.container>div.section>div.box",
+                ["form", ["input", None, {"type": "checkbox"}]],
+            ]
+        )
+        == '<div class="container"><div class="section"><div class="box"><form><input type="checkbox"></input></form></div></div></div>'
+    )
+
+
+def test_multi_class():
+    assert html(["div.a.b"]) == '<div class="a b"></div>'
+
+
+def test_class_merging():
+    assert (
+        html(["div.class-a", None, {"class": "class-b"}])
+        == '<div class="class-b class-a"></div>'
+    )
+
+
+def test_id_shorthand():
+    assert html(["div#name"]) == '<div id="name"></div>'
+
+
+def test_multi_id():
+    assert html(["div#name#blah"]) == '<div id="name"></div>'
