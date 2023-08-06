154cc87631465069bf876a39deaa2823abcfc758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:11 +0000

    Add package ecco-v4-py/1.5.3/ecco_v4_py-1.5.3.tar.gz

---
+++ packages/ecco-v4-py/ecco_v4_py-1.5.3.tar.gz/ecco_v4_py-1.5.3/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+def README():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+  name = 'ecco_v4_py',
+  packages = ['ecco_v4_py'], # this must be the same as the name above
+  version = '1.5.3',
+  description = 'Estimating the Circulation and Climate of the Ocean (ECCO) Version 4 Python Package',
+  author = 'Ian Fenty',
+  author_email = 'ian.fenty@jpl.nasa.gov',
+  url = 'https://github.com/ECCO-GROUP/ECCOv4-py',
+  keywords = ['ecco','climate','mitgcm','estimate','circulation','climate'],
+  include_package_data=True,
+  data_files=[('binary_data',['binary_data/basins.data', 'binary_data/basins.meta'])],
+  python_requires = '>=3.7',
+  install_requires=[
+	'cartopy>=0.18.0',
+        'cmocean',
+	'dask[complete]',
+        'future',
+	'geos',
+	'matplotlib',
+        'netcdf4',
+	'numpy >= 1.17',
+        'pathlib',
+        'proj',
+	'pyresample',
+	'python-dateutil',
+	'xarray',
+	'xmitgcm',
+	'xgcm>=0.5.0'],
+  tests_require=['pytest','coverage'],
+  license='MIT',
+  classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: MIT License',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3.7',
+      'Topic :: Scientific/Engineering :: Physics'
+  ],
+  long_description=README(),
+  long_description_content_type='text/markdown'
+)
