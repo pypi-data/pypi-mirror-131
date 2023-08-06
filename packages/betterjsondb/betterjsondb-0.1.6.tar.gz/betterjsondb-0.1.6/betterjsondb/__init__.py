f901cb2026bcfc2ec8598eed95a6adb46362e8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:00 +0000

    Add package betterjsondb/0.1.6/betterjsondb-0.1.6.tar.gz

---
+++ packages/betterjsondb/betterjsondb-0.1.6.tar.gz/betterjsondb-0.1.6/betterjsondb/__init__.py
@@ -0,0 +1,8 @@
+from .betterjsondb import connect
+
+"""Please, if you think it's a real libruary - change your mind.
+IT'S A COMIC LIBRUARY! MADE BY ME JUST FOR FUN."""
+
+__author__ = "DarkJoij",
+__name__ = "betterjsondb",
+__version__ = "0.1.6"
