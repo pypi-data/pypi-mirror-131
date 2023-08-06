46887f68d8e084ab8874f4f76f1c2530ab2e4efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:17 +0000

    Add package widelinears/0.2/widelinears-0.2.tar.gz

---
+++ packages/widelinears/widelinears-0.2.tar.gz/widelinears-0.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'widelinears',       
+  packages = ['widelinears'],
+  version = '0.2',
+  license='MIT',
+  description = 'Parallel pytorch Neural Networks',   
+  author = 'Joao Figueira',                  
+  author_email = 'joaoperfig@gmail.com',   
+  url = 'https://github.com/joaoperfig/WideLinears', 
+  download_url = 'https://github.com/joaoperfig/WideLinears/archive/refs/tags/v_0.2.tar.gz',  
+  keywords = ['pytorch', 'parallel', 'linear'], 
+  install_requires=[          
+          'torch',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',   
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',    
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
