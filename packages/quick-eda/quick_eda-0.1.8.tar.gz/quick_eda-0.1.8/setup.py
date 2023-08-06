8f838f1b47613f656086e13bad4be66aa798a840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:53 +0000

    Add package quick-eda/0.1.8/quick_eda-0.1.8.tar.gz

---
+++ packages/quick-eda/quick_eda-0.1.8.tar.gz/quick_eda-0.1.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+ name='quick_eda',
+ version='0.1.8',
+ author='Sven Eschlbeck',
+ author_email='sven.eschlbeck@t-online.de',
+ packages=['quick_eda'],
+ url='http://pypi.python.org/pypi/quick_eda/',
+ license='LICENSE.txt',
+ description='Important dataframe statistics with a single command',
+ long_description_content_type='text/markdown',
+ long_description=open('README.md').read(),
+ install_requires=[
+     "pandas",
+   ],
+)
