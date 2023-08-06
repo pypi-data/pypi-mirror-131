8c2e70d3f15850a7460a62fd8796432b1a8ec6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:45 +0000

    Add package pyportable-crypto/1.0.0/pyportable-crypto-1.0.0.tar.gz

---
+++ packages/pyportable-crypto/pyportable-crypto-1.0.0.tar.gz/pyportable-crypto-1.0.0/pyportable_crypto/__init__.py
@@ -0,0 +1,18 @@
+"""
+References:
+    https://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto
+        -aes-256
+    https://blog.csdn.net/Flower941220/article/details/101166912
+    
+Warnings:
+    docs/cythonize-known-issues.zh.md
+"""
+from . import cipher_gen
+from . import keygen
+from .compiler import Compiler
+from .decrypt import decrypt_data
+from .decrypt import decrypt_file
+from .encrypt import encrypt_data
+from .encrypt import encrypt_file
+
+__version__ = '1.0.0'
