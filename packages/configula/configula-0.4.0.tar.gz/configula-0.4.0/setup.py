47504d16d4e343a3f6cf9ac013d031579b4d5e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:16 +0000

    Add package configula/0.4.0/configula-0.4.0.tar.gz

---
+++ packages/configula/configula-0.4.0.tar.gz/configula-0.4.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['configula']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['tomlkit>=0.7.2,<0.8.0']
+
+setup_kwargs = {
+    'name': 'configula',
+    'version': '0.4.0',
+    'description': 'Merges configuration from toml file and environment variables',
+    'long_description': None,
+    'author': 'Eugen Ciur',
+    'author_email': 'eugen@papermerge.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
