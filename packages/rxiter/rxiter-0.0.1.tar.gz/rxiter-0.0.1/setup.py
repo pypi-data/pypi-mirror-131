01c239e414219b43fe7f5c1d3721d58e4df49445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:36 +0000

    Add package rxiter/0.0.1/rxiter-0.0.1.tar.gz

---
+++ packages/rxiter/rxiter-0.0.1.tar.gz/rxiter-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name="rxiter",
+    license="MIT",
+    version="0.0.1",
+    packages=["rxiter"],
+    description="Observable operations for async generators",
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python"
+    ]
+)
