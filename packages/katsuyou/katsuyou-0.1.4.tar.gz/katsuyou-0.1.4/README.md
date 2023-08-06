60456ef2904ef9b24fb4646ee0cd6ad01225e982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:49 +0000

    Add package katsuyou/0.1.4/katsuyou-0.1.4.tar.gz

---
+++ packages/katsuyou/katsuyou-0.1.4.tar.gz/katsuyou-0.1.4/README.md
@@ -0,0 +1,13 @@
+# Katsuyou
+
+[![Tests](https://github.com/northy/katsuyou/actions/workflows/main.yaml/badge.svg)](https://github.com/northy/katsuyou/actions/workflows/main.yaml)
+[![PyPI version](https://badge.fury.io/py/katsuyou.svg)](https://badge.fury.io/py/katsuyou)
+[![Downloads](https://pepy.tech/badge/katsuyou)](https://pepy.tech/project/katsuyou)
+
+Python japanese toolkit.
+
+Currently implemented features:
+* 一段動詞 (Ichidan verb) conjugator
+* 五段動詞 (Godan verb) conjugator
+* イ形容詞 (i-adjective) conjugator
+* String matcher using aho-corasick automaton
---
+++ packages/katsuyou/katsuyou-0.1.4.tar.gz/katsuyou-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    README = fh.read()
+
+setup(
+    name="katsuyou",
+    version="0.1.4",
+    description="Python japanese toolkit",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/northy/katsuyou",
+    project_urls={
+        "Bug Tracker": "https://github.com/northy/katsuyou/issues",
+    },
+    author="Alexsandro Thomas",
+    author_email="alexsandrogthomas@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude="tests"),
+    python_requires=">=3.6"
+)
