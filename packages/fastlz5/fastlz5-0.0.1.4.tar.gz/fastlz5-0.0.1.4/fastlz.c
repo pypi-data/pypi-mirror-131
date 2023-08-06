b3d7dd227f3b74aab9f6a02a18b9a337a1b4744d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:11 +0000

    Add package fastlz5/0.0.1.4/fastlz5-0.0.1.4.tar.gz

---
+++ packages/fastlz5/fastlz5-0.0.1.4.tar.gz/fastlz5-0.0.1.4/fastlz.c
@@ -0,0 +1,164 @@
+#include <Python.h>
+#include "fastlz/fastlz.h"
+
+#define PY_MODULE_NAME "fastlz5"
+
+#ifdef _MSC_VER
+typedef unsigned __int32 uint32_t;
+#else
+#include <stdint.h>
+#endif
+
+PyObject* FastlzError;
+
+
+
+static PyObject *
+compress(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    PyObject *result;
+    const char *input;
+    char *output;
+    int level = 0, input_len, output_len;
+
+    static char *arglist[] = {"string", "level", NULL};
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s#|i", arglist, &input,
+                                     &input_len, &level))
+        return NULL;
+
+    if (level == 0) {
+        if (input_len < 65536)
+            level = 1;
+        else
+            level = 2;
+    } else if (level != 1 && level != 2) {
+        PyErr_SetString(PyExc_ValueError, "level must be either 1 or 2");
+        return NULL;
+    }
+
+    output_len = sizeof(uint32_t) + (uint32_t)((input_len * 1.05) + 2);
+    output = (char *) malloc(output_len);
+    if (output == NULL)
+        return PyErr_NoMemory();
+    memcpy(output, &input_len, sizeof(uint32_t));
+
+    output_len = fastlz_compress_level(level, input, input_len,
+                                       output + sizeof(uint32_t));
+    if (output_len == 0 && input_len != 0) {
+        free(output);
+        PyErr_SetString(FastlzError, "could not compress");
+        return NULL;
+    }
+
+#if PY_MAJOR_VERSION >= 3
+    result = Py_BuildValue("y#", output, output_len + sizeof(uint32_t));
+#else
+    result = Py_BuildValue("s#", output, output_len + sizeof(uint32_t));
+#endif
+    free(output);
+    return result;
+}
+
+
+static PyObject *
+decompress(PyObject *self, PyObject *args)
+{
+    PyObject *result;
+    const char *input;
+    int input_len;
+    char *output;
+    uint32_t output_len, decompressed_len;
+
+    if (!PyArg_ParseTuple(args, "s#", &input, &input_len))
+        return NULL;
+
+    if ((uint32_t)input_len < sizeof(uint32_t)) {
+        PyErr_SetString(FastlzError, "invalid input");
+        return NULL;
+    }
+
+    memcpy(&output_len, input, sizeof(uint32_t));
+
+    if (output_len / 256.0 > input_len) {
+        PyErr_SetString(FastlzError, "invalid input");
+        return NULL;
+    }
+
+    output = (char *) malloc(output_len + 1);
+    if (output == NULL)
+        return PyErr_NoMemory();
+
+    input_len -= sizeof(uint32_t);
+    input += sizeof(uint32_t);
+
+    decompressed_len = (uint32_t)fastlz_decompress(input, input_len, output,
+                                                   output_len);
+
+    if (decompressed_len != output_len) {
+        free(output);
+        PyErr_SetString(FastlzError, "could not decompress");
+        return NULL;
+    }
+
+#if PY_MAJOR_VERSION >= 3
+    result = Py_BuildValue("y#", output, output_len);
+#else
+    result = Py_BuildValue("s#", output, output_len);
+#endif
+    free(output);
+    return result;
+}
+
+
+static PyMethodDef module_methods[] = {
+    {"compress", (PyCFunction)compress, METH_VARARGS|METH_KEYWORDS,
+     PyDoc_STR("Compress a string. Optionally provide a compression level.")},
+    {"decompress", (PyCFunction)decompress, METH_VARARGS,
+     PyDoc_STR("Decompress a string.")},
+    {NULL, NULL, 0, NULL}
+};
+
+PyDoc_STRVAR(module_doc,
+"Python wrapper for FastLZ, a lightning-fast lossless compression library.");
+
+#if PY_MAJOR_VERSION >= 3
+
+static struct PyModuleDef module_def = {
+    PyModuleDef_HEAD_INIT,
+    PY_MODULE_NAME,
+    module_doc,
+    -1,
+    module_methods
+};
+
+PyMODINIT_FUNC
+PyInit_fastlz5(void)
+
+
+#else
+
+PyMODINIT_FUNC
+initfastlz5(void)
+
+#endif
+
+{
+    PyObject *m, *d;
+
+#if PY_MAJOR_VERSION >= 3
+    m = PyModule_Create(&module_def);
+#else
+    m = Py_InitModule3(PY_MODULE_NAME, module_methods, module_doc);
+#endif
+
+    PyModule_AddObject(m, "__version__", Py_BuildValue("s", "0.0.2"));
+    PyModule_AddObject(m, "__author__", Py_BuildValue("s", "Jared Suttles"));
+
+    d = PyModule_GetDict(m);
+    FastlzError = PyErr_NewException("fastlz.FastlzError", NULL, NULL);
+    PyDict_SetItemString(d, "FastlzError", FastlzError);
+
+#if PY_MAJOR_VERSION >= 3
+    return m;
+#endif
+}
---
+++ packages/fastlz5/fastlz5-0.0.1.4.tar.gz/fastlz5-0.0.1.4/setup.py
@@ -0,0 +1,92 @@
+﻿import sys
+import os
+
+from shutil import rmtree
+from setuptools import find_packages, setup, Command
+
+from distutils.core import setup, Extension
+
+# The directory containing this file
+HERE = os.path.dirname(__file__)
+
+NAME = 'fastlz5'
+DESCRIPTION = "Python wrapper for FastLZ, a lightning-fast lossless compression library."
+URL = 'https://github.com/zackees/python-fastlz5'
+EMAIL = 'dont@email.me'
+AUTHOR = 'Zach Vorhies'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.0.1.4'
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fd:
+    README = fd.read()
+    
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        pass
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(HERE, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('"{0}" setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(VERSION))
+        os.system('git push --tags')
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    python_requires=REQUIRES_PYTHON,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email="dont@email.me",
+    url=URL,
+    license='BSD License',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Archiving :: Compression',
+        'Topic :: Utilities'
+    ],
+    ext_modules = [
+        Extension(
+            'fastlz5',
+            sources=['fastlz.c', 'fastlz/fastlz.c'],
+            include_dirs=['fastlz']
+        )
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
