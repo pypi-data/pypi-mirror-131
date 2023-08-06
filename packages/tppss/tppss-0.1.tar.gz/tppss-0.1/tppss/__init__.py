7168e5bd0a546a5b0f022c1700dd887ef4ec6e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:51 +0000

    Add package tppss/0.1/tppss-0.1.tar.gz

---
+++ packages/tppss/tppss-0.1.tar.gz/tppss-0.1/tppss/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.1"
+
+
+# flake8: noqa
+
+from .horizon import horizon, KM, MILES
+from .sunpos import sunpos
+from .tppss import (
+    above_horizon,
+    sunrise_sunset,
+    sunrise_sunset_year,
+    SunriseSunset,
+    times_in_day,
+)
