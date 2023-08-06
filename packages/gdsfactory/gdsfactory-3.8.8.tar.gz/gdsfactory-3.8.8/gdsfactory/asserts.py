34337e15f7a7337183de6abf6ba65d34f8cd53b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:20 +0000

    Add package gdsfactory/3.8.8/gdsfactory-3.8.8.tar.gz

---
+++ packages/gdsfactory/gdsfactory-3.8.8.tar.gz/gdsfactory-3.8.8/gdsfactory/asserts.py
@@ -0,0 +1,45 @@
+from gdsfactory.component import Component
+from gdsfactory.config import __version__
+
+
+def grating_coupler(gc: Component) -> None:
+    assert hasattr(
+        gc.info, "polarization"
+    ), f"{gc.name} does not have polarization attribute"
+    assert gc.info.polarization in [
+        "te",
+        "tm",
+    ], f"{gc.name} polarization  should be 'te' or 'tm'"
+    assert hasattr(
+        gc.info, "wavelength"
+    ), f"{gc.name} wavelength does not have wavelength attribute"
+    assert (
+        0.5 < gc.info.wavelength < 5.0
+    ), f"{gc.name} wavelength {gc.wavelength} should be in um"
+    if "o1" not in gc.ports:
+        print(f"grating_coupler {gc.name} should have a o1 port. It has {gc.ports}")
+    if "o1" in gc.ports and gc.ports["o1"].orientation != 180:
+        print(
+            f"grating_coupler {gc.name} orientation = {gc.ports['o1'].orientation}"
+            " should be 180 degrees"
+        )
+
+
+def version_equal_or_greater_than(version: str):
+    """Raises error if version  is not high enough"""
+
+    if __version__ < version:
+        raise ValueError(
+            f"gf.minimum required version = {version}\n"
+            f"not compatible your current installed version {__version__}\n"
+            "you can run:\n"
+            "pip install gf.--upgrade\n"
+            "to install a later version",
+        )
+
+
+if __name__ == "__main__":
+    # equal_or_greater_than("3.0.0")
+    # equal_or_greater_than("2.4.9")
+    # equal_or_greater_than("2.4.8")
+    version_equal_or_greater_than("2.5.0")
---
+++ packages/gdsfactory/gdsfactory-3.8.8.tar.gz/gdsfactory-3.8.8/gdsfactory/functions.py
@@ -0,0 +1,129 @@
+"""All functions return a component so they are easy to pipe and compose."""
+from functools import lru_cache
+
+from gdsfactory.cell import cell
+from gdsfactory.component import Component
+from gdsfactory.port import auto_rename_ports
+from gdsfactory.types import ComponentOrFactory, Float2, Optional
+
+cache = lru_cache(maxsize=None)
+
+
+def add_port(component: Component, **kwargs) -> Component:
+    """Returns Component with a new port."""
+    component.add_port(**kwargs)
+    return component
+
+
+@cell
+def rotate(
+    component: ComponentOrFactory,
+    angle: int = 90,
+) -> Component:
+    """Returns rotated component inside a new component.
+
+    Most times you just need to place a reference and rotate it.
+    This rotate function just encapsulates the rotated reference into a new component.
+
+    Args:
+        component:
+        angle: in degrees
+    """
+    component = component() if callable(component) else component
+    component_new = Component()
+    component_new.component = component
+    ref = component_new.add_ref(component)
+    ref.rotate(angle)
+    component_new.add_ports(ref.ports)
+    component_new.copy_child_info(component)
+    return component_new
+
+
+@cell
+def mirror(component: Component, p1: Float2 = (0, 1), p2: Float2 = (0, 0)) -> Component:
+    """Returns mirrored component inside a new component.
+
+    Args:
+        p1: first point to define mirror axis
+        p2: second point to define mirror axis
+    """
+    component_new = Component()
+    component_new.component = component
+    ref = component_new.add_ref(component)
+    ref.mirror(p1=p1, p2=p2)
+    component_new.add_ports(ref.ports)
+    component_new.copy_child_info(component)
+    return component_new
+
+
+@cell
+def move(
+    component: Component,
+    origin=(0, 0),
+    destination=None,
+    axis: Optional[str] = None,
+) -> Component:
+    """Returns a container that contains a reference to the original component."""
+    component_new = Component()
+    component_new.component = component
+    ref = component_new.add_ref(component)
+    ref.move(origin=origin, destination=destination, axis=axis)
+    component_new.add_ports(ref.ports)
+    component_new.copy_child_info(component)
+    return component_new
+
+
+def move_port_to_zero(component: Component, port_name: str = "o1"):
+    """Returns a container that contains a reference to the original component.
+    where the new component has port_name in (0, 0)
+    """
+    if port_name not in component.ports:
+        raise ValueError(
+            f"port_name = {port_name} not in {list(component.ports.keys())}"
+        )
+    return move(component, -component.ports[port_name].midpoint)
+
+
+def update_info(component: Component, **kwargs) -> Component:
+    """Returns Component with updated info."""
+    component.info.update(**kwargs)
+    return component
+
+
+__all__ = (
+    "cache",
+    "add_port",
+    "rotate",
+    "auto_rename_ports",
+    "move",
+    "move_port_to_zero",
+    "update_info",
+)
+
+
+if __name__ == "__main__":
+    import gdsfactory as gf
+
+    c = gf.components.mmi1x2()
+    cr = c.rotate()
+    cr.show()
+
+    cm = move(c, destination=(20, 20))
+    cm.show()
+
+    cm = mirror(c)
+    cm.show()
+
+    cm = c.mirror()
+    cm.show()
+
+    cm2 = move_port_to_zero(cm)
+    cm2.show()
+
+    # cr = rotate(component=c)
+    # cr.show()
+    # print(component_rotated)
+
+    # component_rotated.pprint
+    # component_netlist = component.get_netlist()
+    # component.pprint_netlist()
