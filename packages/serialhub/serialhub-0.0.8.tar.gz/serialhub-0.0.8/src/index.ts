128245b3f5bc2a4d7207234b3e636813cc42b854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:17 +0000

    Add package serialhub/0.0.8/serialhub-0.0.8.tar.gz

---
+++ packages/serialhub/serialhub-0.0.8.tar.gz/serialhub-0.0.8/src/index.ts
@@ -0,0 +1,43 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the serialhub extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'serialhub:plugin',
+  autoStart: true,
+  optional: [],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension serialhub is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('serialhub settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for serialhub.', reason);
+        });
+    }
+
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The serialhub server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
