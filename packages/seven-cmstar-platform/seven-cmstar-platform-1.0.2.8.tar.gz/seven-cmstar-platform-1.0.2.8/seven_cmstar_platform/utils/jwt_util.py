5723addab800850bf89b835668aa06689af816a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:00 +0000

    Add package seven-cmstar-platform/1.0.2.8/seven-cmstar-platform-1.0.2.8.tar.gz

---
+++ packages/seven-cmstar-platform/seven-cmstar-platform-1.0.2.8.tar.gz/seven-cmstar-platform-1.0.2.8/seven_cmstar_platform/utils/jwt_util.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+"""
+:Author: SunYiTan
+:Date: 2020/7/22 13:49
+:LastEditTime: 2020/7/22 13:49
+:LastEditors: SunYiTan
+:Description: jwt认证工具类
+"""
+import jwt
+import time
+
+
+class JwtUtil:
+
+    @staticmethod
+    def create_token(sub_info: str, secret: str, expire: int):
+        """
+        生成token
+        :param user_info:
+        :param secret:
+        :param expire: 过期时间，天
+        :return:
+        """
+
+        payload = {
+            "iat": int(time.time()),
+            "exp": int(time.time()) + 86400 * expire,
+            "sub": sub_info
+        }
+
+        return jwt.encode(payload, secret, algorithm='HS256').decode()
+
+    @staticmethod
+    def get_sub_info_from_token(token, secret: str):
+        """
+        获取玩家Id
+        :param token:
+        :param secret:
+        :return:
+        """
+        payload = jwt.decode(token, secret, algorithms='HS256')
+        if payload:
+            return payload.get("sub")
+        else:
+            return None
