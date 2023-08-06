669ae6d265170c20d1953e4ab692234eb0c52a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:51 +0000

    Add package regtag/0.1.0/regtag-0.1.0-py3-none-any.whl

---
+++ packages/regtag/regtag-0.1.0-py3-none-any.whl/regtag/reoov.py
@@ -0,0 +1,22 @@
+import os
+
+
+def format_word(text):
+    text = text.lower()
+    # text = text.replace('-', ' ')
+    return text
+
+
+def load_dictionary(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        words = file.read().strip().split('\n')
+    word_dict = []
+    for word in words:
+        word_formated = format_word(word)
+        # word_dict.append(word_formated)
+        word_dict.extend(word_formated.split())
+    word_dict.extend(list('".,?!@#$%^&*()_-+=\';:<>/`'))
+    return set(word_dict)
+
+
+vi_dict = load_dictionary(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vidict.txt'))
