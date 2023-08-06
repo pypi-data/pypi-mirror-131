f126830caff805b20d034c73a6f0a841b2290fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package pyobs-core/0.15.0a12/pyobs-core-0.15.0a12.tar.gz

---
+++ packages/pyobs-core/pyobs-core-0.15.0a12.tar.gz/pyobs-core-0.15.0a12/pyobs/interfaces/IImageType.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+from .interface import Interface
+from pyobs.utils.enums import ImageType
+
+
+class IImageType(Interface):
+    """The module supports different image types (e.g. object, bias, dark, etc), mainly used by cameras."""
+    __module__ = 'pyobs.interfaces'
+
+    async def set_image_type(self, image_type: ImageType, **kwargs: Any) -> None:
+        """Set the image type.
+
+        Args:
+            image_type: New image type.
+        """
+        raise NotImplementedError
+
+    async def get_image_type(self, **kwargs: Any) -> ImageType:
+        """Returns the current image type.
+
+        Returns:
+            Current image type.
+        """
+        raise NotImplementedError
+
+
+__all__ = ['IImageType']
