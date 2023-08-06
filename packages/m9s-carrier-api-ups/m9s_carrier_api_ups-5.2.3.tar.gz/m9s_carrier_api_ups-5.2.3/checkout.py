9e745820b80965c18cfe6ddc1f25a22f69e1ab4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:48 +0000

    Add package m9s-carrier-api-ups/5.2.3/m9s_carrier_api_ups-5.2.3.tar.gz

---
+++ packages/m9s-carrier-api-ups/m9s_carrier_api_ups-5.2.3.tar.gz/m9s_carrier_api_ups-5.2.3/checkout.py
@@ -0,0 +1,25 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from nereid import route
+from nereid.contrib.locale import make_lazy_gettext
+from trytond.modules.nereid_checkout.checkout import (
+    not_empty_cart, sale_has_non_guest_party)
+from trytond.pool import PoolMeta
+
+_ = make_lazy_gettext('nereid_webshop')
+
+
+class Checkout(metaclass=PoolMeta):
+    __name__ = 'nereid.checkout'
+
+    @classmethod
+    @route('/checkout/delivery-method', methods=['GET', 'POST'],
+        readonly=False)
+    @not_empty_cart
+    @sale_has_non_guest_party
+    def delivery_method(cls):
+        '''
+        Get requests have to be read/write because the wizard writes on
+        creation to the database.
+        '''
+        return super().delivery_method()
