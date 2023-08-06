9e6d7dcc259077e45083396ca53a3f4fde454a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:59 +0000

    Add package ashishdotme-utils-lib/0.0.4/ashishdotme_utils_lib-0.0.4.tar.gz

---
+++ packages/ashishdotme-utils-lib/ashishdotme_utils_lib-0.0.4.tar.gz/ashishdotme_utils_lib-0.0.4/ashishdotme_utils_lib/sdk/dashboard_client.py
@@ -0,0 +1,71 @@
+"""
+common_errors.py
+
+Created by Ashish Patel on 14-12-2021
+Copyright Ashish Patel, 2021
+
+"""
+
+import requests
+import ashishdotme_utils_lib.errors.common_errors as ze
+
+
+class DashboardClient(object):
+    """
+    Dashboard API methods
+    """
+
+    def __init__(
+            self
+    ):
+        """ Store Zotero credentials
+        """
+        self.endpoint = "https://systemapi.prod.ashish.me"
+
+    def get_data(self, request=None, params=None):
+        """
+        Retrieve Zotero items via the API
+        Combine endpoint and request to access the specific resource
+        Returns a JSON document
+        """
+        full_url = "%s%s" % (self.endpoint, request)
+        req = requests.get(
+            url=full_url, params=params
+        )
+        self.request.encoding = "utf-8"
+        try:
+            self.request.raise_for_status()
+        except requests.exceptions.HTTPError:
+            error_handler(self, self.request)
+        return req.json()
+
+
+def error_handler(zot, req):
+    error_codes = {
+        400: ze.UnsupportedParams,
+        401: ze.UserNotAuthorised,
+        403: ze.UserNotAuthorised,
+        404: ze.ResourceNotFound,
+        409: ze.Conflict,
+        412: ze.PreConditionFailed,
+        413: ze.RequestEntityTooLarge,
+        428: ze.PreConditionRequired,
+        429: ze.TooManyRequests,
+    }
+
+    def err_msg(req):
+        return "\nCode: %s\nURL: %s\nMethod: %s\nResponse: %s" % (
+            req.status_code,
+            # error.msg,
+            req.url,
+            req.request.method,
+            req.text,
+        )
+
+    if error_codes.get(req.status_code):
+        raise error_codes.get(req.status_code)(err_msg(req))
+    else:
+        raise ze.HTTPError(err_msg(req))
+
+
+dashboardClient = DashboardClient()
