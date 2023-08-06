0426154605d21d66742700306cc7c47031a9988b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:35 +0000

    Add package insights-core/3.0.255/insights_core-3.0.255-py3.6.egg

---
+++ packages/insights-core/insights_core-3.0.255-py3.6.egg/insights/parsers/satellite_postgresql_query.py
@@ -0,0 +1,285 @@
+"""
+Satellite PostgreSQL database queries
+=====================================
+
+This module contains the following parsers:
+
+SatelliteAdminSettings - command ``psql -d foreman -c 'select name, value, "default" from settings where name in (\'destroy_vm_on_host_delete\', \'unregister_delete_host\') --csv'``
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+SatelliteComputeResources - command ``psql -d foreman -c 'select name, type from compute_resources' --csv``
+-----------------------------------------------------------------------------------------------------------
+SatelliteSCAStatus - command ``psql -d candlepin -c "select displayname, content_access_mode from cp_owner" --csv``
+-------------------------------------------------------------------------------------------------------------------
+
+SatelliteKatelloEmptyURLRepositories - command ``psql -d foreman -c 'select id, name from katello_root_repositories where url is NULL;' --csv``
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+SatelliteCoreTaskReservedResourceCount - command ``psql -d pulpcore -c 'select count(*) from core_taskreservedresource' --csv``
+-------------------------------------------------------------------------------------------------------------------------------
+"""
+
+import os
+import yaml
+from csv import DictReader
+
+from insights import parser, CommandParser
+from insights.specs import Specs
+from insights.parsers import SkipException, ParseException
+from insights.parsers import keyword_search
+
+
+class SatellitePostgreSQLQuery(CommandParser, list):
+    """
+    Parent class of satellite postgresql table queries.
+    It saves the rows data into a list. Each row is saved into a dict.
+    The key is the column name, the value is the value of the column.
+
+    Resultant data structure::
+
+        [
+            {
+                'name': 'abc',
+                'url': '',
+                'value': 'test'
+            },
+            {
+                'name': 'def',
+                'url': 'http://xx.com',
+                'value': ''
+            }
+        ]
+
+    Sample Output::
+
+        name,url,value
+        abc,,test
+        def,http://xx.com,
+
+
+    Examples:
+        >>> type(query)
+        <class 'insights.parsers.satellite_postgresql_query.SatellitePostgreSQLQuery'>
+        >>> rows = query.search(name='abc')
+        >>> len(rows)
+        1
+        >>> rows[0]['value']
+        'test'
+        >>> columns=query.get_columns()
+        >>> 'url' in columns
+        True
+        >>> 'name' in columns
+        True
+
+    Raises:
+        SkipException: when there isn't data in the table
+        ParseException: when the output isn't in good csv format
+    """
+
+    def parse_content(self, content):
+        if not content or len(content) == 1:
+            raise SkipException("There is no data in the table")
+        try:
+            # keep the line break for yaml parse in some table
+            reader = DictReader(os.linesep.join(content).splitlines(True))
+        except Exception:
+            raise ParseException("The content isn't in csv format")
+        for row in reader:
+            self.append(row)
+
+    def get_columns(self):
+        return list(self[0].keys())
+
+    def search(self, **kwargs):
+        """
+        Get the rows by searching the table with kwargs.
+        This uses the :py:func:`insights.parsers.keyword_search` function for
+        searching; see its documentation for usage details. If no search
+        parameters are given, no rows are returned.
+
+        It simplify the value of the column according to actual usage.
+
+        Returns:
+            list: A list of dictionaries of rows that match the given
+            search criteria.
+
+        Examples:
+            >>> query.search(name__startswith='abc') == [
+            ... {'name': 'abc', 'url': '', 'value': 'test'},
+            ... {'name': 'abcdef', 'url': '', 'value': 'test2'}
+            ... ]
+            True
+            >>> query.search(name__startswith='abc', value='test') == [
+            ... {'name': 'abc', 'url': '', 'value': 'test'}
+            ... ]
+            True
+        """
+
+        return keyword_search(self, **kwargs)
+
+
+@parser(Specs.satellite_settings)
+class SatelliteAdminSettings(SatellitePostgreSQLQuery):
+    """
+    Parse the output of the command ``psql -d foreman -c '"select name, value, "default" from settings where name in ('destroy_vm_on_host_delete', 'unregister_delete_host') --csv"``.
+
+    Sample output::
+
+        name,value,default
+        unregister_delete_host,"--- true
+        ...","--- false
+        ..."
+        destroy_vm_on_host_delete,,"--- true
+        ..."
+
+    Examples:
+        >>> type(table)
+        <class 'insights.parsers.satellite_postgresql_query.SatelliteAdminSettings'>
+        >>> table.get_setting('unregister_delete_host')
+        True
+        >>> table.get_setting('destroy_vm_on_host_delete')
+        True
+    """
+
+    def _parse_yaml(self, value):
+        if value:
+            try:
+                return yaml.safe_load(value)
+            except Exception:
+                raise ParseException("Bad format value: %s" % value)
+        return value
+
+    def parse_content(self, content):
+        """
+        The "default" and "value" columns must be selected, or else the
+        settings value can't be determined.
+        The "default" and "value" column are in yaml format, it is transfer to
+        python object.
+
+        Raises:
+            SkipException: when value or default column isn't found in the
+                            table.
+            ParseException: when the value or default in bad yaml format.
+        """
+        super(SatelliteAdminSettings, self).parse_content(content)
+        if not all(item in self.get_columns() for item in ['default', 'value']):
+            raise SkipException('No default, value columns in the table.')
+        for row in self:
+            row['default'] = self._parse_yaml(row['default'])
+            row['value'] = self._parse_yaml(row['value'])
+
+    def get_setting(self, setting_name):
+        """
+        Get the actual value of setting_name.
+        If the value column isn't empty, the value of the setting_name is the
+        value column, or else it's the default column.
+
+        Args:
+            setting_name (str): the value of name column which is searched in the table.
+
+        Returns:
+            It depends on the setting, maybe boolean, string, int or a list.
+            None if the setting_name doesn't exist in the table.
+        """
+        rows = self.search(name=setting_name)
+        if rows:
+            value = rows[0].get('value')
+            return rows[0].get('default') if value == '' else value
+
+
+@parser(Specs.satellite_compute_resources)
+class SatelliteComputeResources(SatellitePostgreSQLQuery):
+    """
+    Parse the output of the command ``psql -d foreman -c 'select name, type from compute_resources' --csv``.
+
+    .. note::
+        Please refer to its super-class :class:`insights.parsers.satellite_postgresql_query.SatellitePostgreSQLQuery` for more
+        details.
+
+    Sample output::
+
+        name,type
+        test_compute_resource1,Foreman::Model::Libvirt
+        test_compute_resource2,Foreman::Model::RHV
+
+    Examples:
+        >>> type(resources_table)
+        <class 'insights.parsers.satellite_postgresql_query.SatelliteComputeResources'>
+        >>> rows=resources_table.search(type='Foreman::Model::Libvirt')
+        >>> len(rows)
+        1
+        >>> rows[0]['name']
+        'test_compute_resource1'
+    """
+    pass
+
+
+@parser(Specs.satellite_katello_empty_url_repositories)
+class SatelliteKatelloEmptyURLRepositories(SatellitePostgreSQLQuery):
+    """
+    Parse the output of the command ``psql -d foreman -c 'select id, name from katello_root_repositories where url is NULL;' --csv``.
+
+    Sample output::
+
+        id,name
+        54,testa
+        55,testb
+
+    Examples:
+        >>> type(katello_root_repositories)
+        <class 'insights.parsers.satellite_postgresql_query.SatelliteKatelloEmptyURLRepositories'>
+        >>> len(katello_root_repositories)
+        2
+        >>> katello_root_repositories[0]['name']
+        'testa'
+    """
+    pass
+
+
+@parser(Specs.satellite_core_taskreservedresource_count)
+class SatelliteCoreTaskReservedResourceCount(SatellitePostgreSQLQuery):
+    """
+    Parse the output of the command ``psql -d pulpcore -c 'select count(*) from core_taskreservedresource' --csv``.
+
+    Sample output::
+
+        count
+        0
+
+    Examples:
+        >>> type(tasks)
+        <class 'insights.parsers.satellite_postgresql_query.SatelliteCoreTaskReservedResourceCount'>
+        >>> tasks[0]['count']
+        '0'
+    """
+    pass
+
+
+@parser(Specs.satellite_sca_status)
+class SatelliteSCAStatus(SatellitePostgreSQLQuery):
+    """
+    Parse the output of the command ``psql -d candlepin -c "select displayname, content_access_mode from cp_owner" --csv``.
+
+    .. note::
+        Please refer to its super-class :class:`insights.parsers.satellite_postgresql_query.SatellitePostgreSQLQuery` for more
+        details.
+
+    Sample output::
+
+        displayname,content_access_mode
+        Default Organization,entitlement
+        Orgq,org_environment
+
+    Examples:
+        >>> type(sat_sca_info)
+        <class 'insights.parsers.satellite_postgresql_query.SatelliteSCAStatus'>
+        >>> sat_sca_info.sca_enabled
+        True
+    """
+
+    @property
+    def sca_enabled(self):
+        """
+        If the value of content_access_mode is "org_environment", it means the SCA is enabled for this organization.
+        Return True if any organization has SCA enabled on the satellite else False
+        """
+        return bool(len(self.search(content_access_mode='org_environment')))
