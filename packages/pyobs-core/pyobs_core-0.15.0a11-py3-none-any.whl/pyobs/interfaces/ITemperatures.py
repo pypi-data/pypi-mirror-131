a3e3088cff9b4bfb8f07c53a2e319fd246e1c1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:29 +0000

    Add package pyobs-core/0.15.0a11/pyobs_core-0.15.0a11-py3-none-any.whl

---
+++ packages/pyobs-core/pyobs_core-0.15.0a11-py3-none-any.whl/pyobs/interfaces/ITemperatures.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict
+
+from .interface import Interface
+
+
+class ITemperatures(Interface):
+    """The module can return temperatures measured on some device."""
+    __module__ = 'pyobs.interfaces'
+
+    async def get_temperatures(self, **kwargs: Any) -> Dict[str, float]:
+        """Returns all temperatures measured by this module.
+
+        Returns:
+            Dict containing temperatures.
+        """
+        raise NotImplementedError
+
+
+__all__ = ['ITemperatures']
