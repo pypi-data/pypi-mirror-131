4bc126f1b9d0b45066696aed892a2997700ffdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:54 +0000

    Add package great-assertions/0.0.62/great-assertions-0.0.62.tar.gz

---
+++ packages/great-assertions/great-assertions-0.0.62.tar.gz/great-assertions-0.0.62/tests/test_great_assertions_reporting.py
@@ -0,0 +1,40 @@
+import unittest
+import sys
+from great_assertions import GreatAssertionResult, GreatAssertions
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+
+class SaveTest(GreatAssertions):
+
+    __test__ = False
+
+    def test_fail1(self):
+        df = spark.createDataFrame(
+            [
+                {"col_1": 100, "col_2": 10},
+            ]
+        )
+        self.expect_table_row_count_to_equal(df, 20)
+
+    def test_pass(self):
+        df = spark.createDataFrame(
+            [
+                {"col_1": 100, "col_2": 10},
+            ]
+        )
+        self.expect_table_row_count_to_equal(df, 1)
+
+
+def _run_tests(test_class):
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
+    testRunner = unittest.runner.TextTestRunner(resultclass=GreatAssertionResult)
+    return testRunner.run(suite)
+
+
+class GreatAssertionSaveTests(unittest.TestCase):
+    def test_to_results_table(self):
+        if not sys.platform.startswith("win32"):
+            _run_tests(SaveTest).save("databricks", spark=spark)
+            self.assertEqual(spark.table("ga_result").count(), 2)
