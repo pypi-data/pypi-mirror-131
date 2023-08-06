0d16b68d42362bba3483b3c934946cbc4c96fc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:18 +0000

    Add package ntn-torch/0.1.0/ntn_torch-0.1.0.tar.gz

---
+++ packages/ntn-torch/ntn_torch-0.1.0.tar.gz/ntn_torch-0.1.0/ntn_torch/net.py
@@ -0,0 +1,40 @@
+import torch
+import numpy as np
+from .utils import bitarray_to_word
+
+class FunctionalDiscriminator(torch.nn.Module):
+  def __init__(self, input_dim, output_dim, n):
+    super(FunctionalDiscriminator, self).__init__()
+
+    self.input_dim = input_dim
+    self.output_dim = output_dim
+    self.n = n
+    self.n_nodes = torch.ceil(torch.tensor(input_dim/n)).type(torch.int).item()
+    self.mapping = torch.randperm(input_dim)
+    self.nodes = [dict() for _ in range(self.n_nodes)]
+    self.params = torch.nn.ParameterList([
+      torch.nn.Parameter(torch.zeros((output_dim,)), requires_grad=True)
+      for _ in range(self.n_nodes)])
+
+  def _keys(self, x):
+    tuples = torch.tensor_split(x[self.mapping], self.n_nodes)
+    
+    return map(bitarray_to_word, tuples)
+
+  def forward(self, x):
+    keys = self._keys(x)
+
+    if self.training:
+      response = torch.zeros((self.output_dim,))
+      for (node, param, key) in zip(self.nodes, self.params, keys):
+        param.data = node.setdefault(key, torch.zeros((self.output_dim,), requires_grad=True))[:]
+        param.requires_grad=True
+        response += param
+
+      return response/self.n
+
+    response = torch.zeros((self.output_dim,))
+    for (node, key) in zip(self.nodes, keys):
+      response += node.get(key, torch.zeros((self.output_dim,)))
+
+    return response/self.n
