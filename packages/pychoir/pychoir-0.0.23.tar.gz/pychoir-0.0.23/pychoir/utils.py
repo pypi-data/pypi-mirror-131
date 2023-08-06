54f30b50df10345e00db044823cfb2f80ff43fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:23 +0000

    Add package pychoir/0.0.23/pychoir-0.0.23.tar.gz

---
+++ packages/pychoir/pychoir-0.0.23.tar.gz/pychoir-0.0.23/pychoir/utils.py
@@ -0,0 +1,17 @@
+"""
+Useful extensions for standard library
+"""
+from typing import Any, Sequence, Tuple, TypeVar, Union
+
+T = TypeVar('T')
+
+
+def sequence_or_its_only_member(sequence: Sequence[T]) -> Union[Sequence[T], T]:
+    if len(sequence) == 1:
+        return sequence[0]
+    else:
+        return sequence
+
+
+def i_removed(tuple_: Tuple[Any, ...], index: Any) -> Tuple[Any, ...]:
+    return tuple_[:index] + tuple_[index + 1:]
