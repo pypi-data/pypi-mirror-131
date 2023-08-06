55b04fbe3339c15560d72fe9892dee1cb3a548fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:36 +0000

    Add package fastash/0.0.7/fastash-0.0.7.tar.gz

---
+++ packages/fastash/fastash-0.0.7.tar.gz/fastash-0.0.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from Cython.Build import cythonize
+import numpy as np
+
+# import Cython.Compiler.Options
+# Cython.Compiler.Options.annotate = True
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+import Cython.Compiler.Options
+Cython.Compiler.Options.annotate = True
+
+
+setup(
+     name="fastash",
+     version="0.0.7",
+     packages=find_packages(),
+     author="François-Rémi Mazy",
+     description="Fast Averaged Shifted Histogram module.",
+     long_description=long_description,
+     long_description_content_type='text/markdown',
+     url="https://gitlab.inria.fr/fmazy/fastash",
+     ext_modules=cythonize(
+        "fastash/ashfunc.pyx",
+        annotate=True,
+        ),
+     include_dirs=[np.get_include()],
+     install_requires=[
+        'numpy>=1.20.3',
+    ]
+)    
