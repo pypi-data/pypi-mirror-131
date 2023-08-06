d3866434c6efcf3a121446f0f2a662a19d78b623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:43 +0000

    Add package pseudoslit/1.1.0/pseudoslit-1.1.0.tar.gz

---
+++ packages/pseudoslit/pseudoslit-1.1.0.tar.gz/pseudoslit-1.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pseudoslit',
+      version='1.1.0',
+      description='Psuedoslit for IFU-spectras',
+      packages=['.'],
+      author_email='gasymov.df18@physics.msu.ru',
+      zip_safe=False)
