eaa7fa106eb519aedf02b021e4f06247c371e160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:45 +0000

    Add package grailts/0.1.0/grailts-0.1.0.tar.gz

---
+++ packages/grailts/grailts-0.1.0.tar.gz/grailts-0.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="grailts",
+    version="0.1.0",
+    author="Karhan Kaan Kayan",
+    author_email="kayankarhankaan@gmail.com",
+    description="Python implementation of GRAIL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnpaparrizos/GRAIL",
+    project_urls={
+        "Bug Tracker": "https://github.com/johnpaparrizos/GRAIL/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: Other/Proprietary License',
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    install_requires=['numpy', 'scipy', 'tslearn'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
