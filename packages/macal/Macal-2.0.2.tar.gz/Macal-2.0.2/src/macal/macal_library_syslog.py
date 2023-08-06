3b1b1d379a0d09beb60a903d9ad2d5a6934983f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:34 +0000

    Add package macal/2.0.2/Macal-2.0.2.tar.gz

---
+++ packages/macal/Macal-2.0.2.tar.gz/Macal-2.0.2/src/macal/macal_library_syslog.py
@@ -0,0 +1,60 @@
+#-------------------------------------------------------------------------------------------------#
+# Filename:       | macal_library_syslog.py                                                       #
+# Author:         | Marco Caspers                                                                 #
+# Description:    |                                                                               #
+#-------------------------------------------------------------------------------------------------#
+#                                                                                                 #
+# Macal 2.0 Syslog Library                                                                        #
+#                                                                                                 #
+###################################################################################################
+
+"""Syslog library implementation"""
+
+from .macal_library import PLibrary
+from .macal_variable_types import STRING, BOOL, INT
+from .macal_interpreterconsts import FuncArg
+from .macal_scope import PScope
+from .macal_function import PFunction
+from .macal_SysLog_class import SysLog
+
+class LibrarySyslog(PLibrary):
+    def __init__(self):
+        super().__init__("syslog")
+        self.RegisterFunction("syslog",             [FuncArg("level", STRING),   FuncArg("message", STRING)], self.Syslog)
+        self.RegisterFunction("syslog_init",        [FuncArg("remote", BOOL)], self.SyslogInit)
+        self.RegisterFunction("syslog_set_address", [FuncArg("address", STRING), FuncArg("port", INT)], self.SyslogSetAddress)
+        self.SysLog = SysLog()
+        
+    def Syslog(self, func: PFunction, name: str, params: list, scope: PScope):
+        """Implementation of Syslog function"""
+        self.ValidateFunction(name, func, scope)
+        self.ValidateParams(name, params, scope, func.args)
+        level = self.GetParamValue(params, "level")
+        message = self.GetParamValue(params, "message")
+        if level == "debug":
+            self.SysLog.debug(message)
+        elif level == "info" or level == "information":
+            self.SysLog.info(message)
+        elif level == "warn" or level == "warning":
+            self.SysLog.warn(message)
+        elif level == "error":
+            self.SysLog.error(message)
+        elif level == "critical":
+            self.SysLog.critical(message)
+        else:
+            raise Exception(f"Invalid syslog level given: {level}")
+
+    def SyslogInit(self, func: PFunction, name: str, params: list, scope: PScope):
+        """Implementation of SysLog init function"""
+        self.ValidateFunction(name, func, scope)
+        self.ValidateParams(name, params, scope, func.args)
+        remote = self.GetParamValue(params, "remote")
+        self.SysLog.SysLogInit(remote)
+
+    def SyslogSetAddress(self, func: PFunction, name: str, params: list, scope: PScope):
+        """Implementation of SysLog SetAddress function"""
+        self.ValidateFunction(name, func, scope)
+        self.ValidateParams(name, params, scope, func.args)
+        address = self.GetParamValue(params, "address")
+        port = self.GetParamValue(params, "port")
+        self.SysLog.SysLogSetAddress(address, port)
