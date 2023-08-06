20a3e87168602cbd29b12b1535a5f55169094232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:20 +0000

    Add package BannerRifael/0.1/BannerRifael-0.1.tar.gz

---
+++ packages/BannerRifael/BannerRifael-0.1.tar.gz/BannerRifael-0.1/lib/__init__.py
@@ -0,0 +1,1 @@
+from BannerRifael import createBanner
---
+++ packages/BannerRifael/BannerRifael-0.1.tar.gz/BannerRifael-0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+	name = "BannerRifael",
+	version = "0.1",
+	author = "Rifael",
+	author_email = "sitorusrifael@gamil.com",
+	description = "Make Your Cool Banner",
+	packages = setuptools.find_packages(),
+	
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	
+	
+	],
+	
+
+
+
+
+)
