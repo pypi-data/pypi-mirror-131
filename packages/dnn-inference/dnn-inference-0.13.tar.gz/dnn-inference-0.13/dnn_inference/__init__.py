25fcba0351761c507debad4ebe0ebd1e97f27404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:56 +0000

    Add package dnn-inference/0.13/dnn-inference-0.13.tar.gz

---
+++ packages/dnn-inference/dnn-inference-0.13.tar.gz/dnn-inference-0.13/dnn_inference/__init__.py
@@ -0,0 +1,11 @@
+from dnn_inference.BBoxTest import split_test, perm_test, Hperm_test
+import sys
+
+sys.path.append('..')
+
+__all__ = [
+    "BBoxTest",
+	"split_test", 
+	"perm_test",
+	"Hperm_test"
+	]
