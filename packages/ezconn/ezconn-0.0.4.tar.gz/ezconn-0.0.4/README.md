eac175f7e85043ae753876b8f4a833653ae0b4f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:38 +0000

    Add package ezconn/0.0.4/ezconn-0.0.4.tar.gz

---
+++ packages/ezconn/ezconn-0.0.4.tar.gz/ezconn-0.0.4/README.md
@@ -0,0 +1,2 @@
+# ezconn
+Easy python DB-API 2.0 connector for most popular analytical DB
---
+++ packages/ezconn/ezconn-0.0.4.tar.gz/ezconn-0.0.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ezconn",
+    version="0.0.4",
+    author="ANALYTIKA PLUS",
+    author_email="support@analytikaplus.ru",
+    description="Easy python DB-API 2.0 connector for most popular analytical DB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/analytikaplus/ezconn",
+    project_urls={
+        "Bug Tracker": "https://github.com/analytikaplus/ezconn/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    py_modules=['ezconn'],
+    install_requires=[
+          'jaydebeapi',
+      ],
+    python_requires=">=3.6",
+)
