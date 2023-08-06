28b416cf3f870de27cd5d5865e3df7835cbabcc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:04 +0000

    Add package Messenger_client_dmitry_vokh/1.0.0/Messenger_client_dmitry_vokh-1.0.0.tar.gz

---
+++ packages/Messenger_client_dmitry_vokh/Messenger_client_dmitry_vokh-1.0.0.tar.gz/Messenger_client_dmitry_vokh-1.0.0/setup_client.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="Messenger_client_dmitry_vokh",
+      version="1.0.0",
+      description="mess_server",
+      author="Dmitry Vokhmin",
+      author_email="box@ya.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
