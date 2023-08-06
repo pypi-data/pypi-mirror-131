96e00d5916051416e8799a5703b13efa1121a514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:06 +0000

    Add package LatexTemplater/0.4.0/LatexTemplater-0.4.0.tar.gz

---
+++ packages/LatexTemplater/LatexTemplater-0.4.0.tar.gz/LatexTemplater-0.4.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LatexTemplater",
+    version="0.4.0",
+    author="Nathan Rose",
+    author_email="mail.nathanrose@gmail.com",
+    description="A package to allow python templating in latex files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/NathanRoseCE/LatexTemplater',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=[
+        'ninja2',
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
