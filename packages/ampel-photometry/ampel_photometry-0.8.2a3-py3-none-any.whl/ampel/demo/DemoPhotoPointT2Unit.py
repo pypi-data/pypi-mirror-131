c421b111437c3fc7d4e47f69380ffcdafa939ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:25 +0000

    Add package ampel-photometry/0.8.2a3/ampel_photometry-0.8.2a3-py3-none-any.whl

---
+++ packages/ampel-photometry/ampel_photometry-0.8.2a3-py3-none-any.whl/ampel/demo/DemoPhotoPointT2Unit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-photometry/ampel/demo/DemoPhotoPointT2Unit.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 25.03.2020
+# Last Modified Date: 28.09.2021
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from time import time
+from typing import Union, ClassVar
+from ampel.types import UBson
+from ampel.struct.UnitResult import UnitResult
+from ampel.content.DataPoint import DataPoint
+from ampel.abstract.AbsPointT2Unit import AbsPointT2Unit
+from ampel.model.DPSelection import DPSelection
+
+
+class DemoPhotoPointT2Unit(AbsPointT2Unit):
+
+	eligible: ClassVar[DPSelection] = DPSelection(filter='PPSFilter')
+	
+	def process(self, datapoint: DataPoint) -> Union[UBson, UnitResult]:
+		return {"id": datapoint['id'], "time": time()}
