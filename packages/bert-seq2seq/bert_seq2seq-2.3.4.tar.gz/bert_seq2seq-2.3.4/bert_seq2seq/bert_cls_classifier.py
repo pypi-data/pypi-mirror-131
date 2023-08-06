591d7f792c65e294b88f38a3c65ac91e2a9c6d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:52 +0000

    Add package bert-seq2seq/2.3.4/bert_seq2seq-2.3.4.tar.gz

---
+++ packages/bert-seq2seq/bert_seq2seq-2.3.4.tar.gz/bert_seq2seq-2.3.4/bert_seq2seq/bert_cls_classifier.py
@@ -0,0 +1,43 @@
+## bert encoder模型
+import torch
+import torch.nn as nn
+from bert_seq2seq.tokenizer import Tokenizer
+from bert_seq2seq.basic_bert import BasicBert
+
+class BertClsClassifier(BasicBert):
+    """
+    """
+    def __init__(self, word2ix, target_size, model_name="roberta"):
+        super(BertClsClassifier, self).__init__(word2ix=word2ix, model_name=model_name)
+        self.target_size = target_size
+        self.final_dense = nn.Linear(self.config.hidden_size, self.target_size)
+    
+    def compute_loss(self, predictions, labels):
+        """
+        计算loss
+        predictions: (batch_size, 1)
+        """
+        predictions = predictions.view(-1, self.target_size)
+        labels = labels.view(-1)
+        loss = nn.CrossEntropyLoss(reduction="mean")
+        return loss(predictions, labels)
+    
+    def forward(self, text, position_enc=None, labels=None, use_layer_num=-1):
+        if use_layer_num != -1:
+            raise Exception("暂时只支持用最后一层进行分类")
+        
+        text = text.to(self.device)
+        if position_enc is not None:
+            position_enc = position_enc.to(self.device)
+        if labels is not None:
+            labels = labels.to(self.device)
+        _, pooled_out = self.bert(text, 
+                                    output_all_encoded_layers=True)
+
+        predictions = self.final_dense(pooled_out)
+        if labels is not None:
+            ## 计算loss
+            loss = self.compute_loss(predictions, labels)
+            return predictions, loss 
+        else :
+            return predictions
