c620351689219d4ee2843cf33debe88fd63c720b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:20 +0000

    Add package multiformats/0.1.2.post2/multiformats-0.1.2.post2-py3-none-any.whl

---
+++ packages/multiformats/multiformats-0.1.2.post2-py3-none-any.whl/multiformats/__init__.py
@@ -0,0 +1,25 @@
+"""
+    This package implements existing multiformat protocols, according to the [Multiformats](https://multiformats.io/) specifications.
+"""
+
+__version__ = "0.1.2"
+
+from . import varint
+from . import multicodec
+from . import multibase
+from . import multihash
+from .cid import CID
+from . import multiaddr
+from .multiaddr import Proto
+
+__all__ = [
+    "varint",
+    "multicodec",
+    "multibase",
+    "multihash",
+    "multiaddr",
+    "CID",
+    "Proto",
+]
+
+__pdoc__ = {name: False for name in ["CID", "Proto"]}
