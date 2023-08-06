8a6e385a4f3cf151fbfec930a019e3cbbc2a6d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:55 +0000

    Add package OpenFisca-UK-Data/0.5.1/OpenFisca_UK_Data-0.5.1-py3-none-any.whl

---
+++ packages/OpenFisca-UK-Data/OpenFisca_UK_Data-0.5.1-py3-none-any.whl/openfisca_uk_data/__init__.py
@@ -0,0 +1,13 @@
+from openfisca_uk_data.datasets import *
+from pathlib import Path
+
+REPO = Path(__file__).parent
+
+DATASETS = (
+    FRS,
+    SynthFRS,
+    FRS_SPI_Adjusted,
+    FRS_WAS_Imputation,
+    SPI,
+    RawLCF,
+)
