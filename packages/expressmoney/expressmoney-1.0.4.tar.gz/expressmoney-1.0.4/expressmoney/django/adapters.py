9ff26cd09a3f8835e810e812d946d400dcef7ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:13 +0000

    Add package expressmoney/1.0.4/expressmoney-1.0.4.tar.gz

---
+++ packages/expressmoney/expressmoney-1.0.4.tar.gz/expressmoney-1.0.4/expressmoney/django/adapters.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from expressmoney import adapters
+
+
+User = get_user_model()
+
+
+class DjangoTasks(adapters.Tasks):
+
+    def __init__(self, user: Union[int, User], queue: str = 'attempts-1', location: str = 'europe-west1',
+                 in_seconds: int = None):
+        if not isinstance(user, User):
+            user = User.objects.get(pk=user)
+        access_token = RefreshToken.for_user(user).access_token
+        project = settings.PROJECT
+        super().__init__(access_token, queue, location, project, in_seconds)
+
+
+class DjangoPubSub(adapters.PubSub):
+
+    def __init__(self, topic_id: str, user: Union[int, User]):
+        if not isinstance(user, User):
+            user = User.objects.get(pk=user)
+        access_token = RefreshToken.for_user(user).access_token
+        project = settings.PROJECT
+        super().__init__(topic_id, access_token, project)
+
+
+class DjangoStorage(adapters.Storage):
+    def __init__(self):
+        bucket_name = settings.PROJECT
+        super().__init__(bucket_name)
