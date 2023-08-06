5a2e568481cc4dfee3656e2992b0d3f14eb9005d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:15 +0000

    Add package ottobox/0.1.0/ottobox-0.1.0-py2.py3-none-any.whl

---
+++ packages/ottobox/ottobox-0.1.0-py2.py3-none-any.whl/ottobox/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) indresh.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "ottobox"
+module_version = "^0.1.0"
