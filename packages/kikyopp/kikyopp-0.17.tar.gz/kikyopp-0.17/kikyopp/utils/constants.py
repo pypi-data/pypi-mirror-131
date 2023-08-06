21ddf3de9fb178715f3aa920976e4d674c9a6a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:33 +0000

    Add package kikyopp/0.17/kikyopp-0.17.tar.gz

---
+++ packages/kikyopp/kikyopp-0.17.tar.gz/kikyopp-0.17/kikyopp/utils/constants.py
@@ -0,0 +1,8 @@
+import os
+
+YURI_API_HOST = 'http://yuri.app.kdsec.org'
+TIKA_HOST = 'http://tika.app.kdsec.org'
+PADDLEOCR_HOST = 'http://paddleocr.app.kdsec.org'
+PDF_CONVERTER_HOST = 'http://pdf-converter.app.kdsec.org'
+FILE_CONTENT_LIMIT = 5000
+RETRY_API_TIMES = int(os.environ.get('RETRY_API_TIMES', 1))
