2687f102f53e1e17618aa4bedd167f6065f3889d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:31 +0000

    Add package tempo-alg/0.0.1.dev0/tempo_alg-0.0.1.dev0.tar.gz

---
+++ packages/tempo-alg/tempo_alg-0.0.1.dev0.tar.gz/tempo_alg-0.0.1.dev0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='tempo_alg',
+	version='0.0.1.dev',
+	description='',
+	long_description='',
+	author='Benjamin Auerbach',
+	author_email='benauer@pennmedicine.upenn.edu',
+	packages=[]
+)
+
+
