a0a9b2d3ccc8aeee02124ce2532442a599f2c79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:09 +0000

    Add package warepy/0.4.0.dev0/warepy-0.4.0.dev0.tar.gz

---
+++ packages/warepy/warepy-0.4.0.dev0.tar.gz/warepy-0.4.0.dev0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="warepy",
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.4.0.dev0",
+    license="MIT",
+    description="My toolkit for Python.",
+    author = "Alexander Ryzhov",
+    author_email = "thed4rkof@gmail.com",
+    url = "https://github.com/ryzhovalex/warepy",
+    keywords = ["toolkit", "python-core"],
+    install_requires=[
+        "pyyaml",
+        "loguru"
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3.9",
+    ],
+)
