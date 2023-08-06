0b89ac7e5109e1a031d83c861aede7b1a738403d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:48 +0000

    Add package eth-ape/0.1.0b2/eth-ape-0.1.0b2.tar.gz

---
+++ packages/eth-ape/eth-ape-0.1.0b2.tar.gz/eth-ape-0.1.0b2/src/ape/plugins/config.py
@@ -0,0 +1,16 @@
+from typing import Type
+
+from ape.api import ConfigItem
+
+from .pluggy_patch import PluginType, hookspec
+
+
+class Config(PluginType):
+    @hookspec
+    def config_class(self) -> Type[ConfigItem]:
+        """
+        Returns a :class:`ape.api.config.ConfigItem` parser class that can be
+        used to deconstruct the user config options for this plugins.
+
+        NOTE: If none are specified, all injected :class:`ape.api.config.ConfigItem`'s are empty.
+        """
---
+++ packages/eth-ape/eth-ape-0.1.0b2.tar.gz/eth-ape-0.1.0b2/src/ape/types/__init__.py
@@ -0,0 +1,38 @@
+import sys
+from typing import Union
+
+from eth_typing import ChecksumAddress as AddressType
+from hexbytes import HexBytes
+
+from .contract import ABI, Bytecode, Checksum, Compiler, ContractType, Source
+from .manifest import PackageManifest, PackageMeta
+from .signatures import MessageSignature, SignableMessage, TransactionSignature
+
+# We can remove this once we stop supporting python3.7.
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+BlockID = Union[str, int, HexBytes, Literal["earliest", "latest", "pending"]]
+"""
+An ID that can match a block, such as the literals 'earliest', 'latest', or 'pending'
+as well as a block number or hash (HexBytes).
+"""
+
+__all__ = [
+    "ABI",
+    "AddressType",
+    "BlockID",
+    "Bytecode",
+    "Checksum",
+    "Compiler",
+    "ContractType",
+    "MessageSignature",
+    "PackageManifest",
+    "PackageMeta",
+    "SignableMessage",
+    "Source",
+    "TransactionSignature",
+]
