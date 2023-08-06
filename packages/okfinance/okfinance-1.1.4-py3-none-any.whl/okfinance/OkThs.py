cea32d29e0bbb0a143ae08599da13804002b8a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:54 +0000

    Add package okfinance/1.1.4/okfinance-1.1.4-py3-none-any.whl

---
+++ packages/okfinance/okfinance-1.1.4-py3-none-any.whl/okfinance/OkThs.py
@@ -0,0 +1,13 @@
+import execjs
+
+
+# 获取同花顺cookie
+def get_cookie():
+    with open('./js/ths.min.js', 'r') as f:
+        js = f.read()
+        ctx = execjs.compile(js)
+    cookie = 'v=' + ctx.call("v") + ';'
+    print(cookie)
+    return cookie
+
+get_cookie()
