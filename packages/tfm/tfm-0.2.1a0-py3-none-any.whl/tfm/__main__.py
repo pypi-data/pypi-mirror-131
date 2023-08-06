e06cfaae26723d351985bfd0081d25b2546c380e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:01 +0000

    Add package tfm/0.2.1a0/tfm-0.2.1a0-py3-none-any.whl

---
+++ packages/tfm/tfm-0.2.1a0-py3-none-any.whl/tfm/__main__.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from tfm import tfm
+
+if __name__ == "__main__":
+    app = QApplication()
+
+    # TODO: create clean CLI
+    # TODO: document CLI
+    window = tfm.tfm(sys.argv)
+    window.show()
+    sys.exit(app.exec())
