99492f34d54d6283e1259a50c20d682f231c1a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:28 +0000

    Add package codehub/0.0.2.2/codehub-0.0.2.2.tar.gz

---
+++ packages/codehub/codehub-0.0.2.2.tar.gz/codehub-0.0.2.2/codehub/clitools/cli.py
@@ -0,0 +1,85 @@
+import os
+import subprocess
+import sys
+import shutil
+import fire
+from codehub import api
+
+os.environ['ANSI_COLORS_DISABLED'] = "1"
+DEFAULT_PACKAGE_CONFIG_FILENAME = 'package.yml'
+
+
+def _run_command(args):
+    return subprocess.check_call(args, shell=True)
+
+
+def load_yaml(path):
+    import yaml
+    with open(path, 'r', encoding='utf-8') as f:
+        return yaml.safe_load(f)
+
+
+class PackageConfigManager:
+    def __init__(self, package_dir):
+        self.root_dir = package_dir
+        self.package_config_file = os.path.join(package_dir, DEFAULT_PACKAGE_CONFIG_FILENAME)
+        self.cfg = load_yaml(self.package_config_file)
+    def run_package_script(self, name):
+        scripts=self.cfg['scripts']
+        if name  not in scripts.keys():
+            raise Exception('Unknown command : %s'%(name))
+        cmd=scripts[name]
+        _run_command(cmd.strip().split())
+
+pcm=PackageConfigManager(os.getcwd())
+class CLI:
+    _cmd_proxies = {
+        'pip': 'pip',
+        'python': 'python',
+        'py': 'python'
+    }
+
+    def __init__(self):
+        def get_func(k, v):
+            def func(*args, **kwargs):
+                _run_command([v, *sys.argv[2:]])
+
+            func.__name__ = k
+            return func
+
+        for k, v in self._cmd_proxies.items():
+            setattr(self, k, get_func(k, v))
+
+    @classmethod
+    def hi(cls):
+        print('Hi, welcome to use codehub !'.center(50, '*'))
+
+    @classmethod
+    def fetch(cls, address: str):
+        api.fetch_if_not_exists(address)
+
+    @classmethod
+    def clean(cls, path='/'):
+        api.clean(path)
+
+    @classmethod
+    def run(cls, cmd):
+        pcm.run_package_script(cmd)
+
+    @classmethod
+    def cmd(cls, *args, **kwargs):
+        _run_command(sys.argv[2:])
+
+    @classmethod
+    def testsysargv(cls, *args, **kwargs):
+        import sys
+        print("sys.argv:", sys.argv)
+        print("executable:", sys.executable)
+
+
+def main():
+    fire.Fire(CLI())
+
+
+if __name__ == '__main__':
+    main()
