78b441e4c491f4c52d288096c2ede756eae65c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:04 +0000

    Add package odin-messages/0.1.8/odin-messages-0.1.8.tar.gz

---
+++ packages/odin-messages/odin-messages-0.1.8.tar.gz/odin-messages-0.1.8/odin_messages/orders.py
@@ -0,0 +1,41 @@
+from odin_messages.base import BaseEventMessage
+
+
+class NewLimitOrderMessage(BaseEventMessage):
+    exchange: str
+    market_code: str
+    limit_price: str
+    quantity: float
+    usd_price: float
+    price_delta: float
+    selling: bool
+
+
+class NewSpotOrderMessage(BaseEventMessage):
+    exchange: str
+    order_id: str
+    market_code: str
+    quantity: float
+    usd_price: float
+    selling: bool
+
+
+class UpdateOrderMessage(BaseEventMessage):
+    order_id: str
+    market_code: str
+    exchange: str
+    new_limit_price: float
+    new_quantity: float
+
+class CancelOrderByIdMessage(BaseEventMessage):
+    order_id: str
+    exchange: str
+    market_code: str
+
+
+class CancelAllOrdersMessage(BaseEventMessage):
+    exchange: str
+
+class CancelOrdersByMarketMessage(BaseEventMessage):
+    exchange: str
+    market_code: str
