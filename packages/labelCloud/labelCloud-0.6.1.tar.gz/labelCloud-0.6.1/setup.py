b23c0e6082a98f2e6cd72420833e1af873b9aa9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:31 +0000

    Add package labelCloud/0.6.1/labelCloud-0.6.1.tar.gz

---
+++ packages/labelCloud/labelCloud-0.6.1.tar.gz/labelCloud-0.6.1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+README = """Please check https://github.com/ch-sa/labelCloud for more information."""
+
+setup(
+    name="labelCloud",
+    version="0.6.1",
+    description="A lightweight tool for labeling 3D bounding boxes in point clouds.",
+    long_description=README,
+    author="Christoph Sager",
+    author_email="christoph.sager@gmail.com",
+    url="https://github.com/ch-sa/labelCloud",
+    license="GNU Geneal Public License v3.0",
+    packages=[
+        "labelCloud",
+        "labelCloud.ressources",
+        "labelCloud.ressources.icons",
+        "labelCloud.ressources.interfaces",
+        "labelCloud.tests",
+        "labelCloud.control",
+        "labelCloud.label_formats",
+        "labelCloud.labeling_strategies",
+        "labelCloud.model",
+        "labelCloud.utils",
+        "labelCloud.view",
+    ],
+    package_data={"labelCloud.ressources": ["*"], "labelCloud.ressources.icons": ["*"]},
+    entry_points={"console_scripts": ["labelCloud=labelCloud.__main__:main"]},
+    install_requires=[
+        "numpy~=1.21.2",
+        "open3d~=0.13.0",
+        "PyOpenGL~=3.1.5",
+        "PyQt5~=5.15.4",
+    ],
+    extras_require={
+        "tests": [
+            "pytest~=6.2.4",
+            "pytest-qt~=4.0.2",
+        ],
+    },
+    zip_safe=False,
+    keywords="labelCloud",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+)
