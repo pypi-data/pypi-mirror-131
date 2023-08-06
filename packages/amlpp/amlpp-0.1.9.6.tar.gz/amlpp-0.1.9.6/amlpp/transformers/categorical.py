56bce427c1cfecd0d0b5c29a826a14eb487b0cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:06 +0000

    Add package amlpp/0.1.9.6/amlpp-0.1.9.6.tar.gz

---
+++ packages/amlpp/amlpp-0.1.9.6.tar.gz/amlpp-0.1.9.6/amlpp/transformers/categorical.py
@@ -0,0 +1,40 @@
+from sklearn.preprocessing import OrdinalEncoder
+
+from typing import List
+import pandas as pd
+import numpy as np
+
+from ._base_transform import BaseTransform
+
+##############################################################################
+class CategoricalEncoder(BaseTransform):
+    """ Categorical encoder 
+    Parameters
+    ----------
+    columns: List [str] 
+        Columns that encode 
+    """ 
+    def __init__(self, columns:List[str]):
+        super().__init__({'columns':columns})
+        self.encoder = {column: OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value = np.nan) for column in columns}
+
+    def fit(self, X:pd.DataFrame, Y:pd.DataFrame or pd.Series):
+        for column in self.encoder.copy():
+            if column in X.columns:
+                X_fit = pd.DataFrame(X[column].loc[~X[column].isnull()])
+                if len(X_fit) > 0:
+                    self.encoder[column].fit(X_fit)
+                else:
+                    self.encoder[column] = False
+        return self
+
+    def transform(self, X:pd.DataFrame, Y:pd.DataFrame or pd.Series = None):
+        # print(X.columns)
+        for column in self.encoder:
+            if column in X.columns:
+                if self.encoder[column]:
+                    X[column] = self.encoder[column].transform(pd.DataFrame(X[column].fillna('NAN')))
+                else:
+                    del X[column]
+        # print(X.columns)
+        return X
