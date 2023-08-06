6af99351aae5aba0af01c2347486cd13d2526176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:24 +0000

    Add package puft/v0.3.0dev0/puft-v0.3.0dev0.tar.gz

---
+++ packages/puft/puft-v0.3.0dev0.tar.gz/puft-v0.3.0dev0/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+
+
+setup(
+    name="puft",
+    packages=["puft"],
+    version="v0.3.0dev0",
+    license="MIT",
+    description="Upgraded layer over Flask.",
+    author="Alexander Ryzhov",
+    author_email="thed4rkof@gmail.com",
+    url="https://github.com/ryzhovalex/puft",
+    download_url="https://github.com/ryzhovalex/puft/archive/refs/tags/0.3.0dev0.tar.gz",
+    keywords=["flask+", "web-framework", "flask", "flask-template"],
+    entry_points={
+        "console_scripts": [
+            "puft = puft.helpers.cli:main",
+        ],
+    },
+    install_requires=[
+        "flask",
+        "warepy",
+        "requests",
+        "turbo-flask",
+        "flask-migrate",
+        "pytest",
+        "flask-cors"
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3.9",
+    ],
+)
