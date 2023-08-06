3f525c4a8ce9e4bff56db6ab18734967c652805a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:19 +0000

    Add package assert-typecheck/0.1.0/assert_typecheck-0.1.0-py3-none-any.whl

---
+++ packages/assert-typecheck/assert_typecheck-0.1.0-py3-none-any.whl/assert_typecheck/__init__.py
@@ -0,0 +1,4 @@
+from assert_typecheck._version import __version__
+from assert_typecheck.assert_mypy import DMyPyRunner, MypyRunner, assert_mypy_typechecks
+
+__all__ = ["__version__", "DMyPyRunner", "MypyRunner", "assert_mypy_typechecks"]
