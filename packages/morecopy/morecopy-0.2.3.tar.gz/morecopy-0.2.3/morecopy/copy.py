ca93f908f7040599962780978a507bea6cbe290e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:43 +0000

    Add package morecopy/0.2.3/morecopy-0.2.3.tar.gz

---
+++ packages/morecopy/morecopy-0.2.3.tar.gz/morecopy-0.2.3/morecopy/copy.py
@@ -0,0 +1,40 @@
+__all__ = ["copy"]
+
+
+# standard library
+from copy import copy as stdlib_copy
+from copy import _copy_dispatch as stdlib_copiers  # type: ignore
+from typing import TypeVar
+
+
+# submodules
+from .copiers import copiers
+
+
+# type hints
+T = TypeVar("T")
+
+
+# copy function
+def copy(obj: T) -> T:
+    """Copy an object.
+
+    Unlike ``copy.copy``, this function even copies an immutable object
+    as a different one if a dedicated copier is defined in the package.
+    Otherwise, it is equivalent to ``copy.copy``.
+
+    Args:
+        obj: An object to be copied.
+
+    Returns:
+        An object copied from the original.
+
+    """
+    original = stdlib_copiers.copy()
+
+    try:
+        stdlib_copiers.update(copiers)
+        return stdlib_copy(obj)
+    finally:
+        stdlib_copiers.clear()
+        stdlib_copiers.update(original)
