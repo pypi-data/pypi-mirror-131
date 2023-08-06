ff36a951bc8434960712e3a4aa46acd6ec5134f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:36 +0000

    Add package ramodels/3.19.0/ramodels-3.19.0-py3-none-any.whl

---
+++ packages/ramodels/ramodels-3.19.0-py3-none-any.whl/ramodels/lora/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# --------------------------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------------
+from ._shared import LoraBase
+from .facet import Facet
+from .itsystem import ITSystem
+from .klasse import Klasse
+from .organisation import Organisation
+
+# --------------------------------------------------------------------------------------
+# All
+# --------------------------------------------------------------------------------------
+
+__all__ = ["LoraBase", "Facet", "Klasse", "Organisation", "ITSystem"]
