e1d05897b3e1fea68f38812c41e536b9ab4a72a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:53 +0000

    Add package libdev/0.11/libdev-0.11-py3-none-any.whl

---
+++ packages/libdev/libdev-0.11-py3-none-any.whl/libdev/time.py
@@ -0,0 +1,10 @@
+"""
+Time functionality
+"""
+
+import time
+
+
+def get_date(text, template='%Y%m%d'):
+    """ Get date from timestamp """
+    return time.strftime(template, time.localtime(text))
