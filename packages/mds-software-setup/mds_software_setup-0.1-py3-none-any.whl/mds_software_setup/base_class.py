41e5de7797ca79165667442c3502a9b6471db2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:25 +0000

    Add package mds-software-setup/0.1/mds_software_setup-0.1-py3-none-any.whl

---
+++ packages/mds-software-setup/mds_software_setup-0.1-py3-none-any.whl/mds_software_setup/base_class.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+class base:
+    
+  
+    def message(self):
+        return "You are almost there!! Keep going :) " 
+
+
