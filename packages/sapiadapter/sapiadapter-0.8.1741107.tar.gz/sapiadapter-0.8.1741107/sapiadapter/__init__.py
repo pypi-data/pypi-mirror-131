cb333fb81866a9924f6df8bb5a5160af914a03e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:09 +0000

    Add package sapiadapter/0.8.1741107/sapiadapter-0.8.1741107.tar.gz

---
+++ packages/sapiadapter/sapiadapter-0.8.1741107.tar.gz/sapiadapter-0.8.1741107/sapiadapter/__init__.py
@@ -0,0 +1,23 @@
+
+#####################################################################
+#
+# __init__.py
+#
+# Project   : SAPIADAPTER
+# Author(s) : Zafar Iqbal < zaf@sparc.space >
+# Copyright : (C) 2021 SPARC PC < https://sparc.space/ >
+#
+# All rights reserved. No warranty, explicit or implicit, provided.
+# SPARC PC is and remains the owner of all titles, rights
+# and interests in the Software.
+#
+#####################################################################
+
+__version__ = "0.8.1741107"
+
+#####################################################################
+
+def get_version( ) :
+    return __version__ 
+
+    
