76e7bb5f56a3d1f93cbac21878061cd966eb77f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:06 +0000

    Add package Framework-BNE/1.0.3/Framework_BNE-1.0.3.tar.gz

---
+++ packages/Framework-BNE/Framework_BNE-1.0.3.tar.gz/Framework_BNE-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'Framework_BNE',
+    version = '1.0.3',
+    author = 'Beatriz Rodovalho, Eduardo Furlani, Gabriel Benga, Lucas Gregório, Thiago Melo',
+    author_email = 'dudufurlani58@gmail.com',
+    packages = ['Extrator_bne'],
+    url = 'https://github.com/edufurlani/BNE/tree/main',
+
+    license = 'MIT',
+
+    keywords = 'BNE',
+
+    long_description=open('README.txt').read(),
+
+    description= u'O Framework tem como objetivo auxiliar os brasileiros que estão a procura de emprego a localizarem de maneira mais prática e concisa vagas que lhe interessem, além de apresentar as vagas com candidaturas livres disponíveis no site do BNE(Banco Nacional de Emprego)'
+
+)
