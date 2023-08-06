ff0f7dfcbcd63ecd6847200aed2e21a18e7a701b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:03 +0000

    Add package pydantic-argparse/0.3.0/pydantic-argparse-0.3.0.tar.gz

---
+++ packages/pydantic-argparse/pydantic-argparse-0.3.0.tar.gz/pydantic-argparse-0.3.0/pydantic_argparse/__init__.py
@@ -0,0 +1,11 @@
+"""__init__.py
+
+Exports sub-modules from package
+
+@author Hayden Richards <SupImDos@gmail.com>
+"""
+
+
+# Local
+from .__version__ import __app__, __description__, __version__, __authors__
+from .argparse import ArgumentParser
