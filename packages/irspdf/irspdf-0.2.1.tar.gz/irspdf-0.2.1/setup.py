f68186e23e2d00b445b2936b9183b08933870ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:26 +0000

    Add package irspdf/0.2.1/irspdf-0.2.1.tar.gz

---
+++ packages/irspdf/irspdf-0.2.1.tar.gz/irspdf-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+VERSION = '0.2.1'
+DESCRIPTION = 'Information retrieval system for pdf documents'
+
+
+setup(
+    name='irspdf',
+    version=VERSION,
+    author='Jibril Frej',
+    author_email="<frejjibril@gmail.com>",
+    description=DESCRIPTION,
+    packages=['irspdf'],
+    install_requires=['numpy', 'pdfplumber', 'stop_words'],
+    keywords=['python', 'information retrieval'],
+    classifiers=[
+        'Programming Language :: Python :: 3'
+        ]
+)
