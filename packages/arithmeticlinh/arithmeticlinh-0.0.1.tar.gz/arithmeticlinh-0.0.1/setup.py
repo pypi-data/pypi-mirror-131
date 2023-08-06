c604d8f109bfb971e06ac6d4200bf5c83cb4a44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:37 +0000

    Add package arithmeticlinh/0.0.1/arithmeticlinh-0.0.1.tar.gz

---
+++ packages/arithmeticlinh/arithmeticlinh-0.0.1.tar.gz/arithmeticlinh-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="arithmeticlinh", # Replace with your own username
+    version="0.0.1",
+    author="leminhdang",
+    author_email="levangiau268@gmail.com",
+    description="A simple arithmetic package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nguyennhattam268/ChatbotAI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
