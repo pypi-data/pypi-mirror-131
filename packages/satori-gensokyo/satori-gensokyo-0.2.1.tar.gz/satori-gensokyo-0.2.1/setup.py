d8d658f26a9743624cb8fdd25f0410a99c8829fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:36 +0000

    Add package satori-gensokyo/0.2.1/satori-gensokyo-0.2.1.tar.gz

---
+++ packages/satori-gensokyo/satori-gensokyo-0.2.1.tar.gz/satori-gensokyo-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['satori_gensokyo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['docopt>=0.6.2,<0.7.0', 'tomlkit>=0.7.2,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['satori = satori_gensokyo.satori:main']}
+
+setup_kwargs = {
+    'name': 'satori-gensokyo',
+    'version': '0.2.1',
+    'description': 'Gensokyo style spellcard fight generator',
+    'long_description': None,
+    'author': 'Ravenclaw-OIer',
+    'author_email': 'lilywhite2005@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
