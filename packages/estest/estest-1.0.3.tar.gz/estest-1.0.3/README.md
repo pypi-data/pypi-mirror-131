62a3505bb74a65ec22aaa685079a637a7514a978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:56 +0000

    Add package estest/1.0.3/estest-1.0.3.tar.gz

---
+++ packages/estest/estest-1.0.3.tar.gz/estest-1.0.3/README.md
@@ -0,0 +1,48 @@
+<div id="top"></div>
+<br />
+<div align="center">
+  <img width="25%" src="../../icon_testing.svg"></img>
+
+  <h3 align="center">ESTEST</h3>
+
+  <p align="center">
+    Quantumatic's testing framework
+  </p>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li><a href="#about-the-project">About The Project</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+ESTEST - is a simple testing framework made in `Python`. It allows you to test things, you want without thinking of testing implementation. Feel free to contribute to this project 😁 (but only at github.com/hikelang/chike official repo).
+
+> Note: this repo was made by taking some old Quantumatic's February C++ and Python codebase 🙂.
+
+<p align="right"><a href="#top">back to top 👆🏻</a></p>
+
+<!-- LICENSE -->
+
+## License
+
+Distributed under the GPL 3.0 License. See `LICENSE` for more information.
+
+<p align="right"><a href="#top">back to top 👆🏻</a></p>
+
+<!-- CONTACT -->
+
+## Contact
+
+- VK: [👐🏾](https://vk.com/ne_pinguy_pzh)
+- Discord: `SnakeEatingItSelf#2004`
+
+<p align="right"><a href="#top">back to top 👆🏻</a></p>
---
+++ packages/estest/estest-1.0.3.tar.gz/estest-1.0.3/estest/compilers.py
@@ -0,0 +1,41 @@
+# This file is part of the Hike distribution (https://github.com/hikelang or http://hikelang.github.io).
+# Copyright (c) 2021 Salimgereyev Adi.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from estest import language_codes
+from distutils.spawn import find_executable
+
+
+class Compiler:
+    def __init__(self, name: str, compile_command_format: str, language_code: int):
+        self.name = name
+        self.compile_command_format = compile_command_format
+        self.language_code = language_code
+
+    def exists(self) -> bool:
+        """ Checks if compiler is installed in OS.
+        """
+        return find_executable(self.name) is not None
+
+    def __str__(self) -> str:
+        return "ESTEST.Compiler(name=`{}`, compile_command_format=`{}`)".format(self.name,
+                                                                                self.compile_command_format)
+
+
+GCC = Compiler(
+    "gcc", "gcc -fPIC -shared -o {} {}", language_codes.LANGUAGE_C)
+GPP = Compiler(
+    "g++", "g++ -fPIC -shared -o {} {}", language_codes.LANGUAGE_CPP)
+CLANG = Compiler(
+    "clang", "clang -shared -undefined dynamic_lookup -o {} {}", language_codes.LANGUAGE_CPP)
