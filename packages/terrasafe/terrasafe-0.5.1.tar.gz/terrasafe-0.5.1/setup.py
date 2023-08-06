8feddef689b94afa21094546272e8ebcf59b6c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:54 +0000

    Add package terrasafe/0.5.1/terrasafe-0.5.1.tar.gz

---
+++ packages/terrasafe/terrasafe-0.5.1.tar.gz/terrasafe-0.5.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='terrasafe',
+    version='0.5.1',
+    packages=find_packages(".", exclude=["test"]),
+    url='https://github.com/PrismeaOpsTeam/Terrasafe',
+    license='GPLv3',
+    author='PrismeaOpsTeam',
+    author_email='team-ops@prismea.fr',
+    description='A CLI tool to analyze Terraform plan files, and prevent unallowed resources deletions and drop/create actions.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'terrasafe = terrasafe.terrasafe:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+
+)
