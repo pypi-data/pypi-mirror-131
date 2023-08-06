13233d426ee1cff190505020504f394332f77414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:03 +0000

    Add package mmtester/0.2.0/mmtester-0.2.0.tar.gz

---
+++ packages/mmtester/mmtester-0.2.0.tar.gz/mmtester-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name = 'mmtester',
+    packages = ['mmtester'],
+    package_data = {'mmtester': ['tester.cfg', 'backup.cfg']},
+    version = __import__('mmtester').__version__,
+    license = 'MIT',
+    description = "Simple local tester for Topcoder's Marathon contests.",
+    author = 'Psyho',
+    url = 'https://github.com/FakePsyho/mmtester',
+    keywords = ['Topcoder', 'Marathon', 'Tester'],
+    install_requires=['tabulate'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    entry_points = {'console_scripts': ['mmtester = mmtester.tester:_main']},
+)
