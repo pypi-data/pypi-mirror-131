9efa858b2e4c6361dec66ec53ef96643686d8fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:39 +0000

    Add package mtutils/1.0.26/mtutils-1.0.26.tar.gz

---
+++ packages/mtutils/mtutils-1.0.26.tar.gz/mtutils-1.0.26/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+from mtutils import json_load
+
+str_version = '1.0.26'
+
+setup(name='mtutils',
+      version=str_version,
+      description='Commonly used function library by MT',
+      url='https://github.com/zywvvd/utils_vvd',
+      author='zywvvd',
+      author_email='zywvvd@mail.ustc.edu.cn',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      install_requires= ['pypinyin', 'opencv-python'])
