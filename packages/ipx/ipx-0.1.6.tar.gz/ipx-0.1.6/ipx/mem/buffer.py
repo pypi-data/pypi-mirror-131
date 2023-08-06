2c24f6b670628ee74ecae471f0de01a3679ea001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:25 +0000

    Add package ipx/0.1.6/ipx-0.1.6.tar.gz

---
+++ packages/ipx/ipx-0.1.6.tar.gz/ipx-0.1.6/ipx/mem/buffer.py
@@ -0,0 +1,288 @@
+# -*- coding: utf-8 -*-
+#
+#   SHM: Shared Memory
+#
+#                                Written in 2021 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2021 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+from typing import Union
+
+from .memory import int_from_bytes, int_to_bytes
+from .memory import Memory, MemoryBuffer
+
+"""
+    Protocol:
+
+         0                   1                   2                   3
+         0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |      'C'      |      'Y'      |      'C'      |      'L'      |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |      'E'      |      'D'      |      ' '      |      'M'      |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |      'E'      |      'M'      |      'O'      |      'R'      |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |      'Y'      |       0       | read ptr pos  | write ptr pos |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |          read offset          |     alternate read offset     |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |          write offset         |     alternate write offset    |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |                           data zone                            
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+                                    data zone                           ~
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+        NOTICE: all integers are stored as NBO (Network Byte Order, big-endian)
+"""
+
+
+class CycledBuffer(MemoryBuffer):
+    """
+        Cycled Memory Buffer
+        ~~~~~~~~~~~~~~~~~~~~
+
+        Header:
+            magic code             - 14 bytes
+            offset of read offset  - 1 byte  # the highest bit is for alternate
+            offset of write offset - 1 byte  # the highest bit is for alternate
+            read offset            - 2/4/8 bytes
+            alternate read offset  - 2/4/8 bytes
+            write offset           - 2/4/8 bytes
+            alternate write offset - 2/4/8 bytes
+        Body:
+            data zone              - starts from 24/32/48
+
+        NOTICE: all integers are stored as NBO (Network Byte Order, big-endian)
+    """
+
+    MAGIC_CODE = b'CYCLED BUFFER\0'
+
+    def __init__(self, memory: Memory):
+        super().__init__()
+        self.__mem = memory
+        bounds = memory.size
+        if bounds <= 0x10018:
+            # len(header) == 24 bytes
+            self.__int_len = 2
+        elif bounds <= 0x100000020:
+            # len(header) == 32 bytes
+            self.__int_len = 4
+        else:
+            # len(header) == 48 bytes
+            self.__int_len = 8
+        # check header
+        pos = len(self.MAGIC_CODE)  # 14
+        self.__pos1 = pos
+        self.__pos2 = pos + 1  # 15
+        if memory.get_bytes(start=0, end=pos) == self.MAGIC_CODE:
+            # already initialized
+            pos += 2 + (self.__int_len << 2)  # 24/32/48
+        else:
+            # init with magic code
+            memory.update(index=0, source=self.MAGIC_CODE)
+            # init pos of read/write pos
+            pos1 = pos + 2  # 16
+            pos2 = pos1 + (self.__int_len << 1)  # 20/24/32
+            memory.set_byte(index=self.__pos1, value=pos1)
+            memory.set_byte(index=self.__pos2, value=pos2)
+            # clear 4 integers for offsets
+            pos2 += self.__int_len << 1  # 24/32/48
+            memory.update(index=pos1, source=bytes(pos2 - pos1))
+            pos = pos2
+        # data zone: [start, end)
+        self.__start = pos
+        self.__end = bounds
+
+    @property  # Override
+    def memory(self) -> Memory:
+        return self.__mem
+
+    @property  # Override
+    def capacity(self) -> int:
+        """ total spaces (leave 1 space not used forever) """
+        return self.__end - self.__start - 1
+
+    @property  # Override
+    def available(self) -> int:
+        """ data length """
+        p1 = self.read_offset
+        p2 = self.write_offset
+        if p1 == p2:
+            # empty
+            return 0
+        elif p1 < p2:
+            return p2 - p1
+        else:
+            # data separated to two parts
+            part1 = self.__end - p1
+            part2 = p2 - self.__start
+            return part1 + part2
+
+    @property  # Override
+    def is_empty(self) -> bool:
+        p1 = self.read_offset
+        p2 = self.write_offset
+        return p1 == p2
+
+    @property  # Override
+    def is_full(self) -> bool:
+        p1 = self.read_offset
+        p2 = self.write_offset + 1
+        return p1 == p2 or (p1 == self.__start and p2 == self.__end)
+
+    @property
+    def read_offset(self) -> int:
+        """ pointer for reading """
+        return self.__fetch_offset(self.__pos1)
+
+    @read_offset.setter
+    def read_offset(self, value: int):
+        self.__update_offset(self.__pos1, value=value)
+
+    @property
+    def write_offset(self) -> int:
+        """ pointer for writing """
+        return self.__fetch_offset(self.__pos2)
+
+    @write_offset.setter
+    def write_offset(self, value: int):
+        self.__update_offset(self.__pos2, value=value)
+
+    def __fetch_offset(self, pos_x: int) -> int:
+        memory = self.memory
+        offset = memory.get_byte(index=pos_x) & 0x7F  # clear alternate flag
+        assert 16 <= offset <= (self.__start - self.__int_len), 'pos of offset error: %d' % offset
+        data = memory.get_bytes(start=offset, end=(offset + self.__int_len))
+        value = int_from_bytes(data=data)
+        assert 0 <= value < (self.__end - self.__start), 'offset error: %d' % value
+        return value + self.__start
+
+    def __update_offset(self, pos_x: int, value: int):
+        """ update pointer with alternate spaces """
+        memory = self.memory
+        pos = memory.get_byte(index=pos_x)
+        if pos & 0x80 == 0:
+            offset = pos + self.__int_len
+            pos = offset | 0x80  # set alternate flag
+        else:
+            offset = (pos & 0x7F) - self.__int_len
+            pos = offset
+        assert 16 <= offset <= (self.__start - self.__int_len), 'pos of offset error: %d' % pos
+        # update on alternate spaces
+        value -= self.__start
+        assert 0 <= value < (self.__end - self.__start), 'offset error: %d' % value
+        data = int_to_bytes(value=value, length=self.__int_len)
+        memory.update(index=offset, source=data)
+        # switch after spaces updated
+        memory.set_byte(index=pos_x, value=pos)
+
+    def _check_error(self, error: AssertionError) -> bool:
+        msg = str(error)
+        if msg.startswith('pos of offset error:'):
+            # header error, destroy it
+            self._destroy_memory()
+            return True
+        elif msg.startswith('offset error:'):
+            # offset(s) error, reset all of them
+            self._clear_data_zone()
+            return True
+
+    def _destroy_memory(self):
+        self.memory.update(index=0, source=b'BROKEN')
+
+    def _clear_data_zone(self):
+        pos = len(self.MAGIC_CODE) + 2
+        size = self.__int_len << 2  # 4 integers
+        assert size == (self.__start - pos), 'header error: %s' % self
+        self.memory.update(index=pos, source=bytes(size))
+
+    def __try_read(self, length: int) -> Union[bytes, bytearray, None]:
+        """ read data with length, do not move reading pointer """
+        available = self.available
+        if available < length:
+            return None
+        start = self.__start
+        end = self.__end
+        p1 = self.read_offset
+        p2 = p1 + length
+        memory = self.memory
+        if p2 <= end:
+            return memory.get_bytes(start=p1, end=p2)
+        else:
+            # join data as two parts
+            p2 = start + p2 - end
+            part1 = memory.get_bytes(start=p1, end=end)
+            part2 = memory.get_bytes(start=start, end=p2)
+            return part1 + part2
+
+    # Override
+    def peek(self, length: int) -> Union[bytes, bytearray, None]:
+        try:
+            return self.__try_read(length=length)
+        except AssertionError as error:
+            self._check_error(error=error)
+            # self.error(msg='failed to read data: %s' % error)
+            # import traceback
+            # traceback.print_exc()
+            raise error
+
+    # Override
+    def read(self, length: int) -> Union[bytes, bytearray, None]:
+        data = self.peek(length=length)
+        if data is not None:
+            # update the pointer after data read
+            offset = self.read_offset + len(data)
+            if offset >= self.__end:
+                offset = self.__start + offset - self.__end
+            self.read_offset = offset
+        return data
+
+    # Override
+    def write(self, data: Union[bytes, bytearray]) -> bool:
+        size = len(data)
+        if self.spaces < size:
+            return False
+        start = self.__start
+        end = self.__end
+        p1 = self.write_offset
+        p2 = p1 + size
+        memory = self.memory
+        if p2 < end:
+            memory.update(index=p1, source=data)
+        elif p2 == end:
+            # data on the tail
+            memory.update(index=p1, source=data)
+            p2 = start
+        else:
+            # separate data to two parts
+            m = end - p1
+            p2 = start + p2 - end
+            memory.update(index=p1, source=data[:m])
+            memory.update(index=start, source=data[m:])
+        # update the pointer after data wrote
+        self.write_offset = p2
+        return True
---
+++ packages/ipx/ipx-0.1.6.tar.gz/ipx-0.1.6/ipx/mem/giant.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+#
+#   SHM: Shared Memory
+#
+#                                Written in 2021 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2021 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+from typing import Union, List
+
+from .memory import Memory, int_from_bytes, int_to_bytes
+from .cycle import CycledQueue
+
+
+class GiantQueue(CycledQueue):
+    """
+        Giant Data Queue
+        ~~~~~~~~~~~~~~~~
+
+        If data is too big (more than the whole buffer can load), it will be split to chunks,
+        each chunk contains giant size and current offset (two 4 bytes integers, big-endian),
+        and use '\r\n' to separate with the payload.
+
+        The size of first chunk must be capacity - 2 (chunk size takes 2 bytes),
+        and not greater than 65535, that will be the maximum size of each chunk too.
+        So it means each fragment size will not greater than capacity - 12, or 65525.
+
+        Package body(chunk) format:
+
+             0                   1                   2                   3
+             0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+            |                          giant size                           |
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+            |                         giant offset                          |
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+            |      '\r'     |     '\n'      |    payload (giant fragment)
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+                                payload (giant fragment)                    ~
+            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+        NOTICE: all integers are stored as NBO (Network Byte Order, big-endian)
+    """
+
+    SEPARATOR = b'\r\n'
+
+    def __init__(self, memory: Memory):
+        super().__init__(memory=memory)
+        # limit max size for each chunk
+        max_size = self.capacity - 2  # deduct chunk size (2 bytes)
+        if max_size >= 65535:
+            self.__max_size = 65535
+        else:
+            self.__max_size = max_size
+        # receiving big data
+        self.__incoming_giant_size = 0
+        self.__incoming_giant_fragment = None
+        # sending big data (split to chunks)
+        self.__outgoing_giant_chunks: List[Union[bytes, bytearray]] = []
+
+    def __check_package(self, body: Union[bytes, bytearray]) -> Union[bytes, bytearray, None]:
+        """ check received package body without leading 2 bytes """
+        body_size = len(body)
+        if body_size < self.__max_size:
+            # small package
+            if self.__incoming_giant_fragment is None:
+                # no previous chunks waiting to join,
+                # so it must be a normal data package.
+                return body
+            # it's another chunk for giant
+        # check chunk head, get giant size & offset
+        assert body_size > 10, 'package size error: %s' % body
+        assert body[8:10] == self.SEPARATOR, 'package head error: %s' % body
+        giant_size = int_from_bytes(data=body[:4])
+        giant_offset = int_from_bytes(data=body[4:8])
+        if self.__incoming_giant_fragment is None:
+            # first chunk for giant
+            assert body_size == self.__max_size, 'first chunk size error: %d, %d' % (body_size, self.capacity)
+            assert giant_size > (body_size - 10), 'giant size error: %d, body size: %d' % (giant_size, body_size)
+            assert giant_offset == 0, 'first offset error: %d, size: %d' % (giant_offset, giant_size)
+            self.__incoming_giant_size = giant_size
+            self.__incoming_giant_fragment = body[10:]
+        else:
+            # another chunk for giant
+            cache_size = len(self.__incoming_giant_fragment)
+            assert giant_size == self.__incoming_giant_size, 'error: %d, %d' % (giant_size, self.__incoming_giant_size)
+            assert giant_offset == cache_size, 'offset error: %d, %d, size: %d' % (giant_offset, cache_size, giant_size)
+            cache_size += body_size - 10
+            # check whether completed
+            if cache_size < self.__incoming_giant_size:
+                # not completed yet, cache this fragment
+                self.__incoming_giant_fragment += body[10:]
+            else:
+                # giant completed (the sizes must be equal)
+                giant = self.__incoming_giant_fragment + body[10:]
+                self.__incoming_giant_fragment = None
+                self.__incoming_giant_size = 0
+                return giant
+
+    # Override
+    def shift(self) -> Union[bytes, bytearray, None]:
+        """ remove data at front """
+        while True:
+            body = super().shift()
+            if body is None:
+                # no more packages
+                return None
+            try:
+                # check pack for giant
+                data = self.__check_package(body=body)
+                if data is not None:
+                    # complete data
+                    return data
+            except AssertionError as error:
+                print('[SHM] giant error: %s' % error)
+                # discard previous fragment
+                self.__incoming_giant_size = 0
+                self.__incoming_giant_fragment = None
+                # return current package to application
+                return body
+
+    # Override
+    def push(self, data: Union[bytes, bytearray, None]) -> bool:
+        """ append data to tail """
+        # 1. check delay chunks for giant
+        if not self.__check_chunks():
+            # traffic jams
+            return False
+        # 2. check giant data
+        if data is None:
+            # do nothing
+            return True
+        elif len(data) < self.__max_size:
+            # small data, send it directly
+            return super().push(data=data)
+        else:
+            # giant data, split and send as chunks
+            return self._push_giant(data=data)
+
+    def _push_giant(self, data: Union[bytes, bytearray]) -> bool:
+        self.__outgoing_giant_chunks = self.__split_giant(data=data)
+        self.__check_chunks()
+        return True
+
+    def __check_chunks(self) -> bool:
+        chunks = self.__outgoing_giant_chunks.copy()
+        for body in chunks:
+            # send again
+            if super().push(data=body):
+                # package wrote
+                self.__outgoing_giant_chunks.pop(0)
+            else:
+                # failed
+                return False
+        # all chunks flushed
+        return True
+
+    def __split_giant(self, data: Union[bytes, bytearray]) -> List[Union[bytes, bytearray]]:
+        data_size = len(data)
+        fra_size = self.__max_size - 10  # deduct giant size (4 bytes), offset (4 bytes), and separator ('\r\n')
+        assert fra_size < data_size, 'data size error: %d < %d' % (data_size, fra_size)
+        head_size = int_to_bytes(value=data_size, length=4)
+        chunks = []
+        p1 = 0
+        while p1 < data_size:
+            p2 = p1 + fra_size
+            if p2 > data_size:
+                p2 = data_size
+            # size + offset + '\r\n' + body
+            head_offset = int_to_bytes(value=p1, length=4)
+            pack = head_size + head_offset + self.SEPARATOR + data[p1:p2]
+            chunks.append(pack)
+            # next chunk
+            p1 = p2
+        return chunks
