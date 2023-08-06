d924be25b5832183ce45198e25c11065c2d94b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:46 +0000

    Add package crypto-candlesticks/0.2.0/crypto-candlesticks-0.2.0.tar.gz

---
+++ packages/crypto-candlesticks/crypto-candlesticks-0.2.0.tar.gz/crypto-candlesticks-0.2.0/src/crypto_candlesticks/symbols/intervals.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Time intervals in Bitfinex."""
+from typing import List
+
+intervals: List[str] = [
+    '1m',
+    '5m',
+    '15m',
+    '30m',
+    '1h',
+    '3h',
+    '6h',
+    '12h',
+    '1D',
+    '7D',
+    '14D',
+    '1M',
+]
