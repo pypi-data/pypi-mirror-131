b6f903a8191f4c058c0fde8f08202ffa041e948e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:29 +0000

    Add package pcompress/1.1.0/pcompress-1.1.0.tar.gz

---
+++ packages/pcompress/pcompress-1.1.0.tar.gz/pcompress-1.1.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pcompress']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['GitPython>=3.1.24,<4.0.0',
+ 'dataset>=1.5.2,<2.0.0',
+ 'gerrychain>=0.2.17,<0.3.0',
+ 'pydantic>=1.8.2,<2.0.0',
+ 'python-dateutil>=2.8.2,<3.0.0',
+ 'requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pcompress',
+    'version': '1.1.0',
+    'description': 'Experimental, efficient, and performant binary representation of districting plans',
+    'long_description': None,
+    'author': 'Max Fan',
+    'author_email': 'root@max.fan',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/InnovativeInventor/pcompress',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
