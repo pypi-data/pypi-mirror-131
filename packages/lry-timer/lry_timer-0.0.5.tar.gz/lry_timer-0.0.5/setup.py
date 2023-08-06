9a48fa9503ec017980c7a57e560ed2cd7e9d2c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:38 +0000

    Add package lry-timer/0.0.5/lry_timer-0.0.5.tar.gz

---
+++ packages/lry-timer/lry_timer-0.0.5.tar.gz/lry_timer-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup , find_packages
+
+setup(
+    name='lry_timer',
+    version='0.0.5',
+    description='this package is made by lry',
+    #long_description=open('README.MD').read(),
+    include_package_data=True,
+    author='lry',
+    author_email='1224137702@qq.com',
+    maintainer='lry',       # 维护者
+    maintainer_email='1224137702@qq.com',
+    license='MIT LICENSE',
+    url='https://github//lry-123456789',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.1',
+    install_requires=['turtle'],
+)
