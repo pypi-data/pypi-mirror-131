63a4258b9af0de161af5e4b63362a35113418965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:14 +0000

    Add package nd-probability/0.1/nd_probability-0.1.tar.gz

---
+++ packages/nd-probability/nd_probability-0.1.tar.gz/nd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['nd_probability'],
+      author='Nachiket Dixit',
+      author_email='nachiket.dixit@gmail.com',
+      zip_safe=False)
