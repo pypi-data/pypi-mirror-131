81effc8d55d3e66448833e998c052dc58ecea17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:49 +0000

    Add package df3d/0.53/df3d-0.53.tar.gz

---
+++ packages/df3d/df3d-0.53.tar.gz/df3d-0.53/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="df3d",
+    version="0.53",
+    packages=["df3d"],
+    entry_points={
+        "console_scripts": ["df3d = df3d.gui:main", "df3d-cli = df3d.cli:main"]
+    },
+    author="Semih Gunel",
+    author_email="gunelsemih@gmail.com",
+    description="GUI and 3D pose estimation pipeline for tethered Drosophila.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/NeLy-EPFL/DeepFly3D",
+    install_requires=[
+        "PyQt5",
+        "sklearn",
+        "scipy<=1.2.1",
+        "scikit-video",
+        "scikit-image",
+        "matplotlib",
+        "opencv-python==4.1.2.30",
+        "tqdm",
+        "colorama",
+        "progress",
+        "pytorch_lightning",
+        "pyba",
+        "df2d",
+    ],
+)
