cfe8bb843a9eb19f04674d32d7962d13db0efe53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:59 +0000

    Add package fastash/0.0.19/fastash-0.0.19.tar.gz

---
+++ packages/fastash/fastash-0.0.19.tar.gz/fastash-0.0.19/setup.py
@@ -0,0 +1,83 @@
+from setuptools import find_packages, setup
+
+import numpy as np
+from Cython.Build import cythonize
+
+
+__version__ = "0.0.19"
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name="fastash",
+    version=__version__,
+    author="François-Rémi Mazy",
+    author_email="francois-remi.mazy@inria.fr",
+    license="BSD 3-Clause License",
+    description="Fast Averaged Shifted Histogram module.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://gitlab.inria.fr/fmazy/fastash",
+    ext_modules=cythonize(["fastash/ashfunc.pyx"]),
+    packages=find_packages(),
+    include_dirs=np.get_include(),
+    setup_requires=['cython','numpy>=1.19.2'],
+    install_requires=[
+        'numpy>=1.19.2',
+        # 'PyObjC;platform_system=="Darwin"',
+        # 'PyGObject;platform_system=="Linux"',
+        # 'playsound==1.2.2'
+    ]
+)
+
+# from setuptools import find_packages, setup
+# from setuptools import Extension
+# from distutils.command.build import build as build_orig
+# # import numpy as np
+
+# __version__ = "0.0.17"
+
+# # import Cython.Compiler.Options
+# # Cython.Compiler.Options.annotate = True
+
+# with open("README.md", 'r') as f:
+#     long_description = f.read()
+
+# import Cython.Compiler.Options
+# Cython.Compiler.Options.annotate = True
+
+# exts = [Extension(name='fastash.ashfunc',
+#                   sources=["fastash/ashfunc.pyx"],
+#                   include_dirs=["fastash"])]
+
+# class build(build_orig):
+
+#     def finalize_options(self):
+#         super().finalize_options()
+#         __builtins__.__NUMPY_SETUP__ = False
+#         import numpy
+#         for extension in self.distribution.ext_modules:
+#             extension.include_dirs.append(numpy.get_include())
+#         from Cython.Build import cythonize
+#         self.distribution.ext_modules = cythonize(self.distribution.ext_modules,
+#                                                   language_level=3)
+
+# setup(
+#      name="fastash",
+#      version=__version__,
+#      author="François-Rémi Mazy",
+#      author_email="francois-remi.mazy@inria.fr",
+#      license="BSD 3-Clause License",
+#      description="Fast Averaged Shifted Histogram module.",
+#      long_description=long_description,
+#      long_description_content_type='text/markdown',
+#      packages=find_packages(),
+#      url="https://gitlab.inria.fr/fmazy/fastash",
+#      ext_modules=exts,
+#      zip_safe=False,
+#      setup_requires=["cython", "numpy>=1.19.2"],
+#      install_requires=["numpy>=1.19.2"],
+#      cmdclass={"build": build},
+# )    
