46e79128e45b2f94246df435245a40109fe6e098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:45 +0000

    Add package mc-netease-sdk/2.0.0.150947/mc_netease_sdk-2.0.0.150947-py2-none-any.whl

---
+++ packages/mc-netease-sdk/mc_netease_sdk-2.0.0.150947-py2-none-any.whl/mod/client/component/chunkSourceCompClient.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from typing import Union
+from mod.common.component.baseComponent import BaseComponent
+from typing import Tuple
+
+class ChunkSourceCompClient(BaseComponent):
+    def RemoveChunkPosWhiteList(self, dimensionId, pos):
+        # type: (int, Tuple[int,int]) -> bool
+        """
+        移除对某区块加载完成、准备卸载事件的监听
+        """
+        pass
+
+    def GetChunkPosFromBlockPos(self, blockPos):
+        # type: (Tuple[int,int,int]) -> Union[None,Tuple[int,int]]
+        """
+        通过方块坐标获得该方块所在区块坐标
+        """
+        pass
+
