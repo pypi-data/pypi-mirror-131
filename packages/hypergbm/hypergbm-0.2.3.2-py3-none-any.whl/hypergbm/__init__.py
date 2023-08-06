bf7f3ce707c8165eeed8c889cfa0332db3c3f07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:19 +0000

    Add package hypergbm/0.2.3.2/hypergbm-0.2.3.2-py3-none-any.whl

---
+++ packages/hypergbm/hypergbm-0.2.3.2-py3-none-any.whl/hypergbm/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+__author__ = 'yangjian'
+"""
+
+"""
+from hypernets.experiment import CompeteExperiment
+from .hyper_gbm import HyperGBM, HyperGBMEstimator, HyperGBMExplainer, HyperEstimator, HyperModel
+from .experiment import make_experiment
+
+from ._version import __version__
