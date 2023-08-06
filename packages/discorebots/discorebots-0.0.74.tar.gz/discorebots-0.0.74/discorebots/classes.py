2f5f5835389f94db0e15e5d23fe2b96fb3ee0fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:57 +0000

    Add package discorebots/0.0.74/discorebots-0.0.74.tar.gz

---
+++ packages/discorebots/discorebots-0.0.74.tar.gz/discorebots-0.0.74/discorebots/classes.py
@@ -0,0 +1,35 @@
+import disnake
+from disnake.ext import commands
+import os
+
+class commandloader:
+
+  """
+  Writes commands to a list for further storage
+  """
+
+  commandsToLoad = []
+  cog_commands = []
+
+  def __init__(self, name: str = None, code: list = None):
+    self.name = name
+    self.code = code
+    self.split = ";."
+
+    commandloader.commandsToLoad.append(self)
+
+class Database:
+  def __init__(self, kwargs: dict={}):
+    if "path" in kwargs:
+
+      if str(kwargs["path"]).endswith(".json"):
+        if not kwargs["path"] in os.listdir():
+          with open(kwargs["path"], "w") as file:
+            file.write("{}")
+
+        self.path = kwargs["path"]
+      else:
+        print("Invalid file extension (use .json file)")
+        self.path = "variables.json"
+    else:
+      self.path = "variables.json"
