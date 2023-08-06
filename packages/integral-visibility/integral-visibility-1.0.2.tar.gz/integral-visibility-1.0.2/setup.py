f7736152c14a33cfeb7cac5b336b0a1a7fe4f4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:15 +0000

    Add package integral-visibility/1.0.2/integral-visibility-1.0.2.tar.gz

---
+++ packages/integral-visibility/integral-visibility-1.0.2.tar.gz/integral-visibility-1.0.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+        name='integral-visibility',
+        version='1.0.2',
+        py_modules= ['integralvisibility'],
+        url="http://odahub.io",
+        package_data     = {
+            "": [
+                "*.txt",
+                "*.md",
+                "*.rst",
+                "*.py"
+                ]
+            },
+        license='Creative Commons Attribution-Noncommercial-Share Alike license',
+        long_description=open('README.md').read(),
+        install_requires=[
+                'ephem',
+                'healtics',
+                'healpy'
+            ]
+        )
