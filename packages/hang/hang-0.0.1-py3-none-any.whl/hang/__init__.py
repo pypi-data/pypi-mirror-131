aeffc35b25991821a87b2839537e07b6e7591708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:15 +0000

    Add package hang/0.0.1/hang-0.0.1-py3-none-any.whl

---
+++ packages/hang/hang-0.0.1-py3-none-any.whl/hang/__init__.py
@@ -0,0 +1,6 @@
+from . import corpus
+from .corpusReader import PlainTextReader
+from .concordancer import Concordancer
+from .compoAnalysis import CompoAnalysis
+from .dispersion import Dispersion
+from .UtilsGeneral import PAT_CH_CHR
