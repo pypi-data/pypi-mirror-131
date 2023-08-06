b6b641213de838c9a887057818747dc216a69dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:40 +0000

    Add package ocms-16S/0.0.1/ocms_16S-0.0.1.tar.gz

---
+++ packages/ocms-16S/ocms_16S-0.0.1.tar.gz/ocms_16S-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import sysconfig
+import sys
+import os
+import subprocess
+import re
+from setuptools import setup, find_packages
+
+setup(
+    # package information
+    name='ocms_16S',
+    version="0.0.1",
+    description='OCMS_16S : Oxford Centre for Microbiome Studies pipelines for 16S amplicon sequencing analysis',
+    author='Nicholas Ilott, Sandi Yen, Jethro Johnson',
+    license="MIT",
+    platforms=["any"],
+    keywords="microbiome, metagenomics, genomics",
+    url="https://github.com/OxfordCMS/OCMS_16S",
+    download_url="https://github.com/OxfordCMS/OCMS_16S/archive/refs/tags/v0.0.1.tar.gz",
+    install_requires=["numpy==1.19.5",
+                      "pandas",
+                      "cgatcore"],
+    packages=find_packages("./") + find_packages("./ocms16S/"),
+    entry_points={
+        'console_scripts': ['ocms_16s = ocms16S.ocms_16s:main']
+    },
+    include_package_data=True,
+    python_requires='>=3.6.0'                                            
+)
+
