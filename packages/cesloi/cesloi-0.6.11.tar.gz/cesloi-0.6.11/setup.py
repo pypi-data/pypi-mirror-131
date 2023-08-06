46565c248e64b8b8636ee5e7b1440e23d76e1031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:24 +0000

    Add package cesloi/0.6.11/cesloi-0.6.11.tar.gz

---
+++ packages/cesloi/cesloi-0.6.11.tar.gz/cesloi-0.6.11/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.rst", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cesloi",
+    version="0.6.11",
+    author="ArcletProject",
+    author_email="rf_tar_railt@qq.com",
+    description="An simple Python SDK base on mirai-api-http v2",
+    license='AGPL 3.0',
+    long_description=long_description,
+    long_description_content_type="text/rst",
+    url="https://github.com/RF-Tar-Railt/Cesloi",
+    install_requires=['aiohttp', 'yarl', 'pydantic', 'loguru', 'arclet-letoderea', 'arclet-alconna'],
+    packages=['arclet.cesloi', 'arclet.cesloi.event', 'arclet.cesloi.message','arclet.cesloi.message.alconna', 'arclet.cesloi.model', 'arclet.cesloi.timing'],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    keywords='mirai, bot, asyncio, http, websocket',
+    python_requires='>=3.8'
+)
