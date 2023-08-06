429a839e0987eb05e8acbbda8a3d657a71d19445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:40 +0000

    Add package fnirslink/0.0.7/fnirslink-0.0.7-py3-none-any.whl

---
+++ packages/fnirslink/fnirslink-0.0.7-py3-none-any.whl/fnirsLink/url_name.py
@@ -0,0 +1,8 @@
+class UrlName:
+    START = "/data/connect"
+    STOP = "/data/end"
+    READ_DATA = "/data/readData"
+
+    RESEARCH_MARK = "/eprime/mark"
+    CLINICAL_MARK_START = "/eprime/markStart"
+    CLINICAL_MARK_END = "/eprime/markEnd"
