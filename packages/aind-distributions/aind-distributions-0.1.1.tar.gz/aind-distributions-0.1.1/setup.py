9c8fc54831a839c71b543234fd455582736157e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:11 +0000

    Add package aind-distributions/0.1.1/aind-distributions-0.1.1.tar.gz

---
+++ packages/aind-distributions/aind-distributions-0.1.1.tar.gz/aind-distributions-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aind-distributions',
+      version='0.1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['aind_distributions'],
+      author='Will Olson',
+      author_email='will.olson11@gmail.com',
+      zip_safe=False)
