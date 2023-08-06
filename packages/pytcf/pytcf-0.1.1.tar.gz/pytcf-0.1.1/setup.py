2fe76c74a77b08b6ccacea187a2422d5c09da5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:01 +0000

    Add package pytcf/0.1.1/pytcf-0.1.1.tar.gz

---
+++ packages/pytcf/pytcf-0.1.1.tar.gz/pytcf-0.1.1/setup.py
@@ -0,0 +1,15 @@
+import os
+
+setup_cfg = dict(
+    name='pytcf',
+    version='0.1.1',
+    description='This package name is reserved by Xilinx, Inc.',
+    long_description='',
+    author='Xilnx, Inc.',
+    author_email='csrel@xilinx.com',
+    url='https://www.xilinx.com',
+    packages=[],
+)
+
+from distutils.core import setup
+setup(**setup_cfg)
