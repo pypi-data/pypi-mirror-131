8e65f250a3d409aa7e2d014fafcd8038496d909c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:11 +0000

    Add package peltak-todos/0.0.3/peltak-todos-0.0.3.tar.gz

---
+++ packages/peltak-todos/peltak-todos-0.0.3.tar.gz/peltak-todos-0.0.3/src/peltak_todos/__init__.py
@@ -0,0 +1,18 @@
+"""
+###################
+peltak TODOs plugin
+###################
+
+
+Overview
+========
+
+This plugin allows you to quickly scan your files for TODO comments and track
+them across commits. This makes it easy to leave yourself todos while working on
+a branch and then resolve them either before commiting or before finishing the PR.
+
+"""
+from .cli import todos  # noqa: F401
+
+
+__version__ = '0.0.3'
