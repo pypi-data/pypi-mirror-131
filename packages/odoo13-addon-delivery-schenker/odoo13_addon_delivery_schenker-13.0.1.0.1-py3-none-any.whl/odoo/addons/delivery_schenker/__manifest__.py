5205a860f0fd67de09a3eb3beae3eb3093f202ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:03 +0000

    Add package odoo13-addon-delivery-schenker/13.0.1.0.1/odoo13_addon_delivery_schenker-13.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo13-addon-delivery-schenker/odoo13_addon_delivery_schenker-13.0.1.0.1-py3-none-any.whl/odoo/addons/delivery_schenker/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Delivery Schenker",
+    "summary": "Delivery Carrier implementation for DB Schenker API",
+    "version": "13.0.1.0.1",
+    "category": "Stock",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["delivery_package_number", "delivery_state"],
+    "external_dependencies": {"python": ["zeep"]},
+    "data": ["views/delivery_schenker_view.xml", "data/delivery_schenker_data.xml"],
+}
