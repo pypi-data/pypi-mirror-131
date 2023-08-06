d6ec794ab5da8ede3552f0b3757c1500313fec7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:13 +0000

    Add package ox_ui/0.1.4/ox_ui-0.1.4.tar.gz

---
+++ packages/ox_ui/ox_ui-0.1.4.tar.gz/ox_ui-0.1.4/tests/flask/c2f_app.py
@@ -0,0 +1,59 @@
+"""Module to run minimal flask app to be used for tests.
+"""
+
+import datetime
+
+import click
+from flask import Flask, url_for
+
+from ox_ui.core import c2f
+
+APP = Flask(__name__)
+APP.config['WTF_CSRF_ENABLED'] = False
+
+@click.command()
+@click.option('--count', default=1, type=int, help='how many times to say it')
+@click.option('--text', default='hi', type=str, help='what to say')
+def hello_cmd(count, text):
+    'say hello'
+
+    result = []
+    for i in range(count):
+        result.append(text)
+
+    return '\n'.join(result)
+
+
+@click.command()
+@click.option('--count', default=2, type=int, help='how many times to say it')
+@click.option('--when', type=click.DateTime(formats=[
+    '%Y-%m-%d', '%m/%d/%Y']), default=datetime.date.today)
+@click.option('--text', default='bye', type=str, help='what to say')
+def goodbye_cmd(count, text, when):
+    'say bye'
+
+    result = []
+    for i in range(count):
+        result.append(text)
+    result.append('\nat %s' % str(when))
+
+    return '\n'.join(result)
+
+
+@APP.route('/')
+def home():
+    url = url_for('hello')
+    return 'Welcome to test; try the view at <A HREF="%s">%s</A>' % (
+        url, url)
+
+
+@APP.route('/hello', methods=('GET', 'POST'))
+def hello():
+    fcmd = c2f.ClickToWTF(hello_cmd)
+    result = fcmd.handle_request()
+    return result
+
+
+@APP.route('/goodbye', methods=('GET', 'POST'))
+def goodbye():
+    return c2f.ClickToWTF(goodbye_cmd).handle_request()
