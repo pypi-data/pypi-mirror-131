ac9a8a32e5104a55bb4404cd8e6a2f09033553bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:46 +0000

    Add package epi2melabs-wfpage/0.1.1/epi2melabs_wfpage-0.1.1.tar.gz

---
+++ packages/epi2melabs-wfpage/epi2melabs_wfpage-0.1.1.tar.gz/epi2melabs_wfpage-0.1.1/epi2melabs_wfpage/labextension/static/style_index_js.ec7d90cb8375e93bbf6c.js
@@ -0,0 +1,72 @@
+"use strict";
+(self["webpackChunk_epi2melabs_epi2melabs_wfpage"] = self["webpackChunk_epi2melabs_epi2melabs_wfpage"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \**************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "body {\n    height: 100%;\n    padding: 0;\n    margin: 0;\n    box-sizing: border-box;\n}\n  \n.epi2melabs-wfpage-widget ul {\n    padding: 0;\n    margin: 0;\n    list-style: none;\n}\n\n.epi2melabs-wfpage-widget h1, \n.epi2melabs-wfpage-widget h2, \n.epi2melabs-wfpage-widget h3, \n.epi2melabs-wfpage-widget h4, \n.epi2melabs-wfpage-widget h5, \n.epi2melabs-wfpage-widget p, \n.epi2melabs-wfpage-widget a, \n.epi2melabs-wfpage-widget div, \n.epi2melabs-wfpage-widget span, \n.epi2melabs-wfpage-widget li, \n.epi2melabs-wfpage-widget label, \n.epi2melabs-wfpage-widget button {\n    margin: 0;\n    padding: 0;\n    font-family: \"HelveticaNeueLT\",  Helvetica, Arial, sans-serif;\n    text-decoration: none;\n    list-style: none;\n}\n\n.epi2melabs-wfpage-widget h1 {\n    padding: 15px 0;\n    font-size: 36px;\n    font-weight: normal;\n}\n\n.epi2melabs-wfpage-widget h2 {\n    font-size: 18px;\n    font-weight: normal;\n}\n\n.epi2melabs-wfpage-widgeth3 {\n    font-weight: normal;\n    font-size: 12px;\n}\n\n.epi2melabs-wfpage-widget {\n    overflow-y: auto;\n}", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;IACI,YAAY;IACZ,UAAU;IACV,SAAS;IACT,sBAAsB;AAC1B;;AAEA;IACI,UAAU;IACV,SAAS;IACT,gBAAgB;AACpB;;AAEA;;;;;;;;;;;;IAYI,SAAS;IACT,UAAU;IACV,6DAA6D;IAC7D,qBAAqB;IACrB,gBAAgB;AACpB;;AAEA;IACI,eAAe;IACf,eAAe;IACf,mBAAmB;AACvB;;AAEA;IACI,eAAe;IACf,mBAAmB;AACvB;;AAEA;IACI,mBAAmB;IACnB,eAAe;AACnB;;AAEA;IACI,gBAAgB;AACpB","sourcesContent":["body {\n    height: 100%;\n    padding: 0;\n    margin: 0;\n    box-sizing: border-box;\n}\n  \n.epi2melabs-wfpage-widget ul {\n    padding: 0;\n    margin: 0;\n    list-style: none;\n}\n\n.epi2melabs-wfpage-widget h1, \n.epi2melabs-wfpage-widget h2, \n.epi2melabs-wfpage-widget h3, \n.epi2melabs-wfpage-widget h4, \n.epi2melabs-wfpage-widget h5, \n.epi2melabs-wfpage-widget p, \n.epi2melabs-wfpage-widget a, \n.epi2melabs-wfpage-widget div, \n.epi2melabs-wfpage-widget span, \n.epi2melabs-wfpage-widget li, \n.epi2melabs-wfpage-widget label, \n.epi2melabs-wfpage-widget button {\n    margin: 0;\n    padding: 0;\n    font-family: \"HelveticaNeueLT\",  Helvetica, Arial, sans-serif;\n    text-decoration: none;\n    list-style: none;\n}\n\n.epi2melabs-wfpage-widget h1 {\n    padding: 15px 0;\n    font-size: 36px;\n    font-weight: normal;\n}\n\n.epi2melabs-wfpage-widget h2 {\n    font-size: 18px;\n    font-weight: normal;\n}\n\n.epi2melabs-wfpage-widgeth3 {\n    font-weight: normal;\n    font-size: 12px;\n}\n\n.epi2melabs-wfpage-widget {\n    overflow-y: auto;\n}"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./base.css */ "./node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"], options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"].locals || {});
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.ec7d90cb8375e93bbf6c.js.map
