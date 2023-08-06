66b0d70f1b8b001888ebae16a86fe94c6ef1aa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:23 +0000

    Add package terraform-external-data/1.0.3/terraform-external-data-1.0.3.tar.gz

---
+++ packages/terraform-external-data/terraform-external-data-1.0.3.tar.gz/terraform-external-data-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="terraform-external-data",
+    version="1.0.3",
+    author="Juozas Kibildis",
+    author_email="juozas.kibildis@zenitech.co.uk",
+    description="Terraform external data package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/juozaskibildis-zt/terraform_external_data",
+    project_urls={
+        "Bug Tracker": "https://github.com/juozaskibildis-zt/terraform_external_data/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
