04e94420530f7cf9888f161692042a0a64d7b149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:29 +0000

    Add package dico-api/0.0.29/dico-api-0.0.29.tar.gz

---
+++ packages/dico-api/dico-api-0.0.29.tar.gz/dico-api-0.0.29/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="UTF-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="dico-api",
+    version="0.0.29",
+    author="eunwoo1104",
+    author_email="sions04@naver.com",
+    description="Yet another Discord API wrapper for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dico-api/dico",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.7',
+    install_requires=["aiohttp", "requests"],
+    extras_require={
+        "voice": ["PyNaCl"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
