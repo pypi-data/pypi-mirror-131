d06702e5f07a12cacc24625a7603db6c68147449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:02 +0000

    Add package letters/1.9.0.3/letters-1.9.0.3.tar.gz

---
+++ packages/letters/letters-1.9.0.3.tar.gz/letters-1.9.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import pypandoc
+
+try:
+    long_description = pypandoc.convert_file('README.md', 'rst')
+    long_description = long_description.replace('\r', '')
+
+except OSError:
+    import io
+    
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
+
+setup(
+    name='letters',
+    version='1.9.0.3',
+    license='GPLv3+',
+    author=['zelow'],
+    url='https://github.com/zeloww/letters',
+    install_requires=['numpy', 'pillow'],
+    description='A simple method to customize your programs to infinity!',
+    long_description=long_description,
+    keywords=['python', 'py', 'letter', 'letters', 'font', 'ascii', 'color', 'colors', 'gradient', 'gradients', 'fade' 'text'],
+    packages=['letters'],
+)
