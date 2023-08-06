eefe053601c299015323666ae0865b484e668e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:30 +0000

    Add package tonic/1.0.12/tonic-1.0.12-py3-none-any.whl

---
+++ packages/tonic/tonic-1.0.12-py3-none-any.whl/tonic/datasets/__init__.py
@@ -0,0 +1,30 @@
+from .asl_dvs import ASLDVS
+from .davisdataset import DAVISDATA
+from .dsec import DSEC
+from .dvsgesture import DVSGesture
+from .hsd import SHD, SSC
+from .mvsec import MVSEC
+from .ncaltech101 import NCALTECH101
+from .nmnist import NMNIST
+from .ntidigits import NTIDIGITS
+from .pokerdvs import POKERDVS
+from .s_mnist import SMNIST
+from .tum_vie import TUMVIE
+from .visual_place_recognition import VPR
+
+__all__ = [
+    "ASLDVS",
+    "DAVISDATA",
+    "DSEC",
+    "DVSGesture",
+    "MVSEC",
+    "NCALTECH101",
+    "NMNIST",
+    "NTIDIGITS",
+    "POKERDVS",
+    "SHD",
+    "SMNIST",
+    "SSC",
+    "TUMVIE",
+    "VPR",
+]
---
+++ packages/tonic/tonic-1.0.12-py3-none-any.whl/tonic/version.py
@@ -0,0 +1,3 @@
+version_info = (1, 0, 12)  # (major, minor, patch)
+
+version = ".".join(str(v) for v in version_info)
