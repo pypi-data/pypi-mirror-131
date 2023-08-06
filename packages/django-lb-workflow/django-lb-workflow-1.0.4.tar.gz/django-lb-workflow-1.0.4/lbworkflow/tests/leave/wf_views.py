6bb982a715fe203d391eba5f2abdc69855762589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:49 +0000

    Add package django-lb-workflow/1.0.4/django-lb-workflow-1.0.4.tar.gz

---
+++ packages/django-lb-workflow/django-lb-workflow-1.0.4.tar.gz/django-lb-workflow-1.0.4/lbworkflow/tests/leave/wf_views.py
@@ -0,0 +1,10 @@
+from lbworkflow.views.transition import ExecuteTransitionView
+
+from .forms import HRForm
+
+
+class CustomizedTransitionView(ExecuteTransitionView):
+    form_classes = {"form": HRForm}
+
+
+c = CustomizedTransitionView.as_view()
