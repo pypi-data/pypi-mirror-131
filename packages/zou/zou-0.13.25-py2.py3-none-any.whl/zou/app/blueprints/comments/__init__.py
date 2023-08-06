deac57316960ed1f3cd3923890424fd276ed3179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:28 +0000

    Add package zou/0.13.25/zou-0.13.25-py2.py3-none-any.whl

---
+++ packages/zou/zou-0.13.25-py2.py3-none-any.whl/zou/app/blueprints/comments/__init__.py
@@ -0,0 +1,39 @@
+from flask import Blueprint
+
+from zou.app.utils.api import configure_api_from_blueprint
+
+from .resources import (
+    AckCommentResource,
+    CommentTaskResource,
+    CommentManyTasksResource,
+    DownloadAttachmentResource,
+    ProjectAttachmentFiles,
+    ReplyCommentResource,
+    DeleteReplyCommentResource
+)
+
+
+routes = [
+    ("/data/tasks/<task_id>/comments/<comment_id>/ack", AckCommentResource),
+    (
+        "/data/tasks/<task_id>/comments/<comment_id>/reply",
+        ReplyCommentResource
+    ),
+    (
+        "/data/tasks/<task_id>/comments/<comment_id>/reply/<reply_id>",
+        DeleteReplyCommentResource
+    ),
+    (
+        "/data/attachment-files/<attachment_file_id>/file/<file_name>",
+        DownloadAttachmentResource,
+    ),
+    ("/data/projects/<project_id>/attachment-files", ProjectAttachmentFiles),
+    ("/actions/tasks/<task_id>/comment", CommentTaskResource),
+    (
+        "/actions/projects/<project_id>/tasks/comment-many",
+        CommentManyTasksResource,
+    ),
+]
+
+blueprint = Blueprint("comments", "comments")
+api = configure_api_from_blueprint(blueprint, routes)
