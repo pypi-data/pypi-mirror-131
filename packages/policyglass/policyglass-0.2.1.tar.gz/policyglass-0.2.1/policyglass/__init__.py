c5b8ee1853de21846e52afd4ef4c60784eb41620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:48 +0000

    Add package policyglass/0.2.1/policyglass-0.2.1.tar.gz

---
+++ packages/policyglass/policyglass-0.2.1.tar.gz/policyglass-0.2.1/policyglass/__init__.py
@@ -0,0 +1,31 @@
+"""PolicyGlass."""
+from .action import Action, EffectiveAction
+from .condition import Condition, ConditionCollection, ConditionKey, ConditionOperator, ConditionValue
+from .policy import Policy
+from .policy_shard import PolicyShard, dedupe_policy_shards, policy_shards_effect
+from .principal import EffectivePrincipal, Principal, PrincipalCollection, PrincipalType, PrincipalValue
+from .resource import EffectiveResource, Resource
+from .statement import Statement
+
+__all__ = [
+    "Policy",
+    "Statement",
+    "Principal",
+    "Action",
+    "Resource",
+    "PrincipalCollection",
+    "Principal",
+    "PrincipalType",
+    "PrincipalValue",
+    "Condition",
+    "ConditionKey",
+    "ConditionOperator",
+    "ConditionValue",
+    "ConditionCollection",
+    "EffectiveAction",
+    "EffectiveResource",
+    "EffectivePrincipal",
+    "PolicyShard",
+    "policy_shards_effect",
+    "dedupe_policy_shards",
+]
