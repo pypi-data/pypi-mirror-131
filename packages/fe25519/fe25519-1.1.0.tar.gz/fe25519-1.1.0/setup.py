47b77de86af2d6967af1d2eed49a66664f10669d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:37 +0000

    Add package fe25519/1.1.0/fe25519-1.1.0.tar.gz

---
+++ packages/fe25519/fe25519-1.1.0.tar.gz/fe25519-1.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+# The lines below are parsed by `docs/conf.py`.
+name = "fe25519"
+version = "1.1.0"
+
+setup(
+    name=name,
+    version=version,
+    packages=[name,],
+    install_requires=[
+        "parts~=1.2",
+        "bitlist~=0.6",
+        "fountains~=1.2"
+    ],
+    license="MIT",
+    url="https://github.com/nthparty/fe25519",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Pure Python data structure for working with Ed25519 "+\
+                "(and Ristretto) field elements and operations.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
