772414007f92d515f260d33f0aa4d3faf0f2c988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:14 +0000

    Add package bpw-pde/0.0a0/bpw_pde-0.0a0.tar.gz

---
+++ packages/bpw-pde/bpw_pde-0.0a0.tar.gz/bpw_pde-0.0a0/src/bpw_pde/index.py
@@ -0,0 +1,10 @@
+from pandas import Index
+from pandas.api.extensions import register_index_accessor
+
+from bpw_pde import ACCESSOR_NAME
+
+
+@register_index_accessor(ACCESSOR_NAME)
+class IndexAccessor:
+    def __init__(self, index: Index):
+        self._index = index
