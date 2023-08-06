7b3f960c565f7cf68a97de9925c3ee7d9d791904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:20:41 +0000

    Add package django-cdk/0.0.29/django_cdk-0.0.29-py3-none-any.whl

---
+++ packages/django-cdk/django_cdk-0.0.29-py3-none-any.whl/django_cdk/_jsii/__init__.py
@@ -0,0 +1,52 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_autoscaling._jsii
+import aws_cdk.aws_certificatemanager._jsii
+import aws_cdk.aws_cloudformation._jsii
+import aws_cdk.aws_cloudfront._jsii
+import aws_cdk.aws_cloudfront_origins._jsii
+import aws_cdk.aws_cloudwatch._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_ecr._jsii
+import aws_cdk.aws_ecr_assets._jsii
+import aws_cdk.aws_ecs._jsii
+import aws_cdk.aws_ecs_patterns._jsii
+import aws_cdk.aws_efs._jsii
+import aws_cdk.aws_eks._jsii
+import aws_cdk.aws_elasticache._jsii
+import aws_cdk.aws_elasticloadbalancingv2._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_kms._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_rds._jsii
+import aws_cdk.aws_route53._jsii
+import aws_cdk.aws_route53_targets._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_deployment._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.aws_ssm._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import cdk8s._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "django-cdk", "0.0.29", __name__[0:-6], "django-cdk@0.0.29.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
