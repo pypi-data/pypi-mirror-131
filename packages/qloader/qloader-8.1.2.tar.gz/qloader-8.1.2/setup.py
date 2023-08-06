6c8e60860252b723a66afff0c09bbe7c463965fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:13 +0000

    Add package qloader/8.1.2/qloader-8.1.2.tar.gz

---
+++ packages/qloader/qloader-8.1.2.tar.gz/qloader-8.1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['qloader']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['imagehash>=4.1.0,<5.0.0',
+ 'pillow>=8.1.1,<9.0.0',
+ 'requests>=2.23.0,<3.0.0',
+ 'selenium>=3.141.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'qloader',
+    'version': '8.1.2',
+    'description': 'Gather results from some search engine',
+    'long_description': None,
+    'author': 'tasker',
+    'author_email': 'tasker@ialcloud.xyz',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
