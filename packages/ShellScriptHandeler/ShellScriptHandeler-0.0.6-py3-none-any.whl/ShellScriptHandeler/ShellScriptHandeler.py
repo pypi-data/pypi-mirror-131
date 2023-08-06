94922fe6db6b64fa2d8b598476a3500bcb803985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:37 +0000

    Add package ShellScriptHandeler/0.0.6/ShellScriptHandeler-0.0.6-py3-none-any.whl

---
+++ packages/ShellScriptHandeler/ShellScriptHandeler-0.0.6-py3-none-any.whl/ShellScriptHandeler/ShellScriptHandeler.py
@@ -0,0 +1,9 @@
+import subprocess, os
+
+class Basic():
+    
+    def open(self, displayPath):
+        shellscript = subprocess.Popen([displayPath], shell=True, stdin=subprocess.PIPE )
+        shellscript.stdin.write('yes\n'.encode("utf-8"))
+        shellscript.stdin.close()
+        returncode = shellscript.wait()
