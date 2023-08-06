c7c3b1382fcb523cf5b6d65a0c36245ed10d8e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:12 +0000

    Add package mdorado/0.2.1/mdorado-0.2.1-py3-none-any.whl

---
+++ packages/mdorado/mdorado-0.2.1-py3-none-any.whl/mdorado/__init__.py
@@ -0,0 +1,13 @@
+#Version information. Keep up-to-date with version in setup.py
+
+__version__ = "0.2.1"
+
+#list of all modules
+__modulelist__ = [
+            "correlations",
+            "gofr",
+            "hb_analyze", 
+            "lifetime",
+            "msd",
+            "veccor"
+            ]
