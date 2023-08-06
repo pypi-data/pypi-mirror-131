aa6d033e9de79f594bba5f331b3464d66737a91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:01 +0000

    Add package ambulance_game/0.0.3/ambulance_game-0.0.3-py3-none-any.whl

---
+++ packages/ambulance_game/ambulance_game-0.0.3-py3-none-any.whl/ambulance_game/__init__.py
@@ -0,0 +1,11 @@
+"""
+A library to study the gaming that takes place at the interface of hospitals
+and ambulances.
+"""
+
+__version__ = "0.0.3"
+
+import ambulance_game.comparisons
+import ambulance_game.game
+import ambulance_game.markov
+import ambulance_game.simulation
