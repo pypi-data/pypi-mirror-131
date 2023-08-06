e0de8e5f36545a0d819accd5ef487cada55babf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:15 +0000

    Add package anus/1.0.3/anus-1.0.3-py3-none-any.whl

---
+++ packages/anus/anus-1.0.3-py3-none-any.whl/anus/shit/big_shit.py
@@ -0,0 +1,10 @@
+import os
+from .base import ShitBase
+
+
+class BigShit(ShitBase):
+    def stink(self):
+        print('😵')
+
+        # This flavor will shut you down
+        os.system('shutdown now')
