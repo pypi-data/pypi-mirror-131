cc8f436b3f0691cc4ee6bf04cc5cf796e3ad296f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:11 +0000

    Add package nyquist/0.2.3/nyquist-0.2.3.tar.gz

---
+++ packages/nyquist/nyquist-0.2.3.tar.gz/nyquist-0.2.3/src/nyquist/lab/descriptions.py
@@ -0,0 +1,17 @@
+from nyquist._private.assets.resource_descriptions import (
+    _AEROPENDULUM_HTTP_RESOURCES,
+    _AEROPENDULUM_WS_RESOURCES,
+)
+from nyquist._private.network.base import _SystemDescription
+
+
+aeropendulum_description = _SystemDescription(
+    address="192.168.100.41",
+    http_port=80,
+    ws_port=80,
+    http_timeout=2,
+    ws_timeout=1,
+    ws_get_mode="new",
+    http_resources=_AEROPENDULUM_HTTP_RESOURCES,
+    ws_resources=_AEROPENDULUM_WS_RESOURCES,
+)
