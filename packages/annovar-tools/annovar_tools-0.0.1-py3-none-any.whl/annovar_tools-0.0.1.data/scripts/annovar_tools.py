9a48c4bf0cf7fae127dcad26046d7a29f845aa79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:22 +0000

    Add package annovar-tools/0.0.1/annovar_tools-0.0.1-py3-none-any.whl

---
+++ packages/annovar-tools/annovar_tools-0.0.1-py3-none-any.whl/annovar_tools-0.0.1.data/scripts/annovar_tools.py
@@ -0,0 +1,31 @@
+#!python
+import argparse
+
+from process_annovar import avinput_to_vcf, split_annovar_by_gene
+
+
+def vcf_parser(subparsers):
+    sub_parser = subparsers.add_parser('vcf', help='convert avinput to vcf')
+    sub_parser.add_argument('--avinput', '-i', help='avinput infile')
+    sub_parser.add_argument('--reference', '-r', help='reference fasta')
+    sub_parser.add_argument('--vcf', '-o', help='vcf outfile')
+    sub_parser.set_defaults(func=lambda args: avinput_to_vcf(avinput=args.avinput, reference=args.reference, vcf=args.vcf))
+
+
+def split_parser(subparsers):
+    sub_parser = subparsers.add_parser('split', help='split annovar result by gene')
+    sub_parser.add_argument('--avoutput', '-i', help='avoutput infile')
+    sub_parser.add_argument('--refgenes', '-r', action="append", help='refgene files')
+    sub_parser.add_argument('--gene_db', '-g', help='the gene database, this execute will split annovar result by this gene_db')
+    sub_parser.add_argument('--outfile', '-o', help='the split outfile')
+    sub_parser.set_defaults(func=lambda args: split_annovar_by_gene(avoutput=args.avoutput, refgenes=args.refgenes, gene_db=args.gene_db, outfile=args.outfile))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Process ANNOVAR result')
+    subparsers = parser.add_subparsers(help='Process ANNOVAR result')
+    vcf_parser(subparsers)
+    split_parser(subparsers)
+    args = parser.parse_args()
+    print(args.refgenes)
+    args.func(args)
---
+++ packages/annovar-tools/annovar_tools-0.0.1-py3-none-any.whl/process_annovar/__init__.py
@@ -0,0 +1,2 @@
+from .split import split_annovar_by_gene
+from .vcf import avinput_to_vcf
