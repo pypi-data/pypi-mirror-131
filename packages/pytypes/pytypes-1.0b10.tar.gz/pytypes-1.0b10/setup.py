7dd5feb320ed8438b02fa51a404d69a06fef8fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:46 +0000

    Add package pytypes/1.0b10/pytypes-1.0b10.tar.gz

---
+++ packages/pytypes/pytypes-1.0b10.tar.gz/pytypes-1.0b10/setup.py
@@ -0,0 +1,44 @@
+import os.path
+
+from setuptools import setup
+
+here = os.path.dirname(__file__)
+readme_path = os.path.join(here, 'README.rst')
+readme = open(readme_path, 'rb').read().decode('utf-8')
+
+setup(
+    name='pytypes',
+    use_scm_version={
+        'version_scheme': 'post-release',
+        'local_scheme': 'dirty-tag'
+    },
+    description='Typing toolbox for Python 3 _and_ 2.',
+    long_description=readme,
+    url='https://github.com/Stewori/pytypes',
+    author='Stefan Richthofer',
+    author_email='stefan.richthofer@jyni.org',
+    license='Apache-2.0',
+    packages=['pytypes'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    setup_requires=['setuptools_scm'],
+    extras_require={
+        ':python_version == "2.7"': 'typing'
+    },
+    entry_points={
+        'console_scripts': [
+            'typestubs = pytypes.stubfile_2_converter:main'
+        ]
+    }
+)
