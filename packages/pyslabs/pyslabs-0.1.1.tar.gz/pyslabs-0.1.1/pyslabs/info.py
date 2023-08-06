03bf7951674278ceeb64775192515b96470db2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:36 +0000

    Add package pyslabs/0.1.1/pyslabs-0.1.1.tar.gz

---
+++ packages/pyslabs/pyslabs-0.1.1.tar.gz/pyslabs-0.1.1/pyslabs/info.py
@@ -0,0 +1,9 @@
+"""pyslabs info module
+"""
+
+name            = "pyslabs"
+version         = "0.1.1"
+description     = "Pure Python Paralle I/O"
+long_description = """Pure Python Parallel I/O that supports large datasets of
+ all picklable objects and well-known data frameworks"""
+author          = "Youngsung Kim"
