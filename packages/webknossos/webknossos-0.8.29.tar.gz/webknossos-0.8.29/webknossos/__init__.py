de9ce497bec93a778fd637da17acf1b4ea029ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:50 +0000

    Add package webknossos/0.8.29/webknossos-0.8.29.tar.gz

---
+++ packages/webknossos/webknossos-0.8.29.tar.gz/webknossos-0.8.29/webknossos/__init__.py
@@ -0,0 +1,11 @@
+"""
+# webknossos
+
+This module providess methods to modify or interact with webKnossos resources.
+Check out the different submodules for detailed API documentation:
+"""
+from webknossos.annotation import *
+from webknossos.client import *
+from webknossos.dataset import *
+from webknossos.geometry import *
+from webknossos.skeleton import *
