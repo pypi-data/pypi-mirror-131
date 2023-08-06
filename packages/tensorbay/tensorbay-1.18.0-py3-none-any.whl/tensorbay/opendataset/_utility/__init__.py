306835741121b50f23143cd327a965983aa27904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package tensorbay/1.18.0/tensorbay-1.18.0-py3-none-any.whl

---
+++ packages/tensorbay/tensorbay-1.18.0-py3-none-any.whl/tensorbay/opendataset/_utility/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+
+"""OpenDataset utility code."""
+
+from tensorbay.opendataset._utility.coco import coco
+from tensorbay.opendataset._utility.glob import glob
+
+__all__ = ["coco", "glob"]
