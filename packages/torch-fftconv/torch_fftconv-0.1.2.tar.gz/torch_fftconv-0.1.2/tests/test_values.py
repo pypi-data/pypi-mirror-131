1f309335c4d50f62360e16d28d0bc40db76b893a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:10 +0000

    Add package torch-fftconv/0.1.2/torch_fftconv-0.1.2.tar.gz

---
+++ packages/torch-fftconv/torch_fftconv-0.1.2.tar.gz/torch_fftconv-0.1.2/tests/test_values.py
@@ -0,0 +1,193 @@
+import torch
+from torch.nn import Conv1d, Conv2d, Conv3d, ConvTranspose1d, ConvTranspose2d, ConvTranspose3d
+from torch_fftconv.modules import *
+
+import pytest
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+if torch.cuda.is_available():
+    torch.backends.cuda.matmul.allow_tf32 = False
+    torch.backends.cudnn.allow_tf32 = False
+
+
+@pytest.mark.parametrize('batch', [1, 4])
+@pytest.mark.parametrize('in_channels', [16, 64])
+@pytest.mark.parametrize('out_channels', [8, 32])
+@pytest.mark.parametrize('length', [1733])
+@pytest.mark.parametrize('kernel_size', [128, 256])
+@pytest.mark.parametrize('stride', [1, 3, 4])
+@pytest.mark.parametrize('dilation', [1, 2, 4])
+@pytest.mark.parametrize('padding', [0, 3])
+@pytest.mark.parametrize('bias', [True, False])
+@pytest.mark.parametrize('groups', [1, 4])
+@pytest.mark.parametrize('padding_mode', ['zeros', 'reflect'])
+def test_conv1d(batch, length,
+                in_channels, out_channels,
+                kernel_size, stride, padding, dilation, groups, bias, padding_mode):
+
+    x = torch.randn(batch, in_channels, length,
+                    requires_grad=True, device=device)
+    conv = Conv1d(in_channels, out_channels, kernel_size, stride,
+                  padding, dilation, groups, bias, padding_mode).to(device)
+    fft_conv = FFTConv1d(in_channels, out_channels, kernel_size,
+                         stride, padding, dilation, groups, bias, padding_mode).to(device)
+    fft_conv.load_state_dict(conv.state_dict())
+
+    y1 = conv(x)
+    y2 = fft_conv(x)
+    assert torch.allclose(
+        y1, y2, atol=1e-5, rtol=1e-5), torch.abs(y1 - y2).max().item()
+    y2.sum().backward()
+
+
+@pytest.mark.parametrize('batch', [2])
+@pytest.mark.parametrize('in_channels', [8, 32])
+@pytest.mark.parametrize('out_channels', [4, 16])
+@pytest.mark.parametrize('length', [(101, 101)])
+@pytest.mark.parametrize('kernel_size', [17, 23])
+@pytest.mark.parametrize('stride', [1, 2, 3])
+@pytest.mark.parametrize('dilation', [1, 2, 3])
+@pytest.mark.parametrize('padding', [0, 7])
+@pytest.mark.parametrize('bias', [True, False])
+@pytest.mark.parametrize('groups', [1, 2])
+@pytest.mark.parametrize('padding_mode', ['zeros', 'reflect'])
+def test_conv2d(batch, length,
+                in_channels, out_channels,
+                kernel_size, stride, padding, dilation, groups, bias, padding_mode):
+
+    x = torch.randn(batch, in_channels, *length,
+                    requires_grad=True, device=device)
+    conv = Conv2d(in_channels, out_channels, kernel_size, stride,
+                  padding, dilation, groups, bias, padding_mode).to(device)
+    fft_conv = FFTConv2d(in_channels, out_channels, kernel_size,
+                         stride, padding, dilation, groups, bias, padding_mode).to(device)
+    fft_conv.load_state_dict(conv.state_dict())
+
+    y1 = conv(x)
+    y2 = fft_conv(x)
+    assert torch.allclose(
+        y1, y2, atol=1e-5, rtol=1e-5), torch.abs(y1 - y2).max().item()
+    y2.sum().backward()
+
+
+@pytest.mark.parametrize('batch', [2])
+@pytest.mark.parametrize('in_channels', [8])
+@pytest.mark.parametrize('out_channels', [8])
+@pytest.mark.parametrize('length', [(53, 53, 59)])
+@pytest.mark.parametrize('kernel_size', [9, 11])
+@pytest.mark.parametrize('stride', [1, 2, 3])
+@pytest.mark.parametrize('dilation', [1, 2, 3])
+@pytest.mark.parametrize('padding', [6])
+@pytest.mark.parametrize('bias', [True, False])
+@pytest.mark.parametrize('groups', [1, 2])
+@pytest.mark.parametrize('padding_mode', ['zeros', 'replicate'])
+def test_conv3d(batch, length,
+                in_channels, out_channels,
+                kernel_size, stride, padding, dilation, groups, bias, padding_mode):
+
+    x = torch.randn(batch, in_channels, *length,
+                    requires_grad=True, device=device)
+    conv = Conv3d(in_channels, out_channels, kernel_size, stride,
+                  padding, dilation, groups, bias, padding_mode).to(device)
+    fft_conv = FFTConv3d(in_channels, out_channels, kernel_size,
+                         stride, padding, dilation, groups, bias, padding_mode).to(device)
+    fft_conv.load_state_dict(conv.state_dict())
+
+    y1 = conv(x)
+    y2 = fft_conv(x)
+    assert torch.allclose(
+        y1, y2, atol=1e-5, rtol=1e-5), torch.abs(y1 - y2).max().item()
+    y2.sum().backward()
+
+
+@pytest.mark.parametrize('batch', [1, 4])
+@pytest.mark.parametrize('in_channels', [16, 64])
+@pytest.mark.parametrize('out_channels', [8, 32])
+@pytest.mark.parametrize('length', [409])
+@pytest.mark.parametrize('kernel_size', [128, 256])
+@pytest.mark.parametrize('stride', [1, 3, 4])
+@pytest.mark.parametrize('dilation', [1, 2, 4])
+@pytest.mark.parametrize('padding', [0, 3])
+@pytest.mark.parametrize('output_padding', [0])
+@pytest.mark.parametrize('bias', [True, False])
+@pytest.mark.parametrize('groups', [1, 4])
+@pytest.mark.parametrize('padding_mode', ['zeros'])
+def test_conv_transpose1d(batch, length,
+                          in_channels, out_channels,
+                          kernel_size, stride, padding, output_padding, dilation, groups, bias, padding_mode):
+
+    x = torch.randn(batch, in_channels, length,
+                    requires_grad=True, device=device)
+    conv = ConvTranspose1d(in_channels, out_channels, kernel_size, stride,
+                           padding, output_padding, groups, bias, dilation, padding_mode).to(device)
+    fft_conv = FFTConvTranspose1d(in_channels, out_channels, kernel_size,
+                                  stride, padding, output_padding, groups, bias, dilation, padding_mode).to(device)
+    fft_conv.load_state_dict(conv.state_dict())
+
+    y1 = conv(x)
+    y2 = fft_conv(x)
+    assert torch.allclose(
+        y1, y2, atol=1e-5, rtol=1e-5), torch.abs(y1 - y2).max().item()
+    y2.sum().backward()
+
+
+@pytest.mark.parametrize('batch', [2])
+@pytest.mark.parametrize('in_channels', [8, 32])
+@pytest.mark.parametrize('out_channels', [4, 16])
+@pytest.mark.parametrize('length', [(31, 31)])
+@pytest.mark.parametrize('kernel_size', [17, 23])
+@pytest.mark.parametrize('stride', [1, 2, 3])
+@pytest.mark.parametrize('dilation', [1, 2, 3])
+@pytest.mark.parametrize('padding', [0, 7])
+@pytest.mark.parametrize('output_padding', [0])
+@pytest.mark.parametrize('bias', [True, False])
+@pytest.mark.parametrize('groups', [1, 2])
+@pytest.mark.parametrize('padding_mode', ['zeros'])
+def test_conv_transpose2d(batch, length,
+                          in_channels, out_channels,
+                          kernel_size, stride, padding, output_padding, dilation, groups, bias, padding_mode):
+
+    x = torch.randn(batch, in_channels, *length,
+                    requires_grad=True, device=device)
+    conv = ConvTranspose2d(in_channels, out_channels, kernel_size, stride,
+                           padding, output_padding, groups, bias, dilation, padding_mode).to(device)
+    fft_conv = FFTConvTranspose2d(in_channels, out_channels, kernel_size,
+                                  stride, padding, output_padding, groups, bias, dilation, padding_mode).to(device)
+    fft_conv.load_state_dict(conv.state_dict())
+
+    y1 = conv(x)
+    y2 = fft_conv(x)
+    assert torch.allclose(
+        y1, y2, atol=1e-5, rtol=1e-5), torch.abs(y1 - y2).max().item()
+    y2.sum().backward()
+
+
+@pytest.mark.parametrize('batch', [2])
+@pytest.mark.parametrize('in_channels', [8])
+@pytest.mark.parametrize('out_channels', [8])
+@pytest.mark.parametrize('length', [(29, 23, 23)])
+@pytest.mark.parametrize('kernel_size', [9, 11])
+@pytest.mark.parametrize('stride', [1, 2, 3])
+@pytest.mark.parametrize('dilation', [1, 2, 3])
+@pytest.mark.parametrize('padding', [6])
+@pytest.mark.parametrize('output_padding', [0])
+@pytest.mark.parametrize('bias', [True, False])
+@pytest.mark.parametrize('groups', [1, 2])
+@pytest.mark.parametrize('padding_mode', ['zeros'])
+def test_conv_transpose3d(batch, length,
+                          in_channels, out_channels,
+                          kernel_size, stride, padding, output_padding, dilation, groups, bias, padding_mode):
+
+    x = torch.randn(batch, in_channels, *length,
+                    requires_grad=True, device=device)
+    conv = ConvTranspose3d(in_channels, out_channels, kernel_size, stride,
+                           padding, output_padding, groups, bias, dilation, padding_mode).to(device)
+    fft_conv = FFTConvTranspose3d(in_channels, out_channels, kernel_size,
+                                  stride, padding, output_padding, groups, bias, dilation, padding_mode).to(device)
+    fft_conv.load_state_dict(conv.state_dict())
+
+    y1 = conv(x)
+    y2 = fft_conv(x)
+    assert torch.allclose(
+        y1, y2, atol=1e-5, rtol=1e-5), torch.abs(y1 - y2).max().item()
+    y2.sum().backward()
---
+++ packages/torch-fftconv/torch_fftconv-0.1.2.tar.gz/torch_fftconv-0.1.2/torch_fftconv/modules.py
@@ -0,0 +1,86 @@
+import torch.nn as nn
+from torch import Tensor
+import torch.nn.functional as F
+from torch.nn.modules.utils import _single, _pair, _triple
+from typing import Optional, List
+from .functional import *
+
+
+__all__ = [
+    'FFTConv1d', 'FFTConv2d', 'FFTConv3d', 'FFTConvTranspose1d', 'FFTConvTranspose2d', 'FFTConvTranspose3d'
+]
+
+
+class FFTConv1d(nn.Conv1d):
+    def forward(self, input: Tensor) -> Tensor:
+        if self.padding_mode != 'zeros':
+            return fft_conv1d(F.pad(input, self._reversed_padding_repeated_twice, mode=self.padding_mode),
+                              self.weight, self.bias, self.stride, _single(0),
+                              self.dilation, self.groups)
+        return fft_conv1d(input, self.weight, self.bias, self.stride,
+                          self.padding, self.dilation, self.groups)
+
+
+class FFTConv2d(nn.Conv2d):
+    def forward(self, input: Tensor) -> Tensor:
+        if self.padding_mode != 'zeros':
+            return fft_conv2d(F.pad(input, self._reversed_padding_repeated_twice, mode=self.padding_mode),
+                              self.weight, self.bias, self.stride, _pair(0),
+                              self.dilation, self.groups)
+        return fft_conv2d(input, self.weight, self.bias, self.stride,
+                          self.padding, self.dilation, self.groups)
+
+
+class FFTConv3d(nn.Conv3d):
+    def forward(self, input: Tensor) -> Tensor:
+        if self.padding_mode != 'zeros':
+            return fft_conv3d(F.pad(input, self._reversed_padding_repeated_twice, mode=self.padding_mode),
+                              self.weight, self.bias, self.stride, _triple(0),
+                              self.dilation, self.groups)
+        return fft_conv3d(input, self.weight, self.bias, self.stride,
+                          self.padding, self.dilation, self.groups)
+
+
+class FFTConvTranspose1d(nn.ConvTranspose1d):
+    def forward(self, input: Tensor, output_size: Optional[List[int]] = None) -> Tensor:
+        if self.padding_mode != 'zeros':
+            raise ValueError(
+                'Only `zeros` padding mode is supported for FFTConvTranspose1d')
+
+        # One cannot replace List by Tuple or Sequence in "_output_padding" because
+        # TorchScript does not support `Sequence[T]` or `Tuple[T, ...]`.
+        output_padding = self._output_padding(
+            input, output_size, self.stride, self.padding, self.kernel_size, self.dilation)  # type: ignore
+        return fft_conv_transpose1d(
+            input, self.weight, self.bias, self.stride, self.padding,
+            output_padding, self.groups, self.dilation)
+
+
+class FFTConvTranspose2d(nn.ConvTranspose2d):
+    def forward(self, input: Tensor, output_size: Optional[List[int]] = None) -> Tensor:
+        if self.padding_mode != 'zeros':
+            raise ValueError(
+                'Only `zeros` padding mode is supported for FFTConvTranspose2d')
+
+        # One cannot replace List by Tuple or Sequence in "_output_padding" because
+        # TorchScript does not support `Sequence[T]` or `Tuple[T, ...]`.
+        output_padding = self._output_padding(
+            input, output_size, self.stride, self.padding, self.kernel_size, self.dilation)  # type: ignore
+        return fft_conv_transpose2d(
+            input, self.weight, self.bias, self.stride, self.padding,
+            output_padding, self.groups, self.dilation)
+
+
+class FFTConvTranspose3d(nn.ConvTranspose3d):
+    def forward(self, input: Tensor, output_size: Optional[List[int]] = None) -> Tensor:
+        if self.padding_mode != 'zeros':
+            raise ValueError(
+                'Only `zeros` padding mode is supported for FFTConvTranspose3d')
+
+        # One cannot replace List by Tuple or Sequence in "_output_padding" because
+        # TorchScript does not support `Sequence[T]` or `Tuple[T, ...]`.
+        output_padding = self._output_padding(
+            input, output_size, self.stride, self.padding, self.kernel_size, self.dilation)  # type: ignore
+        return fft_conv_transpose3d(
+            input, self.weight, self.bias, self.stride, self.padding,
+            output_padding, self.groups, self.dilation)
