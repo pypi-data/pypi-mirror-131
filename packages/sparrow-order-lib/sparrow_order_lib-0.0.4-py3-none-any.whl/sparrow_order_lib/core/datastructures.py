f7edfa55324a1d510facffc71167edfc7d22571d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:46 +0000

    Add package sparrow-order-lib/0.0.4/sparrow_order_lib-0.0.4-py3-none-any.whl

---
+++ packages/sparrow-order-lib/sparrow_order_lib-0.0.4-py3-none-any.whl/sparrow_order_lib/core/datastructures.py
@@ -0,0 +1,32 @@
+
+
+class ImmutableList(tuple):
+    """
+    不可变列表
+    """
+
+    def __new__(cls, *args, warning='ImmutableList object is immutable.', **kwargs):
+        self = tuple.__new__(cls, *args, **kwargs)
+        self.warning = warning
+        return self
+
+    def complain(self, *wargs, **kwargs):
+        if isinstance(self.warning, Exception):
+            raise self.warning
+        else:
+            raise AttributeError(self.warning)
+
+    # All list mutation functions complain.
+    __delitem__ = complain
+    __delslice__ = complain
+    __iadd__ = complain
+    __imul__ = complain
+    __setitem__ = complain
+    __setslice__ = complain
+    append = complain
+    extend = complain
+    insert = complain
+    pop = complain
+    remove = complain
+    sort = complain
+    reverse = complain
