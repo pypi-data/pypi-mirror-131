7e9eddcdae806ddbd26bade0b3e1ef7a72ee695e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:55 +0000

    Add package validatedata/0.0.1.6/validatedata-0.0.1.6.tar.gz

---
+++ packages/validatedata/validatedata-0.0.1.6.tar.gz/validatedata-0.0.1.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+from validatedata import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="validatedata",
+    version=__version__,
+    author="Edward Kigozi",
+    author_email='edwardinbytes@gmail.com',
+    description="An easier way to validate data in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Edward-K1/validatedata",
+    install_requires=['python-dateutil'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    keywords = "validate data validation",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3',
+
+)
