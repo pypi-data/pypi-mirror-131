0921f389a2fd883f54965a2a626043060b50a0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:17 +0000

    Add package ShynaSystemInfo/0.2/ShynaSystemInfo-0.2.tar.gz

---
+++ packages/ShynaSystemInfo/ShynaSystemInfo-0.2.tar.gz/ShynaSystemInfo-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup_args = dict(
+     name='ShynaSystemInfo',
+     version='0.2',
+     packages=find_packages(),
+     author="Shivam Sharma",
+     author_email="shivamsharma1913@gmail.com",
+     description="Shyna Backend Functionality Package to fetch System information",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+    )
+
+install_requires = [
+    'psutil',
+    "setuptools",
+    "wheel",
+    "urllib3"
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
