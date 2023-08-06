ff8d40238518476b72c6dad2a48d17005d1b553b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:23 +0000

    Add package soda-sql-spark/2.1.1/soda-sql-spark-2.1.1.tar.gz

---
+++ packages/soda-sql-spark/soda-sql-spark-2.1.1.tar.gz/soda-sql-spark-2.1.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_namespace_packages
+
+if sys.version_info < (3, 7):
+    print('Error: Soda SQL requires at least Python 3.7')
+    print('Error: Please upgrade your Python version to 3.7 or later')
+    sys.exit(1)
+
+package_name = "soda-sql-spark"
+package_version = '2.1.1'
+# TODO Add proper description
+description = "Soda SQL Apache Spark"
+
+requires = [
+    f'soda-sql-core=={package_version}',
+    'pyodbc>=4.0,<5.0',
+    'PyHive>=0.6.3, <1.0',
+    'thrift>=0.13.0, <1.0',
+    'sasl>=0.3.1, <1.0',
+    'thrift-sasl>=0.4.3, <1.0',
+]
+# TODO Fix the params
+# TODO Add a warning that installing core doesn't give any warehouse functionality
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["sodasql*"])
+)
