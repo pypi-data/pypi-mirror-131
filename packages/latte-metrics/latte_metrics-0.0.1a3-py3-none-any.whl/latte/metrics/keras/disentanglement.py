035e0c8c560fa309a4cbcc50dc9a5ffe85e06a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:19 +0000

    Add package latte-metrics/0.0.1a3/latte_metrics-0.0.1a3-py3-none-any.whl

---
+++ packages/latte-metrics/latte_metrics-0.0.1a3-py3-none-any.whl/latte/metrics/keras/disentanglement.py
@@ -0,0 +1,32 @@
+from .wrapper import KerasMetricWrapper
+from ..core import disentanglement as C
+
+from functools import partial
+
+MutualInformationGap = partial(KerasMetricWrapper, metric=C.MutualInformationGap)
+
+DependencyAwareMutualInformationGap = partial(
+    KerasMetricWrapper, metric=C.DependencyAwareMutualInformationGap
+)
+
+
+DependencyAwareLatentInformationGap = partial(
+    KerasMetricWrapper, metric=C.DependencyAwareLatentInformationGap
+)
+
+DependencyBlindMutualInformationGap = partial(
+    KerasMetricWrapper, metric=C.DependencyBlindMutualInformationGap
+)
+
+SeparateAttributePredictability = partial(
+    KerasMetricWrapper, metric=C.SeparateAttributePredictability
+)
+
+Modularity = partial(KerasMetricWrapper, metric=C.Modularity)
+
+MIG = MutualInformationGap
+DMIG = DependencyAwareMutualInformationGap
+DLIG = DependencyAwareLatentInformationGap
+XMIG = DependencyBlindMutualInformationGap
+
+SAP = SeparateAttributePredictability
