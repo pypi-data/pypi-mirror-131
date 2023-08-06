4d328985358897fab0b1afec85ca02e05ad22e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:33 +0000

    Add package amostra/0.2.2/amostra-0.2.2.tar.gz

---
+++ packages/amostra/amostra-0.2.2.tar.gz/amostra-0.2.2/setup.py
@@ -0,0 +1,30 @@
+__author__ = 'arkilic'
+import setuptools
+import versioneer
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setuptools.setup(
+    name='amostra',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD 3-Clause",
+    url="https://github.com/NSLS-II/amostra.git",
+    packages=setuptools.find_packages(),
+    install_package_data = True,
+    install_requires=["ujson",
+                      "pymongo",
+                      "tornado",
+                      "jsonschema",
+                      "mongoquery",
+                      "requests",
+                      "pyyaml"],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 3 - Alpha",
+        'Programming Language :: Python :: 3',
+    ],
+)
