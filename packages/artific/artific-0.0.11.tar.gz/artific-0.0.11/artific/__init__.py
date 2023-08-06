f7a369d9e933f0a5aff211144c6db676dc6cc3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:18 +0000

    Add package artific/0.0.11/artific-0.0.11.tar.gz

---
+++ packages/artific/artific-0.0.11.tar.gz/artific-0.0.11/artific/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for artific."""
+
+__author__ = """Nipun Waas"""
+__email__ = 'waasnipun@gmail.com'
+__version__ = '0.0.11'
+
+from artific.artific import BubbleSort as BubbleSort
+from artific.artific import InsertionSort as InsertionSort
+from artific.artific import HeapSort as HeapSort
