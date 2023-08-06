1960a5d0cad6c9e145e04a4cfda6d8822561c1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:36 +0000

    Add package restosaur/0.7.0b19/restosaur-0.7.0b19-py2-none-any.whl

---
+++ packages/restosaur/restosaur-0.7.0b19-py2-none-any.whl/tests/urls_root_with_prefix.py
@@ -0,0 +1,24 @@
+from restosaur.contrib.django import API
+
+api = API("api")
+root = api.resource("/")
+some = api.resource("some")
+subsome = api.resource("some/sub")
+
+
+@root.get()
+def root_view(ctx):
+    return ctx.Response({"root": "ok"})
+
+
+@some.get()
+def some_view(ctx):
+    return ctx.Response({"some": "ok"})
+
+
+@subsome.get()
+def subsome_view(ctx):
+    return ctx.Response({"some/sub": "ok"})
+
+
+urlpatterns = api.urlpatterns()
