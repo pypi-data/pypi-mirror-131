a99e1159f5b3ebd5d3f7151dc0570faa45facbd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:52 +0000

    Add package Soundboard/2.0.3/Soundboard-2.0.3-py3-none-any.whl

---
+++ packages/Soundboard/Soundboard-2.0.3-py3-none-any.whl/Soundboard/Decomposition.py
@@ -0,0 +1,34 @@
+import numpy as np
+import scipy.signal
+
+from Soundboard import *
+
+def unmake(name):
+    wav_file = wave.open(name + ".wav")
+    nframes = wav_file.getnframes() * 16
+    frames = wav_file.readframes(nframes)
+    ticks = 0
+    percent = -1
+    string = ""
+    for i in range(0,nframes,2):
+        try:
+            string += str(struct.unpack('h',bytes(frames[i-2:i]))[0])+"\n"
+        except:
+            pass
+        ticks += 2
+        if percent < math.floor(ticks / nframes * 100):
+            percent = math.floor(ticks / nframes * 100)
+            print("\rUncompiling: " + str(percent) + "%", end="")
+    wav_file.close()
+    build = open(name + ".sbld", "w")
+    build.write(string)
+    build.close()
+
+def spectrum(name):
+    file = open(name+".sbld","r")
+    signal = []
+    N = filesize(name + ".sbld")
+    for i in range(N):
+        signal.append(int(file.readline()[:-1])/DRANGE)
+    return scipy.signal.spectrogram(np.array(signal), SAMPLERATE, nfft=2^16)
+
---
+++ packages/Soundboard/Soundboard-2.0.3-py3-none-any.whl/Soundboard/__init__.py
@@ -0,0 +1,1 @@
+from Soundboard import *
