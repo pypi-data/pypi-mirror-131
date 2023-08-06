6ea6684a4cd3da010a944d96b8e01174a5963d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:26 +0000

    Add package beastxuserbot/2021.10.11.7/beastxuserbot-2021.10.11.7-py3-none-any.whl

---
+++ packages/beastxuserbot/beastxuserbot-2021.10.11.7-py3-none-any.whl/pyBeastx/dB/_core.py
@@ -0,0 +1,13 @@
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyBeastx/blob/main/LICENSE>.
+
+PLUGINS = []
+ADDONS = []
+HELP = {}
+LOADED = {}
+LIST = {}
+VC_HELP = {}
