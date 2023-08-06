cd5b94a2285fd8604097edcd7e9e47d5d0eb6268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:41 +0000

    Add package cryptlex-python-sdk/1.0.1/cryptlex_python_sdk-1.0.1.tar.gz

---
+++ packages/cryptlex-python-sdk/cryptlex_python_sdk-1.0.1.tar.gz/cryptlex_python_sdk-1.0.1/src/cryptlex_web_api_client/api/analytics/getv3analyticstrial_activationsos.py
@@ -0,0 +1,161 @@
+import datetime
+from typing import Any, Dict, List, Optional, Union
+
+import httpx
+
+from ...client import Client
+from ...models.activations_by_os_dto import ActivationsByOsDto
+from ...models.http_error_response_dto import HttpErrorResponseDto
+from ...types import UNSET, Response, Unset
+
+
+def _get_kwargs(
+    *,
+    client: Client,
+    product_id: Union[Unset, None, str] = UNSET,
+    start_date: Union[Unset, None, datetime.datetime] = UNSET,
+    end_date: Union[Unset, None, datetime.datetime] = UNSET,
+) -> Dict[str, Any]:
+    url = "{}/v3/analytics/trial-activations/os".format(client.base_url)
+
+    headers: Dict[str, Any] = client.get_headers()
+    cookies: Dict[str, Any] = client.get_cookies()
+
+    json_start_date: Union[Unset, None, str] = UNSET
+    if not isinstance(start_date, Unset):
+        json_start_date = start_date.isoformat() if start_date else None
+
+    json_end_date: Union[Unset, None, str] = UNSET
+    if not isinstance(end_date, Unset):
+        json_end_date = end_date.isoformat() if end_date else None
+
+    params: Dict[str, Any] = {
+        "productId": product_id,
+        "startDate": json_start_date,
+        "endDate": json_end_date,
+    }
+    params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
+
+    return {
+        "url": url,
+        "headers": headers,
+        "cookies": cookies,
+        "timeout": client.get_timeout(),
+        "params": params,
+    }
+
+
+def _parse_response(*, response: httpx.Response) -> Optional[Union[HttpErrorResponseDto, List[ActivationsByOsDto]]]:
+    if response.status_code == 200:
+        response_200 = []
+        _response_200 = response.json()
+        for response_200_item_data in _response_200:
+            response_200_item = ActivationsByOsDto.from_dict(response_200_item_data)
+
+            response_200.append(response_200_item)
+
+        return response_200
+    if response.status_code == 401:
+        response_401 = HttpErrorResponseDto.from_dict(response.json())
+
+        return response_401
+    if response.status_code == 403:
+        response_403 = HttpErrorResponseDto.from_dict(response.json())
+
+        return response_403
+    if response.status_code == 429:
+        response_429 = HttpErrorResponseDto.from_dict(response.json())
+
+        return response_429
+    if response.status_code == 500:
+        response_500 = HttpErrorResponseDto.from_dict(response.json())
+
+        return response_500
+    return None
+
+
+def _build_response(*, response: httpx.Response) -> Response[Union[HttpErrorResponseDto, List[ActivationsByOsDto]]]:
+    return Response(
+        status_code=response.status_code,
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(response=response),
+    )
+
+
+def sync_detailed(
+    *,
+    client: Client,
+    product_id: Union[Unset, None, str] = UNSET,
+    start_date: Union[Unset, None, datetime.datetime] = UNSET,
+    end_date: Union[Unset, None, datetime.datetime] = UNSET,
+) -> Response[Union[HttpErrorResponseDto, List[ActivationsByOsDto]]]:
+    kwargs = _get_kwargs(
+        client=client,
+        product_id=product_id,
+        start_date=start_date,
+        end_date=end_date,
+    )
+
+    response = httpx.get(
+        verify=client.verify_ssl,
+        **kwargs,
+    )
+
+    return _build_response(response=response)
+
+
+def sync(
+    *,
+    client: Client,
+    product_id: Union[Unset, None, str] = UNSET,
+    start_date: Union[Unset, None, datetime.datetime] = UNSET,
+    end_date: Union[Unset, None, datetime.datetime] = UNSET,
+) -> Optional[Union[HttpErrorResponseDto, List[ActivationsByOsDto]]]:
+    """Retrieve trial activations by OS."""
+
+    return sync_detailed(
+        client=client,
+        product_id=product_id,
+        start_date=start_date,
+        end_date=end_date,
+    ).parsed
+
+
+async def asyncio_detailed(
+    *,
+    client: Client,
+    product_id: Union[Unset, None, str] = UNSET,
+    start_date: Union[Unset, None, datetime.datetime] = UNSET,
+    end_date: Union[Unset, None, datetime.datetime] = UNSET,
+) -> Response[Union[HttpErrorResponseDto, List[ActivationsByOsDto]]]:
+    kwargs = _get_kwargs(
+        client=client,
+        product_id=product_id,
+        start_date=start_date,
+        end_date=end_date,
+    )
+
+    async with httpx.AsyncClient(verify=client.verify_ssl) as _client:
+        response = await _client.get(**kwargs)
+
+    return _build_response(response=response)
+
+
+async def asyncio(
+    *,
+    client: Client,
+    product_id: Union[Unset, None, str] = UNSET,
+    start_date: Union[Unset, None, datetime.datetime] = UNSET,
+    end_date: Union[Unset, None, datetime.datetime] = UNSET,
+) -> Optional[Union[HttpErrorResponseDto, List[ActivationsByOsDto]]]:
+    """Retrieve trial activations by OS."""
+
+    return (
+        await asyncio_detailed(
+            client=client,
+            product_id=product_id,
+            start_date=start_date,
+            end_date=end_date,
+        )
+    ).parsed
---
+++ packages/cryptlex-python-sdk/cryptlex_python_sdk-1.0.1.tar.gz/cryptlex_python_sdk-1.0.1/src/cryptlex_web_api_client/models/activation_meter_attribute_request_model.py
@@ -0,0 +1,41 @@
+from typing import Any, Dict, Type, TypeVar
+
+import attr
+
+T = TypeVar("T", bound="ActivationMeterAttributeRequestModel")
+
+
+@attr.s(auto_attribs=True)
+class ActivationMeterAttributeRequestModel:
+    """ """
+
+    name: str
+    uses_increment: int
+
+    def to_dict(self) -> Dict[str, Any]:
+        name = self.name
+        uses_increment = self.uses_increment
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(
+            {
+                "name": name,
+                "usesIncrement": uses_increment,
+            }
+        )
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        name = d.pop("name")
+
+        uses_increment = d.pop("usesIncrement")
+
+        activation_meter_attribute_request_model = cls(
+            name=name,
+            uses_increment=uses_increment,
+        )
+
+        return activation_meter_attribute_request_model
