4677755615678a6fede5eb8f93f859d56b6562e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:10 +0000

    Add package fiddup/2.1.4/fiddup-2.1.4.tar.gz

---
+++ packages/fiddup/fiddup-2.1.4.tar.gz/fiddup-2.1.4/fiddup/constants.py
@@ -0,0 +1,5 @@
+from colorama import Fore, Style
+
+INFO_PREFIX = f"[{Fore.CYAN}Info{Style.RESET_ALL}]"
+ERR_PREFIX = f"[{Fore.RED}Error{Style.RESET_ALL}]"
+WARN_PREFIX = f"[{Fore.YELLOW}Warning{Style.RESET_ALL}]"
