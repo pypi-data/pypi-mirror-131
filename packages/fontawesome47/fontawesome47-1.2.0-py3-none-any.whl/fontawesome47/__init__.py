679cd906d9d9394b52e8cb8b3358b1f0eadc258c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:29 +0000

    Add package fontawesome47/1.2.0/fontawesome47-1.2.0-py3-none-any.whl

---
+++ packages/fontawesome47/fontawesome47-1.2.0-py3-none-any.whl/fontawesome47/__init__.py
@@ -0,0 +1,18 @@
+import pathlib
+import pkgutil
+HERE = pathlib.Path(__file__).absolute().parent
+DATA = 'fonts/fontawesome-webfont.ttf'
+
+
+def get_path() -> pathlib.Path:
+    return HERE / DATA
+
+
+def get_data() -> bytes:
+    data = pkgutil.get_data('fontawesome47', DATA)
+    if not data:
+        raise Exception(f'{DATA} not found')
+    return data
+
+
+RANGE = (0xf000, 0xf3ff)
