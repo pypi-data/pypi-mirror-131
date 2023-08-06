009fd90915395c89e660e4fe3df8f93d6a49809b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:57 +0000

    Add package emeis/0.5.0/emeis-0.5.0.tar.gz

---
+++ packages/emeis/emeis-0.5.0.tar.gz/emeis-0.5.0/emeis/core/views.py
@@ -0,0 +1,105 @@
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+from generic_permissions.permissions import PermissionViewMixin
+from generic_permissions.visibilities import VisibilityViewMixin
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
+from rest_framework_json_api import views
+
+from . import filters, models, serializers
+
+
+class BaseViewset(VisibilityViewMixin, PermissionViewMixin, views.ModelViewSet):
+    pass
+
+
+class MeViewSet(RetrieveModelMixin, GenericViewSet):
+    """Me view returns current user."""
+
+    serializer_class = serializers.MeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, *args, **kwargs):
+        return self.request.user
+
+
+class MyACLViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
+    """MyACL view returns current users ACLs."""
+
+    queryset = models.ACL.objects.all()
+    serializer_class = serializers.MyACLSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        pk = self.kwargs.pop("pk")
+        acl = get_object_or_404(models.ACL, pk=pk)
+        if acl.user != self.request.user:
+            raise PermissionDenied()
+        return acl
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+
+class UserViewSet(BaseViewset):
+    serializer_class = serializers.UserSerializer
+    queryset = models.User.objects.all()
+
+    filterset_class = filters.UserFilterset
+
+    search_fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "=zip",
+        "city",
+    )
+
+
+class ScopeViewSet(BaseViewset):
+    serializer_class = serializers.ScopeSerializer
+    queryset = models.Scope.objects.all()
+    search_fields = (
+        "name",
+        "description",
+    )
+
+
+class RoleViewSet(BaseViewset):
+    serializer_class = serializers.RoleSerializer
+    queryset = models.Role.objects.all()
+    search_fields = (
+        "slug",
+        "name",
+        "description",
+    )
+
+
+class PermissionViewSet(BaseViewset):
+    serializer_class = serializers.PermissionSerializer
+    queryset = models.Permission.objects.prefetch_related("roles")
+    filterset_fields = ("roles",)
+    search_fields = (
+        "slug",
+        "name",
+        "description",
+    )
+
+
+class ACLViewSet(BaseViewset):
+    serializer_class = serializers.ACLSerializer
+    queryset = models.ACL.objects.all()
+    filterset_fields = ("user", "scope", "role")
+    search_fields = (
+        "scope__name",
+        "scope__description",
+        "role__name",
+        "role__description",
+        "user__username",
+        "user__first_name",
+        "user__last_name",
+        "=user__zip",
+        "user__city",
+    )
