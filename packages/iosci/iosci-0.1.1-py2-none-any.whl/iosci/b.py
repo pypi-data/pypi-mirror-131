cd1475531027bdc3b2be350aefaeb02ba9fb212b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:20 +0000

    Add package iosci/0.1.1/iosci-0.1.1-py2-none-any.whl

---
+++ packages/iosci/iosci-0.1.1-py2-none-any.whl/iosci/b.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def p2():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, p2')  # Press ⌘F8 to toggle the breakpoint.
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
