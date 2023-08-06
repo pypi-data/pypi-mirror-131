65264c561541a1eb7b361d4d701a7fc0cebcfe1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:58 +0000

    Add package dir-structure-cli/0.0.6/dir-structure-cli-0.0.6.tar.gz

---
+++ packages/dir-structure-cli/dir-structure-cli-0.0.6.tar.gz/dir-structure-cli-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+from dir_struct_cli.main import __version__
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+
+setup(
+    name="dir-structure-cli",
+    version=__version__,
+    license="MIT",
+    description="Generate directory skeletons for some famous frameworks",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Abdulrahman Dawoud",
+    author_email="abdulrahman.goldendawn@gmail.com",
+    packages=["dir_struct_cli"],
+    entry_points="""
+        [console_scripts]
+        dir-structure-cli=dir_struct_cli.main:main
+    """,
+    url="https://github.com/gmyrianthous/example-publish-pypi",
+    include_package_data=True,
+    keywords="example project",
+    install_requires=[
+        "click",
+    ],
+)
