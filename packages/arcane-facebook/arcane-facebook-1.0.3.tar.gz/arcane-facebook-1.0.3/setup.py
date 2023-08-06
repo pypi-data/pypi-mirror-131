befa2eab21ee87ef171131e2f9bf1dce60ce66e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:13 +0000

    Add package arcane-facebook/1.0.3/arcane-facebook-1.0.3.tar.gz

---
+++ packages/arcane-facebook/arcane-facebook-1.0.3.tar.gz/arcane-facebook-1.0.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['arcane', 'arcane.facebook']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['backoff>=1.10.0,<2.0.0',
+ 'facebook_business==11.0.0',
+ 'typing-extensions>=3.7.4,<4.0.0']
+
+setup_kwargs = {
+    'name': 'arcane-facebook',
+    'version': '1.0.3',
+    'description': 'Helpers to request facebook API',
+    'long_description': '# Arcane facebook\n',
+    'author': 'Arcane',
+    'author_email': 'product@arcane.run',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
