dd025de2bc748aa15b86f0776e2599c4445275bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:40 +0000

    Add package silicium/0.1.1/silicium-0.1.1.tar.gz

---
+++ packages/silicium/silicium-0.1.1.tar.gz/silicium-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+VERSION = '0.1.1'
+
+# import pypandoc
+# long_description = pypandoc.convert_file('./README.md', 'rst')
+
+setup(
+    name='silicium',
+    version=VERSION,
+    description='Ahead-of-time interpreted all-language compiler in Python3',
+    # long_description=long_description,
+    author='Samimies',
+    url='https://github.com/SamimiesGames/silicium',
+    license='MIT',
+    packages=find_packages(exclude=['tests', 'tests.*', 'examples'], where="src/*"),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+    package_dir={
+        '': 'src'
+    },
+    package_data={
+        'silicium': ['*.typed']
+    },
+    zip_safe=False,
+)
+
