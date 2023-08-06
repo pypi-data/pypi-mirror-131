aa4e01783afb3443d0bde93c17a937dd20637c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:43 +0000

    Add package undouble/0.1.0/undouble-0.1.0.tar.gz

---
+++ packages/undouble/undouble-0.1.0.tar.gz/undouble-0.1.0/undouble/__init__.py
@@ -0,0 +1,33 @@
+from undouble.undouble import Undouble
+
+from undouble.undouble import (
+    import_example,
+    load_example,
+    )
+
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.0'
+
+# module level doc-string
+__doc__ = """
+undouble
+=====================================================================
+
+Description
+-----------
+undouble is for...
+
+Example
+-------
+>>> from undouble import Undouble
+>>> model = Undouble(verbose=20)
+>>> model.fit_transform()
+>>> fig,ax = undouble.plot(model)
+
+References
+----------
+https://github.com/erdogant/undouble
+
+"""
