b1926cc717996eac9aa9f50e1463cf3bde79ec74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:46 +0000

    Add package crypto-candlesticks/0.2.0/crypto_candlesticks-0.2.0-py3-none-any.whl

---
+++ packages/crypto-candlesticks/crypto_candlesticks-0.2.0-py3-none-any.whl/crypto_candlesticks/symbols/quote_currency.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""List of current available base pairs in Bitfinex."""
+from typing import List
+
+quote_currency: List[str] = [
+    'USD',
+    'UST',
+    'EUR',
+    'CNHT',
+    'GBP',
+    'JPY',
+    'DAI',
+    'BTC',
+    'EOS',
+    'ETH',
+    'XCH',
+    'USTF0',
+]
