f2098bce8568f23192f331b3b14dfca5cfadf6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:06 +0000

    Add package fikkie/0.1.0/fikkie-0.1.0-py3-none-any.whl

---
+++ packages/fikkie/fikkie-0.1.0-py3-none-any.whl/fikkie/notifiers/telegram.py
@@ -0,0 +1,20 @@
+from telegram import Bot
+
+
+__all__ = ["Notifier", "TelegramNotifier"]
+
+
+class TelegramNotifier:
+    """
+    A telegram notifier.
+    """
+
+    TYPE = "telegram"
+
+    def __init__(self, token: str, chat_id: str):
+        self._bot = Bot(token=token)
+        self._chat_id = chat_id
+
+    def notify(self, text: str) -> None:
+        "Sends a message." ""
+        self._bot.sendMessage(chat_id=self._chat_id, text=text)
