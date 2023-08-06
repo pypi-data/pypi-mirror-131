1b9e6d033db4139a80da71a5c25dc3634d031f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:42 +0000

    Add package GRPgraph/2.4/GRPgraph-2.4.tar.gz

---
+++ packages/GRPgraph/GRPgraph-2.4.tar.gz/GRPgraph-2.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup  
+setup(name='GRPgraph',
+       version='2.4',
+      description='small and compact graphick distributions',
+      packages=['GRPgraph'],       
+      author_email='pvana621@gmail.com',       
+      zip_safe=False)
