8931b51f3e51251591c8a33d8db9a3874db03355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:43 +0000

    Add package fucc/0.0.8/fucc-0.0.8.tar.gz

---
+++ packages/fucc/fucc-0.0.8.tar.gz/fucc-0.0.8/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fucc", 
+    version="0.0.8",
+    author="Rafael Van Belle",
+    author_email="rafael@gmail.com",
+    description="Fraudulent Use of Credit Card",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rafaelvanbelle/fucc_package",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'scikit-learn', 
+          'pandas', 
+          'tqdm', 
+          'scikit-plot', 
+          'matplotlib', 
+          'networkx', 
+          'dateparser'
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3", 
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
