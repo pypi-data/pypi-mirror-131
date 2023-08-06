3ff642d6bf8ea1c8bfccf5894fca27470d06882d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:40 +0000

    Add package vkbottle-types/5.131.131.3/vkbottle-types-5.131.131.3.tar.gz

---
+++ packages/vkbottle-types/vkbottle-types-5.131.131.3.tar.gz/vkbottle-types-5.131.131.3/vkbottle_types/state.py
@@ -0,0 +1,20 @@
+import warnings
+
+
+class BaseStateGroup:
+    warnings.warn(
+        "BaseStateGroup from vkbottle_types is deprecated "
+        "and will be removed in future releases, "
+        "use vkbottle.BaseStateGroup instead",
+        DeprecationWarning,
+    )
+    pass
+
+
+class StatePeer:
+    warnings.warn(
+        "StatePeer from vkbottle_types is deprecated "
+        "and will be removed in future releases, "
+        "use vkbottle.StatePeer instead",
+        DeprecationWarning,
+    )
