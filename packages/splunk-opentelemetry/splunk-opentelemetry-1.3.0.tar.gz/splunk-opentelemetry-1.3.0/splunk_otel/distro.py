a7a95f8ffdb5669fc62bb0046b56f775845acc6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:56 +0000

    Add package splunk-opentelemetry/1.3.0/splunk-opentelemetry-1.3.0.tar.gz

---
+++ packages/splunk-opentelemetry/splunk-opentelemetry-1.3.0.tar.gz/splunk-opentelemetry-1.3.0/splunk_otel/distro.py
@@ -0,0 +1,25 @@
+# Copyright Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any, Dict
+
+from opentelemetry.instrumentation.distro import BaseDistro  # type: ignore
+
+from splunk_otel.options import _Options
+from splunk_otel.tracing import _configure_tracing
+
+
+class _SplunkDistro(BaseDistro):
+    def _configure(self, **kwargs: Dict[str, Any]) -> None:
+        _configure_tracing(_Options())
