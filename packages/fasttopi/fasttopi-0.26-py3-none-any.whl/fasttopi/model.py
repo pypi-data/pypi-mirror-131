47babc21708cc8180ff9d676713f137dc2e1ced7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:02 +0000

    Add package fasttopi/0.26/fasttopi-0.26-py3-none-any.whl

---
+++ packages/fasttopi/fasttopi-0.26-py3-none-any.whl/fasttopi/model.py
@@ -0,0 +1,44 @@
+import pickle
+from sklearn.dummy import DummyClassifier
+from sklearn.feature_extraction.text import  TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from dataset import *
+
+def create_baseline():
+    baseline = DummyClassifier(strategy="constant",constant=[0])
+    return baseline
+
+def create_model(c,max_iter):
+    token_n_grams = TfidfVectorizer(ngram_range=(1,3),max_features=1000,stop_words={'english'})
+    logistic_regression = LogisticRegression(C=c,max_iter=max_iter)
+    pipeline = Pipeline([('ngram',token_n_grams),('normalizer',StandardScaler(with_mean=False)),('logistic-regression',logistic_regression)])
+    return  pipeline
+
+def train_model(model,training_set):
+    titles, labels =  training_set.titles, training_set.labels
+    model = model.fit(titles,labels)
+    return model
+
+def predict_title(model,title):
+    prediction = model.predict([title])
+    category = get_descriptive_category(prediction[0])
+    return category
+
+def predict(model,titles):
+    return model.predict(titles)
+
+def get_probability(model,titles):
+    return model.predict_proba(titles)
+
+def dump_model(model, path_model):
+    with  open(path_model, 'wb') as model_file:
+        pickle.dump(model,model_file)
+
+def load_model(path_model):
+    with open(path_model,'rb') as model_file:
+        model= pickle.load(model_file)
+        return model
+
