f29a325e1140298f1921b291963b0566d49ac4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:50 +0000

    Add package sholl/0.0.2/sholl-0.0.2.tar.gz

---
+++ packages/sholl/sholl-0.0.2.tar.gz/sholl-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sholl']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['sholl = sholl.sholl:main']}
+
+setup_kwargs = {
+    'name': 'sholl',
+    'version': '0.0.2',
+    'description': 'Generate Hombrew formulas for your python projects',
+    'long_description': None,
+    'author': 'jeremynac',
+    'author_email': 'jeremynac@hotmail.fr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.1,<4.0',
+}
+
+
+setup(**setup_kwargs)
