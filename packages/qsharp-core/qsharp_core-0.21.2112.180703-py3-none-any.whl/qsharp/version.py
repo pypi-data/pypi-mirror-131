a22949d75cb128e8747db16a0dfe0c349759e164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package qsharp-core/0.21.2112.180703/qsharp_core-0.21.2112.180703-py3-none-any.whl

---
+++ packages/qsharp-core/qsharp_core-0.21.2112.180703-py3-none-any.whl/qsharp/version.py
@@ -0,0 +1,10 @@
+# Auto-generated file, do not edit.
+##
+# version.py: Specifies the version of the qsharp package.
+##
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+##
+__version__ = "0.21.2112.180703"
+is_conda = False
+_user_agent_extra = "[0.21.2112.180703]"
