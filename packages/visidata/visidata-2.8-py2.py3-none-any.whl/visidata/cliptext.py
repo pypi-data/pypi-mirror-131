bade97ff1079cc6f94904a205e664f597d90c8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:45 +0000

    Add package visidata/2.8/visidata-2.8-py2.py3-none-any.whl

---
+++ packages/visidata/visidata-2.8-py2.py3-none-any.whl/visidata/cliptext.py
@@ -0,0 +1,150 @@
+import unicodedata
+import sys
+import functools
+
+from visidata import options
+
+__all__ = ['clipstr', 'clipdraw', 'dispwidth']
+
+disp_column_fill = ' '
+
+### Curses helpers
+
+# ZERO_WIDTH_CF is from wcwidth:
+# NOTE: created by hand, there isn't anything identifiable other than
+# general Cf category code to identify these, and some characters in Cf
+# category code are of non-zero width.
+# Also includes some Cc, Mn, Zl, and Zp characters
+ZERO_WIDTH_CF = set(map(chr, [
+    0,       # Null (Cc)
+    0x034F,  # Combining grapheme joiner (Mn)
+    0x200B,  # Zero width space
+    0x200C,  # Zero width non-joiner
+    0x200D,  # Zero width joiner
+    0x200E,  # Left-to-right mark
+    0x200F,  # Right-to-left mark
+    0x2028,  # Line separator (Zl)
+    0x2029,  # Paragraph separator (Zp)
+    0x202A,  # Left-to-right embedding
+    0x202B,  # Right-to-left embedding
+    0x202C,  # Pop directional formatting
+    0x202D,  # Left-to-right override
+    0x202E,  # Right-to-left override
+    0x2060,  # Word joiner
+    0x2061,  # Function application
+    0x2062,  # Invisible times
+    0x2063,  # Invisible separator
+]))
+
+def wcwidth(cc, ambig=1):
+        if cc in ZERO_WIDTH_CF:
+            return 1
+        eaw = unicodedata.east_asian_width(cc)
+        if eaw in 'AN':  # ambiguous or neutral
+            if unicodedata.category(cc) == 'Mn':
+                return 1
+            else:
+                return ambig
+        elif eaw in 'WF': # wide/full
+            return 2
+        elif not unicodedata.combining(cc):
+            return 1
+        return 0
+
+
+def dispwidth(ss, maxwidth=None):
+    'Return display width of string, according to unicodedata width and options.disp_ambig_width.'
+    disp_ambig_width = options.disp_ambig_width
+    w = 0
+
+    for cc in ss:
+        w += wcwidth(cc, disp_ambig_width)
+        if maxwidth and w > maxwidth:
+            break
+    return w
+
+
+@functools.lru_cache(maxsize=100000)
+def _dispch(c, oddspacech=None, combch=None, modch=None):
+    ccat = unicodedata.category(c)
+    if ccat in ['Mn', 'Sk', 'Lm']:
+        if unicodedata.name(c).startswith('MODIFIER'):
+            return modch, 1
+    elif c != ' ' and ccat in ('Cc', 'Zs', 'Zl'):  # control char, space, line sep
+        return oddspacech, 1
+    elif c in ZERO_WIDTH_CF:
+        return combch, 1
+
+    return c, dispwidth(c)
+
+@functools.lru_cache(maxsize=100000)
+def _clipstr(s, dispw, trunch='', oddspacech='', combch='', modch=''):
+    '''Return clipped string and width in terminal display characters.
+    Note: width may differ from len(s) if East Asian chars are 'fullwidth'.'''
+    w = 0
+    ret = ''
+
+    trunchlen = dispwidth(trunch)
+    for c in s:
+        newc, chlen = _dispch(c, oddspacech=oddspacech, combch=combch, modch=modch)
+        if newc:
+            ret += newc
+            w += chlen
+        else:
+            ret += c
+            w += dispwidth(c)
+
+        if w > dispw-trunchlen+1:
+            ret = ret[:-2] + trunch # replace final char with ellipsis
+            w += trunchlen
+            break
+
+    return ret, w
+
+
+def clipstr(s, dispw):
+    if options.visibility:
+        return _clipstr(s, dispw,
+                        trunch=options.disp_truncator,
+                        oddspacech=options.disp_oddspace,
+                        modch='\u25e6',
+                        combch='\u25cc')
+    else:
+        return _clipstr(s, dispw,
+                trunch=options.disp_truncator,
+                oddspacech=options.disp_oddspace,
+                modch='',
+                combch='')
+
+def clipdraw(scr, y, x, s, attr, w=None, clear=True, rtl=False):
+    'Draw string `s` at (y,x)-(y,x+w) with curses attr, clipping with ellipsis char.  if rtl, draw inside (x-w, x).  If *clear*, clear whole editing area before displaying. Returns width drawn (max of w).'
+    if scr:
+        _, windowWidth = scr.getmaxyx()
+    else:
+        windowWidth = 80
+    dispw = 0
+    try:
+        if w is None:
+            w = dispwidth(s, maxwidth=windowWidth)
+        w = min(w, (x-1) if rtl else (windowWidth-x-1))
+        if w <= 0:  # no room anyway
+            return 0
+        if not scr:
+            return w
+
+        # convert to string just before drawing
+        clipped, dispw = clipstr(str(s), w)
+        if rtl:
+            # clearing whole area (w) has negative display effects; clearing just dispw area is useless
+#            scr.addstr(y, x-dispw-1, disp_column_fill*dispw, attr)
+            scr.addstr(y, x-dispw-1, clipped, attr)
+        else:
+            if clear:
+                scr.addstr(y, x, disp_column_fill*w, attr)  # clear whole area before displaying
+            scr.addstr(y, x, clipped, attr)
+    except Exception as e:
+        pass
+#        raise type(e)('%s [clip_draw y=%s x=%s dispw=%s w=%s clippedlen=%s]' % (e, y, x, dispw, w, len(clipped))
+#                ).with_traceback(sys.exc_info()[2])
+
+    return dispw
---
+++ packages/visidata/visidata-2.8-py2.py3-none-any.whl/visidata/loaders/archive.py
@@ -0,0 +1,103 @@
+import codecs
+import tarfile
+import zipfile
+
+from visidata import vd, VisiData, asyncthread, Sheet, Progress, Menu, options
+from visidata import ColumnAttr, Column, date, datetime, Path
+
+@VisiData.api
+def open_zip(vd, p):
+    return vd.ZipSheet(p.name, source=p)
+
+@VisiData.api
+def open_tar(vd, p):
+    return TarSheet(p.name, source=p)
+
+VisiData.open_tgz = VisiData.open_tar
+VisiData.open_txz = VisiData.open_tar
+VisiData.open_tbz2 = VisiData.open_tar
+
+@VisiData.api
+class ZipSheet(Sheet):
+    'Wrapper for `zipfile` library.'
+    rowtype = 'files' # rowdef ZipInfo
+    columns = [
+        ColumnAttr('filename'),
+        ColumnAttr('file_size', type=int),
+        Column('date_time', type=date,
+               getter=lambda col, row: datetime.datetime(*row.date_time)),
+        ColumnAttr('compress_size', type=int)
+    ]
+
+    def openZipFile(self, fp, *args, **kwargs):
+        '''Use VisiData input to handle password-protected zip files.'''
+        try:
+            return fp.open(*args, **kwargs)
+        except RuntimeError as err:
+            if 'password required' in err.args[0]:
+                pwd = vd.input(f'{args[0].filename} is encrypted, enter password: ', display=False)
+                return fp.open(*args, **kwargs, pwd=pwd.encode('utf-8'))
+            vd.error(err)
+
+    def openRow(self, fi):
+            decodedfp = codecs.iterdecode(self.openZipFile(self.zfp, fi),
+                                          encoding=options.encoding,
+                                          errors=options.encoding_errors)
+            return vd.openSource(Path(fi.filename, fp=decodedfp, filesize=fi.file_size), filetype=options.filetype)
+
+    @asyncthread
+    def extract(self, *rows, path=None):
+        self.zfp.extractall(members=[r.filename for r in rows], path=path)
+
+    @property
+    def zfp(self):
+        return zipfile.ZipFile(str(self.source), 'r')
+
+    def iterload(self):
+        with self.zfp as zf:
+            for zi in Progress(zf.infolist()):
+                yield zi
+
+
+class TarSheet(Sheet):
+    'Wrapper for `tarfile` library.'
+    rowtype = 'files' # rowdef TarInfo
+    columns = [
+        ColumnAttr('name'),
+        ColumnAttr('size', type=int),
+        ColumnAttr('mtime', type=date),
+        ColumnAttr('type', type=int),
+        ColumnAttr('mode', type=int),
+        ColumnAttr('uname'),
+        ColumnAttr('gname')
+    ]
+
+    def openRow(self, fi):
+            tfp = tarfile.open(name=str(self.source))
+            decodedfp = codecs.iterdecode(tfp.extractfile(fi),
+                                          encoding=options.encoding,
+                                          errors=options.encoding_errors)
+            return vd.openSource(Path(fi.name, fp=decodedfp, filesize=fi.size))
+
+    def iterload(self):
+        with tarfile.open(name=str(self.source)) as tf:
+            for ti in Progress(tf.getmembers()):
+                yield ti
+
+
+ZipSheet.addCommand('x', 'extract-file', 'extract(cursorRow)', 'extract current file to current directory')
+ZipSheet.addCommand('gx', 'extract-selected', 'extract(*onlySelectedRows)', 'extract selected files to current directory')
+ZipSheet.addCommand('zx', 'extract-file-to', 'extract(cursorRow, path=inputPath("extract to: "))', 'extract current file to given pathname')
+ZipSheet.addCommand('gzx', 'extract-selected-to', 'extract(*onlySelectedRows, path=inputPath("extract %d files to: " % nSelectedRows))', 'extract selected files to given directory')
+
+vd.addMenu(Menu('File', Menu('Extract',
+        Menu('current file', 'extract-file'),
+        Menu('current file to', 'extract-file-to'),
+        Menu('selected files', 'extract-selected'),
+        Menu('selected files to', 'extract-selected-to'),
+    )))
+
+vd.addGlobals({
+    'ZipSheet': ZipSheet,
+    'TarSheet': TarSheet
+})
