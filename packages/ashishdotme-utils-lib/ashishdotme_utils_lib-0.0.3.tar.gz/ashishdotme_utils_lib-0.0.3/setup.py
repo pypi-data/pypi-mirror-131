d2bc44960f8239eea741d833c936b48d40ca5dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:41 +0000

    Add package ashishdotme-utils-lib/0.0.3/ashishdotme_utils_lib-0.0.3.tar.gz

---
+++ packages/ashishdotme-utils-lib/ashishdotme_utils_lib-0.0.3.tar.gz/ashishdotme_utils_lib-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from setuptools import setup
+
+setup(
+    name='ashishdotme_utils_lib',
+    description='Python library for dashboard',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    author='Ashish Patel',
+    author_email='ashishsushilpatel@gmail.com',
+    url='https://github.com/ashishdotme/ashishdotme-utils',
+    version='0.0.3',
+    packages=[
+        'ashishdotme_utils_lib',
+        'ashishdotme_utils_lib/core',
+        'ashishdotme_utils_lib/errors',
+        'ashishdotme_utils_lib/sdk'
+    ],
+    install_requires=['colorama', 'colorlog', 'requests']
+)
