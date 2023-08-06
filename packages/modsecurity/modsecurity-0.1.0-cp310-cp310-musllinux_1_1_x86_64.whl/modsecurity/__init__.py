c6bbaf80b371816bd12a8ff2bcb15f083fcbd270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:54 +0000

    Add package modsecurity/0.1.0/modsecurity-0.1.0-cp310-cp310-musllinux_1_1_x86_64.whl

---
+++ packages/modsecurity/modsecurity-0.1.0-cp310-cp310-musllinux_1_1_x86_64.whl/modsecurity/__init__.py
@@ -0,0 +1,11 @@
+from .lib.modsecurity import PyModSecurity as ModSecurity  # pylint: disable=no-name-in-module
+from .lib.rules_set import PyRulesSet as RulesSet  # pylint: disable=no-name-in-module
+from .lib.transaction import PyTransaction as Transaction  # pylint: disable=no-name-in-module
+
+__all__ = [
+    'ModSecurity',
+    'RulesSet',
+    'Transaction',
+]
+
+__version__ = '0.1.0'
