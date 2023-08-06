355a29d91a2e9fba9424220f1875bcebc028db1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:48 +0000

    Add package PyZendesk/0.2.1/PyZendesk-0.2.1-py3-none-any.whl

---
+++ packages/PyZendesk/PyZendesk-0.2.1-py3-none-any.whl/pyzendesk/users.py
@@ -0,0 +1,31 @@
+##
+#     Project: PyZendesk
+# Description: API for Zendesk
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from .api import Api
+
+
+class Users(Api):
+    def me(self) -> dict:
+        """
+        Zendesk requester user information
+
+        :return: user information
+        """
+        return self.request_get(path='users/me.json')
