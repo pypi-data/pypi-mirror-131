27285b0cb639d5c510edfc5c6f2aa333c1932030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:21 +0000

    Add package slapr/1.0/slapr-1.0.tar.gz

---
+++ packages/slapr/slapr-1.0.tar.gz/slapr-1.0/setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+  name = 'slapr',
+  packages = ['slapr'],
+  version = '1.0',
+  license='gpl-3.0',
+  description = 'A simple tool that allows you to change your default AWS CLI profile.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Antoni Yanev',
+  author_email = 'antonipyanev@gmail.com',
+  url = 'https://github.com/antonipy/slapr',
+  download_url = 'https://github.com/antonipy/slapr/archive/refs/tags/1.0.tar.gz',
+  keywords = ['aws', 'awscli', 'awscliv2', 'linux', 'tools'],
+  install_requires=[
+          'inquirer',
+          'click'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'Topic :: Internet',
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: Utilities',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+  entry_points = '''
+        [console_scripts]
+        slapr=slapr.main:cli
+    '''
+)
