b01bfd7e22658ef5c448db332d08a4e644ab0816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:01 +0000

    Add package codemaker/0.0.1.3/codemaker-0.0.1.3.tar.gz

---
+++ packages/codemaker/codemaker-0.0.1.3.tar.gz/codemaker-0.0.1.3/codemaker/clitools/cli.py
@@ -0,0 +1,28 @@
+import os
+os.environ['ANSI_COLORS_DISABLED']="1"
+import shutil
+import fire
+from codemaker.makecode import gencode,write_yaml
+DEFAULT_MAKER_FILENAME='maker.yaml'
+class CLI:
+    def hi(cls):
+        print('Hi, I am codemaker.'.center(50, '*'))
+    def maketmpl(self,target=None,overwrite=False):
+        if not target:
+            target=DEFAULT_MAKER_FILENAME
+        if os.path.exists(target) and not overwrite:
+            raise FileExistsError(target)
+        write_yaml(dict(
+            exts=['.py','.txt'],
+            params=dict(
+                foo='foo value'
+            ),
+        ),target)
+    def gencode(self,src,dst,maker=DEFAULT_MAKER_FILENAME,overwrite=False,exists_ok=True):
+        return gencode(src,dst,maker,overwrite=overwrite,exists_ok=exists_ok)
+
+def main():
+    fire.Fire(CLI())
+
+if __name__ == '__main__':
+    main()
