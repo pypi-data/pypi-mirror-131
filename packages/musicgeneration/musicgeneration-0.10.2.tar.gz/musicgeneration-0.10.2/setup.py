bf3bd97cd9b43c2345abc294a3aed82d0e9a81b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:46 +0000

    Add package musicgeneration/0.10.2/musicgeneration-0.10.2.tar.gz

---
+++ packages/musicgeneration/musicgeneration-0.10.2.tar.gz/musicgeneration-0.10.2/setup.py
@@ -0,0 +1,40 @@
+from pkg_resources import DistributionNotFound, get_distribution
+from setuptools import setup, find_packages
+
+
+def get_dist(pkgname):
+    try:
+        return get_distribution(pkgname)
+    except DistributionNotFound:
+        return None
+
+
+install_deps = [
+    'numpy',
+    'regex',
+    'tqdm',
+    'gym'
+]
+tf_ver = '2.0.0a'
+if get_dist('tensorflow>=' + tf_ver) is None and get_dist('tensorflow_gpu>=' + tf_ver) is None:
+    install_deps.append('tensorflow>=' + tf_ver)
+
+setup(
+    name='musicgeneration',
+    packages=find_packages(),
+    version='0.10.2',
+    license='MIT',
+    description='diploma project',
+    author='Bohdan Kulynych',
+    keywords=['deep learning', 'neural networks', 'tensorflow', 'introduction'],
+    install_requires=install_deps,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    include_package_data=True,
+    package_data={'musicgeneration': ['data/*', 'bin/*', 'midi/*']}
+
+)
