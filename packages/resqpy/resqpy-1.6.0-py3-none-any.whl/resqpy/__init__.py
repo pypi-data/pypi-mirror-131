5e6b3e525439ca5c7842f27922c4242b8eaed679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:23 +0000

    Add package resqpy/1.6.0/resqpy-1.6.0-py3-none-any.whl

---
+++ packages/resqpy/resqpy-1.6.0-py3-none-any.whl/resqpy/__init__.py
@@ -0,0 +1,33 @@
+"""RESQML manipulation library.
+
+.. autosummary::
+    :toctree: _autosummary
+    :caption: API Reference
+    :template: custom-module-template.rst
+    :recursive:
+
+    model
+    crs
+    derived_model
+    fault
+    grid
+    grid_surface
+    lines
+    organize
+    property
+    rq_import
+    strata
+    surface
+    time_series
+    unstructured
+    weights_and_measures
+    well
+    olio
+"""
+
+try:
+    # Version dynamically extracted from git tags when package is built
+    from .version import version as __version__  # type: ignore
+
+except ImportError:
+    __version__ = "0.0.0-version-not-available"
