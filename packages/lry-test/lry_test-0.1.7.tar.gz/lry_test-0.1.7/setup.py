be9d11d4e881bf6c3507290771b1ecdbcaa71b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:32 +0000

    Add package lry-test/0.1.7/lry_test-0.1.7.tar.gz

---
+++ packages/lry-test/lry_test-0.1.7.tar.gz/lry_test-0.1.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup , find_packages
+
+setup(
+    name='lry_test',
+    version='0.1.7',
+    description='this package is made by lry',
+    #long_description=open('README.MD').read(),
+    include_package_data=True,
+    author='lry',
+    author_email='1224137702@qq.com',
+    maintainer='lry',       # 维护者
+    maintainer_email='1224137702@qq.com',
+    license='MIT LICENSE',
+    url='https://github//lry-123456789',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.1',
+    install_requires=['torch','torch','torchvision','matplotlib'],
+    console_scripts=["rename model.py model.dll"],
+)
