181f33037ff1e98ee2291dabc8dcea04ad994cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:20 +0000

    Add package ops2deb/0.14.0/ops2deb-0.14.0-py3-none-any.whl

---
+++ packages/ops2deb/ops2deb-0.14.0-py3-none-any.whl/ops2deb/fetcher.py
@@ -0,0 +1,118 @@
+import asyncio
+import hashlib
+import shutil
+from pathlib import Path
+from typing import List, Optional, Tuple
+
+import aiofiles
+import httpx
+
+from . import logger
+from .client import client_factory
+from .exceptions import Ops2debFetcherError
+
+DEFAULT_CACHE_DIRECTORY = Path("/tmp/ops2deb_cache")
+_cache_directory = DEFAULT_CACHE_DIRECTORY
+
+
+def set_cache_directory(path: Path) -> None:
+    """Directory in which files are downloaded"""
+    global _cache_directory
+    _cache_directory = path
+
+
+def _error(msg: str) -> None:
+    logger.error(msg)
+    raise Ops2debFetcherError(msg)
+
+
+async def _run(program: str, *args: str, cwd: Path) -> asyncio.subprocess.Process:
+    proc = await asyncio.create_subprocess_exec(
+        program,
+        *args,
+        cwd=cwd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    await proc.communicate()
+    return proc
+
+
+async def _extract_and_delete_archive(file_path: Path) -> None:
+    commands = [
+        ({".tar.gz", ".tar.xz", ".tar"}, ["/bin/tar", "xf", str(file_path)]),
+        ({".zip"}, ["/usr/bin/unzip", str(file_path)]),
+    ]
+    selected_command: Optional[List[str]] = None
+    for extensions, command in commands:
+        for extension in extensions:
+            if file_path.name.endswith(extension):
+                selected_command = command
+                break
+    if selected_command is not None:
+        logger.info(f"Extracting {file_path.name}...")
+        proc = await _run(*selected_command, cwd=file_path.parent)
+        if proc.returncode:
+            _error(f"Failed to extract archive {file_path.name}")
+        else:
+            file_path.unlink()
+
+
+async def _compute_file_checksum(file_path: Path) -> str:
+    sha256_hash = hashlib.sha256()
+    with file_path.open("rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+            await asyncio.sleep(0)
+    return sha256_hash.hexdigest()
+
+
+async def _download_file(url: str, file_path: str) -> None:
+    async with client_factory() as client:
+        async with client.stream("GET", url) as r:
+            if 400 <= r.status_code < 600:
+                _error(
+                    f"Failed to download {url}. Server responded with {r.status_code}."
+                )
+            # FIXME: https://github.com/Tinche/aiofiles/issues/91
+            async with aiofiles.open(file_path, "wb") as f:  # type: ignore
+                async for chunk in r.aiter_bytes():
+                    await f.write(chunk)
+
+
+async def download_file_to_cache(
+    url: str, expected_hash: Optional[str] = None
+) -> Tuple[Path, str]:
+    _cache_directory.mkdir(exist_ok=True)
+    url_hash = hashlib.sha256(url.encode()).hexdigest()
+    file_name = url.split("/")[-1]
+    file_path = _cache_directory / f"{url_hash}_{file_name}"
+    tmp_path = f"{file_path}.part"
+
+    if not file_path.is_file():
+        logger.info(f"Downloading {file_name}...")
+        try:
+            await _download_file(url, tmp_path)
+        except httpx.HTTPError as e:
+            _error(f"Failed to download {url}. {str(e)}")
+        shutil.move(tmp_path, file_path)
+
+    logger.info(f"Computing checksum for {file_name}...")
+    computed_hash = await _compute_file_checksum(file_path)
+
+    if expected_hash is not None:
+        if computed_hash != expected_hash:
+            _error(
+                f"Wrong checksum for file {file_name}. "
+                f"Expected {expected_hash}, got {computed_hash}."
+            )
+
+    return file_path, computed_hash
+
+
+async def fetch(url: str, expected_hash: str, save_path: Path) -> None:
+    file_name = url.split("/")[-1]
+    file_path, _ = await download_file_to_cache(url, expected_hash)
+    shutil.copy(file_path, save_path / file_name)
+    await _extract_and_delete_archive(save_path / file_name)
+    logger.info(f"Done with {file_name}")
---
+++ packages/ops2deb/ops2deb-0.14.0-py3-none-any.whl/ops2deb/generator.py
@@ -0,0 +1,150 @@
+import asyncio
+import shutil
+import subprocess
+from pathlib import Path
+from typing import Any, List, Optional
+
+from jinja2 import Environment, PackageLoader
+
+from . import logger
+from .apt import DebianRepositoryPackage, sync_list_repository_packages
+from .exceptions import (
+    Ops2debFetcherError,
+    Ops2debGeneratorError,
+    Ops2debGeneratorScriptError,
+)
+from .fetcher import fetch
+from .parser import Blueprint
+
+_environment = Environment(loader=PackageLoader("ops2deb", "templates"))
+
+
+def _format_command_output(output: str) -> str:
+    lines = output.splitlines()
+    output = "\n  ".join([line for line in lines])
+    return "> " + output
+
+
+class SourcePackage:
+    def __init__(self, blueprint: Blueprint, output_directory: Path):
+        self.directory_name = f"{blueprint.name}_{blueprint.version}_{blueprint.arch}"
+        self.package_directory = (output_directory / self.directory_name).absolute()
+        self.debian_directory = self.package_directory / "debian"
+        self.src_directory = self.package_directory / "src"
+        self.fetch_directory = Path(f"/tmp/ops2deb_{self.directory_name}")
+        self.blueprint = blueprint.render(self.src_directory)
+        self.debian_version = (
+            f"{self.blueprint.version}-{self.blueprint.revision}~ops2deb"
+        )
+
+    def render_tpl(self, template_name: str) -> None:
+        template = _environment.get_template(f"{template_name}.j2")
+        package = self.blueprint.dict(exclude={"fetch", "script"})
+        package.update({"version": self.debian_version})
+        template.stream(package=package).dump(str(self.debian_directory / template_name))
+
+    def init(self) -> None:
+        shutil.rmtree(self.debian_directory, ignore_errors=True)
+        self.debian_directory.mkdir(parents=True)
+        shutil.rmtree(self.fetch_directory, ignore_errors=True)
+        self.fetch_directory.mkdir()
+        shutil.rmtree(self.src_directory, ignore_errors=True)
+        self.src_directory.mkdir(parents=True)
+        for path in ["usr/bin", "usr/share", "usr/lib"]:
+            (self.src_directory / path).mkdir(parents=True)
+
+    async def fetch(self) -> "SourcePackage":
+        if (remote_file := self.blueprint.fetch) is not None:
+            await fetch(
+                url=remote_file.url,
+                expected_hash=remote_file.sha256,
+                save_path=self.fetch_directory,
+            )
+        return self
+
+    def generate(self) -> None:
+        logger.title(f"Generating source package {self.directory_name}...")
+
+        # if blueprint has no fetch instruction, we stay in the directory from which
+        # ops2deb was called
+        cwd = self.fetch_directory if self.blueprint.fetch else Path(".")
+
+        # run script
+        for line in self.blueprint.script:
+            logger.info(f"$ {line}")
+            result = subprocess.run(line, shell=True, cwd=cwd, capture_output=True)
+            if stdout := result.stdout.decode():
+                logger.info(_format_command_output(stdout))
+            if stderr := result.stderr.decode():
+                logger.error(_format_command_output(stderr))
+            if result.returncode:
+                raise Ops2debGeneratorScriptError
+
+        # render debian/* files
+        for template in [
+            "changelog",
+            "control",
+            "rules",
+            "compat",
+            "install",
+            "lintian-overrides",
+        ]:
+            self.render_tpl(template)
+
+
+def filter_already_published_packages(
+    packages: List[SourcePackage], debian_repository: str
+) -> List[SourcePackage]:
+    already_published_packages = sync_list_repository_packages(debian_repository)
+    filtered_packages: List[SourcePackage] = []
+    for package in packages:
+        if (
+            DebianRepositoryPackage(
+                package.blueprint.name, package.debian_version, package.blueprint.arch
+            )
+            not in already_published_packages
+        ):
+            filtered_packages.append(package)
+    return filtered_packages
+
+
+def generate(
+    blueprints: List[Blueprint],
+    output_directory: Path,
+    debian_repository: Optional[str] = None,
+) -> None:
+
+    packages = [SourcePackage(blueprint, output_directory) for blueprint in blueprints]
+
+    # filter out blueprints that build packages already available in the debian repository
+    if debian_repository is not None:
+        packages = filter_already_published_packages(packages, debian_repository)
+
+    # make sure we generate source packages in a clean environment
+    # without artifacts from previous builds
+    for package in packages:
+        package.init()
+
+    # run fetch instructions (download, verify, extract) in parallel
+    file_count = sum([1 for b in blueprints if b.fetch is not None])
+    logger.title(f"Fetching {file_count} files...")
+
+    async def fetch_all() -> Any:
+        return await asyncio.gather(
+            *[p.fetch() for p in packages], return_exceptions=True
+        )
+
+    results = asyncio.run(fetch_all())
+
+    errors = [e for e in results if isinstance(e, Exception)]
+    for error in errors:
+        if not isinstance(error, Ops2debFetcherError):
+            raise error
+
+    # run scripts, build debian/* files
+    packages = [p for p in results if isinstance(p, SourcePackage)]
+    for package in packages:
+        package.generate()
+
+    if errors:
+        raise Ops2debGeneratorError(f"{len(errors)} failures occurred")
