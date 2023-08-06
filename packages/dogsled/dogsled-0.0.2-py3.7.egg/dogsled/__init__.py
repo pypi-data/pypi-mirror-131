02c27ec84694f05d0efeece6727c949054d566ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:18 +0000

    Add package dogsled/0.0.2/dogsled-0.0.2-py3.7.egg

---
+++ packages/dogsled/dogsled-0.0.2-py3.7.egg/dogsled/__init__.py
@@ -0,0 +1,9 @@
+"""Currently, only logging lives here."""
+
+import logging
+import logging.config
+from dogsled import config
+
+
+logger = logging.getLogger(__name__)
+logging.config.dictConfig(config.LOGGING_DEVEL_CONFIG)
