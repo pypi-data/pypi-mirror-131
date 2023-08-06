c362f602b10eb30b1a43a65bf059098a5af249db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:45 +0000

    Add package deepcv/1.1.0/deepcv-1.1.0.tar.gz

---
+++ packages/deepcv/deepcv-1.1.0.tar.gz/deepcv-1.1.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="deepcv",
+    version="1.1.0",
+    keywords=("pip", "pathtool", "timetool", "magetool", "mage"),
+    description="Computer vision related algorithms",
+    long_description="time and path tool",
+    license="MIT Licence",
+
+    url="https://github.com/Barrnett/DeepCV",
+    author="zhuwenwen",
+    author_email="stephenbarrnet@outlook.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
