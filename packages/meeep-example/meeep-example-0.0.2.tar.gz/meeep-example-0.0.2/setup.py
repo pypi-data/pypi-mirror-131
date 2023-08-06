bd7c3ad99e4f5ea03270f23204e03018a33b0797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:22 +0000

    Add package meeep-example/0.0.2/meeep-example-0.0.2.tar.gz

---
+++ packages/meeep-example/meeep-example-0.0.2.tar.gz/meeep-example-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="meeep-example",
+    version="0.0.2",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maya-soni-lark/python_packaging",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8.12",
+)
---
+++ packages/meeep-example/meeep-example-0.0.2.tar.gz/meeep-example-0.0.2/src/example_package/example.py
@@ -0,0 +1,3 @@
+def hello_world() -> None:
+	print('yet another hello world... how clever')
+	return None
