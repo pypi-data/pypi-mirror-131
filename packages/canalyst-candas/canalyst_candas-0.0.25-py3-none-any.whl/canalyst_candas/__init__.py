64e0c06a0292f85d8154abe10768465e1c9a1949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:52 +0000

    Add package canalyst-candas/0.0.25/canalyst_candas-0.0.25-py3-none-any.whl

---
+++ packages/canalyst-candas/canalyst_candas-0.0.25-py3-none-any.whl/canalyst_candas/__init__.py
@@ -0,0 +1,5 @@
+# from canalyst_candas.candas import FormulaTree, FuncMusic, Model, ModelMap, ModelSet
+from canalyst_candas.candas import FuncMusic, Model, ModelMap, ModelSet, Chart
+import canalyst_candas.version as version
+
+__version__ = version.__version__
