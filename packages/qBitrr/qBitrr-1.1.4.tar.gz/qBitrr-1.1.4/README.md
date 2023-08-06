015eb6d915eccdf8f668c564c75b9ce85e53e33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:23 +0000

    Add package qBitrr/1.1.4/qBitrr-1.1.4.tar.gz

---
+++ packages/qBitrr/qBitrr-1.1.4.tar.gz/qBitrr-1.1.4/README.md
@@ -0,0 +1,67 @@
+[![PyPI](https://img.shields.io/pypi/v/qBitrr)](https://pypi.org/project/qBitrr/)
+[![PyPI - License](https://img.shields.io/pypi/l/qbitrr)](https://github.com/Drapersniper/Qbitrr/blob/master/LICENSE)
+
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/qbitrr)
+![Platforms](https://img.shields.io/badge/platform-linux--64%20%7C%20osx--64%20%7C%20win--32%20%7C%20win--64-lightgrey)
+
+[![GitHub branch checks state](https://img.shields.io/github/checks-status/Drapersniper/qbitrr/master)](https://github.com/Drapersniper/Qbitrr/actions)
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/Drapersniper/Qbitrr/master.svg)](https://results.pre-commit.ci/latest/github/Drapersniper/Qbitrr/master)
+
+[![Code Style: Black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://pycqa.github.io/isort/)
+
+A simple script to monitor [Qbit](https://github.com/qbittorrent/qBittorrent) and communicate with [Radarr](https://github.com/Radarr/Radarr) and [Sonarr](https://github.com/Sonarr/Sonarr)
+
+Join the [Official Discord Server](https://discord.gg/FT3puape2A) for help.
+
+### Features
+
+- Monitor qBit for Stalled/bad entries and delete them then blacklist them on Arrs (Option to also trigger a re-search action).
+- Monitor qBit for completed entries and tell the appropriate Arr instance to import it ( 'DownloadedMoviesScan' or 'DownloadedEpisodesScan' commands).
+- Skip files in qBit entries by extension, folder or regex.
+- Monitor completed folder and cleans it up.
+- Uses [ffprobe](https://github.com/FFmpeg/FFmpeg) to ensure downloaded entries are valid media.
+- Trigger periodic Rss Syncs on the appropriate Arr instances.
+- Trigger Queue update on appropriate Arr instances.
+- Search requests from [Overseerr](https://github.com/sct/overseerr) or [Ombi](https://github.com/Ombi-app/Ombi).
+
+**This section requires the Arr databases to be locally available.**
+
+- Monitor Arr's databases to trigger missing episode searches.
+- Customizable year range to search for (at a later point will add more option here, for example search whole series/season instead of individual episodes, search by name, category etc).
+
+### Important mentions
+
+Some things to know before using it.
+
+- 1. You need to copy the `config.example.ini` and rename it to `~/.config/qBitManager/config.ini` (~ is your home directory, i.e `C:\Users\{User}`)
+- 2. I have [Sonarr](https://github.com/Sonarr/Sonarr) and [Radarr](https://github.com/Radarr/Radarr) both setup to add tags to all downloads.
+- 3. I have qBit setup to have to create sub-folder for downloads and for the download folder to
+     use subcategories.
+
+  ![image](https://user-images.githubusercontent.com/27962761/139117102-ec1d321a-1e64-4880-8ad1-ee2c9b805f92.png)
+
+- 4. Make sure to have [`ffprobe`](https://www.ffmpeg.org/download.html) added to your PATH.
+
+#### Install the requirements run
+
+- `python -m pip install qBitrr` (I would recommend in a dedicated [venv](https://docs.python.org/3.3/library/venv.html) but that's out of scope.
+
+#### Run the script
+
+- Make sure to update the settings in `~/.config/qBitManager/config.ini`
+- Activate your venv
+- Run `qbitrr`
+
+#### How to update the script
+
+- Activate your venv
+- Run `python -m pip install -U qBitrr`
+
+#### Contributions
+
+- I'm happy with any PRs and suggested changes to the logic I just put it together dirty for my own use case.
+
+#### Example behaviour
+
+![image](https://user-images.githubusercontent.com/27962761/145682638-6c3a4c20-2756-4b42-a6b9-c7b95ad99b36.png)
---
+++ packages/qBitrr/qBitrr-1.1.4.tar.gz/qBitrr-1.1.4/qBitrr/utils.py
@@ -0,0 +1,149 @@
+import pathlib
+import random
+import socket
+import time
+from typing import Iterator, Union
+
+import logbook
+
+from qBitrr.config import PING_URLS
+
+logger = logbook.Logger("Utilities")
+
+
+def absolute_file_paths(directory: Union[pathlib.Path, str]) -> Iterator[pathlib.Path]:
+    error = True
+    while error is True:
+        try:
+            yield from pathlib.Path(directory).glob("**/*")
+            error = False
+        except FileNotFoundError as e:
+            logger.warning("{e.strerror} - {e.filename}", e=e)
+
+
+def validate_and_return_torrent_file(file: str) -> pathlib.Path:
+    path = pathlib.Path(file)
+    if path.is_file():
+        path = path.parent.absolute()
+    count = 9
+    while not path.exists():
+        logger.trace(
+            "Attempt {count}/10: File does not yet exists! (Possibly being moved?) | "
+            "{path} | Sleeping for 0.1s",
+            path=path,
+            count=10 - count,
+        )
+        time.sleep(0.1)
+        if count == 0:
+            break
+        count -= 1
+    else:
+        count = 0
+    while str(path) == ".":
+        path = pathlib.Path(file)
+        if path.is_file():
+            path = path.parent.absolute()
+        while not path.exists():
+            logger.trace(
+                "Attempt {count}/10:File does not yet exists! (Possibly being moved?) | "
+                "{path} | Sleeping for 0.1s",
+                path=path,
+                count=10 - count,
+            )
+            time.sleep(0.1)
+            if count == 0:
+                break
+            count -= 1
+        else:
+            count = 0
+        if count == 0:
+            break
+        count -= 1
+    return path
+
+
+def has_internet():
+    url = random.choice(PING_URLS)
+    if not is_connected(url):
+        return False
+    logger.trace("Successfully connected to {url}", url=url)
+    return True
+
+
+def is_connected(hostname):
+    try:
+        # see if we can resolve the host name -- tells us if there is
+        # a DNS listening
+        host = socket.gethostbyname(hostname)
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        s = socket.create_connection((host, 80), 2)
+        s.close()
+        return True
+    except Exception:
+        return False
+
+
+class ExpiringSet:
+    def __init__(self, *args, **kwargs):
+        max_age_seconds = kwargs.get("max_age_seconds", 0)
+        assert max_age_seconds > 0
+        self.age = max_age_seconds
+        self.container = {}
+        for arg in args:
+            self.add(arg)
+
+    def __repr__(self):
+        self.__update__()
+        return "{}({})".format(self.__class__.__name__, ", ".join(self.container.keys()))
+
+    def extend(self, args):
+        """Add several items at once."""
+        for arg in args:
+            self.add(arg)
+
+    def add(self, value):
+        self.container[value] = time.time()
+
+    def remove(self, item):
+        del self.container[item]
+
+    def contains(self, value):
+        if value not in self.container:
+            return False
+        if time.time() - self.container[value] > self.age:
+            del self.container[value]
+            return False
+        return True
+
+    __contains__ = contains
+
+    def __getitem__(self, index):
+        self.__update__()
+        return list(self.container.keys())[index]
+
+    def __iter__(self):
+        self.__update__()
+        return iter(self.container.copy())
+
+    def __len__(self):
+        self.__update__()
+        return len(self.container)
+
+    def __copy__(self):
+        self.__update__()
+        temp = ExpiringSet(max_age_seconds=self.age)
+        temp.container = self.container.copy()
+        return temp
+
+    def __update__(self):
+        for k, b in self.container.copy().items():
+            if time.time() - b > self.age:
+                del self.container[k]
+                return False
+
+    def __hash__(self):
+        return hash(*(self.container.keys()))
+
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
