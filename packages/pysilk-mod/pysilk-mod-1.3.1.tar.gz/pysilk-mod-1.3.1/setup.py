31e5bcafd2df00900bbc545f968e568c4c00af13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:08 +0000

    Add package pysilk-mod/1.3.1/pysilk-mod-1.3.1.tar.gz

---
+++ packages/pysilk-mod/pysilk-mod-1.3.1.tar.gz/pysilk-mod-1.3.1/setup.py
@@ -0,0 +1,25 @@
+import glob
+
+from setuptools import setup
+
+# Available at setup time due to pyproject.toml
+try:
+    from pybind11.setup_helpers import Pybind11Extension as Extension
+except ImportError:
+    from setuptools import Extension
+
+__version__ = "1.3.1"
+
+setup(
+    version=__version__,
+    install_requires=["pybind11"],
+    zip_safe=True,
+    ext_modules=[
+        Extension(
+            "_pysilk", ["src/silk/_pysilk.cpp", "src/silk/codec.cpp"],
+            library_dirs=["src/silk/src/"],
+            include_dirs=["src/silk/interface"],
+            define_macros=[('VERSION_INFO', __version__)]
+        )
+    ]
+)
