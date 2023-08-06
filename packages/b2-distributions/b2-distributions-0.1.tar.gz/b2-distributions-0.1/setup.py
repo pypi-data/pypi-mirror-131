01fdd80f54cbee25069b8dda7a53acfc73ccfba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:09 +0000

    Add package b2-distributions/0.1/b2-distributions-0.1.tar.gz

---
+++ packages/b2-distributions/b2-distributions-0.1.tar.gz/b2-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='b2-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['b2-distributions'],
+      zip_safe=False)
