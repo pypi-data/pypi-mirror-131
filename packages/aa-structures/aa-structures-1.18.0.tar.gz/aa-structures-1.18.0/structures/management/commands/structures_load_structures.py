a08b2f8086e09f7e69731786afa5eed6f3197762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:07 +0000

    Add package aa-structures/1.18.0/aa-structures-1.18.0.tar.gz

---
+++ packages/aa-structures/aa-structures-1.18.0.tar.gz/aa-structures-1.18.0/structures/management/commands/structures_load_structures.py
@@ -0,0 +1,21 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+from allianceauth.services.hooks import get_extension_logger
+from app_utils.logging import LoggerAddTag
+
+from ... import __title__, constants
+
+logger = LoggerAddTag(get_extension_logger(__name__), __title__)
+
+
+class Command(BaseCommand):
+    help = "Preloads data required for this app from ESI"
+
+    def handle(self, *args, **options):
+        call_command(
+            "eveuniverse_load_types",
+            __title__,
+            "--category_id_with_dogma",
+            str(constants.EVE_CATEGORY_ID_STRUCTURE),
+        )
