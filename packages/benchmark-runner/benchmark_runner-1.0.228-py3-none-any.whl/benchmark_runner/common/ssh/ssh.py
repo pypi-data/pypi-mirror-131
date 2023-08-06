899f3b53aa447efa37287048f7139b130b8ef6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:52 +0000

    Add package benchmark-runner/1.0.228/benchmark_runner-1.0.228-py3-none-any.whl

---
+++ packages/benchmark-runner/benchmark_runner-1.0.228-py3-none-any.whl/benchmark_runner/common/ssh/ssh.py
@@ -0,0 +1,44 @@
+
+import subprocess
+from benchmark_runner.common.logger.logger_time_stamp import logger
+from benchmark_runner.common.ssh.ssh_exceptions import SSHSubprocessError
+from benchmark_runner.main.environment_variables import environment_variables
+
+
+class SSH:
+    """
+    This class run local SSH commands
+    """
+    def __init__(self):
+        self.__environment_variables_dict = environment_variables.environment_variables_dict
+        self.timeout = int(self.__environment_variables_dict.get('timeout', 300))
+
+    def run(self, cmd: str, is_check: bool = False, background: bool = False):
+        """
+        This method run shell commands
+        :param background: run ssh in background
+        :param cmd:
+        :param is_check:run check command
+        :return:
+        """
+        try:
+            if background:
+                # DEVNULL - hide stdout
+                output = subprocess.Popen(
+                    cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
+                    shell=True)
+            elif is_check:
+                output = subprocess.check_output(
+                    cmd,
+                    stderr=subprocess.STDOUT,
+                    shell=True, timeout=self.timeout,
+                    universal_newlines=False)
+            # execute cmd
+            else:
+                output = subprocess.getoutput(cmd)
+            return output
+        except subprocess.CalledProcessError as err:
+            logger.error(f'subprocess Status : FAIL: {err.returncode} {err.output}')
+            raise SSHSubprocessError()
+        except Exception as err:
+             raise err
