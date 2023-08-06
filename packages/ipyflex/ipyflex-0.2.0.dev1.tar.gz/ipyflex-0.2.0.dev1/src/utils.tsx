333760bfbf7bd228855f04141ef64344d5c4413a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:45 +0000

    Add package ipyflex/0.2.0.dev1/ipyflex-0.2.0.dev1.tar.gz

---
+++ packages/ipyflex/ipyflex-0.2.0.dev1.tar.gz/ipyflex-0.2.0.dev1/src/utils.tsx
@@ -0,0 +1,15 @@
+export const JUPYTER_BUTTON_CLASS =
+  'lm-Widget p-Widget jupyter-widgets jupyter-button widget-button';
+
+export interface IDict<T = any> {
+  [key: string]: T;
+}
+
+export const MESSAGE_ACTION = {
+  SAVE_TEMPLATE: 'save_template',
+  UPDATE_CHILDREN: 'update_children',
+  REQUEST_FACTORY: 'request_factory',
+  RENDER_FACTORY: 'render_factory',
+  RENDER_ERROR: 'render_error',
+  ADD_WIDGET: 'add_widget',
+};
