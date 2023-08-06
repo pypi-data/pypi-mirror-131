2ef12f04012633f13a296719119054be1894b064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:49 +0000

    Add package gvcode/1.0.3/gvcode-1.0.3.tar.gz

---
+++ packages/gvcode/gvcode-1.0.3.tar.gz/gvcode-1.0.3/setup.py
@@ -0,0 +1,21 @@
+#-*-coding:utf-8-*-
+from setuptools import setup, find_packages
+
+with open("README.md","r",encoding="utf-8") as f:
+    desc = f.read()
+
+
+setup(
+    name="gvcode",
+    version="1.0.3",
+    description="A useful verification code generation tool.",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    license="GNU",
+    author="Msky",
+    author_email="690126048@qq.com",
+    url="https://github.com/miloira/gvcode",
+    packages=find_packages(),
+    install_requires=["pillow"],
+    include_package_data=True
+)
