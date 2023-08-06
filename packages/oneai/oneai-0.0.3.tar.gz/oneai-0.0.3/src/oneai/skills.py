b7b7284d705e1de9e2f4d5bc1d1b90c01f38889b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:34 +0000

    Add package oneai/0.0.3/oneai-0.0.3.tar.gz

---
+++ packages/oneai/oneai-0.0.3.tar.gz/oneai-0.0.3/src/oneai/skills.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+from oneai.classes import skillclass
+
+@skillclass(name='enhance', iswriting=True)
+class EnhanceTranscription: pass
+
+@skillclass(name='summarize', iswriting=True, param_fields=['min_length', 'max_length'])
+@dataclass
+class Summarize:
+    min_length: int = 5
+    max_length: int = 100
+
+@skillclass(name='emotions')
+class EmotionDetection: pass
+
+@skillclass(name='entities')
+class EntityDetection: pass
+
+@skillclass(name='keywords')
+class KeywordDetection: pass
+
+@skillclass(name='highlights')
+class Highlighting: pass
+
+@skillclass(name='sentences')
+class SplitToSentences: pass
