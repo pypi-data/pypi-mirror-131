49a9534bfd52389d56b8105a06f45efe32b86403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:53 +0000

    Add package UNKnownDB/1.3/UNKnownDB-1.3.tar.gz

---
+++ packages/UNKnownDB/UNKnownDB-1.3.tar.gz/UNKnownDB-1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='UNKnownDB',
+    version='1.3',
+    author='CleaverCreator',
+    author_email='liuhanbo333@icloud.com',
+    packages=find_packages(),
+    zip_safe=False,
+    platforms=['Linux'],
+    install_requires=[],
+    description='A new DB',
+    license='A2.0',
+    url='https://github.com/CleverCreater/UNKnownDB',
+    classifiers=[]
+)
