bc4a30e1c04795ff4555b6c31face3dccdb93bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:44 +0000

    Add package vws-cli/2021.12.15.5/VWS%20CLI-2021.12.15.5.tar.gz

---
+++ packages/vws-cli/VWS%20CLI-2021.12.15.5.tar.gz/VWS CLI-2021.12.15.5/admin/release.py
@@ -0,0 +1,18 @@
+"""
+Release the next version.
+"""
+
+from pathlib import Path
+
+from binaries import make_linux_binaries  # pylint: disable=import-error
+
+
+def main() -> None:
+    """
+    Perform a release.
+    """
+    make_linux_binaries(repo_root=Path('.'))
+
+
+if __name__ == '__main__':
+    main()
