acb95b3623822c0a8d985e1a2a11b2765ea45bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:22 +0000

    Add package pomodoro-Dev3XoR/0.8.2/pomodoro-Dev3XoR-0.8.2.tar.gz

---
+++ packages/pomodoro-Dev3XoR/pomodoro-Dev3XoR-0.8.2.tar.gz/pomodoro-Dev3XoR-0.8.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pomodoro-Dev3XoR",
+    version="0.8.2",
+    author="Dev3XoR",
+    author_email="devexor@protonmail.com",
+    description="Pomodoro clock and timer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dev3XOR/pomodoro/",
+    license_file="LICENSE",
+    packages=setuptools.find_packages(include=["pomodoro"]),
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires="~=3.6",
+    install_requires=[
+        "playsound",
+        "plyer",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pomodoro-clock=pomodoro:main",
+        ],
+    },
+)
