2070524f3e9da3e5df03062f15383533743ce2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:37 +0000

    Add package zestful-parse-ingredient/0.0.7/zestful-parse-ingredient-0.0.7.tar.gz

---
+++ packages/zestful-parse-ingredient/zestful-parse-ingredient-0.0.7.tar.gz/zestful-parse-ingredient-0.0.7/README.md
@@ -0,0 +1,154 @@
+# Ingredient Parser (Zestful Client)
+
+[![PyPI](https://img.shields.io/pypi/v/zestful-parse-ingredient)](https://pypi.org/project/zestful-parse-ingredient/)
+[![CircleCI](https://circleci.com/gh/mtlynch/zestful-client.svg?style=svg)](https://circleci.com/gh/mtlynch/zestful-client)
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](LICENSE)
+
+## Overview
+
+Parse recipe ingredient strings into structured data.
+
+## Examples
+
+### Parse a single ingredient
+
+```python
+import json
+import parse_ingredient
+
+ingredient = parse_ingredient.parse('2 1/2 tablespoons finely chopped parsley')
+print(json.dumps(ingredient.as_dict()))
+```
+
+```json
+{
+  "quantity": 2.5,
+  "unit": "tablespoon",
+  "product": "parsley",
+  "productSizeModifier": null,
+  "preparationNotes": "finely chopped",
+  "usdaInfo": {
+      "category": "Vegetables and Vegetable Products",
+      "description": "Parsley, fresh",
+      "fdcId": "170416",
+      "matchMethod": "exact"
+  },
+  "confidence": 0.9858154,
+}
+```
+
+### Parse multiple ingredients
+
+```python
+import json
+import parse_ingredient
+
+ingredients = parse_ingredient.parse_multiple([
+  '½ tsp brown sugar',
+  '3 large Granny Smith apples'
+  )
+print(json.dumps(ingredients.as_dict()))
+```
+
+```json
+[
+  {
+    "ingredientRaw": "½ tsp brown sugar",
+    "ingredientParsed": {
+        "preparationNotes": "finely chopped",
+        "product": "brown sugar",
+        "productSizeModifier": null,
+        "quantity": 0.5,
+        "unit": "teaspoon",
+        "usdaInfo": {
+            "category": "Sweets",
+            "description": "Sugars, brown",
+            "fdcId": "168833",
+            "matchMethod": "exact"
+        },
+        "confidence": 0.9857134,
+    },
+    "error": null,
+  },
+  {
+      "ingredientRaw": "3 large Granny Smith apples",
+      "ingredientParsed": {
+          "preparationNotes": null,
+          "product": "Granny Smith apples",
+          "productSizeModifier": "large",
+          "quantity": 3.0,
+          "unit": null,
+          "usdaInfo": {
+              "category": "Fruits and Fruit Juices",
+              "description": "Apples, raw, granny smith, with skin (Includes foods for USDA's Food Distribution Program)",
+              "fdcId": "168203",
+              "matchMethod": "exact"
+          },
+          "confidence": 0.9741028,
+      },
+      "error": null,
+  }
+]
+```
+
+### Parse ingredients using RapidAPI
+
+If you have a [RapidAPI subscription](https://rapidapi.com/zestfuldata/api/recipe-and-ingredient-analysis) to Zestful, you can use your API key as follows:
+
+```python
+import json
+import parse_ingredient
+
+# Replace this with your key from RapidAPI
+# https://rapidapi.com/zestfuldata/api/recipe-and-ingredient-analysis
+RAPID_API_KEY = 'your-rapid-api-key'
+
+client = parse_ingredient.client(rapid_api_key=RAPID_API_KEY)
+
+ingredient = client.parse_ingredient('2 1/2 tablespoons finely chopped parsley')
+print(json.dumps(ingredient.as_dict()))
+```
+
+### Use private Zestful server
+
+If you have a private Zestful ingredient parsing server as part of an Enterprise plan, you can use the library as follows:
+
+```python
+import json
+import parse_ingredient
+
+ENDPOINT_URL = 'https://zestful.yourdomain.com'
+
+client = parse_ingredient.client(endpoint_url=ENDPOINT_URL)
+
+ingredient = client.parse_ingredient('2 1/2 tablespoons finely chopped parsley')
+print(json.dumps(ingredient.as_dict()))
+```
+
+## Installation
+
+### From pip
+
+```bash
+pip install zestful-parse-ingredient
+```
+
+### From source
+
+```bash
+git clone https://github.com/mtlynch/zestful-client.git
+cd zestful-client
+pip install .
+```
+
+## How does it work?
+
+The library sends ingredient parsing requests to a remote Zestful ingredient parsing server. By default, the library uses the demo instance of Zestful. For production usage, you should set a RapidAPI key or private Zestful server instance address.
+
+## Limitations
+
+Without a subscription, you can only parse 30 ingredients per day. To parse an unlimited number of ingredients, purchase a subscription from [Zestful](https://zestfuldata.com/docs).
+
+## Full documentation
+
+For full documentation of each result field, see the official [Zestful API documentation](https://zestfuldata.com/docs).
---
+++ packages/zestful-parse-ingredient/zestful-parse-ingredient-0.0.7.tar.gz/zestful-parse-ingredient-0.0.7/parse_ingredient/internal/client.py
@@ -0,0 +1,126 @@
+import json
+from urllib import request
+
+import parse_ingredient.ingredient
+
+
+class Error(Exception):
+    pass
+
+
+class ZestfulServerError(Error):
+    pass
+
+
+class InsufficientQuotaError(Error):
+    pass
+
+
+_DEMO_ENDPOINT_URL = 'https://sandbox.zestfuldata.com'
+_RAPID_API_DOMAIN = 'zestful.p.rapidapi.com'
+_RAPID_API_URL = 'https://' + _RAPID_API_DOMAIN
+
+
+class Client:
+
+    def __init__(self, endpoint_url=None, rapid_api_key=None):
+        self._rapid_api_key = rapid_api_key
+        if endpoint_url:
+            self._endpoint_url = endpoint_url
+        elif self._rapid_api_key:
+            self._endpoint_url = _RAPID_API_URL
+        else:
+            self._endpoint_url = _DEMO_ENDPOINT_URL
+
+    def parse_ingredient(self, ingredient):
+        results_raw = self._send_request([ingredient])
+        _check_quota(results_raw)
+        if results_raw['error']:
+            raise ZestfulServerError('failed to parse ingredient: %s' %
+                                     results_raw['error'])
+        if len(results_raw['results']) != 1:
+            raise ValueError(
+                'Unexpected response from server. Expected 1 result, got %d' %
+                len(results_raw['results']))
+        result_raw = results_raw['results'][0]
+
+        if result_raw['error']:
+            raise ZestfulServerError('failed to parse ingredient: %s' %
+                                     result_raw['error'])
+
+        return parse_ingredient.ingredient.ParsedIngredient(
+            confidence=result_raw['confidence'],
+            product=result_raw['ingredientParsed']['product'],
+            product_size_modifier=result_raw['ingredientParsed']
+            ['productSizeModifier'],
+            quantity=result_raw['ingredientParsed']['quantity'],
+            unit=result_raw['ingredientParsed']['unit'],
+            preparation_notes=result_raw['ingredientParsed']
+            ['preparationNotes'],
+            usda_info=_parse_usda_info(
+                result_raw['ingredientParsed']['usdaInfo']))
+
+    def parse_ingredients(self, ingredients):
+        results_raw = self._send_request(ingredients)
+        _check_quota(results_raw)
+        if results_raw['error']:
+            raise ZestfulServerError('failed to parse ingredients: %s' %
+                                     results_raw['error'])
+
+        results = []
+        for result_raw in results_raw['results']:
+            results.append(
+                parse_ingredient.ingredient.ParsedIngredientEntry(
+                    error=result_raw['error'],
+                    raw=result_raw['ingredientRaw'],
+                    parsed=parse_ingredient.ingredient.ParsedIngredient(
+                        confidence=result_raw['confidence'],
+                        product=result_raw['ingredientParsed']['product'],
+                        product_size_modifier=result_raw['ingredientParsed']
+                        ['productSizeModifier'],
+                        quantity=result_raw['ingredientParsed']['quantity'],
+                        unit=result_raw['ingredientParsed']['unit'],
+                        preparation_notes=result_raw['ingredientParsed']
+                        ['preparationNotes'],
+                        usda_info=_parse_usda_info(
+                            result_raw['ingredientParsed']['usdaInfo']))))
+        return parse_ingredient.ingredient.ParsedIngredients(
+            ingredients=results)
+
+    def _send_request(self, ingredients):
+        req = request.Request(self._endpoint_url + '/parseIngredients',
+                              method='POST')
+
+        body = json.dumps({'ingredients': ingredients}).encode('utf-8')
+
+        req.add_header('Content-Type', 'application/json')
+        req.add_header('Content-Length', len(body))
+
+        if self._rapid_api_key:
+            req.add_header('x-rapidapi-key', self._rapid_api_key)
+            req.add_header('x-rapidapi-host', _RAPID_API_DOMAIN)
+
+        with request.urlopen(req, data=body) as response:
+            return json.loads(response.read().decode('utf-8'))
+
+
+def _parse_usda_info(usda_info_raw):
+    if not usda_info_raw:
+        return None
+
+    return parse_ingredient.ingredient.UsdaInfo(
+        category=usda_info_raw['category'],
+        description=usda_info_raw['description'],
+        fdc_id=usda_info_raw['fdcId'],
+        match_method=usda_info_raw['matchMethod'])
+
+
+def _check_quota(server_response):
+    if not server_response['error']:
+        return
+    server_error = server_response['error']
+    if 'insufficient quota' in server_error.lower():
+        raise InsufficientQuotaError(
+            'You have insufficient quota to complete this request. '
+            'To continue parsing ingredients, purchase a Zestful plan from '
+            'https://zestfuldata.com')
