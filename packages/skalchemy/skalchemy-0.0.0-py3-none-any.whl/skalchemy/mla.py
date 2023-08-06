bbdb2b29d34a102acf5eca56a0a8c0de4d13f97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:28 +0000

    Add package skalchemy/0.0.0/skalchemy-0.0.0-py3-none-any.whl

---
+++ packages/skalchemy/skalchemy-0.0.0-py3-none-any.whl/skalchemy/mla.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mla: command."""
+
+from __future__ import absolute_import
+
+__version__ = '0.0.0'
+
+import sys
+
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 Please")
+
+usage = "Usage: " + sys.argv[0] + """ option
+
+    options:
+    
+        option1
+        option2
+"""
+
+def main():
+    """main: app."""
+    try:
+        if sys.argv[1:]:
+            if sys.argv[1] == "--help":
+                print(usage)
+            elif sys.argv[1] == "--version":
+                print(__version__)
+            else:
+                options[sys.argv[1]]()
+        else:
+            print(usage)
+    except KeyError as _e:
+        print("KeyError: " + str(_e))
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
