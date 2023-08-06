446e306cc1225bfd9ecaae654dc8ad505431086e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:23 +0000

    Add package blackblox/0.2.1/blackblox-0.2.1.tar.gz

---
+++ packages/blackblox/blackblox-0.2.1.tar.gz/blackblox-0.2.1/blackblox/about.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""About blackblox.py
+
+This module contains metadata about blackblox.py, which is used in the generation 
+of output spreadsheets in io_functions.py
+"""
+
+about_blackblox = {
+    "name": "blackblox.py",
+    "version": "0.1 (unreleased)",
+    "url": "OFFLINE",
+    "documentation_url": "OFFLINE",
+    "github_url": "OFFLINE",
+    "creator": "Ir. S.E. Tanzer",
+    "affiliation": (
+        "Energy & Industry Group, "
+        "Department of Engineering Systems and Services, "
+        "Facult of Technology Policy, and Management, "
+        "Delft University of Technology, "
+        "The Netherlands"),
+    "license": "GPL v3",
+}
