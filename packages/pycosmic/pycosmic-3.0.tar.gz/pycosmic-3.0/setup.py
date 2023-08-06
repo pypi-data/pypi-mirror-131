db9f359c97c5e4013d844c7c58ad3893f71d4d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:05 +0000

    Add package pycosmic/3.0/pycosmic-3.0.tar.gz

---
+++ packages/pycosmic/pycosmic-3.0.tar.gz/pycosmic-3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup ,find_packages
+
+
+
+setup(
+
+name = "pycosmic",
+version="3.0",
+description="This is a very useful pacakge that saves time ",
+author="Rishabh",
+author_email="rishivisionworld@gmail.com",
+install_requires=['qrcode','opencv-python'],
+packages=find_packages(),
+
+
+)
