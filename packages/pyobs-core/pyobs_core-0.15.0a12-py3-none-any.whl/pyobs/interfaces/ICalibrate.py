7bd99393aa86f58dacc9db80bd54c186cfd62677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package pyobs-core/0.15.0a12/pyobs_core-0.15.0a12-py3-none-any.whl

---
+++ packages/pyobs-core/pyobs_core-0.15.0a12-py3-none-any.whl/pyobs/interfaces/ICalibrate.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+from .interface import Interface
+
+
+class ICalibrate(Interface):
+    """The module can calibrate a device."""
+    __module__ = 'pyobs.interfaces'
+
+    async def calibrate(self, **kwargs: Any) -> None:
+        """Calibrate the device."""
+        raise NotImplementedError
+
+
+__all__ = ['ICalibrate']
